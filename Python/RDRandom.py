303f658582b5efc53d65ea5fabf2c24b4ab8e884
Greg Landrum <greg.landrum@gmail.com>
2006-10-03 05:25:56 +0000

    expose the shuffle function as well; if we ever go back to an ancient version of python, this will need to be updated

--- Python/RDRandom.py
+++ Python/RDRandom.py
@@ -15,13 +15,16 @@ if sys.hexversion >= 0x20303f0:
   _randGen = _random.WichmannHill()
   random = _randGen.random
   randrange = _randGen.randrange
+  shuffle = _randGen.shuffle
   def seed(val):
-    global _randGen,random,randrange
+    global _randGen,random,randrange,shuffle
     _randGen = _random.WichmannHill()
     _randGen.whseed(val)
     random = _randGen.random
     randrange = _randGen.randrange
+    shuffle = _randGen.shuffle
 else:
   random = _random.random
   randrange = _random.randrange
   seed = _random.whseed
+  shuffle=lambda x:raise NotImplementedError,'shuffle not implemented for older python versions'
