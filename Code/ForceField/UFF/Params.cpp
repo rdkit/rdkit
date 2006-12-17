8fc39b160b4bd2be604aaf502e6278188486c16e
Greg Landrum <greg.landrum@gmail.com>
2006-12-17 14:56:04 +0000

    minor windows compat changes

--- Code/ForceField/UFF/Params.cpp
+++ Code/ForceField/UFF/Params.cpp
@@ -4,8 +4,12 @@
 //
 //   @@ All Rights Reserved  @@
 //
+#ifdef WIN32
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
 #include "Params.h"
-#include <math.h>
+
 #include <iostream>
 #include <sstream>
 #include <RDGeneral/StreamOps.h>
