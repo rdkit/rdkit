afef4a16198a645707394fc593127e92234d1695
Santosh Putta <puttas@users.sourceforge.net>
2007-06-10 23:21:03 +0000

    Adding an optional directory argument

--- Code/GraphMol/SmilesParse/genparse.py
+++ Code/GraphMol/SmilesParse/genparse.py
@@ -1,5 +1,9 @@
 import os,sys
 
+if len(sys.argv) > 2:
+    runDir = sys.argv[2]
+    os.chdir(runDir)
+    
 bindir = 'c:\\cygwin\\bin\\';
 os.system('%sbison -p yy%s_ -t -d -o %s.tab.cpp %s.y' % (bindir,sys.argv[1],sys.argv[1],sys.argv[1]) )
 
