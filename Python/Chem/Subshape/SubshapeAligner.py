1280afd881c3b174c493b6fe3c0667cbc54088f1
Greg Landrum <greg.landrum@gmail.com>
2007-07-15 12:09:39 +0000

    er, oops

--- Python/Chem/Subshape/SubshapeAligner.py
+++ Python/Chem/Subshape/SubshapeAligner.py
@@ -57,17 +57,17 @@ def GetShapeShapeDistance(s1,s2,distMetric):
 # clusters a set of alignments and returns the cluster centroid
 def ClusterAlignments(mol,alignments,builder,
                       neighborTol=0.1,
-                      distMetric=SubshapeAligner.SubshapeDistanceMetric.PROTRUDE,
+                      distMetric=SubshapeDistanceMetric.PROTRUDE,
                       tempConfId=1001):
   from ML.Cluster import Butina
   dists = []
   for i in range(len(alignments)):
-    SubshapeAligner.TransformMol(mol,alignments[i].transform,newConfId=tempConfId)
+    TransformMol(mol,alignments[i].transform,newConfId=tempConfId)
     shapeI=builder.GenerateSubshapeShape(mol,tempConfId,addSkeleton=False)
     for j in range(i):
-      SubshapeAligner.TransformMol(mol,alignments[j].transform,newConfId=tempConfId+1)
+      TransformMol(mol,alignments[j].transform,newConfId=tempConfId+1)
       shapeJ=builder.GenerateSubshapeShape(mol,tempConfId+1,addSkeleton=False)
-      d = SubshapeAligner.GetShapeShapeDistance(shapeI,shapeJ,distMetric)
+      d = GetShapeShapeDistance(shapeI,shapeJ,distMetric)
       dists.append(d)
       mol.RemoveConformer(tempConfId+1)
     mol.RemoveConformer(tempConfId)
