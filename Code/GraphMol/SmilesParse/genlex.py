81deaebf402a738e97841dc962036104d5c0cebc
Greg Landrum <greg.landrum@gmail.com>
2008-01-16 17:27:24 +0000

    we don't need the genlex.py and genparse.py stuff anymore, so remove it and update the project file to just use flex and bison directly.

--- Code/GraphMol/SmilesParse/genlex.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-
-bindir = 'c:/cygwin/bin/';
-
-if len(sys.argv) > 2:
-    runDir = sys.argv[2]
-    os.chdir(runDir)
-os.system('%sflex -Pyy%s_ -olex.yy.tmp.cpp %s.ll' % (bindir,sys.argv[1],sys.argv[1] ))
-os.system('%sgrep -v unistd lex.yy.tmp.cpp > lex.yy%s.cpp' % (bindir,sys.argv[1] ))
