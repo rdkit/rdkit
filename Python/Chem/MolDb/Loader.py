6e6be158e04a1122d780d698e6ee1b3d772f3123
Greg Landrum <greg.landrum@gmail.com>
2008-02-19 06:28:53 +0000

    add some functionality and robustify a bit

--- Python/Chem/MolDb/Loader.py
+++ Python/Chem/MolDb/Loader.py
@@ -1,10 +1,11 @@
 # $Id$
 #
-#  Copyright (C) 2007 Greg Landrum
+#  Copyright (C) 2007-2008 Greg Landrum
 #   @@ All Rights Reserved @@
 #
 import Chem
 from Chem import AllChem
+from Chem import Lipinski,Descriptors,Crippen
 from Dbase.DbConnection import DbConnect
 from Dbase import DbModule
 
@@ -15,7 +16,9 @@ logger.setLevel(logging.INFO)
 
 def ProcessMol(mol,typeConversions,globalProps,nameProp='_Name',nameCol='compound_id',
                redraw=False,keepHs=False,
-               skipProps=False,skipSmiles=False):
+               skipProps=False,addComputedProps=False,
+               skipSmiles=False,
+               uniqNames=None,namesSeen=None):
   if not mol:
     raise ValueError,'no molecule'
   if keepHs:
@@ -26,12 +29,28 @@ def ProcessMol(mol,typeConversions,globalProps,nameProp='_Name',nameCol='compoun
     nm = None
   if not nm:
     nm = 'Mol_%d'%nDone
+  if uniqNames and nm in namesSeen:
+    logger.error('duplicate compound id (%s) encountered. second instance skipped.'%nm)
+    return None
+  namesSeen.add(nm)
   row = [nm]
   if not skipProps:
+    if addComputedProps:
+      nHD=Lipinski.NumHDonors(m)
+      m.SetProp('DonorCount',str(nHD))
+      nHA=Lipinski.NumHAcceptors(m)
+      m.SetProp('AcceptorCount',str(nHA))
+      nRot=Lipinski.NumRotatableBonds(m)
+      m.SetProp('RotatableBondCount',str(nRot))
+      MW=Descriptors.MolWt(m)
+      m.SetProp('AMW',str(MW))
+      logp=Crippen.MolLogP(m)
+      m.SetProp('MolLogP',str(logp))
+
     pns = list(mol.GetPropNames())
     pD={}
     for pi,pn in enumerate(pns):
-      if pn==nameCol: continue
+      if pn.lower()==nameCol.lower(): continue
       pv = mol.GetProp(pn).strip()
       if pv.find('>')<0 and pv.find('<')<0:
         colTyp = globalProps.get(pn,2)
@@ -55,7 +74,7 @@ def ProcessMol(mol,typeConversions,globalProps,nameProp='_Name',nameCol='compoun
   row.append(DbModule.binaryHolder(mol.ToBinary()))
   row.append(pD)
   return row
-          
+
 def ConvertRows(rows,globalProps,defaultVal,skipSmiles):
   for i,row in enumerate(rows):
     newRow = []
@@ -69,20 +88,23 @@ def ConvertRows(rows,globalProps,defaultVal,skipSmiles):
       newRow.append(row[2])
     rows[i] = newRow
 
-  
 def LoadDb(suppl,dbName,nameProp='_Name',nameCol='compound_id',silent=False,
            redraw=False,errorsTo=None,keepHs=False,defaultVal='N/A',skipProps=False,
-           regName='molecules',skipSmiles=False,maxRowsCached=-1):
-  nMols = len(suppl)
+           regName='molecules',skipSmiles=False,maxRowsCached=-1,
+           uniqNames=False,addComputedProps=False,lazySupplier=False):
+  if not lazySupplier:
+    nMols = len(suppl)
+  else:
+    nMols=-1
   if not silent:
     logger.info("Generating molecular database in file %s"%dbName)
-    logger.info("  Processing %d molecules"%nMols)
+    if not lazySupplier:
+      logger.info("  Processing %d molecules"%nMols)
   rows = []
   globalProps = {}
-  
+  namesSeen = set()
   nDone = 0
   typeConversions={0:('varchar',str),1:('float',float),2:('int',int)}
-  suppl.reset()
   for m in suppl:
     nDone +=1
     if not m:
@@ -93,15 +115,23 @@ def LoadDb(suppl,dbName,nameProp='_Name',nameCol='compound_id',silent=False,
         else:
           logger.warning('full error file support not complete')
       continue
-    row=ProcessMol(m,typeConversions,globalProps,nameProp,nameCol,redraw,
-                   keepHs,skipProps,skipSmiles)
+
+    row=ProcessMol(m,typeConversions,globalProps,nameProp=nameProp,
+                   nameCol=nameCol,redraw=redraw,
+                   keepHs=keepHs,skipProps=skipProps,
+                   addComputedProps=addComputedProps,skipSmiles=skipSmiles,
+                   uniqNames=uniqNames,namesSeen=namesSeen)
+    if row is None: continue
     rows.append(row)
     if not silent and not nDone%100:
       logger.info('  done %d'%nDone)
     if len(rows)==maxRowsCached:
       break
-    
-  typs = ['%s varchar not null'%nameCol]
+
+  nameDef='%s varchar not null'%nameCol
+  if uniqNames:
+    nameDef += ' unique'
+  typs = [nameDef]
   pns = []
   for pn,v in globalProps.iteritems():
     addNm = pn
@@ -110,11 +140,14 @@ def LoadDb(suppl,dbName,nameProp='_Name',nameCol='compound_id',silent=False,
     addNm = addNm.replace(' ','_')
     addNm = addNm.replace(':','_')
     typs.append('%s %s'%(addNm,typeConversions[v][0]))
+    pns.append(pn.lower())
 
   if not skipSmiles:
-    typs.append('smiles varchar')
+    if 'smiles' not in pns:
+      typs.append('smiles varchar')
+    else:
+      typs.append('cansmiles varchar')
   typs.append('molpkl %s'%(DbModule.binaryTypeName))
-
   conn = DbConnect(dbName)
   curs = conn.GetCursor()
   try:
@@ -123,6 +156,7 @@ def LoadDb(suppl,dbName,nameProp='_Name',nameCol='compound_id',silent=False,
     pass
   curs.execute('create table %s (%s)'%(regName,','.join(typs)))
   qs = ','.join([DbModule.placeHolder for x in typs])
+
 
   ConvertRows(rows,globalProps,defaultVal,skipSmiles)
   curs.executemany('insert into %s values (%s)'%(regName,qs),rows)
@@ -144,8 +178,12 @@ def LoadDb(suppl,dbName,nameProp='_Name',nameCol='compound_id',silent=False,
           logger.warning('full error file support not complete')
       continue
     tmpProps={}
-    row=ProcessMol(m,typeConversions,tmpProps,nameProp,nameCol,redraw,
-                   keepHs,skipProps,skipSmiles)
+    row=ProcessMol(m,typeConversions,globalProps,nameProp=nameProp,
+                   nameCol=nameCol,redraw=redraw,
+                   keepHs=keepHs,skipProps=skipProps,
+                   addComputedProps=addComputedProps,skipSmiles=skipSmiles,
+                   uniqNames=uniqNames,namesSeen=namesSeen)
+    if not row: continue
     rows.append(row)
     if not silent and not nDone%100:
       logger.info('  done %d'%nDone)
@@ -158,5 +196,3 @@ def LoadDb(suppl,dbName,nameProp='_Name',nameCol='compound_id',silent=False,
     ConvertRows(rows,globalProps,defaultVal,skipSmiles)
     curs.executemany('insert into %s values (%s)'%(regName,qs),rows)
     conn.Commit()
-
-
