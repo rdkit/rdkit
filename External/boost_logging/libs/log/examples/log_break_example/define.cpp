ce3f04fee91b49a062f721e0a2ad0f75978cc675
Greg Landrum <greg.landrum@gmail.com>
2008-05-06 08:22:50 +0000

    remove boost::logging from the distribution, it's obsolete and we don't use it anymore.

--- External/boost_logging/libs/log/examples/log_break_example/define.cpp
+++
@@ -1,7 +0,0 @@
-// define.cpp - define the logs
-#include "declare.h"
-
-BOOST_DEFINE_LOG(app, "app")
-BOOST_DEFINE_LOG(dbg, "app.dbg")
-BOOST_DEFINE_LOG(warn, "app.warn")
-
--- External/boost_logging/libs/log/examples/rotating_logs/define.cpp
+++
@@ -1,5 +0,0 @@
-// define.cpp - define the logs
-#include "declare.h"
-
-BOOST_DEFINE_LOG(app, "app")
-
