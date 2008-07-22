49c775e5a0f33a8fa76fa2b3fb914f13c0a2e02c
Greg Landrum <greg.landrum@gmail.com>
2008-07-22 04:17:50 +0000

    update a bit to use subprocess.call for python2.5 and greater

--- Python/TestRunner.py
+++ Python/TestRunner.py
@@ -1,11 +1,15 @@
 #  $Id$
 #
-#  Copyright (c) 2003-2006 Rational Discovery LLC
+#  Copyright (c) 2003-2008 Greg Landrum and Rational Discovery LLC
 #
 #   @@ All Rights Reserved  @@
 #
 import RDConfig
 import os,sys,time
+if sys.version_info[0]>2 or sys.version_info[1]>4:
+  import subprocess
+else:
+  subprocess=None
 
 TEST_FAILED=-1
 TEST_PASSED=0
@@ -13,17 +17,25 @@ TEST_PASSED=0
 def RunTest(exeName,args,extras):
   if exeName=="python":
     exeName=RDConfig.pythonExe
-
   args = args.split(' ')  
 
   startDir = os.getcwd()
   if extras.has_key('dir'):
     os.chdir(extras['dir'])
   expectedReturn = extras.get('returns',0)
-  if not os.path.exists(exeName):
-    print >>sys.stderr,"Could not find executable: %s."%exeName
-    return TEST_FAILED
-  retVal = RDConfig.spawn(os.P_WAIT,exeName,[exeName]+args,os.environ)
+  if not subprocess:
+    retVal = spawn(os.P_WAIT,exeName,[exeName]+args,os.environ)
+    if not os.path.exists(exeName):
+      print >>sys.stderr,"Could not find executable: %s."%exeName
+      return TEST_FAILED
+  else:
+    try:
+      retVal = subprocess.call([exeName]+list(args))
+    except OSError:
+      print >>sys.stderr,"Could not find executable: %s."%exeName
+      return TEST_FAILED
+      
+
   if extras.has_key('dir'):
     os.chdir(startDir)
   if retVal!=expectedReturn:
