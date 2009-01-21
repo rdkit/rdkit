0a3d58d6c2290660182a08f3c9f4c257a6e52f1f
Greg Landrum <greg.landrum@gmail.com>
2009-01-21 19:16:49 +0000

    make testing more automatic

--- Python/ML/InfoTheory/test_list.py
+++ Python/ML/InfoTheory/test_list.py
@@ -6,3 +6,8 @@ tests=[
 longTests=[
     ("python","testBitRanker.py",{}),
   ]
+if __name__=='__main__':
+  import sys
+  import TestRunner
+  failed,tests = TestRunner.RunScript('test_list.py',0,1)
+  sys.exit(len(failed))
