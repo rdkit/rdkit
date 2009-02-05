83231d3f0c8c5931d1a7898330ee6ee8e186da42
Greg Landrum <greg.landrum@gmail.com>
2009-02-05 05:49:11 +0000

    we don't want backslashes in these paths on windows

--- Code/CompleteJamroot.py
+++ Code/CompleteJamroot.py
@@ -5,7 +5,8 @@ except ImportError:
     print >>sys.stderr,"ERROR: numpy module not found.\nThe RDKit python wrappers will not build correctly, but the rest of the code should be fine."
     sys.exit(0)
 
-incDir = os.path.join(os.path.split(numpy.__file__)[0],'core','include')
+numpyDir=os.path.split(numpy.__file__)[0].replace('\\','/')
+incDir = '/'.join((numpyDir,'core','include'))
 inD = file('Jamroot.in','r').read()
 print >>file('Jamroot','w+'),inD.replace('NUMPY_INCLUDE_DIR',incDir)
     
