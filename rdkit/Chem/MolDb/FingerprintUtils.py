fb4136f1c55f4d30266feeca1417eabf85e60dd6
Greg Landrum <greg.landrum@gmail.com>
2009-07-13 08:57:14 +0000

    ongoing improvements

--- rdkit/Chem/MolDb/FingerprintUtils.py
+++ rdkit/Chem/MolDb/FingerprintUtils.py
@@ -5,6 +5,43 @@
 #
 import cPickle
 from rdkit import DataStructs
+from rdkit import Chem
+
+class LayeredOptions:
+  loadLayerFlags=0xFFFFFFFF
+  searchLayerFlags=0x7
+  minPath=1
+  maxPath=6
+  fpSize=1024
+  wordSize=32
+  nWords=fpSize//wordSize
+  @staticmethod
+  def GetFingerprint(mol,query=True):
+    if query:
+      flags=LayeredOptions.searchLayerFlags
+    else:
+      flags=LayeredOptions.loadLayerFlags
+    return Chem.LayeredFingerprint(mol,layerFlags=flags,
+                                   minPath=LayeredOptions.minPath,maxPath=LayeredOptions.maxPath,
+                                   fpSize=LayeredOptions.fpSize)
+  @staticmethod
+  def GetWords(mol,query=True):
+    txt = LayeredOptions.GetFingerprint(mol,query=query).ToBitString()
+    words = [int(txt[x:x+32],2) for x in range(0,len(txt),32)]
+    return  words
+
+  @staticmethod
+  def GetQueryText(mol,query=True):
+    words = LayeredOptions.GetWords(mol,query=query)
+    colqs = []
+    for idx,word in enumerate(words):
+      if not word:
+        continue
+      idx = idx+1
+      colqs.append('%(word)d&Col_%(idx)d=%(word)d'%locals())
+    return  ' and '.join(colqs)
+
+
 
 def BuildSigFactory(options=None,fdefFile=None,
                     bins=[(2,3),(3,4),(4,5),(5,6),(6,7),(7,8),(8,100)],
