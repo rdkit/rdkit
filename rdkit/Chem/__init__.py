13c568ef2bf9485002c11acb731a8acd27dce545
Greg Landrum <greg.landrum@gmail.com>
2009-08-08 10:25:59 +0000

    get "from rdkit import Chem" working for minimal

--- rdkit/Chem/__init__.py
+++ rdkit/Chem/__init__.py
@@ -22,63 +22,6 @@ _HasSubstructMatchStr=rdchem._HasSubstructMatchStr
 from rdchem import *
 from rdmolfiles import *
 from rdmolops import *
-from rdsubgraphs import *
-from rdchemtransforms import *
-from rdMolDescriptors import RDKFingerprint,LayeredFingerprint
-
-def GetSmartsMatchCDXML(mol,patt,maps,which=0,showAllAtoms=0):
-  try:
-    from rdkit.Chem import CDXMLWriter
-  except:
-    CDXMLWriter = None
-  if CDXMLWriter is None:
-    return ''
-  try:
-    from cStringIO import StringIO
-  except:
-    from StringIO import StringIO
-  if not showAllAtoms and (which < 0 or not len(maps) or len(maps)<which+1):
-    return ''
-  io = StringIO()
-  matchBonds = []
-  matchAtoms = []
-  if not showAllAtoms:
-    mapL = maps[which]
-    for bondIdx in range(patt.GetNumBonds()):
-      bnd = patt.GetBondWithIdx(bondIdx)
-      begIdx = bnd.GetBeginAtomIdx()
-      endIdx = bnd.GetEndAtomIdx()
-      molBegIdx = mapL[begIdx]
-      molEndIdx = mapL[endIdx]
-      matchBonds.append(mol.GetBondBetweenAtoms(molBegIdx,molEndIdx).GetIdx())
-  else:
-    for mapL in maps:
-      for idx in mapL:
-        if idx not in matchAtoms: matchAtoms.append(idx)
-
-  CDXMLWriter.MolToCDXML(mol,io,highlightBonds=matchBonds,
-                         highlightAtoms=matchAtoms)
-  cdxml = io.getvalue()
-  cdxml = cdxml.replace('><','>\n<')
-  return cdxml
-
-def DisplaySmartsMatch(mol,patt,maps,which=0,showAllAtoms=0):
-  try:
-    from rdkit.utils import chemdraw
-  except:
-    chemdraw = None
-  cdxml = GetSmartsMatchCDXML(mol,patt,maps,which=which,showAllAtoms=showAllAtoms)
-  if cdxml and chemdraw:
-    chemdraw.CDXDisplay(cdxml)
-  return cdxml
-  
-def QuickSmartsMatch(smi,sma,unique=1,display=0):
-  m = MolFromSmiles(smi)
-  p = MolFromSmarts(sma)
-  res = m.GetSubstructMatches(p,unique)
-  if display:
-    DisplaySmartsMatch(m,p,res)
-  return res  
 
 def CanonSmiles(smi,useChiral=1):
   m = MolFromSmiles(smi)
