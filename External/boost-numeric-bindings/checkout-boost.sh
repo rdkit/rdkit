1ee235865d52dbb42a1b301dab3a9784de092d9f
Greg Landrum <greg.landrum@gmail.com>
2009-10-31 06:12:30 +0000

    should not need the numeric bindings anymore

--- External/boost-numeric-bindings/checkout-boost.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-mkdir -p bindings/{libs,boost}/numeric
-(cd bindings/boost/numeric;
-  svn co http://svn.boost.org/svn/boost/sandbox/boost/numeric/bindings)
-(cd bindings/libs/numeric;
-  svn co http://svn.boost.org/svn/boost/sandbox/libs/numeric/bindings)
-
