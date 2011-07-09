db63eca04c5617976ad9e8b3d41375a3aa3ddcb8
Greg Landrum <greg.landrum@gmail.com>
2011-07-09 06:21:08 +0000

    work to get the inchi code working with a pre-installed inchi library

---
+++ Code/cmake/Modules/FindInchi.cmake
@@ -0,0 +1,24 @@
+# - Try to find Inchi lib
+# Once done this will define
+#
+#  INCHI_FOUND - system has inchi lib
+#  INCHI_INCLUDE_DIR - the inchi include directory
+#  INCHI_LIBRARIES - the inchi library
+
+# Taken from the open babel distribution
+#  https://openbabel.svn.sourceforge.net/svnroot/openbabel/openbabel/trunk/cmake/modules/FindInchi.cmake
+# Copyright (c) 2010 Marcus D. Hanwell, <marcus@cryos.org>
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+if(INCHI_INCLUDE_DIR AND INCHI_LIBRARY)
+  # in cache already
+  set(INCHI_FOUND TRUE)
+else()
+  find_path(INCHI_INCLUDE_DIR NAMES inchi_api.h PATHS /usr/include/inchi )
+  find_library(INCHI_LIBRARY NAMES inchi Inchi)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(INCHI DEFAULT_MSG INCHI_LIBRARY
+    INCHI_INCLUDE_DIR)
+  mark_as_advanced(INCHI_INCLUDE_DIR INCHI_LIBRARIES)
+endif()
