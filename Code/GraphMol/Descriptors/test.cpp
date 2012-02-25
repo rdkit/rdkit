d00ee0825dc7dff8e6bc0177cc2109e33b09325b
Greg Landrum <greg.landrum@gmail.com>
2012-02-25 16:27:54 +0000

    another update

--- Code/GraphMol/Descriptors/test.cpp
+++ Code/GraphMol/Descriptors/test.cpp
@@ -39,7 +39,7 @@ void test1(){
   BOOST_LOG(rdErrorLog) << "-------------------------------------" << std::endl;
   BOOST_LOG(rdErrorLog) << "    Test Crippen parameter acquisition." << std::endl;
 
-  CrippenParamCollection *params=CrippenParamCollection::getParams();
+  const CrippenParamCollection *params=CrippenParamCollection::getParams();
   TEST_ASSERT(params);
   
   CrippenParams p=*(params->begin());
@@ -704,6 +704,62 @@ void testIssue3433771(){
   BOOST_LOG(rdErrorLog) << "  done" << std::endl;
 }
 
+#ifdef RDK_TEST_MULTITHREADED
+namespace {
+  void runblock(const std::vector<ROMol *> &mols,unsigned int count,unsigned int idx){
+    for(unsigned int j=0;j<1000;j++){
+      for(unsigned int i=0;i<mols.size();++i){
+        if(i%count != idx) continue;
+        ROMol *mol = mols[i];
+        int nHBD=calcNumHBD(*mol);
+        int nHBA=calcNumHBA(*mol);
+        int nAmide=calcNumAmideBonds(*mol);
+        double logp,mr;
+        calcCrippenDescriptors(*mol,logp,mr);
+      }
+    }
+  };
+}
+
+#include <boost/thread.hpp>  
+void testMultiThread(){
+  BOOST_LOG(rdErrorLog) << "-------------------------------------" << std::endl;
+  BOOST_LOG(rdErrorLog) << "    Test multithreading" << std::endl;
+
+  std::string fName = getenv("RDBASE");
+  fName += "/Code/GraphMol/Descriptors/vendor.simpairs.20k.txt";
+  SmilesMolSupplier suppl(fName," \t",1,0);
+  std::cerr<<"reading molecules"<<std::endl;
+  std::vector<ROMol *> mols;
+  while(!suppl.atEnd()&&mols.size()<100){
+    ROMol *mol=0;
+    try{
+      mol=suppl.next();
+    } catch(...){
+      continue;
+    }
+    if(!mol) continue;
+    mols.push_back(mol);
+  }
+  boost::thread_group tg;
+
+  std::cerr<<"processing"<<std::endl;
+  unsigned int count=4;
+  for(unsigned int i=0;i<count;++i){
+    std::cerr<<" launch :"<<i<<std::endl;std::cerr.flush();
+    tg.add_thread(new boost::thread(runblock,mols,count,i));
+  }
+  tg.join_all();
+
+  for(unsigned int i=0;i<mols.size();++i) delete mols[i];
+
+
+  BOOST_LOG(rdErrorLog) << "  done" << std::endl;
+}
+#else
+void testMultiThread(){
+}
+#endif
 
 
 //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
@@ -725,4 +781,5 @@ int main(){
   testIssue3415534();
 #endif
   testIssue3433771();
+  testMultiThread();
 }
--- Code/Numerics/Optimizer/BFGSOpt.h
+++ Code/Numerics/Optimizer/BFGSOpt.h
@@ -7,6 +7,9 @@
 //  which is included in the file license.txt, found at the root
 //  of the RDKit source tree.
 //
+#include <math.h>
+#include <RDGeneral/Invariant.h>
+#include <cstring>
 
 namespace BFGSOpt {
   const double FUNCTOL=1e-4;  //!< Default tolerance for function convergence in the minimizer
@@ -36,12 +39,108 @@ namespace BFGSOpt {
       -  1: the stepsize got too small.  This probably indicates success.
       - -1: the direction is bad (orthogonal to the gradient)
   */
+  template <typename EnergyFunctor>
   void linearSearch(unsigned int dim,double *oldPt,double oldVal,
                     double *grad,double *dir,double *newPt,
                     double &newVal,
-                    double (*func)(double *),
-                    double maxStep,int &resCode);
+                    EnergyFunctor func,
+                    double maxStep,int &resCode){
+    PRECONDITION(oldPt,"bad input array");
+    PRECONDITION(grad,"bad input array");
+    PRECONDITION(dir,"bad input array");
+    PRECONDITION(newPt,"bad input array");
 
+    double sum=0.0,slope=0.0,test=0.0,lambda=0.0;
+    double lambda2=0.0,lambdaMin=0.0,tmpLambda=0.0,val2=0.0;
+
+    resCode=0;
+
+    // get the length of the direction vector:
+    sum=0.0;
+    for(unsigned int i=0;i<dim;i++)
+      sum +=dir[i]*dir[i];
+    sum=sqrt(sum);
+
+    // rescale if we're trying to move too far:
+    if(sum>maxStep){
+      for(unsigned int i=0;i<dim;i++)
+	dir[i] *= maxStep/sum;
+    }
+      
+    // make sure our direction has at least some component along
+    // -grad
+    slope=0.0;
+    for(unsigned int i=0;i<dim;i++){
+      slope += dir[i]*grad[i];
+    }
+    if(slope>=0.0){
+      resCode=-1;
+      return;
+    }
+
+    test=0.0;
+    for(unsigned int i=0;i<dim;i++){
+      double temp=fabs(dir[i])/std::max(fabs(oldPt[i]),1.0);
+      if(temp>test) test=temp;
+    }
+
+    lambdaMin = MOVETOL/test;
+    lambda = 1.0;
+    while(1){
+      //std::cout << "\t" << lambda << " " << lambdaMin << std::endl;
+      if(lambda<lambdaMin){
+	// the position change is too small... set the resCode and return
+	for(unsigned int i=0;i<dim;i++){
+	  newPt[i]=oldPt[i];
+        }
+	resCode=1;
+	return;
+      }
+      for(unsigned int i=0;i<dim;i++){
+	newPt[i]=oldPt[i]+lambda*dir[i];
+      }
+      newVal = func(newPt);
+
+      if( newVal-oldVal <= FUNCTOL*lambda*slope ){
+	// we're converged on the function:
+	return;
+      } else {
+	// if we made it this far, we need to backtrack:
+	if(lambda==1.0){
+	  // it's the first step:
+	  tmpLambda = -slope / (2.0*(newVal-oldVal-slope));
+	} else {
+	  double rhs1 = newVal-oldVal-lambda*slope;
+	  double rhs2 = val2-oldVal-lambda2*slope;
+	  double a = (rhs1/(lambda*lambda) - rhs2/(lambda2*lambda2))/
+	    (lambda-lambda2);
+	  double b = (-lambda2*rhs1/(lambda*lambda)+lambda*rhs2/(lambda2*lambda2))/
+	    (lambda-lambda2);
+	  if( a==0.0 ){
+	    tmpLambda = -slope/(2.0*b);
+	  } else {
+	    double disc=b*b-3*a*slope;
+	    if(disc<0.0){
+	      tmpLambda = 0.5*lambda;
+	    } else if(b<=0.0) {
+	      tmpLambda = (-b+sqrt(disc))/(3.0*a);
+	    } else {
+	      tmpLambda = -slope/(b+sqrt(disc));
+	    }
+	  }
+	  if( tmpLambda > 0.5*lambda ){
+	    tmpLambda = 0.5*lambda;
+	  }
+	}
+      }
+      lambda2 = lambda;
+      val2 = newVal;
+      lambda = std::max(tmpLambda,0.1*lambda);
+    }
+  }
+
+#define CLEANUP() { delete [] grad; delete [] dGrad; delete [] hessDGrad;\
+ delete [] newPos; delete [] xi; delete [] invHessian; }
   //! Do a BFGS minimization of a function.
   /*!
      See Numerical Recipes in C, Section 10.7 for a description of the algorithm.
@@ -60,12 +159,135 @@ namespace BFGSOpt {
       -  0: success
       -  1: too many iterations were required
   */
+  template <typename EnergyFunctor,typename GradientFunctor>
   int minimize(unsigned int dim,double *pos,
                double gradTol,
                unsigned int &numIters,
                double &funcVal,
-               double (*func)(double *),
-               double (*gradFunc)(double *,double*),
+               EnergyFunctor func,
+               GradientFunctor gradFunc,
                double funcTol=TOLX,
-               unsigned int maxIts=MAXITS);
+               unsigned int maxIts=MAXITS){
+    PRECONDITION(pos,"bad input array");
+    PRECONDITION(gradTol>0,"bad tolerance");
+
+    double sum,maxStep,fp;
+
+    double *grad,*dGrad,*hessDGrad;
+    double *newPos,*xi;
+    double *invHessian;
+
+    grad = new double[dim];
+    dGrad = new double[dim];
+    hessDGrad = new double[dim];
+    newPos = new double[dim];
+    xi = new double[dim];
+    invHessian = new double[dim*dim];
+
+    // evaluate the function and gradient in our current position:
+    fp=func(pos);
+    gradFunc(pos,grad);
+
+    sum = 0.0;
+    memset(invHessian,0,dim*dim*sizeof(double));
+    for(unsigned int i=0;i<dim;i++){
+      unsigned int itab=i*dim;
+      // initialize the inverse hessian to be identity:
+      invHessian[itab+i]=1.0;
+      // the first line dir is -grad:
+      xi[i] = -grad[i];
+      sum += pos[i]*pos[i];
+    }
+    // pick a max step size:
+    maxStep = MAXSTEP * std::max(sqrt(sum),static_cast<double>(dim));
+
+
+    for(unsigned int iter=1;iter<=maxIts;iter++){
+      numIters=iter;
+      int status;
+
+      // do the line search:
+      linearSearch(dim,pos,fp,grad,xi,newPos,funcVal,func,maxStep,status);
+      CHECK_INVARIANT(status>=0,"bad direction in linearSearch");
+
+      // save the function value for the next search:
+      fp = funcVal;
+
+      // set the direction of this line and save the gradient:
+      double test=0.0;
+      for(unsigned int i=0;i<dim;i++){
+        xi[i] = newPos[i]-pos[i];
+        pos[i] = newPos[i];
+        double temp=fabs(xi[i])/std::max(fabs(pos[i]),1.0);
+        if(temp>test) test=temp;
+        dGrad[i] = grad[i];
+      }
+      if(test<TOLX) {
+        CLEANUP();
+        return 0;
+      }
+
+      // update the gradient:
+      double gradScale=gradFunc(pos,grad);
+
+      // is the gradient converged?
+      test=0.0;
+      double term=std::max(funcVal*gradScale,1.0);
+      for(unsigned int i=0;i<dim;i++){
+        double temp=fabs(grad[i])*std::max(fabs(pos[i]),1.0);
+        test=std::max(test,temp);
+        dGrad[i] = grad[i]-dGrad[i];
+      }
+      test /= term;
+      if(test<gradTol){
+        CLEANUP();
+        return 0;
+      }
+
+      //for(unsigned int i=0;i<dim;i++){
+        // figure out how much the gradient changed:
+      //}
+    
+      // compute hessian*dGrad:
+      double fac=0,fae=0,sumDGrad=0,sumXi=0;
+      for(unsigned int i=0;i<dim;i++){
+        unsigned int itab=i*dim;
+        hessDGrad[i] = 0.0;
+        for(unsigned int j=0;j<dim;j++){
+          hessDGrad[i] += invHessian[itab+j]*dGrad[j];
+        }
+
+        fac += dGrad[i]*xi[i];
+        fae += dGrad[i]*hessDGrad[i];
+        sumDGrad += dGrad[i]*dGrad[i];
+        sumXi += xi[i]*xi[i];
+      }
+      if(fac > sqrt(EPS*sumDGrad*sumXi)){
+        fac = 1.0/fac;
+        double fad = 1.0/fae;
+        for(unsigned int i=0;i<dim;i++){
+          dGrad[i] = fac*xi[i] - fad*hessDGrad[i];
+        }
+        for(unsigned int i=0;i<dim;i++){
+          unsigned int itab=i*dim;
+          for(unsigned int j=i;j<dim;j++){
+            invHessian[itab+j] += fac*xi[i]*xi[j] -
+              fad*hessDGrad[i]*hessDGrad[j] +
+              fae*dGrad[i]*dGrad[j];
+            invHessian[j*dim+i] = invHessian[itab+j];
+          }
+        }
+      }
+      // generate the next direction to move:
+      for(unsigned int i=0;i<dim;i++){
+        unsigned int itab=i*dim;
+        xi[i] = 0.0;
+        for(unsigned int j=0;j<dim;j++){
+          xi[i] -= invHessian[itab+j]*grad[j];
+        }
+      }
+    }
+    CLEANUP();
+    return 1;
+  }
 }
