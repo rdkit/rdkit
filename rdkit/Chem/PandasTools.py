7840f00863b8e7a66e0d874afa7e6124ab263928
Nikolas Fechner <nhfechner@users.sourceforge.net>
2013-04-19 09:35:47 +0000

    new module that allows to use rdkit molecule objects in pandas dataframes

---
+++ rdkit/Chem/PandasTools.py
@@ -0,0 +1,191 @@
+'''
+Importing pandasTools enables several features that allow for using RDKit molecules as columns of a Pandas dataframe.
+If the dataframe is containing a molecule format in a column (e.g. smiles), like in this example:
+>>> from rdkit.Chem import PandasTools
+>>> import pandas as pd
+>>> import os
+>>> from rdkit import RDConfig
+>>> antibiotics = pd.DataFrame(columns=['Name','Smiles'])
+>>> antibiotics = antibiotics.append({'Smiles':'CC1(C(N2C(S1)C(C2=O)NC(=O)CC3=CC=CC=C3)C(=O)O)C','Name':'Penicilline G'}, ignore_index=True)#Penicilline G
+>>> antibiotics = antibiotics.append({'Smiles':'CC1(C2CC3C(C(=O)C(=C(C3(C(=O)C2=C(C4=C1C=CC=C4O)O)O)O)C(=O)N)N(C)C)O','Name':'Tetracycline'}, ignore_index=True)#Tetracycline
+>>> antibiotics = antibiotics.append({'Smiles':'CC1(C(N2C(S1)C(C2=O)NC(=O)C(C3=CC=CC=C3)N)C(=O)O)C','Name':'Ampicilline'}, ignore_index=True)#Ampicilline
+>>> print antibiotics.columns
+Index([Name, Smiles], dtype=object)
+>>> print antibiotics
+            Name                                             Smiles
+0  Penicilline G    CC1(C(N2C(S1)C(C2=O)NC(=O)CC3=CC=CC=C3)C(=O)O)C
+1   Tetracycline  CC1(C2CC3C(C(=O)C(=C(C3(C(=O)C2=C(C4=C1C=CC=C4...
+2  Ampicilline  CC1(C(N2C(S1)C(C2=O)NC(=O)C(C3=CC=CC=C3)N)C(=O...
+
+a new column can be created holding the respective RDKit molecule objects. The fingerprint can be included to accelerate substructure searches on the dataframe.
+
+>>> PandasTools.AddMoleculeColumnToFrame(antibiotics,'Smiles','Molecule',includeFingerprints=True)
+>>> print antibiotics.columns
+Index([Name, Smiles, Molecule], dtype=object)
+
+A substructure filter can be applied on the dataframe using the RDKit molecule column, because the ">=" operator has been modified to work as a substructure check.
+Such the antibiotics containing the beta-lactam ring "C1C(=O)NC1" can be obtained by
+
+>>> beta_lactam = Chem.MolFromSmiles('C1C(=O)NC1')
+>>> beta_lactam_antibiotics = antibiotics[antibiotics['Molecule'] >= beta_lactam]
+>>> print beta_lactam_antibiotics[['Name','Smiles']]
+            Name                                             Smiles
+0  Penicilline G    CC1(C(N2C(S1)C(C2=O)NC(=O)CC3=CC=CC=C3)C(=O)O)C
+2  Ampicilline  CC1(C(N2C(S1)C(C2=O)NC(=O)C(C3=CC=CC=C3)N)C(=O...
+
+
+It is also possible to load an SDF file can be load into a dataframe.
+
+>>> sdfFile = os.path.join(RDConfig.RDDataDir,'NCI/first_200.props.sdf')
+>>> frame = PandasTools.LoadSDF(sdfFile,smilesName='SMILES',molColName='Molecule',includeFingerprints=True)
+>>> frame.info
+<bound method DataFrame.info of <class 'pandas.core.frame.DataFrame'>
+Int64Index: 200 entries, 0 to 199
+Data columns:
+AMW                       200  non-null values
+CLOGP                     200  non-null values
+CP                        200  non-null values
+CR                        200  non-null values
+DAYLIGHT.FPG              200  non-null values
+DAYLIGHT_CLOGP            200  non-null values
+FP                        200  non-null values
+ID                        200  non-null values
+ISM                       200  non-null values
+LIPINSKI_VIOLATIONS       200  non-null values
+NUM_HACCEPTORS            200  non-null values
+NUM_HDONORS               200  non-null values
+NUM_HETEROATOMS           200  non-null values
+NUM_LIPINSKIHACCEPTORS    200  non-null values
+NUM_LIPINSKIHDONORS       200  non-null values
+NUM_RINGS                 200  non-null values
+NUM_ROTATABLEBONDS        200  non-null values
+P1                        30  non-null values
+SMILES                    200  non-null values
+Molecule                  200  non-null values
+dtypes: object(20)>
+
+In order to support rendering the molecules as images in the HTML export of the dataframe, the __str__ method is duck-typed to return a base64 encoded PNG:
+>>> molX = Chem.MolFromSmiles('Fc1cNc2ccccc12')
+>>> print molX
+<img src="data:image/png;base64,..." alt="Mol"/>
+'''
+
+from rdkit import Chem
+from rdkit.Chem import Draw
+from base64 import b64encode
+from StringIO import StringIO
+import pandas as pd
+
+
+
+def _get_image(x):
+  """displayhook function for PIL Images, rendered as PNG"""
+  import pandas
+  sio = StringIO()    
+  x.save(sio,format='PNG')
+  s = b64encode(sio.getvalue())
+  if len(s)+100 > pd.get_option("display.max_colwidth"):
+    pd.set_option("display.max_colwidth",len(s)+100)
+  return s
+
+from rdkit import DataStructs
+
+from rdkit.Avalon import pyAvalonTools as pyAvalonTools
+
+def _molge(x,y):
+  """Allows for substructure check using the >= operator (X has substructyre Y -> X >= Y) by duck-typing the __ge__ function 
+  This has the effect that the pandas/numpy rowfilter can be used for substructure filtering (filtered = dframe[dframe['RDKitColumn'] >= SubstructureMolecule])
+  """
+  if x is None or y is None: return False
+  if hasattr(x,'_substructfp'):
+    if not hasattr(y,'_substructfp'):
+      #y._substructfp=Chem.LayeredFingerprint2(y)
+      y._substructfp=pyAvalonTools.GetAvalonFP(y,isQuery=True,bitFlags=pyAvalonTools.avalonSSSBits)
+    if not DataStructs.AllProbeBitsMatch(y._substructfp,x._substructfp):
+      return False
+  return x.HasSubstructMatch(y)
+            
+Chem.Mol.__ge__ = _molge # lambda x,y: x.HasSubstructMatch(y)
+
+def PrintAsBase64PNGString(x,renderer = None):
+  '''returns the molecules as base64 encoded PNG image
+  '''
+  return '<img src="data:image/png;base64,%s" alt="Mol"/>'%_get_image(Draw.MolToImage(x))
+
+#Chem.Mol.__str__ = lambda x: '<img src="data:image/png;base64,%s" alt="Mol"/>'%get_image(Draw.MolToImage(x))
+Chem.Mol.__str__ = PrintAsBase64PNGString
+
+def _MolPlusFingerprintFromSmiles(smi):
+  '''Precomputes Avalon fingerprints and stores results in molecule objects to accelerate substructure matching
+  '''
+  m = Chem.MolFromSmiles(smi)
+  if m is not None:
+    #m._substructfp = Chem.LayeredFingerprint2(m)
+    #m._substructfp=pyAvalonTools.GetAvalonFP(m,bitFlags=pyAvalonTools.avalonSSSBits)
+    m._substructfp=pyAvalonTools.GetAvalonFP(smi,True,bitFlags=pyAvalonTools.avalonSSSBits)
+  return m
+
+def AddMoleculeColumnToFrame(frame, smilesCol='Smiles', molCol = 'ROMol',includeFingerprints=False):
+  '''Converts the molecules contains in "smilesCol" to RDKit molecules and appends them to the dataframe "frame" using the specified column name.
+  If desired, the avalon fingerprint can be computed and stored within the molecule objects to accelerate substructure matching
+  '''
+  if not includeFingerprints:
+    frame[molCol]=frame.apply(lambda x: Chem.MolFromSmiles(x[smilesCol]), axis=1)
+  else:
+    frame[molCol]=frame.apply(lambda x: _MolPlusFingerprintFromSmiles(x[smilesCol]), axis=1)      
+
+    
+def LoadSDF(filename, smilesName='SMILES', idName='ID',molColName = 'ROMol',includeFingerprints=False):
+  """ Read file in SDF format and return as Panda data frame """
+  df = None
+  with open(filename, 'rU') as f:
+    for i, mol in enumerate(Chem.ForwardSDMolSupplier(f)):
+      if mol is None: continue
+      row = dict((k, mol.GetProp(k)) for k in mol.GetPropNames())
+      if mol.HasProp('_Name'): row[idName] = mol.GetProp('_Name')
+      row[smilesName] = Chem.MolToSmiles(mol)
+      row = pd.DataFrame(row, index=[i])
+      if df is None:
+        df = row
+      else:
+        df = df.append(row)
+  AddMoleculeColumnToFrame(df, smilesCol=smilesName, molCol = molColName,includeFingerprints=includeFingerprints)
+  return df  
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS+doctest.NORMALIZE_WHITESPACE)
+
+#
+#  Copyright (c) 2013, Novartis Institutes for BioMedical Research Inc.
+#  All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: 
+#
+#     * Redistributions of source code must retain the above copyright 
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following 
+#       disclaimer in the documentation and/or other materials provided 
+#       with the distribution.
+#     * Neither the name of Novartis Institutes for BioMedical Research Inc. 
+#       nor the names of its contributors may be used to endorse or promote 
+#       products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+    
