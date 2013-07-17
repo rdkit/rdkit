e196794970621126339f38bd86c208b6c42ca67b
Greg Landrum <greg.landrum@gmail.com>
2013-07-17 12:58:57 -0400

    go back to using strings in the dictionary to avoid the memory leaks

--- Code/RDGeneral/Dict.h
+++ Code/RDGeneral/Dict.h
@@ -17,25 +17,14 @@
 
 #include <map>
 #include <string>
-#include <cstring>
-#include <cstdlib>
 #include <vector>
-#include <iostream>
 #include <boost/any.hpp>
 #include <RDBoost/Exceptions.h>
 #include <boost/lexical_cast.hpp>
 
 namespace RDKit{
   typedef std::vector<std::string> STR_VECT;
-  namespace detail {
-    struct strcmpFunctor {
-      bool operator()(const char *a, const char *b) const {
-        return std::strcmp(a, b) < 0;
-      }
-    };
-  }
 
-  
   //! \brief The \c Dict class can be used to store objects of arbitrary
   //!        type keyed by \c strings.
   //!
@@ -43,7 +32,7 @@ namespace RDKit{
   //!
   class Dict {
   public:
-    typedef std::map<const char *, boost::any, detail::strcmpFunctor> DataType;
+    typedef std::map<const std::string, boost::any> DataType;
     Dict(){
       _data.clear();
     };
@@ -61,10 +50,11 @@ namespace RDKit{
     //! \brief Returns whether or not the dictionary contains a particular
     //!        key.
     bool hasVal(const char *what) const{
-      return _data.find(what)!=_data.end();
+      std::string key(what);
+      return hasVal(key);
     };
     bool hasVal(const std::string &what) const {
-      return hasVal(what.c_str());
+      return _data.find(what)!=_data.end();
     };
 
     //----------------------------------------------------------
@@ -95,34 +85,37 @@ namespace RDKit{
           a KeyErrorException will be thrown.
     */
     template <typename T>
-    T getVal(const char *what) const {
+    void getVal(const std::string &what,T &res) const {
+      res = getVal<T>(what);
+    };
+    //! \overload
+    template <typename T>
+    T getVal(const std::string &what) const {
       DataType::const_iterator pos=_data.find(what);
       if(pos==_data.end())
 	throw KeyErrorException(what);
       const boost::any &val = pos->second;
       return fromany<T>(val);
-    };
-    //! \overload
-    template <typename T>
-    void getVal(const char *what,T &res) const {
-      res = getVal<T>(what);
-    };
-
-    //! \overload
-    template <typename T>
-    void getVal(const std::string &what,T &res) const {
-      res = getVal<T>(what.c_str());
-    };
-    //! \overload
-    template <typename T>
-    T getVal(const std::string &what) const {
-      return getVal<T>(what.c_str());
     }
 
     //! \overload
-    void getVal(const char *what, std::string &res) const;
+    template <typename T>
+    T getVal(const char *what,T &res) const {  // FIX: it doesn't really fit that this returns T
+      std::string key(what);
+      res = getVal<T>(key);
+      return res; 
+    };
     //! \overload
-    void getVal(const std::string &what, std::string &res) const { getVal(what.c_str(),res); };
+    template <typename T>
+    T getVal(const char *what) const {
+      std::string key(what);
+      return getVal<T>(key);
+    };
+
+    //! \overload
+    void getVal(const std::string &what, std::string &res) const;
+    //! \overload
+    void getVal(const char *what, std::string &res) const { getVal(std::string(what),res); };
 
     //----------------------------------------------------------
     //! \brief Sets the value associated with a key
@@ -138,19 +131,21 @@ namespace RDKit{
             the value will be replaced.
     */
     template <typename T>
-    void setVal(const char *what, T &val){
-      _data[strdup(what)] = toany(val);
+    void setVal(const std::string &what, T &val){
+      _data[what] = toany(val);
     };
     //! \overload
     template <typename T>
-    void setVal(const std::string &what, T &val){
-      setVal(what.c_str(),val);
+    void setVal(const char *what, T &val){
+      std::string key = what;
+      setVal(key,val);
     };
     //! \overload
-    void setVal(const char *what, const char *val){
-      std::string v(val);
-      setVal(what,v);
+    void setVal(const std::string &what, const char *val){
+      std::string h(val);
+      setVal(what,h);
     }
+
 
 
     //----------------------------------------------------------
@@ -164,15 +159,16 @@ namespace RDKit{
         - If the dictionary does not contain the key \c what,
           a KeyErrorException will be thrown.
     */
-    void clearVal(const char *what) {
+    void clearVal(const std::string &what) {
       if(! this->hasVal(what) ) throw KeyErrorException(what);
       _data.erase(what);
     };
-    //! \overload
-    void clearVal(const std::string &what) {
-      clearVal(what.c_str());
-    };
 
+    //! \overload
+    void clearVal(const char *what) {
+      std::string key=what;
+      clearVal(key);
+    };
 
     //----------------------------------------------------------
     //! \brief Clears all keys (and values) from the dictionary.
--- Code/RDGeneral/testDict.cpp
+++ Code/RDGeneral/testDict.cpp
@@ -150,7 +150,7 @@ void testConstReturns(){
 #else
   {
     //int nreps=100000000;
-    int nreps=5000000;
+    int nreps=100000;
     Dict d;
     std::string v="foo";
     boost::any anyv(v);
@@ -202,8 +202,9 @@ void testConstReturns(){
     ls=0;
     BOOST_LOG(rdErrorLog) << "ref with hasVal" << std::endl;    
     start = std::clock();
+    std::string k="foo";
     for(int i=0;i<nreps;++i){
-      if(d.hasVal("foo")){
+      if(d.hasVal(k)){
         const std::string &nv=d.fromany<const std::string &>(anyv);
         ls+= nv.size();
       }
