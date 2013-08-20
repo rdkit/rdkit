7b805988f1426a361b4c8a0e8f0e507dd63ca9c8
Greg Landrum <greg.landrum@gmail.com>
2013-08-20 17:07:37 +0200

    rename AtomPeptideMonomerInfo->AtomPDBMonomerInfo; remove element and charge fields

--- Code/GraphMol/MonomerInfo.h
+++ Code/GraphMol/MonomerInfo.h
@@ -25,7 +25,7 @@ namespace RDKit{
   public:
     typedef enum {
       UNKNOWN=0,
-      PEPTIDERESIDUE,
+      PDBRESIDUE,
       OTHER
     } AtomMonomerType;
 
@@ -49,42 +49,33 @@ namespace RDKit{
   };
 
   //! Captures atom-level information about peptide residues
-  class AtomPeptideResidueInfo : public AtomMonomerInfo {
+  class AtomPDBResidueInfo : public AtomMonomerInfo {
   public:
-    AtomPeptideResidueInfo() : AtomMonomerInfo(PEPTIDERESIDUE) {};
-    AtomPeptideResidueInfo(const AtomPeptideResidueInfo &other) : AtomMonomerInfo(other),
-                                                                  d_serialNumber(other.d_serialNumber),
-                                                                  d_altLoc(other.d_altLoc),
-                                                                  d_residueName(other.d_residueName),
-                                                                  d_chainId(other.d_chainId),
-                                                                  d_insertionCode(other.d_insertionCode),
-                                                                  d_occupancy(other.d_occupancy),
-                                                                  d_tempFactor(other.d_tempFactor),
-                                                                  d_element(other.d_element),
-                                                                  d_charge(other.d_charge) {};
+    AtomPDBResidueInfo() : AtomMonomerInfo(PDBRESIDUE) {};
+    AtomPDBResidueInfo(const AtomPDBResidueInfo &other) : AtomMonomerInfo(other),
+                                                          d_serialNumber(other.d_serialNumber),
+                                                          d_altLoc(other.d_altLoc),
+                                                          d_residueName(other.d_residueName),
+                                                          d_chainId(other.d_chainId),
+                                                          d_insertionCode(other.d_insertionCode),
+                                                          d_occupancy(other.d_occupancy),
+                                                          d_tempFactor(other.d_tempFactor) {};
 
-    AtomPeptideResidueInfo(std::string atomName,
-                           unsigned int serialNumber=0,
-                           std::string altLoc="",
-                           std::string residueName="",
-                           std::string chainId="",
-                           std::string insertionCode="",
-                           double occupancy=1.0,
-                           double tempFactor=0.0,
-                           std::string element="",
-                           std::string charge="") : AtomMonomerInfo(PEPTIDERESIDUE,atomName),
-                                                    d_serialNumber(serialNumber),
-                                                    d_altLoc(altLoc),
-                                                    d_residueName(residueName),
-                                                    d_chainId(chainId),
-                                                    d_insertionCode(insertionCode),
-                                                    d_occupancy(occupancy),
-                                                    d_tempFactor(tempFactor),
-                                                    d_element(element),
-                                                    d_charge(charge) {};
-    
-                           
-    
+    AtomPDBResidueInfo(std::string atomName,
+                       unsigned int serialNumber=0,
+                       std::string altLoc="",
+                       std::string residueName="",
+                       std::string chainId="",
+                       std::string insertionCode="",
+                       double occupancy=1.0,
+                       double tempFactor=0.0) :  AtomMonomerInfo(PDBRESIDUE,atomName),
+                                                 d_serialNumber(serialNumber),
+                                                 d_altLoc(altLoc),
+                                                 d_residueName(residueName),
+                                                 d_chainId(chainId),
+                                                 d_insertionCode(insertionCode),
+                                                 d_occupancy(occupancy),
+                                                 d_tempFactor(tempFactor) {};
     
     unsigned int getSerialNumber() const { return d_serialNumber; };
     void setSerialNumber(unsigned int val) { d_serialNumber=val; };
@@ -100,13 +91,9 @@ namespace RDKit{
     void setOccupancy(double val) { d_occupancy=val; };
     double getTempFactor() const { return d_tempFactor; };
     void setTempFactor(double val) { d_tempFactor=val; };
-    const std::string &getElement() const { return d_element; };
-    void setElement(const std::string &val) { d_element=val; };
-    const std::string &getCharge() const { return d_charge; };
-    void setCharge(const std::string &val) { d_charge=val; };
 
     AtomMonomerInfo *copy() const {
-      return static_cast<AtomMonomerInfo *>(new AtomPeptideResidueInfo(*this));
+      return static_cast<AtomMonomerInfo *>(new AtomPDBResidueInfo(*this));
     }
     
   private:
@@ -119,12 +106,10 @@ namespace RDKit{
     std::string d_insertionCode;
     double d_occupancy;
     double d_tempFactor;
-    std::string d_element;
-    std::string d_charge;
   };
     
 };
-//! allows AtomPeptideResidueInfo objects to be dumped to streams
-std::ostream & operator<<(std::ostream& target, const RDKit::AtomPeptideResidueInfo &apri);
+//! allows AtomPDBResidueInfo objects to be dumped to streams
+std::ostream & operator<<(std::ostream& target, const RDKit::AtomPDBResidueInfo &apri);
 
 #endif
