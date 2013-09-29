c3fb4a2f0ce2197bafedb51228747b1b37455cd1
Greg Landrum <greg.landrum@gmail.com>
2013-09-29 15:16:46 +0200

    add these temporarily

---
+++ Code/GraphMol/FileParsers/pdb_test/pdb2smi.cpp
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <GraphMol/GraphMol.h>
+#include <GraphMol/RDKitBase.h>
+#include <GraphMol/FileParsers/MolSupplier.h>
+#include <GraphMol/FileParsers/MolWriters.h>
+#include <GraphMol/FileParsers/PDBSupplier.h>
+
+
+int main(int argc, char *argv[])
+{
+  const char *inpname;
+  const char *outname;
+
+  if (argc == 2) {
+    inpname = argv[1];
+    outname = "-";
+  } else if (argc == 3) {
+    inpname = argv[1];
+    outname = argv[2];
+  } else {
+    fputs("usage:  pdb2smi <infile.pdb> [<outfile.smi>]\n",stderr);
+    exit(1);
+  }
+
+  // fileName, sanitize=true, removeHs=true, strictParsing=true
+  RDKit::PDBMolSupplier supplier(inpname,true,true,0);
+  // fileName, delimiter=" ", nameHeader="Name"
+  // includeHeader=true, isomericSmiles=false, kekuleSmiles=false
+  RDKit::SmilesWriter writer(outname," ","Name",false,true,false);
+  while (!supplier.atEnd()) {
+    RDKit::ROMol *mol = supplier.next();
+    if (mol) {
+      writer.write(*mol);
+      delete mol;
+    } else std::cout << "#" << std::endl;
+  }
+  writer.close();
+  return 0;
+}
+
---
+++ Code/GraphMol/FileParsers/pdb_test/smi2pdb.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <GraphMol/GraphMol.h>
+#include <GraphMol/RDKitBase.h>
+#include <GraphMol/FileParsers/MolSupplier.h>
+#include <GraphMol/FileParsers/MolWriters.h>
+#include <GraphMol/FileParsers/PDBWriter.h>
+
+
+int main(int argc, char *argv[])
+{
+  const char *inpname;
+  const char *outname;
+
+  if (argc == 2) {
+    inpname = argv[1];
+    outname = "-";
+  } else if (argc == 3) {
+    inpname = argv[1];
+    outname = argv[2];
+  } else {
+    fputs("usage:  smi2pdb <infile.smi> [<outfile.pdb>]\n",stderr);
+    exit(1);
+  }
+
+  // filenName, delimiter=" \t", smilesColumn=0, nameColumn=1
+  // titleLine=true, sanitize=true
+  RDKit::SmilesMolSupplier supplier(inpname," \t",0,1,false,true);
+  RDKit::PDBWriter writer(outname);
+  while (!supplier.atEnd()) {
+    RDKit::ROMol *mol = supplier.next();
+    if (mol) {
+      writer.write(*mol);
+      delete mol;
+    }
+  }
+  writer.close();
+  return 0;
+}
+
