3b6568c18603dd3a29e9ae79c064edc3339506ca
Greg Landrum <greg.landrum@gmail.com>
2013-10-14 06:05:12 +0200

    basic unit testing for the first part

---
+++ Contrib/fraggle/UnitTestFraggle.py
@@ -0,0 +1,44 @@
+# $Id$
+#
+#   @@ All Rights Reserved @@
+#  This file is part of the RDKit.
+#  The contents are covered by the terms of the BSD license
+#  which is included in the file license.txt, found at the root
+#  of the RDKit source tree.
+#
+from rdkit import RDConfig
+import unittest,cPickle,os
+from rdkit import Chem
+import fraggle
+
+class TestCase(unittest.TestCase):
+  def testFragmentation(self):
+    """ 
+    
+    """
+    mol = Chem.MolFromSmiles('COc1cc(CN2CCC(CC2)NC(=O)c2cncc(C)c2)c(OC)c2ccccc12')
+    frags = fraggle.generate_fraggle_fragmentation(mol)
+    self.failUnlessEqual(len(frags),16)
+
+    expected=('[*]C(=O)NC1CCN(Cc2cc(OC)c3ccccc3c2OC)CC1',
+ '[*]C(=O)c1cncc(C)c1.[*]C1CCN(Cc2cc(OC)c3ccccc3c2OC)CC1',
+ '[*]C(=O)c1cncc(C)c1.[*]c1cc(OC)c2ccccc2c1OC',
+ '[*]C1CCN(Cc2cc(OC)c3ccccc3c2OC)CC1',
+ '[*]Cc1cc(OC)c2ccccc2c1OC.[*]C(=O)c1cncc(C)c1',
+ '[*]Cc1cc(OC)c2ccccc2c1OC.[*]NC(=O)c1cncc(C)c1',
+ '[*]Cc1cc(OC)c2ccccc2c1OC.[*]c1cncc(C)c1',
+ '[*]NC(=O)c1cncc(C)c1.[*]c1cc(OC)c2ccccc2c1OC',
+ '[*]NC1CCN(Cc2cc(OC)c3ccccc3c2OC)CC1',
+ '[*]NC1CCN(Cc2cc(OC)c3ccccc3c2OC)CC1.[*]c1cncc(C)c1',
+ '[*]c1c(CN2CCC(NC(=O)c3cncc(C)c3)CC2)cc(OC)c2ccccc12',
+ '[*]c1c([*])c(OC)c(CN2CCC(NC(=O)c3cncc(C)c3)CC2)cc1OC',
+ '[*]c1cc(CN2CCC(NC(=O)c3cncc(C)c3)CC2)c(OC)c2ccccc12',
+ '[*]c1cc(OC)c2ccccc2c1OC.[*]N1CCC(NC(=O)c2cncc(C)c2)CC1',
+ '[*]c1cncc(C)c1.[*]C1CCN(Cc2cc(OC)c3ccccc3c2OC)CC1',
+ '[*]c1cncc(C)c1.[*]c1cc(OC)c2ccccc2c1OC')
+    for smi in expected:
+        self.failUnless(smi in frags)
+
+if __name__ == '__main__':
+  unittest.main()
+
