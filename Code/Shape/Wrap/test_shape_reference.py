5fb182e1193a24177ae5d869110f04b17e03fe89
Jan Domanski <jandom@gmail.com>
2013-10-22 12:38:39 +0100

    Shape-it testing
    - added reference tests, based on the CLI version of the code,
    - added sanity checks with rod-like, dot-like molecules

---
+++ Code/Shape/Wrap/test_shape_reference.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import rdkit
+import rdkit.Chem.rdShape
+#print rdkit.Chem.rdShape.Align.__doc__
+from rdkit import Chem
+from rdkit import RDConfig
+import unittest, os
+
+class TestCase(unittest.TestCase) :
+    def test_shape_reference(self):
+        #mol= Chem.MolFromMolFile(os.path.join(RDConfig.RDBaseDir,'Code/GraphMol/Depictor','test_data/7UPJ_xtal.mol'))
+        f1 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','reference.sdf')
+        f2 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','database.sdf')
+        d1 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','output_rdkit_1.tab')
+        d2 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','output_rdkit_2.tab')
+
+
+        mol1 = Chem.SDMolSupplier(f1).next()
+        supp = Chem.SDMolSupplier(f1)
+        mol2 = supp.next()
+        score = rdkit.Chem.rdShape.Align(mol1, mol2, maxIter=10)
+        print score
+        
+        supp = Chem.SDMolSupplier(f2)
+        tanimoto = [rdkit.Chem.rdShape.Align(mol1, m, maxIter=0) for m in supp]
+        
+        
+        import pandas as pd
+        df1 = pd.read_csv(d1, sep="\t")
+        df2 = pd.read_csv(d2, sep="\t")
+        df = pd.DataFrame(tanimoto, columns=["Shape-it::Tanimoto",])
+        
+        import numpy as np
+        np.testing.assert_array_almost_equal(df["Shape-it::Tanimoto"], df1["Shape-it::Tanimoto"], decimal=3)
+        np.testing.assert_array_almost_equal(df["Shape-it::Tanimoto"], df2["Shape-it::Tanimoto"], decimal=3)
+
---
+++ Code/Shape/Wrap/test_shape_sanity.py
@@ -0,0 +1,38 @@
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import rdkit.Chem.rdShape
+import numpy.testing
+import unittest, os
+from rdkit import RDConfig
+
+class TestCase(unittest.TestCase) :
+    def test_shape_sanity(self):
+        rod = "C#CC#CC#CC#C"
+        small_rod = "C#CC#C"
+        dot = "C"
+        
+        rod, small_rod, dot = [Chem.MolFromSmiles(smi) for smi in [rod, small_rod, dot]]
+        
+        print [AllChem.EmbedMolecule(mol) for mol in [rod, small_rod, dot ]]
+        
+        print [AllChem.UFFOptimizeMolecule(mol) for mol in [rod, small_rod, dot ]]
+        
+        print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'rod.mol'),'w'),Chem.MolToMolBlock(rod)
+        print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'small_rod.mol'),'w'),Chem.MolToMolBlock(small_rod)
+        print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'dot.mol'),'w'),Chem.MolToMolBlock(dot)
+        
+        score = rdkit.Chem.rdShape.Align(rod, small_rod, maxIter=10)
+        print "rod-small_rod", score
+        numpy.testing.assert_approx_equal(0.50, score, significant=2)
+        
+        
+        score1 = rdkit.Chem.rdShape.Align(rod, dot, maxIter=10)
+        print "rod-dot", score1
+        score2 = rdkit.Chem.rdShape.Align(small_rod, dot, maxIter=10)
+        print "small_rod-dot", score2
+        numpy.testing.assert_approx_equal(score1*2, score2, significant=1)
+        
+        score = rdkit.Chem.rdShape.Align(dot, dot, maxIter=10)
+        print "dot-dot", score
+        numpy.testing.assert_approx_equal(1., score, significant=2)
