646879e9c2c18dce63dafecd3f363ce62757e3a1
Maltos <maltos@gmx.de>
2013-11-11 00:11:46 +0100

    removed stupid comments

--- rdkit/Chem/ChemUtils/CSVToSDF.py
+++ rdkit/Chem/ChemUtils/CSVToSDF.py
@@ -17,50 +17,34 @@ def CSVToSDF(csvFile,outFile):
     sdw.close()
 
 def _CSVToMOL(csvEntry,colNames,name=''):
+    i = 1
+    if name == '': name=str(i)
     if type(csvEntry)==str:
         csvValues = csvEntry.split('\t')
         cpd = Chem.MolFromSmiles(csvValues[0])
         AllChem.Compute2DCoords(cpd)
-        i = 1
-        if name == '': name=str(i)
         cpd.SetProp("_Name",str(name))      
         for prop in csvValues[1:]:
             cpd.SetProp(colNames[i],prop)
             i += 1
             return cpd
     else:
-        i = 1
         for key in colNames:
             if key=='Smiles':
-#                print csvEntry[key]
                 cpd = Chem.MolFromSmiles(str(csvEntry[key]))
                 AllChem.Compute2DCoords(cpd)
-                cpd.SetProp("_Name",str(i))
+                cpd.SetProp("_Name",str(name))
             else:
-#                print key
- #               print csvEntry[key]
                 cpd.SetProp(str(key),str(csvEntry[key]))
         return cpd
 
-#def __CSVToMOL(csvEntry,colNames,name=''):
- #   import sys
-  #  for key in colNames:
-#	print csvEntry[key]
- #   sys.exit(1)
-
 def CSVToMOL(csvData,colNames,name=''):
-#    import sys
-#    print type(csvData)
     if type(csvData) != str:
         result = []
-#	print csvData
+        t = 0
         for csvEntry in csvData:
-#	    if type(csvEntry)==dict:
-#		__CSVToMOL(csvEntry,colNames,name='')
-#	    print type(csvEntry)
-#	    print csvEntry
-#	    sys.exit(1)
-            result.append(_CSVToMOL(csvEntry,colNames,len(result)))
+            result.append(_CSVToMOL(csvEntry,colNames,t))
+            t += 1
         return result
     else:
         return _CSVToMOL(csvData,colNames,name)
