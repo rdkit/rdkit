976b3de719939a2d15f4ac6cb5a02a665fce4c57
Greg Landrum <greg.landrum@gmail.com>
2013-11-13 03:14:56 +0100

    rationalize the testing code a bit

--- Code/Shape/Wrap/test_shape_reference.py
+++ Code/Shape/Wrap/test_shape_reference.py
@@ -13,27 +13,22 @@ class TestCase(unittest.TestCase) :
         f1 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','reference.sdf')
         f2 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','database.sdf')
         d1 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','output_rdkit_1.tab')
-        d2 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','output_rdkit_2.tab')
+        #d2 = os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data','output_rdkit_2.tab')
 
 
         mol1 = Chem.SDMolSupplier(f1).next()
         supp = Chem.SDMolSupplier(f1)
         mol2 = supp.next()
         score = rdkit.Chem.rdShape.Align(mol1, mol2, maxIter=10)
-        print score
+        self.failUnlessAlmostEqual(score,1.0,1)
         
         supp = Chem.SDMolSupplier(f2)
         tanimoto = [rdkit.Chem.rdShape.Align(mol1, m, maxIter=0) for m in supp]
         
-        
-        import pandas as pd
-        df1 = pd.read_csv(d1, sep="\t")
-        df2 = pd.read_csv(d2, sep="\t")
-        df = pd.DataFrame(tanimoto, columns=["Shape-it::Tanimoto",])
-        
-        import numpy as np
-        np.testing.assert_array_almost_equal(df["Shape-it::Tanimoto"], df1["Shape-it::Tanimoto"], decimal=3)
-        np.testing.assert_array_almost_equal(df["Shape-it::Tanimoto"], df2["Shape-it::Tanimoto"], decimal=3)
+        df1 = [x.strip().split('\t') for x in file(d1).readlines()]
+        taniCol = df1[0].index('Shape-it::Tanimoto')
+        for i,tani in enumerate(tanimoto):
+            self.failUnlessAlmostEqual(float(df1[i+1][taniCol]),tani,3)
 
 if __name__ == '__main__':
   unittest.main()
--- Code/Shape/Wrap/test_shape_sanity.py
+++ Code/Shape/Wrap/test_shape_sanity.py
@@ -14,28 +14,28 @@ class TestCase(unittest.TestCase) :
         
         rod, small_rod, dot = [Chem.MolFromSmiles(smi) for smi in [rod, small_rod, dot]]
         
-        print [AllChem.EmbedMolecule(mol) for mol in [rod, small_rod, dot ]]
+        [AllChem.EmbedMolecule(mol) for mol in [rod, small_rod, dot ]]
+        [AllChem.UFFOptimizeMolecule(mol) for mol in [rod, small_rod, dot ]]
         
-        print [AllChem.UFFOptimizeMolecule(mol) for mol in [rod, small_rod, dot ]]
-        
-        print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'rod.mol'),'w'),Chem.MolToMolBlock(rod)
-        print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'small_rod.mol'),'w'),Chem.MolToMolBlock(small_rod)
-        print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'dot.mol'),'w'),Chem.MolToMolBlock(dot)
+        #print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'rod.mol'),'w'),Chem.MolToMolBlock(rod)
+        #print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'small_rod.mol'),'w'),Chem.MolToMolBlock(small_rod)
+        #print >>file(os.path.join(RDConfig.RDBaseDir,'Code','Shape','test_data', 'dot.mol'),'w'),Chem.MolToMolBlock(dot)
         
         score = rdkit.Chem.rdShape.Align(rod, small_rod, maxIter=10)
         print "rod-small_rod", score
-        numpy.testing.assert_approx_equal(0.50, score, significant=2)
+        self.failUnlessAlmostEqual(0.510,score,2)
         
         
         score1 = rdkit.Chem.rdShape.Align(rod, dot, maxIter=10)
         print "rod-dot", score1
+        self.failUnlessAlmostEqual(0.185,score1,2)
         score2 = rdkit.Chem.rdShape.Align(small_rod, dot, maxIter=10)
         print "small_rod-dot", score2
-        numpy.testing.assert_approx_equal(score1*2, score2, significant=1)
+        self.failUnlessAlmostEqual(score1*2,score2,1)
         
         score = rdkit.Chem.rdShape.Align(dot, dot, maxIter=10)
         print "dot-dot", score
-        numpy.testing.assert_approx_equal(1., score, significant=2)
+        self.failUnlessAlmostEqual(1.0,score,2)
 
 if __name__ == '__main__':
   unittest.main()
