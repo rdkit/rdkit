52069683b9c451388c7f3c4c61e7a98ab792dcb1
Sereina Riniker <sereina.riniker@gmail.com>
2014-04-08 10:38:52 +0200

    function UnSetBitsFromList added

--- Code/DataStructs/Wrap/wrap_ExplicitBV.cpp
+++ Code/DataStructs/Wrap/wrap_ExplicitBV.cpp
@@ -51,11 +51,13 @@ struct EBV_wrapper {
                                   python::init<unsigned int>())
     .def(python::init<std::string>())
     .def("SetBit",(bool (EBV::*)(unsigned int))&EBV::setBit,
-         "Turns on a particular bit on.  Returns the original state of the bit.\n")
+         "Turns on a particular bit.  Returns the original state of the bit.\n")
     .def("SetBitsFromList",(void (*)(EBV *,python::object))SetBitsFromList,
          "Turns on a set of bits.  The argument should be a tuple or list of bit ids.\n")
     .def("UnSetBit",(bool (EBV::*)(unsigned int))&EBV::unsetBit,
-         "Turns on a particular bit off.  Returns the original state of the bit.\n")
+         "Turns off a particular bit.  Returns the original state of the bit.\n")
+    .def("UnSetBitsFromList",(void (*)(EBV *,python::object))UnSetBitsFromList,
+         "Turns off a set of bits.  The argument should be a tuple or list of bit ids.\n")
     .def("GetBit",(bool (EBV::*)(unsigned int) const)&EBV::getBit,
          "Returns the value of a bit.\n")
     .def("GetNumBits",&EBV::getNumBits,
