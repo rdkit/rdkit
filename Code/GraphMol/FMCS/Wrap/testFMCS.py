c6deb3f49205eed0bd0ffcb710a1ad7e929ff24f
Greg Landrum <greg.landrum@gmail.com>
2014-06-09 22:58:21 +0200

    add some real python tests

--- Code/GraphMol/FMCS/Wrap/testFMCS.py
+++ Code/GraphMol/FMCS/Wrap/testFMCS.py
@@ -1,116 +1,86 @@
 from rdkit import RDConfig
-import os,sys,math
+import os,sys
 import unittest
-from rdkit import DataStructs
 from rdkit import Chem
-from rdkit.Geometry import rdGeometry as geom
-from rdkit.Chem import rdMolTransforms as rdmt
+from rdkit.Chem import rdFMCS
 
-def feq(v1, v2, tol=1.0e-4):
-    return abs(v1-v2) < tol
-
-def ptEq(pt, tpt, tol=0.0001):
-    pt -= tpt
-    return feq(pt.Length(), 0.0)
 
 class TestCase(unittest.TestCase):
     def setUp(self) :
         pass
+    def test1(self):
+        smis=(    "Cc1nc(CN(C(C)c2ncccc2)CCCCN)ccc1 CHEMBL1682991", #-- QUERY
+        "Cc1ccc(CN(C(C)c2ccccn2)CCCCN)nc1 CHEMBL1682990",
+        "Cc1cccnc1CN(C(C)c1ccccn1)CCCCN CHEMBL1682998",
+        "CC(N(CCCCN)Cc1c(N)cccn1)c1ccccn1 CHEMBL1682987",
+        "Cc1cc(C)c(CN(C(C)c2ccccn2)CCCCN)nc1 CHEMBL1682992",
+        "Cc1cc(C(C)N(CCCCN)Cc2c(C)cccn2)ncc1 CHEMBL1682993",
+        "Cc1nc(C(C)N(CCCCN)Cc2nc3c([nH]2)cccc3)ccc1 CHEMBL1682878",
+        "CC(c1ncccc1)N(CCCCN)Cc1nc2c([nH]1)cccc2 CHEMBL1682867",
+        "CC(N(CCCCN)Cc1c(C(C)(C)C)cccn1)c1ccccn1 CHEMBL1682989",
+        "CC(N(CCCCN)Cc1c(C(F)(F)F)cccn1)c1ccccn1 CHEMBL1682988",
+            )
+        ms = [Chem.MolFromSmiles(x.split()[0]) for x in smis]
+        qm = ms[0]
+        ms = ms[1:]
+        mcs = rdFMCS.FindMCS(ms)
+        self.failUnlessEqual(mcs.numBonds,21)
+        self.failUnlessEqual(mcs.numAtoms,21)
+        self.failUnlessEqual(mcs.smartsString,'[#6](:[#6]:[#6]):[#6]:[#7]:[#6]-[#6]-[#7](-[#6](-[#6])-[#6]1:[#6]:[#6]:[#6]:[#6]:[#7]:1)-[#6]-[#6]-[#6]-[#6]-[#7]')
+        qm = Chem.MolFromSmarts(mcs.smartsString)
+        self.failUnless(qm is not None)
+        for m in ms:
+            self.failUnless(m.HasSubstructMatch(qm))
+        
+        mcs = rdFMCS.FindMCS(ms,threshold=0.8)
+        self.failUnlessEqual(mcs.numBonds,21)
+        self.failUnlessEqual(mcs.numAtoms,21)
+        self.failUnlessEqual(mcs.smartsString,'[#6](:[#6]:[#6]):[#6]:[#7]:[#6]-[#6]-[#7](-[#6](-[#6])-[#6]1:[#6]:[#6]:[#6]:[#6]:[#7]:1)-[#6]-[#6]-[#6]-[#6]-[#7]')
+        qm = Chem.MolFromSmarts(mcs.smartsString)
+        self.failUnless(qm is not None)
+        for m in ms:
+            self.failUnless(m.HasSubstructMatch(qm))
 
-    def test1Canonicalization(self) :
-        mol = Chem.MolFromSmiles("C")
-        conf = Chem.Conformer(1)
-        conf.SetAtomPosition(0, (4.0, 5.0, 6.0))
-        mol.AddConformer(conf,1)
+    def test2(self):
+        smis=(
+            "CHEMBL122452 CN(CCCN(C)CCc1ccccc1)CCOC(c1ccccc1)c1ccccc1",
+            "CHEMBL123252 CN(CCCc1ccccc1)CCCN(C)CCOC(c1ccccc1)c1ccccc1",
+            "CHEMBL121611 Fc1ccc(C(OCCNCCCNCCc2ccccc2)c2ccc(F)cc2)cc1",
+            "CHEMBL121050 O=C(Cc1ccccc1)NCCCCNCCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL333667 O=C(Cc1ccccc1)NCCNCCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL121486 O=C(Cc1ccc(Br)cc1)NC=CNCCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL123830 O=C(Cc1ccc(F)cc1)NCCNCCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL420900 O=C(Cc1ccccc1)NCCCNCCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL121460 CN(CCOC(c1ccc(F)cc1)c1ccc(F)cc1)CCN(C)CCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL120901 COC(=O)C1C2CCC(CC1C(=O)Oc1ccccc1)N2C",
+            "CHEMBL122859 O=C1CN(CCc2ccccc2)CCN1CCOC(c1ccc(F)cc1)c1ccc(F)cc1",
+            "CHEMBL121027 CN(CCOC(c1ccccc1)c1ccccc1)CCN(C)CCc1ccc(F)cc1",
+            )
+        
+        ms = [Chem.MolFromSmiles(x.split()[1]) for x in smis]
+        qm = ms[0]
+        ms = ms[1:]
+        mcs = rdFMCS.FindMCS(ms)
+        self.failUnlessEqual(mcs.numBonds,9)
+        self.failUnlessEqual(mcs.numAtoms,10)
+        self.failUnlessEqual(mcs.smartsString,'[#6]-,:[#6]-,:[#6](-,:[#6]-,:[#6]-,:[#6])-[#6]-[#8]-[#6]:,-[#6]')
+        qm = Chem.MolFromSmarts(mcs.smartsString)
+        self.failUnless(qm is not None)
+        for m in ms:
+            self.failUnless(m.HasSubstructMatch(qm))
 
-        conf = mol.GetConformer()
-        pt  = rdmt.ComputeCentroid(conf)
-        self.failUnless(ptEq(pt, geom.Point3D(4.0, 5.0, 6.0)))
-
-        fileN = os.path.join(RDConfig.RDBaseDir,'Code','GraphMol','MolTransforms',
-                                            'test_data','1oir.mol')
-        m = Chem.MolFromMolFile(fileN)
-        cpt = rdmt.ComputeCentroid(m.GetConformer())
-        trans = rdmt.ComputeCanonicalTransform(m.GetConformer(), cpt)
-        trans2 = rdmt.ComputeCanonicalTransform(m.GetConformer())
-        for i in range(4):
-            for j in range(4) :
-                self.failUnless(feq(trans[i,j], trans2[i, j]))
-        rdmt.TransformConformer(m.GetConformer(), trans2)
-        m2 = Chem.MolFromMolFile(fileN)
-        rdmt.CanonicalizeConformer(m2.GetConformer())
-        nats = m.GetNumAtoms()
-        cnf1 = m.GetConformer()
-        cnf2 = m2.GetConformer()
-        for i in range(nats):
-            p1 = list(cnf1.GetAtomPosition(i))
-            p2 = list(cnf2.GetAtomPosition(i))
-            self.failUnless(feq(p1[0],p2[0]))
-            self.failUnless(feq(p1[1],p2[1]))
-            self.failUnless(feq(p1[2],p2[2]))
-
-        m3 = Chem.MolFromMolFile(fileN)
-        rdmt.CanonicalizeMol(m3)
-        cnf1 = m.GetConformer()
-        cnf2 = m3.GetConformer()
-        for i in range(nats):
-            p1 = list(cnf1.GetAtomPosition(i))
-            p2 = list(cnf2.GetAtomPosition(i))
-            self.failUnless(feq(p1[0],p2[0]))
-            self.failUnless(feq(p1[1],p2[1]))
-            self.failUnless(feq(p1[2],p2[2]))
-
-    def testGetSetBondLength(self):
-        file = os.path.join(RDConfig.RDBaseDir, 'Code', 'GraphMol', 'MolTransforms',
-                             'test_data', '3-cyclohexylpyridine.mol')
-
-        m = Chem.MolFromMolFile(file, True, False)
-        conf = m.GetConformer()
-        dist = rdmt.GetBondLength(conf, 0, 19)
-        self.failUnlessAlmostEqual(dist, 1.36, 2)
-        rdmt.SetBondLength(conf, 0, 19, 2.5)
-        dist = rdmt.GetBondLength(conf, 0, 19)
-        self.failUnlessAlmostEqual(dist, 2.5, 1)
-        rdmt.SetBondLength(conf, 19, 0, 3.0)
-        dist = rdmt.GetBondLength(conf, 0, 19)
-        self.failUnlessAlmostEqual(dist, 3.0, 1)
-
-    def testGetSetAngle(self):
-        file = os.path.join(RDConfig.RDBaseDir, 'Code', 'GraphMol', 'MolTransforms',
-                             'test_data', '3-cyclohexylpyridine.mol')
-
-        m = Chem.MolFromMolFile(file, True, False)
-        conf = m.GetConformer()
-        angle = rdmt.GetAngleDeg(conf, 0, 19, 21)
-        self.failUnlessAlmostEqual(angle, 109.7, 1)
-        rdmt.SetAngleDeg(conf, 0, 19, 21, 125.0)
-        angle = rdmt.GetAngleDeg(conf, 0, 19, 21);
-        self.failUnlessAlmostEqual(angle, 125.0, 1)
-        rdmt.SetAngleRad(conf, 21, 19, 0, math.pi / 2.)
-        angle = rdmt.GetAngleRad(conf, 0, 19, 21)
-        self.failUnlessAlmostEqual(angle, math.pi / 2., 1)
-        angle = rdmt.GetAngleDeg(conf, 0, 19, 21)
-        self.failUnlessAlmostEqual(angle, 90.0, 1)
-
-    def testGetSetDihedral(self):
-        file = os.path.join(RDConfig.RDBaseDir, 'Code', 'GraphMol', 'MolTransforms',
-                             'test_data', '3-cyclohexylpyridine.mol')
-
-        m = Chem.MolFromMolFile(file, True, False)
-        conf = m.GetConformer()
-        dihedral = rdmt.GetDihedralDeg(conf, 0, 19, 21, 24)
-        self.failUnlessAlmostEqual(dihedral, 176.05, 2)
-        rdmt.SetDihedralDeg(conf, 8, 0, 19, 21, 65.0)
-        dihedral = rdmt.GetDihedralDeg(conf, 8, 0, 19, 21)
-        self.failUnlessAlmostEqual(dihedral, 65.0, 1)
-        rdmt.SetDihedralDeg(conf, 8, 0, 19, 21, -130.0)
-        dihedral = rdmt.GetDihedralDeg(conf, 8, 0, 19, 21)
-        self.failUnlessAlmostEqual(dihedral, -130.0, 1)
-        rdmt.SetDihedralRad(conf, 21, 19, 0, 8, -2. / 3. * math.pi)
-        dihedral = rdmt.GetDihedralRad(conf, 8, 0, 19, 21)
-        self.failUnlessAlmostEqual(dihedral, -2. / 3. * math.pi, 1)
-        dihedral = rdmt.GetDihedralDeg(conf, 8, 0, 19, 21)
-        self.failUnlessAlmostEqual(dihedral, -120.0, 1)
+        mcs = rdFMCS.FindMCS(ms,threshold=0.8)
+        self.failUnlessEqual(mcs.numBonds,20)
+        self.failUnlessEqual(mcs.numAtoms,19)
+        self.failUnlessEqual(mcs.smartsString,'[#6]1:[#6]:[#6]:[#6](:[#6]:[#6]:1)-[#6](-[#8]-[#6]-[#6]-[#7]-[#6]-[#6])-[#6]2:[#6]:[#6]:[#6]:[#6]:[#6]:2')
+        qm = Chem.MolFromSmarts(mcs.smartsString)
+        self.failUnless(qm is not None)
+        nHits=0
+        for m in ms:
+            if m.HasSubstructMatch(qm):
+                nHits+=1
+        self.failUnless(nHits>=int(0.8*len(smis)))
+        
             
 if __name__=="__main__":
     unittest.main()
