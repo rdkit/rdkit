55ad544ddecbea383a8047dec1e95e81d1c1187e
Greg Landrum <greg.landrum@gmail.com>
2014-08-10 08:00:37 +0200

    first pass at FMCS in the java wrappers, including a basic test. this is nowhere near done

---
+++ Code/JavaWrappers/gmwrapper/src-test/org/RDKit/FMCSTests.java
@@ -0,0 +1,63 @@
+/* 
+ *
+ *  Copyright (c) 2014, Novartis Institutes for BioMedical Research Inc.
+ *  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: 
+ *
+ *     * Redistributions of source code must retain the above copyright 
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following 
+ *       disclaimer in the documentation and/or other materials provided 
+ *       with the distribution.
+ *     * Neither the name of Novartis Institutes for BioMedical Research Inc. 
+ *       nor the names of its contributors may be used to endorse or promote 
+ *       products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.RDKit;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class FMCSTests extends GraphMolTest {
+	private ROMol m;
+
+	@Before public void setUp() {
+	}
+
+	@Test 
+	public void test1Basics() {
+		ROMol_Vect mols = new ROMol_Vect();
+		mols.add(RWMol.MolFromSmiles("c1ccccc1OC"));
+		mols.add(RWMol.MolFromSmiles("c1ccccc1C"));
+		mols.add(RWMol.MolFromSmiles("c1c(C)cccc1OC"));
+                
+                MCSResult mcs=RDKFuncs.findMCS(mols);
+                assertEquals(6,mcs.getNumAtoms());
+                assertEquals(6,mcs.getNumBonds());
+                assertEquals("[#6]1:[#6]:[#6]:[#6]:[#6]:[#6]:1",mcs.getSmartsString());
+                assertEquals(false,mcs.getCanceled());
+
+	}
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("org.RDKit.FMCSTests");
+	}
+
+}
