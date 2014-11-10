5d575a4b876e8a0fc77e79ba5dd9ad325b890b69
Greg Landrum <greg.landrum@gmail.com>
2014-11-10 04:54:32 +0100

    Fixes #378

--- Code/GraphMol/SmilesParse/smarts.yy
+++ Code/GraphMol/SmilesParse/smarts.yy
@@ -160,7 +160,12 @@ mol: atomd {
   }
   mp->addBond($2);
   delete $2;
+}
 
+| mol SEPARATOR_TOKEN atomd {
+  RWMol *mp = (*molList)[$$];
+  $3->setProp("_SmilesStart",1,true);
+  mp->addAtom($3,true,true);
 }
 
 | mol ring_number {
@@ -222,16 +227,6 @@ mol: atomd {
   delete m2_p;
   int sz = molList->size();
   if ( sz==$2+1) {
-    molList->resize( sz-1 );
-  }
-}
-
-| mol SEPARATOR_TOKEN mol {
-  RWMol *m1_p = (*molList)[$1],*m2_p=(*molList)[$3];
-  SmilesParseOps::AddFragToMol(m1_p,m2_p,Bond::IONIC,Bond::NONE,true);
-  delete m2_p;
-  int sz = molList->size();
-  if ( sz==$3+1) {
     molList->resize( sz-1 );
   }
 }
--- Code/GraphMol/SmilesParse/smiles.tab.hpp.cmake
+++ Code/GraphMol/SmilesParse/smiles.tab.hpp.cmake
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.5.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
 /* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
-   
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,66 +26,70 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-
-/* Tokens.  */
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     AROMATIC_ATOM_TOKEN = 258,
-     ATOM_TOKEN = 259,
-     ORGANIC_ATOM_TOKEN = 260,
-     NONZERO_DIGIT_TOKEN = 261,
-     ZERO_TOKEN = 262,
-     GROUP_OPEN_TOKEN = 263,
-     GROUP_CLOSE_TOKEN = 264,
-     SEPARATOR_TOKEN = 265,
-     LOOP_CONNECTOR_TOKEN = 266,
-     MINUS_TOKEN = 267,
-     PLUS_TOKEN = 268,
-     CHIRAL_MARKER_TOKEN = 269,
-     CHI_CLASS_TOKEN = 270,
-     CHI_CLASS_OH_TOKEN = 271,
-     H_TOKEN = 272,
-     AT_TOKEN = 273,
-     PERCENT_TOKEN = 274,
-     COLON_TOKEN = 275,
-     BOND_TOKEN = 276,
-     ATOM_OPEN_TOKEN = 277,
-     ATOM_CLOSE_TOKEN = 278,
-     EOS_TOKEN = 279
-   };
+#ifndef YY_YYSMILES_SCRATCH_RDKIT_GIT_CODE_GRAPHMOL_SMILESPARSE_SMILES_TAB_HPP_INCLUDED
+# define YY_YYSMILES_SCRATCH_RDKIT_GIT_CODE_GRAPHMOL_SMILESPARSE_SMILES_TAB_HPP_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yysmiles_debug;
 #endif
 
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    AROMATIC_ATOM_TOKEN = 258,
+    ATOM_TOKEN = 259,
+    ORGANIC_ATOM_TOKEN = 260,
+    NONZERO_DIGIT_TOKEN = 261,
+    ZERO_TOKEN = 262,
+    GROUP_OPEN_TOKEN = 263,
+    GROUP_CLOSE_TOKEN = 264,
+    SEPARATOR_TOKEN = 265,
+    LOOP_CONNECTOR_TOKEN = 266,
+    MINUS_TOKEN = 267,
+    PLUS_TOKEN = 268,
+    CHIRAL_MARKER_TOKEN = 269,
+    CHI_CLASS_TOKEN = 270,
+    CHI_CLASS_OH_TOKEN = 271,
+    H_TOKEN = 272,
+    AT_TOKEN = 273,
+    PERCENT_TOKEN = 274,
+    COLON_TOKEN = 275,
+    BOND_TOKEN = 276,
+    ATOM_OPEN_TOKEN = 277,
+    ATOM_CLOSE_TOKEN = 278,
+    EOS_TOKEN = 279
+  };
+#endif
 
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
 {
-
-/* Line 2068 of yacc.c  */
-#line 56 "smiles.yy"
+#line 57 "smiles.yy" /* yacc.c:1909  */
 
   int                      moli;
   RDKit::Atom * atom;
   RDKit::Bond * bond;
   int                      ival;
 
-
-
-/* Line 2068 of yacc.c  */
-#line 83 "/scratch/RDKit_git/Code/GraphMol/SmilesParse/smiles.tab.hpp"
-} YYSTYPE;
+#line 86 "/scratch/RDKit_git/Code/GraphMol/SmilesParse/smiles.tab.hpp" /* yacc.c:1909  */
+};
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
 
 
+int yysmiles_parse (const char *input, std::vector<RDKit::RWMol *> *molList, std::list<unsigned int> *branchPoints, void *scanner);
 
+#endif /* !YY_YYSMILES_SCRATCH_RDKIT_GIT_CODE_GRAPHMOL_SMILESPARSE_SMILES_TAB_HPP_INCLUDED  */
