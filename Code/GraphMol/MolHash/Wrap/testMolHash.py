f38204252ac6e0028598a0c1b93efa8fd7bb2f48
Greg Landrum <greg.landrum@gmail.com>
2015-02-17 04:03:48 +0100

    Add initial version of molecule hashing

---
+++ Code/GraphMol/MolHash/Wrap/testMolHash.py
@@ -0,0 +1,13 @@
+from rdkit import RDConfig
+import os,sys
+import unittest
+from rdkit import Chem
+from rdkit.Chem import rdMolHash
+
+class TestCase(unittest.TestCase):
+    def setUp(self) :
+        pass
+
+            
+if __name__=="__main__":
+    unittest.main()
