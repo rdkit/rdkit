9fd56d66f6077ea59485b32744f9a3ce8f5345fa
Greg Landrum <greg.landrum@gmail.com>
2015-03-16 08:48:04 +0100

    Fixes: #455

--- Code/PgSQL/rdkit/sql/inchi.sql
+++ Code/PgSQL/rdkit/sql/inchi.sql
@@ -2,3 +2,8 @@ select mol_inchi('c1ccccc1'::mol);
 select mol_inchikey('c1ccccc1'::mol);
 select mol_inchi('Cc1cc(C)[n+]c(C)c1'::mol);
 select mol_inchikey('Cc1cc(C)[n+]c(C)c1'::mol);
+select mol_inchi(mol_from_ctab((Chr(10) || Chr(10) || Chr(10) ||
+'  0  0  0  0  0  0  0  0  0  0999 V2000' ||
+Chr(10) ||
+'M  END')::cstring));
+
