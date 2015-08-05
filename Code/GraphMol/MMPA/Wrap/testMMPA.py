e396e5b35836ad84705733d750feda889ded3079
Greg Landrum <greg.landrum@gmail.com>
2015-08-05 15:54:48 +0200

    a few more python3 tweaks

--- Code/GraphMol/MMPA/Wrap/testMMPA.py
+++ Code/GraphMol/MMPA/Wrap/testMMPA.py
@@ -13,69 +13,69 @@ class TestCase(unittest.TestCase):
     def test1(self) :
         m = Chem.MolFromSmiles('c1ccccc1OC')
         frags = rdMMPA.FragmentMol(m)
-        self.failUnlessEqual(len(frags),3)
+        self.assertEqual(len(frags),3)
         for frag in frags:
-            self.failUnlessEqual(len(frag),2)        
+            self.assertEqual(len(frag),2)        
         frags = sorted(frags,key=natoms)
-        self.failUnlessEqual(frags[0][0],None)
-        self.failUnlessEqual(frags[1][0],None)
-        self.failIfEqual(frags[2][0],None)
-        self.failIfEqual(frags[0][1],None)
-        self.failIfEqual(frags[1][1],None)
-        self.failIfEqual(frags[2][1],None)
+        self.assertEqual(frags[0][0],None)
+        self.assertEqual(frags[1][0],None)
+        self.assertNotEqual(frags[2][0],None)
+        self.assertNotEqual(frags[0][1],None)
+        self.assertNotEqual(frags[1][1],None)
+        self.assertNotEqual(frags[2][1],None)
 
-        self.failUnlessEqual(frags[0][1].GetNumAtoms(),m.GetNumAtoms()+2)
-        self.failUnlessEqual(frags[1][1].GetNumAtoms(),m.GetNumAtoms()+2)
+        self.assertEqual(frags[0][1].GetNumAtoms(),m.GetNumAtoms()+2)
+        self.assertEqual(frags[1][1].GetNumAtoms(),m.GetNumAtoms()+2)
 
         fs = Chem.GetMolFrags(frags[0][1],asMols=True)
-        self.failUnlessEqual(len(fs),2)
-        self.failUnlessEqual(Chem.MolToSmiles(fs[0],True),'c1ccc([*:1])cc1')
-        self.failUnlessEqual(Chem.MolToSmiles(fs[1],True),'CO[*:1]')
+        self.assertEqual(len(fs),2)
+        self.assertEqual(Chem.MolToSmiles(fs[0],True),'c1ccc([*:1])cc1')
+        self.assertEqual(Chem.MolToSmiles(fs[1],True),'CO[*:1]')
 
         fs = Chem.GetMolFrags(frags[1][1],asMols=True)
-        self.failUnlessEqual(len(fs),2)
-        self.failUnlessEqual(Chem.MolToSmiles(fs[0],True),'c1ccc(O[*:1])cc1')
-        self.failUnlessEqual(Chem.MolToSmiles(fs[1],True),'C[*:1]')
+        self.assertEqual(len(fs),2)
+        self.assertEqual(Chem.MolToSmiles(fs[0],True),'c1ccc(O[*:1])cc1')
+        self.assertEqual(Chem.MolToSmiles(fs[1],True),'C[*:1]')
 
         fs = Chem.GetMolFrags(frags[2][0],asMols=True)
-        self.failUnlessEqual(len(fs),1)
-        self.failUnlessEqual(Chem.MolToSmiles(fs[0],True),'O([*:1])[*:2]')
+        self.assertEqual(len(fs),1)
+        self.assertEqual(Chem.MolToSmiles(fs[0],True),'O([*:1])[*:2]')
         fs = Chem.GetMolFrags(frags[2][1],asMols=True)
-        self.failUnlessEqual(len(fs),2)
-        self.failUnlessEqual(Chem.MolToSmiles(fs[0],True),'c1ccc([*:1])cc1')
-        self.failUnlessEqual(Chem.MolToSmiles(fs[1],True),'C[*:2]')
+        self.assertEqual(len(fs),2)
+        self.assertEqual(Chem.MolToSmiles(fs[0],True),'c1ccc([*:1])cc1')
+        self.assertEqual(Chem.MolToSmiles(fs[1],True),'C[*:2]')
 
         
     def test2(self) :
         m = Chem.MolFromSmiles('c1ccccc1OC')
         frags = rdMMPA.FragmentMol(m,resultsAsMols=False)
-        self.failUnlessEqual(len(frags),3)
+        self.assertEqual(len(frags),3)
         for frag in frags:
-            self.failUnlessEqual(len(frag),2)        
+            self.assertEqual(len(frag),2)        
         frags = sorted(frags)
-        self.failUnlessEqual(frags[0][0],'')
-        self.failUnlessEqual(frags[1][0],'')
-        self.failIfEqual(frags[2][0],'')
-        self.failIfEqual(frags[0][1],'')
-        self.failIfEqual(frags[1][1],'')
-        self.failIfEqual(frags[2][1],'')
+        self.assertEqual(frags[0][0],'')
+        self.assertEqual(frags[1][0],'')
+        self.assertNotEqual(frags[2][0],'')
+        self.assertNotEqual(frags[0][1],'')
+        self.assertNotEqual(frags[1][1],'')
+        self.assertNotEqual(frags[2][1],'')
         
-        self.failUnlessEqual(frags[0][1],'CO[*:1].c1ccc(cc1)[*:1]')
-        self.failUnlessEqual(frags[1][1],'C[*:1].c1ccc(cc1)O[*:1]')
-        self.failUnlessEqual(frags[2][0],'O([*:1])[*:2]')
-        self.failUnlessEqual(frags[2][1],'C[*:2].c1ccc([*:1])cc1')
+        self.assertEqual(frags[0][1],'CO[*:1].c1ccc(cc1)[*:1]')
+        self.assertEqual(frags[1][1],'C[*:1].c1ccc(cc1)O[*:1]')
+        self.assertEqual(frags[2][0],'O([*:1])[*:2]')
+        self.assertEqual(frags[2][1],'C[*:2].c1ccc([*:1])cc1')
 
     def test3(self) :
         m = Chem.MolFromSmiles('c1ccccc1OC')
         frags = rdMMPA.FragmentMol(m,resultsAsMols=False,pattern='cO')
-        self.failUnlessEqual(len(frags),1)
+        self.assertEqual(len(frags),1)
         for frag in frags:
-            self.failUnlessEqual(len(frag),2)        
+            self.assertEqual(len(frag),2)        
         frags = sorted(frags)
-        self.failUnlessEqual(frags[0][0],'')
-        self.failIfEqual(frags[0][1],'')
+        self.assertEqual(frags[0][0],'')
+        self.assertNotEqual(frags[0][1],'')
         
-        self.failUnlessEqual(frags[0][1],'CO[*:1].c1ccc(cc1)[*:1]')
+        self.assertEqual(frags[0][1],'CO[*:1].c1ccc(cc1)[*:1]')
 
     def test4(self): # currently failing
         m = Chem.MolFromSmiles('Cc1ccccc1NC(=O)C(C)[NH+]1CCCC1') # ZINC00000051
@@ -83,11 +83,11 @@ class TestCase(unittest.TestCase):
         for frag in sorted(frags):
             print(frag)
         cores = set(x[0] for x in frags)
-        self.failUnless('C([*:1])([*:2])[*:3]' in cores)
-        self.failUnless('O=C(N[*:3])C([*:1])[*:2]' in cores)
-        self.failUnlessEqual(len(frags),18)
+        self.assertTrue('C([*:1])([*:2])[*:3]' in cores)
+        self.assertTrue('O=C(N[*:3])C([*:1])[*:2]' in cores)
+        self.assertEqual(len(frags),18)
         for frag in frags:
-            self.failUnlessEqual(len(frag),2)        
+            self.assertEqual(len(frag),2)        
 
 
 
