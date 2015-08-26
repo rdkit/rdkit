e2487ffe79c393a6b0e472882bfb6eb66a3bcb8b
Greg Landrum <greg.landrum@gmail.com>
2015-08-26 04:59:52 +0200

    update a PAINS based on input from Simon Saubern

--- Data/Pains/test_data/run_tests.py
+++ Data/Pains/test_data/run_tests.py
@@ -14,12 +14,14 @@ class TestCase(unittest.TestCase):
   def setUp(self):
     self.basePath=os.path.join(RDConfig.RDDataDir,'Pains')
     self.painsFile = os.path.join(self.basePath,'wehi_pains.csv')
-    self.painsDefs = [x for x in csv.reader(open(self.painsFile,'r'))]
+    with open(self.painsFile,'r') as inf:
+      self.painsDefs = [x for x in csv.reader(inf)]
     self.matchers = [Chem.MolFromSmarts(x[0],mergeHs=True) for x in self.painsDefs]
 
   def test1(self):
     " molecules that we know should match "
-    testData = [x.strip().split() for x in open(os.path.join(self.basePath,'test_data','test_set3.txt'),'r') if x[0] != '#']
+    with open(os.path.join(self.basePath,'test_data','test_set3.txt'),'r') as inf:
+      testData = [x.strip().split() for x in inf if x[0] != '#']
     for line in testData:
       self.assertEqual(len(line),5)
       id_ = int(line[0])
