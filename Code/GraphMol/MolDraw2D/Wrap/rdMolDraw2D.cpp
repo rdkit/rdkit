33d055b384c7e24eb35477dc16718280f47bf1d4
Greg Landrum <greg.landrum@gmail.com>
2015-09-12 05:35:06 +0200

    add FontSize() to MolDraw2D c++ api

--- Code/GraphMol/MolDraw2D/Wrap/rdMolDraw2D.cpp
+++ Code/GraphMol/MolDraw2D/Wrap/rdMolDraw2D.cpp
@@ -131,6 +131,7 @@ BOOST_PYTHON_MODULE(rdMolDraw2D) {
   docString="Drawer abstract base class";
   python::class_<RDKit::MolDraw2D,boost::noncopyable>("MolDraw2D",docString.c_str(),python::no_init)
     .def("SetFontSize",&RDKit::MolDraw2D::setFontSize,"change the default font size")
+    .def("FontSize",&RDKit::MolDraw2D::fontSize,"get the default font size")
     .def("DrawMolecule",RDKit::drawMoleculeHelper1,
          (python::arg("self"),python::arg("mol"),
           python::arg("highlightAtoms")=python::object(),
