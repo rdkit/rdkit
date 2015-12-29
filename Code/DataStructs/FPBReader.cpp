3813e9c8028b59cfb22e7cb8b9839cdf730f47d1
Greg Landrum <greg.landrum@gmail.com>
2015-12-29 16:33:17 +0100

    we can now also read IDs

--- Code/DataStructs/FPBReader.cpp
+++ Code/DataStructs/FPBReader.cpp
@@ -39,7 +39,7 @@ void readChunk(std::istream &istrm, std::string &nm, boost::uint64_t &sz,
   } else {
     data = NULL;
   }
-  std::cerr << "  CHUNKSZ: " << sz << " name: " << nm << std::endl;
+  // std::cerr << "  CHUNKSZ: " << sz << " name: " << nm << std::endl;
 }
 
 struct FPBReader_impl {
@@ -50,6 +50,9 @@ struct FPBReader_impl {
   std::vector<boost::uint16_t> popCounts;
   const boost::uint8_t *dp_fpData;      // do not free this
   const boost::uint8_t *dp_arenaChunk;  // this is what should be freed
+  boost::uint32_t num4ByteElements, num8ByteElements;  // for finding ids
+  const boost::uint8_t *dp_idOffsets;                  // do not free this
+  const boost::uint8_t *dp_idChunk;                    // free this
 };
 
 void extractPopCounts(FPBReader_impl *dp_impl, boost::uint64_t sz,
@@ -107,7 +110,7 @@ ExplicitBitVect *extractFP(const FPBReader_impl *dp_impl, unsigned int which) {
   PRECONDITION(dp_impl, "bad reader pointer");
   PRECONDITION(dp_impl->dp_fpData, "bad fpdata pointer");
 
-  if (which > dp_impl->len) {
+  if (which >= dp_impl->len) {
     throw ValueErrorException("bad index");
   }
   const boost::uint8_t *fpData =
@@ -116,6 +119,90 @@ ExplicitBitVect *extractFP(const FPBReader_impl *dp_impl, unsigned int which) {
       new boost::dynamic_bitset<boost::uint8_t>(fpData,
                                                 fpData + dp_impl->nBits / 8);
   return new ExplicitBitVect((boost::dynamic_bitset<> *)fpbs);
+};
+
+void extractIds(FPBReader_impl *dp_impl, boost::uint64_t sz,
+                const boost::uint8_t *chunk) {
+  PRECONDITION(dp_impl, "bad pointer");
+  /* Documentation from Andrew's code on the structure of the arena:
+
+  The actual layout inside of the chunk is:
+   <num_4byte_elements: 4 bytes> -- the number of 4 byte offsets.
+   <num_8byte_elements: 4 bytes> -- the number of 8 byte offsets
+   Note: the number of indicies is num_4byte_elements + num_8byte_elements + 1
+   because even with no elements there will be the initial '\0\0\0\0'.
+
+   <id 0> + NUL   -- the first string, with an added NUL terminator
+   <id 1> + NUL   -- the second string, with an added NUL terminator
+       ....
+   <id N> + NUL   -- the last string, with an added NUL terminator
+
+   <offset 0: 4 bytes>    -- the offset relative to the start of <text 0>.
+                    (This always contains the 4 bytes "\0\0\0\0")
+      ...
+   <offset num_4byte_elements: 4 bytes>   -- the last offset stored in 4 bytes
+
+        (Note: This next section exists only when <num 8 byte offsets> > 0)
+   <offset num_4byte_elements+1: 8 bytes>    -- the first offset stored in 8
+  bytes
+      ...
+   <offset num_4byte_elements+num_8byte_elements: 8 bytes>   -- the last offset
+  stored in 8 bytes
+
+  To get the identifier for record at position P >= 0:
+    chunk_size = size of the chunk
+    num_4byte_elements = decode bytes[0:4] as uint32
+    num_8byte_elements = decode bytes[4:8] as uint32
+    if P >= num_4byte_elements + num_8byte_elements:
+        record does not exist
+    offset_start = chunk_size - num_4byte_elements*4 - num_8byte_elements*8
+    if P < num_4byte_elements:
+      start, end = decode bytes[offset_start:offset_start+8] as (uint32, uint32)
+    elif P == N4:
+      start, end = decode bytes[offset_start:offset_start+12] as (uint32,
+  uint64)
+    else:
+      start, end = decode bytes[offset_start:offset_start+16] as (uint64,
+  uint64)
+    id = bytes[start:end-1]
+   */
+  dp_impl->dp_idChunk = chunk;
+  dp_impl->num4ByteElements = *((boost::uint32_t *)chunk);
+  chunk += sizeof(boost::uint32_t);
+  dp_impl->num8ByteElements = *((boost::uint32_t *)chunk);
+  chunk += sizeof(boost::uint32_t);
+  dp_impl->dp_idOffsets = dp_impl->dp_idChunk + sz -
+                          (dp_impl->num4ByteElements + 1) * 4 -
+                          dp_impl->num8ByteElements * 8;
+};
+
+std::string extractId(const FPBReader_impl *dp_impl, unsigned int which) {
+  PRECONDITION(dp_impl, "bad reader pointer");
+  PRECONDITION(dp_impl->dp_idOffsets, "bad idOffsets pointer");
+
+  if (which >= dp_impl->num4ByteElements + dp_impl->num8ByteElements) {
+    throw ValueErrorException("bad index");
+  }
+  std::string res;
+
+  boost::uint64_t offset = 0, len = 0;
+  if (which < dp_impl->num4ByteElements) {
+    offset = *(boost::uint32_t *)(dp_impl->dp_idOffsets + which * 4);
+    len = *(boost::uint32_t *)(dp_impl->dp_idOffsets + (which + 1) * 4);
+  } else if (which == dp_impl->num4ByteElements) {
+    // FIX: this code path is not yet tested
+    offset = *(boost::uint32_t *)(dp_impl->dp_idOffsets + which * 4);
+    len = *(boost::uint64_t *)(dp_impl->dp_idOffsets + (which + 1) * 4);
+  } else {
+    // FIX: this code path is not yet tested
+    offset = *(boost::uint64_t *)(dp_impl->dp_idOffsets +
+                                  dp_impl->num4ByteElements * 4 + which * 8);
+    len = *(boost::uint64_t *)(dp_impl->dp_idOffsets +
+                               dp_impl->num4ByteElements * 4 + (which + 1) * 8);
+  }
+  len -= offset;
+  res = std::string((const char *)(dp_impl->dp_idChunk + offset), len);
+  return res;
 };
 }  // end of detail namespace
 
@@ -143,6 +230,8 @@ void FPBReader::init() {
       detail::extractArena(dp_impl, chunkSz, chunk);
       chunk = NULL;
     } else if (chunkNm == "FPID") {
+      detail::extractIds(dp_impl, chunkSz, chunk);
+      chunk = NULL;
     }
     delete[] chunk;
   }
@@ -150,7 +239,16 @@ void FPBReader::init() {
 };
 
 void FPBReader::destroy() {
-  if (dp_impl) delete[] dp_impl->dp_arenaChunk;
+  if (dp_impl) {
+    delete[] dp_impl->dp_arenaChunk;
+    dp_impl->dp_arenaChunk = NULL;
+
+    delete[] dp_impl->dp_idChunk;
+    dp_impl->dp_idChunk = NULL;
+
+    dp_impl->dp_fpData = NULL;
+    dp_impl->dp_idOffsets = NULL;
+  }
   delete dp_impl;
 };
 
@@ -167,8 +265,8 @@ std::string FPBReader::getId(unsigned int idx) const {
   PRECONDITION(df_init, "not initialized");
   PRECONDITION(dp_impl, "no impl");
   URANGE_CHECK(idx, dp_impl->len);
-  // STUB
-  std::string res = "ZINC00902219";
+
+  std::string res = detail::extractId(dp_impl, idx);
   return res;
 };
 unsigned int FPBReader::length() const {
--- Code/DataStructs/testFPB.cpp
+++ Code/DataStructs/testFPB.cpp
@@ -25,6 +25,20 @@ void test1FPBReaderBasics() {
     FPBReader fps(filename);
     fps.init();
     TEST_ASSERT(fps.length() == 100);
+    {  // get* version
+      std::string nm = fps.getId(0);
+      TEST_ASSERT(nm == "ZINC00902219");
+      ExplicitBitVect *fp = fps.getFP(0);
+      TEST_ASSERT(fp);
+      TEST_ASSERT(fp->getNumBits() == 2048);
+      TEST_ASSERT(fp->getNumOnBits() == 17);
+      unsigned int obs[17] = {1,   80,  183, 222,  227,  231,  482,  650, 807,
+                              811, 831, 888, 1335, 1411, 1664, 1820, 1917};
+      for (unsigned int i = 0; i < fp->getNumOnBits(); ++i) {
+        TEST_ASSERT(fp->getBit(obs[i]));
+      }
+      delete fp;
+    }
     {  // operator[] version
       std::pair<ExplicitBitVect *, std::string> tpl = fps[0];
       ExplicitBitVect *fp = tpl.first;
@@ -39,19 +53,20 @@ void test1FPBReaderBasics() {
       delete fp;
       TEST_ASSERT(tpl.second == "ZINC00902219");
     }
-    {  // get* version
-      ExplicitBitVect *fp = fps.getFP(0);
+    {  // test another fp
+      ExplicitBitVect *fp = fps.getFP(3);
       TEST_ASSERT(fp);
       TEST_ASSERT(fp->getNumBits() == 2048);
-      TEST_ASSERT(fp->getNumOnBits() == 17);
-      unsigned int obs[17] = {1,   80,  183, 222,  227,  231,  482,  650, 807,
-                              811, 831, 888, 1335, 1411, 1664, 1820, 1917};
+      TEST_ASSERT(fp->getNumOnBits() == 20);
+      unsigned int obs[20] = {1,   8,    80,   95,   222,  227, 457,
+                              482, 650,  680,  715,  807,  831, 845,
+                              888, 1226, 1556, 1711, 1917, 1982};
       for (unsigned int i = 0; i < fp->getNumOnBits(); ++i) {
         TEST_ASSERT(fp->getBit(obs[i]));
       }
       delete fp;
-      std::string nm = fps.getId(0);
-      TEST_ASSERT(nm == "ZINC00902219");
+      std::string nm = fps.getId(3);
+      TEST_ASSERT(nm == "ZINC04803506");
     }
   }
   BOOST_LOG(rdInfoLog) << "Finished" << std::endl;
