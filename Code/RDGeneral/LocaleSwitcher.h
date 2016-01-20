a254034c1d548f45b9a23da3c34311a651f02b9c
Brian Kelley <brian.kelley@novartis.com>
2016-01-20 14:24:11 -0500

    Moves LocaleSwitcher Implementation details to LocaleSwitcher.cpp
     This helps hide the OS dependent implementations and compile
     time flags.

--- Code/RDGeneral/LocaleSwitcher.h
+++ Code/RDGeneral/LocaleSwitcher.h
@@ -1,90 +1,50 @@
+//  Copyright (c) 2016, Novartis Institutes for BioMedical Research Inc.
+//  All rights reserved.
 //
-//  Copyright (C) 2012 Greg Landrum
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
 //
-//  @@ All Rights Reserved @@
-//  This file is part of the RDKit.
-//  The contents are covered by the terms of the BSD license
-//  which is included in the file license.txt, found at the root
-//  of the RDKit source tree.
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//     * Neither the name of Novartis Institutes for BioMedical Research Inc.
+//       nor the names of its contributors may be used to endorse or promote
+//       products derived from this software without specific prior written
+//       permission.
 //
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 #ifndef _RD_LOCALESWITCHER_H
 #define _RD_LOCALESWITCHER_H
-
-#include <clocale>
-#ifndef _MSC_VER
-# include <xlocale.h>
-#else
-# include <locale.h>
-#endif
 
 namespace RDKit {
 namespace Utils {
 // allows an RAII-like approach to ensuring the locale is temporarily "C"
 // instead of whatever we started in.
+
+namespace detail {
+class LocaleSwitcherImpl; // concrete OS dependent implementation
+}
+
 class LocaleSwitcher {
- public:
-#ifdef _MSC_VER  
-
-  LocaleSwitcher() {
-    if (!Recurse()) {
-#ifdef RDK_THREADSAFE_SSS
-    _configthreadlocale(_ENABLE_PER_THREAD_LOCALE);
-    ::setlocale(LC_ALL, "C");
-#else    
-    std::setlocale(LC_ALL, "C");
-#endif
-    Recurse(1);
-    switched = true;
-    }
-  }
-  ~LocaleSwitcher() {
-    if (switched) {
-#ifdef RDK_THREADSAFE_SSS
-    ::setlocale(LC_ALL, "C");
-#else    
-    std::setlocale(LC_ALL, "");
-#endif
-    Recurse(-1);
-    }
-  }
+  detail::LocaleSwitcherImpl *impl;
 public:
-  bool switched;
-#else
-  locale_t loc;
-  locale_t old_loc;
-
-LocaleSwitcher() : old_locale(setlocale(LC_ALL, NULL)) {
-    // set locale for this thread
-
-    if (!Recurse() && old_locale != "C") {
-      Recurse(1);
-      old_loc = uselocale(0);
-      loc = newlocale(LC_ALL_MASK, "C", (locale_t)0);
-      uselocale(loc);
-      // Don't free "C" or "GLOBAL" Locales
-    } else
-      old_locale = "C";
-  }
-  ~LocaleSwitcher() {
-    if (old_locale != "C") {
-      uselocale(old_loc);
-      freelocale(loc);
-      Recurse(-1);
-    }
-  }
-  
-public:
-  std::string old_locale;
-
-#endif
-
-  // Recurse(1) recurse into switcher
-  // Recurse(-1) end recursion
-  //  Always turns current state (note: thread safe
-  //   when RDK_THREAFSAFE_SSS defined)
-  static int Recurse(int state=0);
-  
+  LocaleSwitcher();
+  ~LocaleSwitcher();
 };
 }
 }
