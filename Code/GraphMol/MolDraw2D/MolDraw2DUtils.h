1307dea95ac0d09b36efa23bc941982ac6ac165c
Greg Landrum <greg.landrum@gmail.com>
2016-02-17 04:56:54 +0100

    documentation update

--- Code/GraphMol/MolDraw2D/MolDraw2DUtils.h
+++ Code/GraphMol/MolDraw2D/MolDraw2DUtils.h
@@ -16,6 +16,23 @@
 
 namespace RDKit {
 namespace MolDraw2DUtils {
+//! Does some cleanup operations on the molecule to prepare it to draw nicely
+/*
+The operations include: kekulization, addition of chiral Hs, wedging of bonds at
+chiral centers, and generation of a 2D conformation if the molecule does not
+already have a conformation
+
+\param mol: the molecule to be modified
+\param kekulize: toggles kekulization (this can fail, see below)
+\param addChiralHs: adds Hs to the graph on chiral atoms
+\param wedgeBonds: calls WedgeMolBonds()
+\param forceCoords: generates a 2D conformation even if one is present already
+
+NOTE: the kekulization step can fail, throwing a MolSanitizeExecption. If this
+happens the molecule will be in an inconsistent, partially kekulized, state.
+This isn't normally a problem for molecules that have been sanitized, but can be
+problematic if the molecules have been modified post santitization.
+*/
 void prepareMolForDrawing(RWMol &mol, bool kekulize = true,
                           bool addChiralHs = true, bool wedgeBonds = true,
                           bool forceCoords = false);
