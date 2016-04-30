ed6cb752c971118cf100fabbd51eb26fd03c6165
Greg Landrum <greg.landrum@gmail.com>
2016-04-30 05:17:41 +0200

    allow FPBReader::init() to be called multiple times

--- Code/DataStructs/FPBReader.cpp
+++ Code/DataStructs/FPBReader.cpp
@@ -551,6 +551,8 @@ void containingNeighbors(const FPBReader_impl *dp_impl,
 
 void FPBReader::init() {
   PRECONDITION(dp_istrm, "no stream");
+  if (df_init) return;
+
   dp_impl = new detail::FPBReader_impl;
   dp_impl->istrm = dp_istrm;
   dp_impl->df_lazy = df_lazyRead;
