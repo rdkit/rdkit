b84cbe97fc26033b88cbadc1820f8efb64903634
Brian Kelley <brian.kelley@novartis.com>
2016-06-04 13:11:51 -0400

    Split up thread safe and non thread safe implementations

--- Code/RDGeneral/tags-nonthreadsafe.h
+++ Code/RDGeneral/tags-nonthreadsafe.h
@@ -1,6 +1,6 @@
 /* 
 *
-*  Copyright (c) 2015, Novartis Institutes for BioMedical Research Inc.
+*  Copyright (c) 2016, Novartis Institutes for BioMedical Research Inc.
 *  All rights reserved.
 * 
 * Redistribution and use in source and binary forms, with or without
@@ -29,17 +29,63 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#ifndef RDKIT_TAGS_H
-#define RDKIT_TAGS_H
+#ifndef RDKIT_TAGS_NONTHREADSAFE_H
+#define RDKIT_TAGS_NONTHREADSAFE_H
+
+#include "types.h"
+
+#include "BoostStartInclude.h"
+#include <boost/unordered_map.hpp>
+#include "BoostEndInclude.h"
 
 
+namespace RDKit
+{
 
-#ifdef  RDK_THREADSAFE_SSS
-#include "tags-threadsafe.h"
-#else
-#include "tags-nonthreadsafe.h"
+// RDTags is designed to be used as a static singleton
+class RDTags
+{
+  RDTags(const RDTags&);
+  RDTags& operator=(const RDTags&);
+public:
+  // Load the known keys
+  RDTags() {
+    for(int i=0;i<=common_properties::MAX;++i) {
+      keyIntMap[common_properties::getPropName(i)] = i;
+      keys.push_back(common_properties::getPropName(i));
+    }
+  };
+
+  //! \brief Get the current string key for the integer key.
+  //!  throws std::out_of_range when key is out
+  //!  of range.  
+  const std::string &get(int tag) const {
+    return keys[tag]; // raises exception
+  }
+
+  //! \brief Return the int key for the specified string key
+  //! If the string key has never been used before, it is
+  //!  added to the global map and a new integer key is created.
+  int get(const std::string &key) const
+  {
+    boost::unordered_map<std::string, int>::const_iterator it=keyIntMap.find(key);
+    
+    if (it != keyIntMap.end()) {
+      return it->second;
+    }
+
+    int res = static_cast<int>(keyIntMap.size());
+    std::pair<std::string, int> p(key,res);
+    keyIntMap.insert(p);
+    keys.push_back(key);
+    return res;
+  }
+  
+public:
+  mutable boost::unordered_map<std::string, int> keyIntMap;
+  mutable std::vector<std::string > keys;
+};
+
+}
+
 #endif
-
-#endif
-
-
