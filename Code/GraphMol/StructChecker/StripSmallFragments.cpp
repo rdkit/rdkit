7ae60a5492d05d6f16abf4f5e32922057d891b5d
Greg Landrum <greg.landrum@gmail.com>
2016-08-26 07:56:10 +0200

    update

--- Code/GraphMol/StructChecker/StripSmallFragments.cpp
+++ Code/GraphMol/StructChecker/StripSmallFragments.cpp
@@ -14,61 +14,65 @@
 #include "StripSmallFragments.h"
 
 namespace RDKit {
- namespace StructureCheck {
+namespace StructureCheck {
 
-    static inline 
-    std::string getMolecularFormula(const ROMol &mol) {
-         return RDKit::Descriptors::calcMolFormula(mol);
+static inline std::string getMolecularFormula(const ROMol &mol) {
+  return RDKit::Descriptors::calcMolFormula(mol);
+}
+
+void AddMWMF(RWMol &mol,
+             bool pre) {  // set formula & mass properties "MW_PRE" "MW_POST"
+  double mass = 0.0;
+  mass = RDKit::Descriptors::calcExactMW(mol);
+  /*
+          for (unsigned i = 0; i < mol.getNumAtoms(); i++) {
+               const Atom& atom = *mol.getAtomWithIdx(i);
+               mass += atom.getMass();
+               mass += atom.getNumImplicitHs() * 1.0080; // and add implicit
+     Hydrogens mass
+           }
+  */
+  std::string formula = getMolecularFormula(mol);
+  if (!formula.empty()) mol.setProp((pre ? "MF_PRE" : "MF_POST"), formula);
+  char propertyValue[64];
+  snprintf(propertyValue, sizeof(propertyValue), "%g", mass);
+  mol.setProp((pre ? "MW_PRE" : "MW_POST"), mass);
+}
+
+bool StripSmallFragments(RWMol &mol) {
+  bool removed = false;
+  // there may be an argument about how much sense this makes, but it's
+  // consistent with the avalon toolkit behavior
+  if (mol.hasProp(RDKit::common_properties::_MolFileChiralFlag)) {
+    mol.clearProp(RDKit::common_properties::_MolFileChiralFlag);
+  }
+  std::vector<int> frags;
+  std::map<unsigned, unsigned> frag_count;
+
+  unsigned int nFrags = RDKit::MolOps::getMolFrags(mol, frags);
+  if (nFrags > 1) {
+    unsigned maxFragSize = 0;
+    unsigned maxSizeFragIdx = 0;
+    for (unsigned i = 0; i < frags.size(); i++) {
+      if (frag_count.find(frags[i]) != frag_count.end()) {
+        frag_count.at(frags[i]) = frag_count.at(frags[i]) + 1;
+      } else {
+        frag_count.insert(std::pair<unsigned, unsigned>(frags[i], 1));
+      }
+
+      if (frag_count.at(frags[i]) > maxFragSize) {
+        maxFragSize = frag_count.at(frags[i]);
+        maxSizeFragIdx = frags[i];
+      }
     }
+    for (int i = frags.size() - 1; i >= 0; i--)
+      if (frags[i] != maxSizeFragIdx) {
+        mol.removeAtom(i);
+      }
+    removed = true;
+  }
+  return removed;
+}
 
-    void AddMWMF(RWMol &mol, bool pre) { // set formula & mass properties "MW_PRE" "MW_POST"
-         double mass = 0.0;
-         mass = RDKit::Descriptors::calcExactMW(mol);
-/*
-        for (unsigned i = 0; i < mol.getNumAtoms(); i++) {
-             const Atom& atom = *mol.getAtomWithIdx(i);
-             mass += atom.getMass();
-             mass += atom.getNumImplicitHs() * 1.0080; // and add implicit Hydrogens mass
-         }
-*/
-         std::string formula = getMolecularFormula(mol);
-         if(!formula.empty())
-            mol.setProp((pre ? "MF_PRE" : "MF_POST"), formula);
-         char propertyValue[64];
-         snprintf(propertyValue, sizeof(propertyValue), "%g", mass);
-         mol.setProp((pre ? "MW_PRE" : "MW_POST"), mass);
-    }
-
-     bool StripSmallFragments(RWMol &mol) {
-        bool removed = false;
-
-        std::vector<int>                        frags;
-        std::map<unsigned, unsigned>     frag_count;
-        
-        unsigned int nFrags=RDKit::MolOps::getMolFrags(mol, frags);
-        if (nFrags > 1) {
-            unsigned maxFragSize = 0;
-            unsigned maxSizeFragIdx = 0;
-            for (unsigned i = 0; i < frags.size(); i++) {
-               if (frag_count.find(frags[i]) != frag_count.end()) {
-                  frag_count.at(frags[i]) = frag_count.at(frags[i]) + 1;
-               } else {
-                  frag_count.insert( std::pair<unsigned, unsigned>(frags[i],1));
-               }
-                
-               if (frag_count.at(frags[i]) > maxFragSize) {
-                  maxFragSize = frag_count.at(frags[i]);
-                  maxSizeFragIdx = frags[i];
-               }
-            }
-            for (int i = frags.size() -1; i >= 0; i--)
-                if(frags[i] != maxSizeFragIdx) {
-                   mol.removeAtom(i);
-                }
-            removed = true;
-        }
-        return removed;
-    }
-
- }// namespace StructureCheck
-} // namespace RDKit
+}  // namespace StructureCheck
+}  // namespace RDKit
