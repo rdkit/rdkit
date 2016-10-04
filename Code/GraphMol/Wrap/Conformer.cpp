0e7b51f0d5921e859c3844a347d92abebf8c8bbe
Jan Domanski <jandom@gmail.com>
2016-10-04 15:42:31 +0100

    Conformer GetPos returns a numpy array rather than a tuple of tuples

--- Code/GraphMol/Wrap/Conformer.cpp
+++ Code/GraphMol/Wrap/Conformer.cpp
@@ -19,13 +19,37 @@
 #include <GraphMol/Conformer.h>
 #include <RDBoost/PySequenceHolder.h>
 //#include "seqs.hpp"
-
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include <numpy/arrayobject.h>
 namespace python = boost::python;
 
 namespace RDKit {
 RDGeom::Point3D GetAtomPos(const Conformer *conf, unsigned int aid) {
   RDGeom::Point3D res = conf->getAtomPos(aid);
   return res;
+}
+
+PyObject* GetPos(const Conformer *conf) {
+    RDGeom::POINT3D_VECT pos = conf->getPositions();
+
+    // define a 2D array with the following size
+    npy_intp dims[2];
+    dims[0] = pos.size();
+    dims[1] = 3;
+
+    // initialize the array
+    PyArrayObject *res = (PyArrayObject *)PyArray_SimpleNew(2, dims, NPY_DOUBLE);
+
+    // represent the array as a 1D/flat array of doubles
+    double *resData = reinterpret_cast<double *>(PyArray_DATA(res));
+
+    // manually insert the data, 3 corresponsd to the x, y and z dimensions
+    for (unsigned int i = 0; i < pos.size(); ++i) {
+        resData[3*i+0] = pos[i].x;
+        resData[3*i+1] = pos[i].y;
+        resData[3*i+2] = pos[i].z;
+    }
+    return PyArray_Return(res);
 }
 
 void SetAtomPos(Conformer *conf, unsigned int aid, python::object loc) {
