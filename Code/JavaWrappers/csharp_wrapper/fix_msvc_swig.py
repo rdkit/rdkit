f5c378f5dd9c5a03a180cb628782b5f43ed5ab97
Greg Landrum <greg.landrum@gmail.com>
2016-10-13 16:38:34 +0200

    switch to an alternate strategy and get it working for the java wrappers too this now builds for me on 64bit win10 with MSVC14

--- Code/JavaWrappers/csharp_wrapper/fix_msvc_swig.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-
-for file in sys.argv[1:]:
-    if os.path.exists(file):
-        text = open(file).read()
-        text = text.replace("|| (LONG_MAX == INT_MAX)", "")
-        open(file,'w').write(text)
