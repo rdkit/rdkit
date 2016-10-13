0aa3bc624871b5912d2a0dd38f804a279610d65e
Greg Landrum <greg.landrum@gmail.com>
2016-10-13 14:41:13 +0200

    Fixes #1089 (#1093)

---
+++ rdkit/Chem/Draw/UnitTestIPython.py
@@ -0,0 +1,44 @@
+#
+#  Copyright (C) 2016  greg Landrum
+#
+#   @@ All Rights Reserved @@
+#  This file is part of the RDKit.
+#  The contents are covered by the terms of the BSD license
+#  which is included in the file license.txt, found at the root
+#  of the RDKit source tree.
+#
+""" unit testing code for IPython/Jupyter integration
+"""
+import unittest
+from rdkit import Chem
+from rdkit.Chem import Draw
+try:
+    from rdkit.Chem.Draw import IPythonConsole
+    from IPython.core.display import SVG
+except ImportError:
+    IPythonConsole = None
+
+class TestCase(unittest.TestCase):
+  def testGithub1089(self):
+    if IPythonConsole is None:
+        return
+    m = Chem.MolFromSmiles('CCCC')
+
+    IPythonConsole.ipython_useSVG = False
+    res = Draw.MolsToGridImage((m,m))
+    self.assertFalse(isinstance(res,SVG))
+
+    res = Draw.MolsToGridImage((m,m),useSVG=True)
+    self.assertTrue(isinstance(res,SVG))
+
+    IPythonConsole.ipython_useSVG = True
+    res = Draw.MolsToGridImage((m,m))
+    self.assertTrue(isinstance(res,SVG))
+
+
+
+
+
+
+if __name__ == '__main__':
+  unittest.main()
