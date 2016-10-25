a5845f0c32447e132366dbe86394abb74125e494
Greg Landrum <greg.landrum@gmail.com>
2016-10-25 21:04:11 +0200

    add more detailed error reporting when python tests fail in TestRunner.py (#1122)

--- rdkit/TestRunner.py
+++ rdkit/TestRunner.py
@@ -106,6 +106,9 @@ def RunScript(script, doLongTests, verbose):
 
   nTests = len(tests) + len(longTests)
   del sys.modules[script]
+  if verbose and failed:
+    for exeName,args,extras in failed:
+        print("!!! TEST FAILURE: ",exeName,args,extras,file=sys.stderr)
   return failed, nTests
 
 
