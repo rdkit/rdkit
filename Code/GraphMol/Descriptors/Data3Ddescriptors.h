1a40858e7a5483d571e710fde2f5bf786e07ed87
thegodone <guillaume.godin@gmail.com>
2016-10-31 16:59:09 +0100

    adding 3D data declaration

---
+++ Code/GraphMol/Descriptors/Data3Ddescriptors.h
@@ -0,0 +1,30 @@
+#include <string>
+
+using namespace std;
+
+#ifndef _Data3Ddescriptors
+
+#define _Data3Ddescriptors
+
+class Data3Ddescriptors
+{
+   private:
+		static double mw[53];
+		static double vdW[53];
+		static double neg[53];
+		static double pol[53];
+		static double ionpol[83];
+		static double rcov[83];
+
+   public:
+	   Data3Ddescriptors();
+	   double* getMW();
+	   double* getVDW();
+	   double* getNEG();
+	   double* getPOL();
+	   double* getIonPOL();
+	   double* getRCOV();
+
+};
+#endif 
+
