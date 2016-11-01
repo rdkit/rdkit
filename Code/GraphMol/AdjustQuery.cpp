6eb647baa7e45d76e9129224ca27834b8a00834b
Greg Landrum <greg.landrum@gmail.com>
2016-11-01 21:43:06 +0000

    backup

--- Code/GraphMol/AdjustQuery.cpp
+++ Code/GraphMol/AdjustQuery.cpp
@@ -9,6 +9,7 @@
 //
 #include <GraphMol/GraphMol.h>
 #include <GraphMol/QueryAtom.h>
+#include <GraphMol/QueryBond.h>
 #include <GraphMol/MolOps.h>
 #include <GraphMol/QueryOps.h>
 #include <GraphMol/AtomIterators.h>
@@ -52,12 +53,48 @@ void adjustQueryProperties(RWMol &mol, const AdjustQueryParameters *inParams) {
     }
   }
 
+  if (params.makeAtomsGeneric) {
+    for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
+      if (!((params.makeAtomsGenericFlags & ADJUST_IGNORECHAINS) &&
+            !ringInfo->numAtomRings(i)) &&
+          !((params.makeAtomsGenericFlags & ADJUST_IGNORERINGS) &&
+            ringInfo->numAtomRings(i)) &&
+          !((params.adjustDegreeFlags & ADJUST_IGNOREMAPPED) &&
+            isMapped(mol.getAtomWithIdx(i)))) {
+        QueryAtom *qa = new QueryAtom();
+        qa->setQuery(makeAtomNullQuery());
+        mol.replaceAtom(i, qa);
+        delete qa;
+      }
+    }
+  }  // end of makeAtomsGeneric
+  if (params.makeBondsGeneric) {
+    for (unsigned int i = 0; i < mol.getNumBonds(); ++i) {
+      if (!((params.makeBondsGenericFlags & ADJUST_IGNORECHAINS) &&
+            !ringInfo->numBondRings(i)) &&
+          !((params.makeBondsGenericFlags & ADJUST_IGNORERINGS) &&
+            ringInfo->numBondRings(i))) {
+        QueryBond *qb = new QueryBond();
+        qb->setQuery(makeBondNullQuery());
+        mol.replaceBond(i, qb);
+        delete qb;
+      }
+    }
+  }  // end of makeBondsGeneric
   for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
     Atom *at = mol.getAtomWithIdx(i);
     // pull properties we need from the atom here, once we
     // create a query atom they may no longer be valid.
     unsigned int nRings = ringInfo->numAtomRings(i);
     int atomicNum = at->getAtomicNum();
+    if (params.makeDummiesQueries && atomicNum == 0 && !at->hasQuery() &&
+        !at->getIsotope()) {
+      QueryAtom *qa = new QueryAtom();
+      qa->setQuery(makeAtomNullQuery());
+      mol.replaceAtom(i, qa);
+      delete qa;
+      at = mol.getAtomWithIdx(i);
+    }  // end of makeDummiesQueries
     if (params.adjustDegree &&
         !((params.adjustDegreeFlags & ADJUST_IGNORECHAINS) && !nRings) &&
         !((params.adjustDegreeFlags & ADJUST_IGNORERINGS) && nRings) &&
@@ -96,14 +133,6 @@ void adjustQueryProperties(RWMol &mol, const AdjustQueryParameters *inParams) {
       }
       qa->expandQuery(makeAtomInNRingsQuery(nRings));
     }  // end of adjust ring count
-    if (params.makeDummiesQueries && atomicNum == 0 && !at->hasQuery() &&
-        !at->getIsotope()) {
-      QueryAtom *qa = new QueryAtom();
-      qa->setQuery(makeAtomNullQuery());
-      mol.replaceAtom(i, qa);
-      delete qa;
-      at = mol.getAtomWithIdx(i);
-    }  // end of makeDummiesQueries
   }    // end of loop over atoms
   if (params.makeBondsGeneric) {
     ROMol::EDGE_ITER firstB, lastB;
