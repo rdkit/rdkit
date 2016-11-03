1e009ea6dc3f55bc3ec5a6326116a2c7c136a9ea
thegodone <guillaume.godin@gmail.com>
2016-11-03 20:42:25 +0100

    correction of the PBF file + python function 3D

--- Code/GraphMol/Descriptors/test3D.py
+++ Code/GraphMol/Descriptors/test3D.py
@@ -3,62 +3,31 @@ from rdkit import rdBase
 
 from rdkit.Chem import rdMolDescriptors as rdMD
 from rdkit.Chem import AllChem
-
+import time
 print rdBase.rdkitVersion
 print rdBase.boostVersion
 
 
-def get3D(m):
-	m = Chem.AddHs(m)
-	AllChem.EmbedMolecule(m)
-	AllChem.MMFFOptimizeMolecule(m)
+def get3D(m,is3d):
+	if not is3d:
+		m = Chem.AddHs(m)
+		AllChem.EmbedMolecule(m)
+		AllChem.MMFFOptimizeMolecule(m)
 	r= rdMD.CalcAUTOCORR3D(m)+rdMD.CalcRDF(m)+rdMD.CalcMORSE(m)+rdMD.CalcWHIM(m)+rdMD.CalcGETAWAY(m)
 	return r
 
 
 m = Chem.MolFromSmiles('Cc1ccccc1')
+thefile = open('test.txt', 'w')
+filename="/Users/mbp/Github/rdkit_mine/Code/GraphMol/Descriptors/test_data/PBF_egfr.sdf"
+suppl = Chem.SDMolSupplier(filename,removeHs=False)
+mols = [x for x in suppl]
+start = time.time()
+for m in mols:
+	r= get3D(m,True)
+	for item in r:
+  		thefile.write("%.3f," % item)
+  	thefile.write("\n")
 
-
-r= get3D(m)
-print len(r)
-
-
-# from rdkit import Chem
-# from rdkit.Chem import Descriptors
-# #from rdkit import rdBase
-# #print rdBase.rdkitVersion
-# #print rdBase.boostVersion
-
-# m = Chem.MolFromSmiles('c1ccccc1C(=O)O')
-
-# #print dir(Descriptors)
-
-# #print Descriptors.CalcRadiusOfGyration(m)
-
-
-# from rdkit.Chem import rdMolDescriptors
-
-# from rdkit.Chem import AllChem
-
-# m=Chem.AddHs(m)
-# AllChem.EmbedMolecule(m) #,useRandomCoords=True)
-# AllChem.MMFFOptimizeMolecule(m)
-
-# # print rdMolDescriptors.CalcRadiusOfGyration(m)
-# # print rdMolDescriptors.CalcTPSA(m)
-# # print rdMolDescriptors.CalcLabuteASA(m)
-# # print rdMolDescriptors.CalcAsphericity(m)
-# # print rdMolDescriptors.CalcInertialShapeFactor(m)
-# # print rdMolDescriptors.CalcNPR1(m)
-# # print rdMolDescriptors.CalcNPR2(m)
-# # print rdMolDescriptors.CalcPBF(m)
-# # print rdMolDescriptors.CalcPMI1(m)
-# # print rdMolDescriptors.CalcPMI2(m)
-# # print rdMolDescriptors.CalcPMI3(m)
-# # print rdMolDescriptors.CalcEccentricity(m)
-# # print rdMolDescriptors.CalcSpherocityIndex(m)
-# print "------"
-# print rdMolDescriptors.CalcRDF(m)
-# print "------"
-
-# print rdMolDescriptors.CalcMORSE(m)
+end = time.time()
+print end - start
