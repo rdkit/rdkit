feb570cb884ae53d1924e64dc151fefd7e2f5791
gedeck <peter.gedeck@gmail.com>
2016-12-05 13:27:13 -0500

    Doctests are executed from the two UnitTestXXX.py files

--- rdkit/VLib/test_list.py
+++ rdkit/VLib/test_list.py
@@ -1,9 +1,5 @@
 tests = [
-  ("python", "Node.py", {}),
-  ("python", "Filter.py", {}),
-  ("python", "Output.py", {}),
-  ("python", "Supply.py", {}),
-  ("python", "Transform.py", {}),
+  ("python", "UnitTestVLib", {}),
   ("python", "test_list.py", {'dir': 'NodeLib'}),
 ]
 
