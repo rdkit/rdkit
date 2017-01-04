cdb4359aacb86997921a396d49eeb2c0a4b9581a
gedeck <peter.gedeck@gmail.com>
2017-01-04 12:52:40 -0500

    Unicode error caused by logger

--- rdkit/Chem/MolKey/MolKey.py
+++ rdkit/Chem/MolKey/MolKey.py
@@ -412,7 +412,7 @@ def GetKeyForCTAB(ctab, stereo_info=None, stereo_comment=None, logger=None):
   try:
     err, inchi, fixed_mol = GetInchiForCTAB(ctab)
   except BadMoleculeException:
-    logger.warn('Corrupt molecule substituting no-struct: --->\n{0}\n<----'.format(ctab))
+    logger.warn('Corrupt molecule substituting no-struct: --->\n{0}\n<----'.format(ctab).encode())
     err = NULL_MOL
     key = _identify(err, '', '', None, None)
     return MolKeyResult(key, err, '', '', None, None)
