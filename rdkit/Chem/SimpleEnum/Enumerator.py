52b80668fd82aef1534053dbf1d130ef37310a8d
gedeck <peter.gedeck@gmail.com>
2017-01-15 09:41:02 -0500

    Review tests in rdkit.Chem.SimpleEnum (#1248)
    * 1194: Review assignments of range in Python code 
    
    Task-Url: https://github.com/rdkit/rdkit/issues/1194
    Either wrapped the range expression into a list or made sure that the
    code is working with a range object.
    
    * Increased test coverage
    
    * Reduce tests to additional tests

--- rdkit/Chem/SimpleEnum/Enumerator.py
+++ rdkit/Chem/SimpleEnum/Enumerator.py
@@ -1,19 +1,19 @@
 #
 #  Copyright (c) 2014, Novartis Institutes for BioMedical Research Inc.
 #  All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
-# met: 
+# met:
 #
-#     * Redistributions of source code must retain the above copyright 
+#     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following 
-#       disclaimer in the documentation and/or other materials provided 
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
 #       with the distribution.
-#     * Neither the name of Novartis Institutes for BioMedical Research Inc. 
-#       nor the names of its contributors may be used to endorse or promote 
+#     * Neither the name of Novartis Institutes for BioMedical Research Inc.
+#       nor the names of its contributors may be used to endorse or promote
 #       products derived from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -30,13 +30,13 @@
 #
 # Created by Greg Landrum, May 2009
 from __future__ import print_function
-from rdkit import RDConfig
-from rdkit import Chem
-from rdkit.Chem import AllChem
-from rdkit.Chem import FunctionalGroups
-from rdkit.Chem import rdChemReactions
 
 import os
+
+from rdkit import Chem
+from rdkit import RDConfig
+from rdkit.Chem import AllChem
+from rdkit.Chem import rdChemReactions
 
 
 def PreprocessReaction(reaction, funcGroupFilename=None, propName='molFileValue'):
@@ -57,15 +57,15 @@ def PreprocessReaction(reaction, funcGroupFilename=None, propName='molFileValue'
   >>> reactantLabels
   (((0, 'halogen.bromine.aromatic'),), ((1, 'boronicacid'),))
 
-  If there are functional group labels in the input reaction (via atoms with molFileValue properties),
-  the corresponding atoms will have queries added to them so that they only match such things. We can
-  see this here:
+  If there are functional group labels in the input reaction (via atoms with molFileValue
+  properties), the corresponding atoms will have queries added to them so that they only
+  match such things. We can see this here:
   >>> rxn = AllChem.ReactionFromRxnFile(testFile)
   >>> rxn.Initialize()
   >>> r1 = rxn.GetReactantTemplate(0)
   >>> m1 = Chem.MolFromSmiles('CCBr')
   >>> m2 = Chem.MolFromSmiles('c1ccccc1Br')
-  
+
   These both match because the reaction file itself just has R1-Br:
   >>> m1.HasSubstructMatch(r1)
   True
@@ -181,7 +181,7 @@ def EnumerateReaction(
   6
   >>> print(smis)
   ['CCCc1ccccc1', 'CCCc1ccccn1', 'CCCc1cccnc1', 'CCc1ccccc1', 'CCc1ccccn1', 'CCc1cccnc1']
-  
+
   The nastiness can be avoided at the cost of some memory by asking for only unique products:
   >>> prods = EnumerateReaction(rxn,(reacts1,reacts2),uniqueProductsOnly=True)
   >>> prods = list(prods)
@@ -190,7 +190,7 @@ def EnumerateReaction(
   >>> print(sorted([Chem.MolToSmiles(x[0]) for x in prods]))
   ['CCCc1ccccc1', 'CCCc1ccccn1', 'CCCc1cccnc1', 'CCc1ccccc1', 'CCc1ccccn1', 'CCc1cccnc1']
 
-  
+
   """
   nWarn, nError, nReacts, nProds, reactantLabels = PreprocessReaction(reaction)
   if nError:
@@ -214,16 +214,16 @@ def EnumerateReaction(
     return _uniqueOnly(ps)
 
 
-#------------------------------------
+# ------------------------------------
 #
 #  doctest boilerplate
 #
-def _test():
-  import doctest, sys
-  return doctest.testmod(sys.modules["__main__"])
-
-
-if __name__ == '__main__':
+def _runDoctests(verbose=None):  # pragma: nocover
   import sys
-  failed, tried = _test()
+  import doctest
+  failed, _ = doctest.testmod(optionflags=doctest.ELLIPSIS, verbose=verbose)
   sys.exit(failed)
+
+
+if __name__ == '__main__':  # pragma: nocover
+  _runDoctests()
