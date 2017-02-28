d9ef4f03b81ec4aa1cbbe6facc75b10a6fd72956
thegodone <guillaume.godin@gmail.com>
2017-02-28 08:45:55 +0100

    fix Name

--- Code/GraphMol/Descriptors/AUTOCORR3D.cpp
+++ Code/GraphMol/Descriptors/AUTOCORR3D.cpp
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2012, Institue of Cancer Research.
+//  Copyright (c) 2016, Guillaume GODIN.
 //  All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -54,167 +54,167 @@
 
 using namespace Eigen;
 namespace RDKit {
-namespace Descriptors {
-
-namespace {
-
-MolData3Ddescriptors moldata3D;
-
-double* retreiveMat(MatrixXd matrix) {
-  double* arrayd = matrix.data();
-  return arrayd;
-}
-
-double* retreiveVect(VectorXd matrix) {
-  double* arrayd = matrix.data();
-  return arrayd;
-}
-
-VectorXd getEigenVect(std::vector<double> v) {
-  double* varray_ptr = &v[0];
-  Map<VectorXd> V(varray_ptr, v.size());
-  return V;
-}
-
-double* AppendDouble(double* w, double* Append, int length, int pos) {
-  for (int i = pos; i < pos + length; i++) {
-    w[i] = Append[i - pos];
-  }
-
-  return w;
-}
-
-double* GetGeodesicMatrix(double* dist, int lag, int numAtoms) {
-  int sizeArray = numAtoms * numAtoms;
-  double* Geodesic = new double[sizeArray];
-  for (int i = 0; i < sizeArray; i++) {
-    if (dist[i] == lag)
-      Geodesic[i] = 1;
-    else
-      Geodesic[i] = 0;
-  }
-
-  return Geodesic;
-}
-
-void get3DautocorrelationDesc(double* dist3D, double* dist, int numAtoms,
-                              const ROMol& mol, std::vector<double>& res) {
-  Map<MatrixXd> dm(dist3D, numAtoms, numAtoms);
-
-  std::vector<double> wp = moldata3D.GetRelativePol(mol);
-
-  VectorXd Wp = getEigenVect(wp);
-
-  std::vector<double> wm = moldata3D.GetRelativeMW(mol);
-
-  VectorXd Wm = getEigenVect(wm);
-
-  std::vector<double> wi = moldata3D.GetRelativeIonPol(mol);
-
-  VectorXd Wi = getEigenVect(wi);
-
-  std::vector<double> wv = moldata3D.GetRelativeVdW(mol);
-
-  VectorXd Wv = getEigenVect(wv);
-
-  std::vector<double> we = moldata3D.GetRelativeENeg(mol);
-
-  VectorXd We = getEigenVect(we);
-
-  std::vector<double> wu = moldata3D.GetUn(numAtoms);
-
-  VectorXd Wu = getEigenVect(wu);
-
-  std::vector<double> ws = moldata3D.GetIState(mol);
-
-  VectorXd Ws = getEigenVect(ws);
-
-  std::vector<double> wr = moldata3D.GetRelativeRcov(mol);
-
-  VectorXd Wr = getEigenVect(wr);
-
-  MatrixXd Bi;
-  MatrixXd tmp;
-  double TDBmat[8][10];
-  double dtmp;
-
-  for (int i = 0; i < 10; i++) {
-    double* Bimat = GetGeodesicMatrix(dist, i + 1, numAtoms);
-    Map<MatrixXd> Bi(Bimat, numAtoms, numAtoms);
-    MatrixXd RBi = Bi.cwiseProduct(dm);
-    double Bicount = (double)Bi.sum();
-
-    tmp = Wu.transpose() * RBi * Wu / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[0][i] = dtmp;
-
-    tmp = Wm.transpose() * RBi * Wm / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[1][i] = dtmp;
-
-    tmp = Wv.transpose() * RBi * Wv / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[2][i] = dtmp;
-
-    tmp = We.transpose() * RBi * We / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[3][i] = dtmp;
-
-    tmp = Wp.transpose() * RBi * Wp / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[4][i] = dtmp;
-
-    tmp = Wi.transpose() * RBi * Wi / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[5][i] = dtmp;
-
-    tmp = Ws.transpose() * RBi * Ws / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[6][i] = dtmp;
-
-    tmp = Wr.transpose() * RBi * Wr / Bicount;
-    dtmp = (double)tmp(0);
-    if (std::isnan(dtmp)) dtmp = 0.0;
-    TDBmat[7][i] = dtmp;
-  }
-
-  // update the Output vector!
-  for (unsigned int j = 0; j < 8; ++j) {
-    for (unsigned int i = 0; i < 10; ++i) {
-      res[j * 10 + i] = TDBmat[j][i];
-    }
-  }
-}
-
-void Get3Dauto(double* dist3D, double* dist, int numAtoms, const ROMol& mol,
-               std::vector<double>& res) {
-  // std::vector<std::string>
-  // AUTOCORRNAMES={"TDB01u","TDB02u","TDB03u","TDB04u","TDB05u","TDB06u","TDB07u","TDB08u","TDB09u","TDB10u","TDB01m","TDB02m","TDB03m","TDB04m","TDB05m","TDB06m","TDB07m","TDB08m","TDB09m","TDB10m","TDB01v","TDB02v","TDB03v","TDB04v","TDB05v","TDB06v","TDB07v","TDB08v","TDB09v","TDB10v","TDB01e","TDB02e","TDB03e","TDB04e","TDB05e","TDB06e","TDB07e","TDB08e","TDB09e","TDB10e","TDB01p","TDB02p","TDB03p","TDB04p","TDB05p","TDB06p","TDB07p","TDB08p","TDB09p","TDB10p","TDB01i","TDB02i","TDB03i","TDB04i","TDB05i","TDB06i","TDB07i","TDB08i","TDB09i","TDB10i","TDB01s","TDB02s","TDB03s","TDB04s","TDB05s","TDB06s","TDB07s","TDB08s","TDB09s","TDB10s","TDB01r","TDB02r","TDB03r","TDB04r","TDB05r","TDB06r","TDB07r","TDB08r","TDB09r","TDB10r"};
-  get3DautocorrelationDesc(dist3D, dist, numAtoms, mol, res);
-}
-
-}  // end of anonymous namespace
-
-void AUTOCORR3D(const ROMol& mol, std::vector<double>& res, int confId) {
-  PRECONDITION(mol.getNumConformers() >= 1, "molecule has no conformers")
-  int numAtoms = mol.getNumAtoms();
-
-  const Conformer& conf = mol.getConformer(confId);
-
-  double* dist = MolOps::getDistanceMat(mol, false);
-  double* dist3D = MolOps::get3DDistanceMat(mol, confId);
-
-  res.clear();
-  res.resize(80);
-  Get3Dauto(dist3D, dist, numAtoms, mol, res);
-}
-
-}  // end of Descriptors namespace
+    namespace Descriptors {
+        
+        namespace {
+            
+            MolData3Ddescriptors moldata3D;
+            
+            double* retreiveMat(MatrixXd matrix) {
+                double* arrayd = matrix.data();
+                return arrayd;
+            }
+            
+            double* retreiveVect(VectorXd matrix) {
+                double* arrayd = matrix.data();
+                return arrayd;
+            }
+            
+            VectorXd getEigenVect(std::vector<double> v) {
+                double* varray_ptr = &v[0];
+                Map<VectorXd> V(varray_ptr, v.size());
+                return V;
+            }
+            
+            double* AppendDouble(double* w, double* Append, int length, int pos) {
+                for (int i = pos; i < pos + length; i++) {
+                    w[i] = Append[i - pos];
+                }
+                
+                return w;
+            }
+            
+            double* GetGeodesicMatrix(double* dist, int lag, int numAtoms) {
+                int sizeArray = numAtoms * numAtoms;
+                double* Geodesic = new double[sizeArray];
+                for (int i = 0; i < sizeArray; i++) {
+                    if (dist[i] == lag)
+                        Geodesic[i] = 1;
+                    else
+                        Geodesic[i] = 0;
+                }
+                
+                return Geodesic;
+            }
+            
+            void get3DautocorrelationDesc(double* dist3D, double* dist, int numAtoms,
+                                          const ROMol& mol, std::vector<double>& res) {
+                Map<MatrixXd> dm(dist3D, numAtoms, numAtoms);
+                
+                std::vector<double> wp = moldata3D.GetRelativePol(mol);
+                
+                VectorXd Wp = getEigenVect(wp);
+                
+                std::vector<double> wm = moldata3D.GetRelativeMW(mol);
+                
+                VectorXd Wm = getEigenVect(wm);
+                
+                std::vector<double> wi = moldata3D.GetRelativeIonPol(mol);
+                
+                VectorXd Wi = getEigenVect(wi);
+                
+                std::vector<double> wv = moldata3D.GetRelativeVdW(mol);
+                
+                VectorXd Wv = getEigenVect(wv);
+                
+                std::vector<double> we = moldata3D.GetRelativeENeg(mol);
+                
+                VectorXd We = getEigenVect(we);
+                
+                std::vector<double> wu = moldata3D.GetUn(numAtoms);
+                
+                VectorXd Wu = getEigenVect(wu);
+                
+                std::vector<double> ws = moldata3D.GetIState(mol);
+                
+                VectorXd Ws = getEigenVect(ws);
+                
+                std::vector<double> wr = moldata3D.GetRelativeRcov(mol);
+                
+                VectorXd Wr = getEigenVect(wr);
+                
+                MatrixXd Bi;
+                MatrixXd tmp;
+                double TDBmat[8][10];
+                double dtmp;
+                
+                for (int i = 0; i < 10; i++) {
+                    double* Bimat = GetGeodesicMatrix(dist, i + 1, numAtoms);
+                    Map<MatrixXd> Bi(Bimat, numAtoms, numAtoms);
+                    MatrixXd RBi = Bi.cwiseProduct(dm);
+                    double Bicount = (double)Bi.sum();
+                    
+                    tmp = Wu.transpose() * RBi * Wu / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[0][i] = dtmp;
+                    
+                    tmp = Wm.transpose() * RBi * Wm / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[1][i] = dtmp;
+                    
+                    tmp = Wv.transpose() * RBi * Wv / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[2][i] = dtmp;
+                    
+                    tmp = We.transpose() * RBi * We / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[3][i] = dtmp;
+                    
+                    tmp = Wp.transpose() * RBi * Wp / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[4][i] = dtmp;
+                    
+                    tmp = Wi.transpose() * RBi * Wi / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[5][i] = dtmp;
+                    
+                    tmp = Ws.transpose() * RBi * Ws / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[6][i] = dtmp;
+                    
+                    tmp = Wr.transpose() * RBi * Wr / Bicount;
+                    dtmp = (double)tmp(0);
+                    if (std::isnan(dtmp)) dtmp = 0.0;
+                    TDBmat[7][i] = dtmp;
+                }
+                
+                // update the Output vector!
+                for (unsigned int j = 0; j < 8; ++j) {
+                    for (unsigned int i = 0; i < 10; ++i) {
+                        res[j * 10 + i] = TDBmat[j][i];
+                    }
+                }
+            }
+            
+            void Get3Dauto(double* dist3D, double* dist, int numAtoms, const ROMol& mol,
+                           std::vector<double>& res) {
+                // std::vector<std::string>
+                // AUTOCORRNAMES={"TDB01u","TDB02u","TDB03u","TDB04u","TDB05u","TDB06u","TDB07u","TDB08u","TDB09u","TDB10u","TDB01m","TDB02m","TDB03m","TDB04m","TDB05m","TDB06m","TDB07m","TDB08m","TDB09m","TDB10m","TDB01v","TDB02v","TDB03v","TDB04v","TDB05v","TDB06v","TDB07v","TDB08v","TDB09v","TDB10v","TDB01e","TDB02e","TDB03e","TDB04e","TDB05e","TDB06e","TDB07e","TDB08e","TDB09e","TDB10e","TDB01p","TDB02p","TDB03p","TDB04p","TDB05p","TDB06p","TDB07p","TDB08p","TDB09p","TDB10p","TDB01i","TDB02i","TDB03i","TDB04i","TDB05i","TDB06i","TDB07i","TDB08i","TDB09i","TDB10i","TDB01s","TDB02s","TDB03s","TDB04s","TDB05s","TDB06s","TDB07s","TDB08s","TDB09s","TDB10s","TDB01r","TDB02r","TDB03r","TDB04r","TDB05r","TDB06r","TDB07r","TDB08r","TDB09r","TDB10r"};
+                get3DautocorrelationDesc(dist3D, dist, numAtoms, mol, res);
+            }
+            
+        }  // end of anonymous namespace
+        
+        void AUTOCORR3D(const ROMol& mol, std::vector<double>& res, int confId) {
+            PRECONDITION(mol.getNumConformers() >= 1, "molecule has no conformers")
+            int numAtoms = mol.getNumAtoms();
+            
+            const Conformer& conf = mol.getConformer(confId);
+            
+            double* dist = MolOps::getDistanceMat(mol, false);
+            double* dist3D = MolOps::get3DDistanceMat(mol, confId);
+            
+            res.clear();
+            res.resize(80);
+            Get3Dauto(dist3D, dist, numAtoms, mol, res);
+        }
+        
+    }  // end of Descriptors namespace
 }  // end of RDKit namespace
