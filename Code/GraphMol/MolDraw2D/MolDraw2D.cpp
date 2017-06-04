bfcfc8067b7e8ed544c1d6fccc44cf1d51235fa4
Greg Landrum <greg.landrum@gmail.com>
2017-06-04 07:45:12 +0200

    fix (for real) the drawing of degree zero atoms

--- Code/GraphMol/MolDraw2D/MolDraw2D.cpp
+++ Code/GraphMol/MolDraw2D/MolDraw2D.cpp
@@ -1602,7 +1602,19 @@ pair<string, MolDraw2D::OrientType> MolDraw2D::getAtomSymbolAndOrientation(
         // put the number as a subscript
         h += string("<sub>") + lexical_cast<string>(num_h) + string("</sub>");
       }
-      if (orient == MolDraw2D::W || !atom.getDegree()) {
+      // last check: degree zero atoms from the last three periods should have
+      // the Hs first
+      if (!atom.getDegree()) {
+        static int HsListedFirstSrc[] = {8, 9, 16, 17, 34, 35, 52, 53, 84, 85};
+        std::vector<int> HsListedFirst(
+            HsListedFirstSrc,
+            HsListedFirstSrc + sizeof(HsListedFirstSrc) / sizeof(int));
+        if (std::find(HsListedFirst.begin(), HsListedFirst.end(),
+                      atom.getAtomicNum()) != HsListedFirst.end()) {
+          orient = MolDraw2D::W;
+        }
+      }
+      if (orient == MolDraw2D::W) {
         preText.push_back(h);
       } else {
         postText.push_back(h);
