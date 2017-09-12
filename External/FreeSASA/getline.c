8d8e1eb62951ffbdea4d9a6e3f2b4f889d9e67f9
Brian Kelley <brian.kelley@novartis.com>
2017-09-12 16:16:59 -0400

    Adds FreeSASA adapter

---
+++ External/FreeSASA/getline.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+
+int
+getline (lineptr, n, stream)
+     char **lineptr;
+     size_t *n;
+     FILE *stream;
+{
+  // do nothing implementation
+  //  we don't use these functionalities of freesasa
+  return 0;
+}
