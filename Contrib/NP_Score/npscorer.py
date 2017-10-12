b941e1024cb2e588c1b13725c4b3b3f1db28ebb1
Axel Pahl <axelpahl@gmx.de>
2017-10-12 09:08:01 +0200

    namedtuple, no rounding

--- Contrib/NP_Score/npscorer.py
+++ Contrib/NP_Score/npscorer.py
@@ -18,6 +18,7 @@ from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors
 import sys, math, gzip, pickle
 import os.path
+from collections import namedtuple
 
 
 def readNPModel(filename=os.path.join(os.path.dirname(__file__), 'publicnp.model.gz')):
@@ -34,7 +35,7 @@ def scoreMolWConfidence(mol, fscore):
   between 0..1 that descibes how many fragments of the tested molecule
   were found in the model data set (1: all fragments were found).
 
-  Returns tuple(NP Likeness, Confidence)"""
+  Returns namedtuple NPLikeness(nplikeness, confidence)"""
 
   if mol is None:
     raise ValueError('invalid molecule')
@@ -50,21 +51,23 @@ def scoreMolWConfidence(mol, fscore):
       score += fscore[bit]
 
   score /= float(mol.GetNumAtoms())
-  confidence = round(float(bits_found / len(bits)), 3)
+  confidence = float(bits_found / len(bits))
 
   # preventing score explosion for exotic molecules
   if score > 4:
     score = 4. + math.log10(score - 4. + 1.)
-  if score < -4:
+  elif score < -4:
     score = -4. - math.log10(-4. - score + 1.)
-  return score, confidence
+  NPLikeness = namedtuple("NPLikeness", "nplikeness,confidence")
+  return NPLikeness(score, confidence)
 
 
 def scoreMol(mol, fscore):
   """Calculates the Natural Product Likeness of a molecule.
 
   Returns the score as float in the range -5..5."""
-  return scoreMolWConfidence(mol, fscore)[0]
+  return scoreMolWConfidence(mol, fscore).nplikeness
+
 
 def processMols(fscore, suppl):
   sys.stderr.write("calculating ...\n")
