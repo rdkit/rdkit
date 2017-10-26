57c6de712bc4340778c373153bcc5f6701269c63
Brian Cole <coleb@deshawresearch.com>
2017-10-26 18:08:43 -0400

    This makes assignStereochemistry cleanIt=True not remove CIS/TRANS bond stereo chemistry that was manually added as described in issue #1614.
    Incorrect CIS/TRANS stereochemistry will still be removed by
    'cleanIt=true' if symmetry is detected. However, this symmetry
    detection doesn't work in more complex pseudo-stereo chemistry cases:
    bond stereo that depends on other bond stereo to break symmetry; and
    bond stereo that depends on other atom stereo centers to break
    symmetry. Test cases for these cases have been added ifdef'd in based
    on USE_NEW_STEREOCHEMISTRY.
    
    However, getting USE_NEW_STEREOCHEMISTRY to work in a copacetic way is
    not trivial, I tried a little bit here to no avail. I'm leaving the
    test cases checked in as they should be useful when we decide to make
    the plunge into using Canon::chiralRankMolAtoms for symmetry detection
    instead of the CIP ranks.
    
    So this fixes at least the glaring issue of STEREOCIS and STEREOTRANS
    being incorrectly removed by 'cleanIt=true' when it is indeed valid
    stereo. The checks made for symmetry are rudimentary, but don't feel
    complete.

--- Code/GraphMol/MolOps.h
+++ Code/GraphMol/MolOps.h
@@ -820,11 +820,19 @@ void detectBondStereochemistry(ROMol &mol, int confId = -1);
 
 //! Assign stereochemistry tags to atoms (i.e. R/S) and bonds (i.e. Z/E)
 /*!
+  Does the CIP stereochemistry assignment for the molecule's atoms
+  (R/S) and double bond (Z/E). Chiral atoms will have a property
+  '_CIPCode' indicating their chiral code.
 
-  \param mol     the molecule of interest
-  \param cleanIt toggles removal of stereo flags from double bonds that can
-                 not have stereochemistry
-  \param force   forces the calculation to be repeated even if it has
+  \param mol     the molecule to use
+  \param cleanIt whether atoms with a chiral specifier that aren't
+                 actually chiral (e.g. atoms with duplicate
+                 substituents or only 2 substituents, etc.) will have
+                 their chiral code set to CHI_UNSPECIFIED. Bonds with
+                 STEREOCIS/STEREOTRANS specified that have duplicate
+                 substituents based upon the CIP atom ranks will be
+                 marked STEREONONE.
+  \param force   causes the calculation to be repeated even if it has
                  already been done
   \param flagPossibleStereoCenters   set the _ChiralityPossible property on
                                      atoms that are possible stereocenters
