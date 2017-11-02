rdkit/rdkit

commit 1027d4469545653180fff9a38dc8224bd50e8b0d

Update IPythonConsole and PandasTools to use new drawing code (#1577)
* backup

* restructure rdkit.Chem.Draw a bit (need more work here);
switch PandasTools over to use that code

* typo

* limit max number of molecules displayed by default (otherwise jupyter can freak out)

* switch to using the new SVG drawing code

* update the PNG version to the new code as well

* get a bit more robust w.r.t. lengths of optional arguments (allow the molecule list to be shorter than the other properties)
rearrange where the preparation for drawing is done for images

* disable kekulization of structures with aromatic query bonds during drawing
This just generates lots of warnings

rdkit/Chem/Draw/IPythonConsole.py
@@ -27,6 +27,7 @@ import copy
 import os
 import json
 import uuid
+import warnings
 import numpy
 try:
   import Image
@@ -99,28 +100,9 @@ def _toPNG(mol):
     highlightAtoms = mol.__sssAtoms
   else:
     highlightAtoms = []
-  try:
-    mol.GetAtomWithIdx(0).GetExplicitValence()
-  except RuntimeError:
-    mol.UpdatePropertyCache(False)
-
-  if not hasattr(rdMolDraw2D, 'MolDraw2DCairo'):
-    mc = copy.deepcopy(mol)
-    try:
-      img = Draw.MolToImage(mc, size=molSize, kekulize=kekulizeStructures,
-                            highlightAtoms=highlightAtoms)
-    except ValueError:  # <- can happen on a kekulization failure
-      mc = copy.deepcopy(mol)
-      img = Draw.MolToImage(mc, size=molSize, kekulize=False, highlightAtoms=highlightAtoms)
-    bio = BytesIO()
-    img.save(bio, format='PNG')
-    return bio.getvalue()
-  else:
-    nmol = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=kekulizeStructures)
-    d2d = rdMolDraw2D.MolDraw2DCairo(molSize[0], molSize[1])
-    d2d.DrawMolecule(nmol, highlightAtoms=highlightAtoms)
-    d2d.FinishDrawing()
-    return d2d.GetDrawingText()
+  kekulize=kekulizeStructures
+  return Draw._moltoimg(mol,molSize,highlightAtoms,"",returnPNG=True,
+                        kekulize=kekulize)
 
 
 def _toSVG(mol):
@@ -130,20 +112,7 @@ def _toSVG(mol):
     highlightAtoms = mol.__sssAtoms
   else:
     highlightAtoms = []
-  try:
-    mol.GetAtomWithIdx(0).GetExplicitValence()
-  except RuntimeError:
-    mol.UpdatePropertyCache(False)
-
-  try:
-    mc = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=kekulizeStructures)
-  except ValueError:  # <- can happen on a kekulization failure
-    mc = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=False)
-  d2d = rdMolDraw2D.MolDraw2DSVG(molSize[0], molSize[1])
-  d2d.DrawMolecule(mc, highlightAtoms=highlightAtoms)
-  d2d.FinishDrawing()
-  svg = d2d.GetDrawingText()
-  return svg.replace("svg:", "")
+  return Draw._moltoSVG(mol,molSize,highlightAtoms,"",kekulize)
 
 
 def _toReactionPNG(rxn):
@@ -190,7 +159,7 @@ def display_pil_image(img):
 _MolsToGridImageSaved = None
 
 
-def ShowMols(mols, **kwargs):
+def ShowMols(mols, maxMols=50, **kwargs):
   global _MolsToGridImageSaved
   if 'useSVG' not in kwargs:
     kwargs['useSVG'] = ipython_useSVG
@@ -198,6 +167,13 @@ def ShowMols(mols, **kwargs):
     fn = _MolsToGridImageSaved
   else:
     fn = Draw.MolsToGridImage
+  if len(mols)>maxMols:
+    warnings.warn("Truncating the list of molecules to be displayed to %d. Change the maxMols value to display more."%(maxMols))
+    mols = mols[:maxMols]
+    for prop in ('legends','highlightAtoms','highlightBonds'):
+      if prop in kwargs:
+        kwargs[prop] = kwargs[prop][:maxMols]
+
   res = fn(mols, **kwargs)
   if kwargs['useSVG']:
     return SVG(res)

rdkit/Chem/Draw/__init__.py
@@ -303,35 +303,75 @@ def MolsToImage(mols, subImgSize=(200, 200), legends=None, **kwargs):
     res.paste(MolToImage(mol, subImgSize, legend=legends[i], **kwargs), (i * subImgSize[0], 0))
   return res
 
+from io import BytesIO
 def _drawerToImage(d2d):
   try:
     import Image
   except ImportError:
     from PIL import Image
-  from io import BytesIO
   sio = BytesIO(d2d.GetDrawingText())
   return Image.open(sio)
 
-def _moltoimg(mol, sz, highlights, legend, **kwargs):
+def _okToKekulizeMol(mol,kekulize):
+  if kekulize:
+    for bond in mol.GetBonds():
+      if bond.GetIsAromatic() and bond.HasQuery():
+        return False
+    return True
+  return kekulize
+
+def _moltoimg(mol, sz, highlights, legend, returnPNG=False, **kwargs):
+  try:
+    mol.GetAtomWithIdx(0).GetExplicitValence()
+  except RuntimeError:
+    mol.UpdatePropertyCache(False)
+
+  kekulize=_okToKekulizeMol(mol,kwargs.get('kekulize', True))
+
+  try:
+    mc = rdMolDraw2D.PrepareMolForDrawing(mol,kekulize=kekulize)
+  except ValueError:  # <- can happen on a kekulization failure
+    mc = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=False)
   if not hasattr(rdMolDraw2D, 'MolDraw2DCairo'):
-    img = MolToImage(mol, sz, legend=legend, highlightAtoms=highlights, **kwargs)
+    img = MolToImage(mc, sz, legend=legend, highlightAtoms=highlights, **kwargs)
+    if returnPNG:
+      bio = BytesIO()
+      img.save(bio, format='PNG')
+      img = bio.getvalue()
   else:
-    nmol = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=kwargs.get('kekulize', True))
     d2d = rdMolDraw2D.MolDraw2DCairo(sz[0], sz[1])
-    d2d.DrawMolecule(nmol, legend=legend, highlightAtoms=highlights)
+    d2d.DrawMolecule(mc, legend=legend, highlightAtoms=highlights)
     d2d.FinishDrawing()
-    img = _drawerToImage(d2d)
+    if returnPNG:
+        img = d2d.GetDrawingText()
+    else:
+        img = _drawerToImage(d2d)
   return img
+
+def _moltoSVG(mol, sz, highlights, legend, kekulize, **kwargs):
+  try:
+    mol.GetAtomWithIdx(0).GetExplicitValence()
+  except RuntimeError:
+    mol.UpdatePropertyCache(False)
+
+  kekulize=_okToKekulizeMol(mol,kekulize)
+
+  try:
+    mc = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=kekulize)
+  except ValueError:  # <- can happen on a kekulization failure
+    mc = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=False)
+  d2d = rdMolDraw2D.MolDraw2DSVG(sz[0], sz[1])
+  d2d.DrawMolecule(mc, legend=legend, highlightAtoms=highlights)
+  d2d.FinishDrawing()
+  svg = d2d.GetDrawingText()
+  return svg.replace("svg:", "")
+
 
 
 def _MolsToGridImage(mols, molsPerRow=3, subImgSize=(200, 200), legends=None,
-                     highlightAtomLists=None, **kwargs):
+                     highlightAtomLists=None, highlightBondLists=None, **kwargs):
   """ returns a PIL Image of the grid
   """
-  try:
-    import Image
-  except ImportError:
-    from PIL import Image
   if legends is None:
     legends = [''] * len(mols)
 
@@ -339,29 +379,39 @@ def _MolsToGridImage(mols, molsPerRow=3, subImgSize=(200, 200), legends=None,
   if len(mols) % molsPerRow:
     nRows += 1
 
-  res = Image.new("RGBA", (molsPerRow * subImgSize[0], nRows * subImgSize[1]), (255, 255, 255, 0))
-  for i, mol in enumerate(mols):
-    row = i // molsPerRow
-    col = i % molsPerRow
-    highlights = None
-    if highlightAtomLists and highlightAtomLists[i]:
-      highlights = highlightAtomLists[i]
-    if mol is not None:
-      img = _moltoimg(mol, subImgSize, highlights, legends[i], **kwargs)
-      res.paste(img, (col * subImgSize[0], row * subImgSize[1]))
+  if not hasattr(rdMolDraw2D, 'MolDraw2DCairo'):
+    try:
+      import Image
+    except ImportError:
+      from PIL import Image
+    res = Image.new("RGBA", (molsPerRow * subImgSize[0], nRows * subImgSize[1]), (255, 255, 255, 0))
+    for i, mol in enumerate(mols):
+      row = i // molsPerRow
+      col = i % molsPerRow
+      highlights = None
+      if highlightAtomLists and highlightAtomLists[i]:
+        highlights = highlightAtomLists[i]
+      if mol is not None:
+        img = _moltoimg(mol, subImgSize, highlights, legends[i], **kwargs)
+        res.paste(img, (col * subImgSize[0], row * subImgSize[1]))
+  else:
+    fullSize = (molsPerRow * subImgSize[0], nRows * subImgSize[1])
+    d2d = rdMolDraw2D.MolDraw2DCairo(fullSize[0],fullSize[1],subImgSize[0], subImgSize[1])
+    d2d.DrawMolecules(mols,legends=legends,highlightAtoms=highlightAtomLists,
+                      highlightBonds=highlightBondLists,**kwargs)
+    d2d.FinishDrawing()
+    res = _drawerToImage(d2d)
+
   return res
 
 
-def _MolsToGridSVG(mols, molsPerRow=3, subImgSize=(200, 200), legends=None, highlightAtomLists=None,
+def _MolsToGridSVG(mols, molsPerRow=3, subImgSize=(200, 200), legends=None,
+                   highlightAtomLists=None, highlightBondLists=None,
                    stripSVGNamespace=True, **kwargs):
   """ returns an SVG of the grid
   """
-  matcher = re.compile(r'^(<.*>\n)(<svg:rect .*</svg\:rect>\n)(.*)</svg\:svg>', re.DOTALL)
   if legends is None:
     legends = [''] * len(mols)
-  hdr = ''
-  ftr = '</svg:svg>'
-  rect = ''
 
   nRows = len(mols) // molsPerRow
   if len(mols) % molsPerRow:
@@ -370,43 +420,37 @@ def _MolsToGridSVG(mols, molsPerRow=3, subImgSize=(200, 200), legends=None, high
   blocks = [''] * (nRows * molsPerRow)
 
   fullSize = (molsPerRow * subImgSize[0], nRows * subImgSize[1])
-  for i, mol in enumerate(mols):
-    highlights = None
-    if highlightAtomLists and highlightAtomLists[i]:
-      highlights = highlightAtomLists[i]
-    if mol is not None:
-      nmol = rdMolDraw2D.PrepareMolForDrawing(mol, kekulize=kwargs.get('kekulize', True))
-      d2d = rdMolDraw2D.MolDraw2DSVG(subImgSize[0], subImgSize[1])
-      d2d.DrawMolecule(nmol, legend=legends[i], highlightAtoms=highlights)
-      d2d.FinishDrawing()
-      txt = d2d.GetDrawingText()
-      h, r, b = matcher.match(txt).groups()
-      if not hdr:
-        hdr = h.replace("width='%dpx' height='%dpx' >" % subImgSize,
-                        "width='%dpx' height='%dpx' >" % fullSize)
-      if not rect:
-        rect = r
-      blocks[i] = b
-  for i, elem in enumerate(blocks):
-    row = i // molsPerRow
-    col = i % molsPerRow
-    elem = rect + elem
-    blocks[i] = '<g transform="translate(%d,%d)" >%s</g>' % (col * subImgSize[0],
-                                                             row * subImgSize[1], elem)
-  res = hdr + '\n'.join(blocks) + ftr
+
+  d2d = rdMolDraw2D.MolDraw2DSVG(fullSize[0],fullSize[1],subImgSize[0], subImgSize[1])
+  d2d.DrawMolecules(mols,legends=legends,highlightAtoms=highlightAtomLists,
+                    highlightBonds=highlightBondLists,**kwargs)
+  d2d.FinishDrawing()
+  res = d2d.GetDrawingText()
   if stripSVGNamespace:
     res = res.replace('svg:', '')
   return res
 
 
 def MolsToGridImage(mols, molsPerRow=3, subImgSize=(200, 200), legends=None,
-                    highlightAtomLists=None, useSVG=False, **kwargs):
+                    highlightAtomLists=None, highlightBondLists=None,
+                    useSVG=False, **kwargs):
+  if legends and len(legends)>len(mols):
+    legends = legends[:len(mols)]
+  if highlightAtomLists and len(highlightAtomLists)>len(mols):
+    highlightAtomLists = highlightAtomLists[:len(mols)]
+  if highlightBondLists and len(highlightBondLists)>len(mols):
+    highlightBondLists = highlightBondLists[:len(mols)]
+
   if useSVG:
     return _MolsToGridSVG(mols, molsPerRow=molsPerRow, subImgSize=subImgSize, legends=legends,
-                          highlightAtomLists=highlightAtomLists, **kwargs)
+                          highlightAtomLists=highlightAtomLists,
+                          highlightBondLists=highlightBondLists,
+                          **kwargs)
   else:
     return _MolsToGridImage(mols, molsPerRow=molsPerRow, subImgSize=subImgSize, legends=legends,
-                            highlightAtomLists=highlightAtomLists, **kwargs)
+                            highlightAtomLists=highlightAtomLists,
+                            highlightBondLists=highlightBondLists,
+                            **kwargs)
 
 
 def _legacyReactionToImage(rxn, subImgSize=(200, 200), **kwargs):

rdkit/Chem/PandasTools.py
@@ -103,6 +103,7 @@ from rdkit import Chem
 from rdkit import DataStructs
 from rdkit.Chem import AllChem
 from rdkit.Chem import Draw
+from rdkit.Chem.Draw import rdMolDraw2D
 from rdkit.Chem import SDWriter
 from rdkit.Chem import rdchem
 from rdkit.Chem.Scaffolds import MurckoScaffold
@@ -185,10 +186,8 @@ def patchPandasHeadMethod(self, n=5):
 
 
 def _get_image(x):
-  """displayhook function for PIL Images, rendered as PNG"""
-  bio = BytesIO()
-  x.save(bio, format='PNG')
-  s = b64encode(bio.getvalue()).decode('ascii')
+  """displayhook function for PNG data"""
+  s = b64encode(x).decode('ascii')
   pd.set_option('display.max_columns', len(s) + 1000)
   pd.set_option('display.max_rows', len(s) + 1000)
   if len(s) + 100 > pd.get_option("display.max_colwidth"):
@@ -253,10 +252,10 @@ def PrintAsBase64PNGString(x, renderer=None):
   else:
     highlightAtoms = []
   if molRepresentation.lower() == 'svg':
-    return _get_svg_image(x, highlightAtoms=highlightAtoms, size=molSize)
+    return _get_svg_image(x, highlightAtoms=highlightAtoms, size=molSize, kekulize=True)
   else:
-    return '<img src="data:image/png;base64,%s" alt="Mol"/>' % _get_image(
-      Draw.MolToImage(x, highlightAtoms=highlightAtoms, size=molSize))
+    data = Draw._moltoimg(x,molSize,highlightAtoms,"",returnPNG=True, kekulize=True)
+    return '<img src="data:image/png;base64,%s" alt="Mol"/>' % _get_image(data)
 
 
 def PrintDefaultMolRep(x):

commit b841ccaa33b2712ac8bf812aeb6530f9b3558b8d

Fixing bug in IPythonConsole SVG rendering introduced in 1027d4469545653180fff9a38dc8224bd50e8b0d

rdkit/Chem/Draw/IPythonConsole.py
@@ -112,6 +112,7 @@ def _toSVG(mol):
     highlightAtoms = mol.__sssAtoms
   else:
     highlightAtoms = []
+  kekulize=kekulizeStructures
   return Draw._moltoSVG(mol,molSize,highlightAtoms,"",kekulize)
 
 