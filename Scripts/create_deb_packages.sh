8b93145ec7d235bfc67b41cbc4fc79a08ae65de9
Francois BERENGER <ligand@free.fr>
2017-11-14 14:45:47 +0900

    more cmake options

--- Scripts/create_deb_packages.sh
+++ Scripts/create_deb_packages.sh
@@ -6,10 +6,29 @@
 
 mkdir -p build
 cd build
-cmake -DRDK_INSTALL_INTREE=OFF -DCMAKE_INSTALL_PREFIX=/usr ../
+cmake -Wno-dev \
+    -DRDK_INSTALL_INTREE=OFF \
+    -DRDK_BUILD_INCHI_SUPPORT=ON \
+    -DRDK_BUILD_AVALON_SUPPORT=ON \
+    -DRDK_BUILD_PYTHON_WRAPPERS=ON \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DRDKit_VERSION=`date +%Y.%m` \
+    ../
 nprocs=`getconf _NPROCESSORS_ONLN`
 make -j $nprocs
 cpack -G DEB
 
-# # to install them
+# # to install all the necessary dependencies
+# sudo apt-get install \
+#      fonts-freefont-ttf \
+#      libboost-python1.58.0 \
+#      libboost-regex1.58.0 \
+#      libboost-system1.58.0 \
+#      libboost-thread1.58.0 \
+#      libc6 \
+#      libgcc1 \
+#      libpython2.7 \
+#      libstdc++6 \
+#      python
+# # to install the freshly built rdkit packages
 # sudo dpkg -i *.deb
