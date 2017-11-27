85ab51b9f2f48a2e92a0152cfa0f945cb6ddcaf6
Greg Landrum <greg.landrum@gmail.com>
2017-11-27 15:09:50 +0100

    Fix github1657 (#1665)
    * re-enable some tests that were commented out (also reformatting)
    
    * re-enable some tests that were commented out (also reformatting)
    
    * Fixes #1657

--- Code/GraphMol/ChemReactions/Enumerate/testEnumerate.cpp
+++ Code/GraphMol/ChemReactions/Enumerate/testEnumerate.cpp
@@ -31,6 +31,7 @@
 //
 
 #include <RDGeneral/utils.h>
+#include <RDGeneral/Exceptions.h>
 #include <GraphMol/RDKitBase.h>
 #include <GraphMol/RDKitQueries.h>
 #include <GraphMol/SmilesParse/SmilesParse.h>
@@ -102,7 +103,7 @@ void testSamplers() {
   randBBs.initialize(rxn, bbs);
   EvenSamplePairsStrategy even;
   even.initialize(rxn, bbs);
-  std::vector<boost::shared_ptr<EnumerationStrategyBase> > enumerators;
+  std::vector<boost::shared_ptr<EnumerationStrategyBase>> enumerators;
   enumerators.push_back(
       boost::shared_ptr<EnumerationStrategyBase>(cart.copy()));
   enumerators.push_back(
@@ -175,36 +176,34 @@ void testEnumerations() {
     EnumerateLibrary en(*rxn, bbs);
     size_t i = 0;
     for (; (bool)en; ++i) {
-      std::vector<std::vector<std::string> > res = en.nextSmiles();
+      std::vector<std::vector<std::string>> res = en.nextSmiles();
       TEST_ASSERT(res.size() == 1);
       TEST_ASSERT(res[0].size() == 1);
       TEST_ASSERT(res[0][0] == smiresults[i]);
-      TEST_ASSERT(i<=6);
+      TEST_ASSERT(i <= 6);
     }
     TEST_ASSERT(i == 6);
     // tests reset
     en.resetState();
     i = 0;
     for (; (bool)en; ++i) {
-      std::vector<std::vector<std::string> > res = en.nextSmiles();
+      std::vector<std::vector<std::string>> res = en.nextSmiles();
       TEST_ASSERT(res.size() == 1);
       TEST_ASSERT(res[0].size() == 1);
       TEST_ASSERT(res[0][0] == smiresults[i]);
-      TEST_ASSERT(i<=6);
+      TEST_ASSERT(i <= 6);
     }
     TEST_ASSERT(i == 6);
-
   }
 
 #ifdef RDK_USE_BOOST_SERIALIZATION
   {
-
     boost::shared_ptr<EnumerateLibrary> en(
         new EnumerateLibrary(*rxn, bbs, RandomSampleStrategy()));
 
-    std::vector<std::vector<std::vector<std::string> > >smir;
+    std::vector<std::vector<std::vector<std::string>>> smir;
     for (size_t j = 0; j < 10; ++j) {
-      std::vector<std::vector<std::string> > smiles = en->nextSmiles();
+      std::vector<std::vector<std::string>> smiles = en->nextSmiles();
       smir.push_back(smiles);
     }
 
@@ -237,41 +236,86 @@ void testEnumerations() {
   delete rxn;
 }
 
-const char *rxndata = "$RXN\nUntitled Document-1\n  ChemDraw10291618492D\n\n  3  1\n$MOL\n\n\n\n  2  1  0  0  0  0  0  0  0  0999 V2000\n    0.4125    0.0000    0.0000 N   0  0  0  0  0  0  0  0  0  3  0  0\n   -0.4125    0.0000    0.0000 R2  0  0  0  0  0  0  0  0  0  2  0  0\n  1  2  1  0        0\nM  END\n$MOL\n\n\n\n  2  1  0  0  0  0  0  0  0  0999 V2000\n   -0.4125    0.0000    0.0000 R1  0  0  0  0  0  0  0  0  0  1  0  0\n    0.4125    0.0000    0.0000 Cl  0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0        0\nM  END\n$MOL\n\n\n\n  2  1  0  0  0  0  0  0  0  0999 V2000\n    0.4125    0.0000    0.0000 N   0  0  0  0  0  0  0  0  0  5  0  0\n   -0.4125    0.0000    0.0000 R4  0  0  0  0  0  0  0  0  0  4  0  0\n  1  2  1  0        0\nM  END\n$MOL\n\n\n\n 14 15  0  0  0  0  0  0  0  0999 V2000\n    0.5072   -0.5166    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    0.5072    0.3084    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.2949   -0.7616    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0\n    1.7817   -0.0880    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.2967    0.5794    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n    1.5558   -1.5443    0.0000 R1  0  0  0  0  0  0  0  0  0  1  0  0\n   -0.2073    0.7208    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.9218    0.3083    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.9217   -0.5167    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -0.2073   -0.9292    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0\n   -1.6362    0.7208    0.0000 N   0  0  0  0  0  0  0  0  0  3  0  0\n    1.5452    1.3661    0.0000 N   0  0  0  0  0  0  0  0  0  5  0  0\n    2.3507    1.5443    0.0000 R4  0  0  0  0  0  0  0  0  0  4  0  0\n   -2.3507    0.3083    0.0000 R2  0  0  0  0  0  0  0  0  0  2  0  0\n  1  2  2  0        0\n  1  3  1  0        0\n  3  4  1  0        0\n  4  5  1  0        0\n  5  2  1  0        0\n  3  6  1  0        0\n  2  7  1  0        0\n  7  8  2  0        0\n  8  9  1  0        0\n  9 10  2  0        0\n 10  1  1  0        0\n  8 11  1  0        0\n 12 13  1  0        0\n 11 14  1  0        0\n 12  5  1  0        0\nM  END\n";
+const char *rxndata =
+    "$RXN\nUntitled Document-1\n  ChemDraw10291618492D\n\n  3  1\n$MOL\n\n\n\n "
+    " 2  1  0  0  0  0  0  0  0  0999 V2000\n    0.4125    0.0000    0.0000 N  "
+    " 0  0  0  0  0  0  0  0  0  3  0  0\n   -0.4125    0.0000    0.0000 R2  0 "
+    " 0  0  0  0  0  0  0  0  2  0  0\n  1  2  1  0        0\nM  "
+    "END\n$MOL\n\n\n\n  2  1  0  0  0  0  0  0  0  0999 V2000\n   -0.4125    "
+    "0.0000    0.0000 R1  0  0  0  0  0  0  0  0  0  1  0  0\n    0.4125    "
+    "0.0000    0.0000 Cl  0  0  0  0  0  0  0  0  0  0  0  0\n  1  2  1  0     "
+    "   0\nM  END\n$MOL\n\n\n\n  2  1  0  0  0  0  0  0  0  0999 V2000\n    "
+    "0.4125    0.0000    0.0000 N   0  0  0  0  0  0  0  0  0  5  0  0\n   "
+    "-0.4125    0.0000    0.0000 R4  0  0  0  0  0  0  0  0  0  4  0  0\n  1  "
+    "2  1  0        0\nM  END\n$MOL\n\n\n\n 14 15  0  0  0  0  0  0  0  0999 "
+    "V2000\n    0.5072   -0.5166    0.0000 C   0  0  0  0  0  0  0  0  0  0  0 "
+    " 0\n    0.5072    0.3084    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n    1.2949   -0.7616    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n    1.7817   -0.0880    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n    1.2967    0.5794    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n    1.5558   -1.5443    0.0000 R1  0  0  0  0  0  0  0  0  0  1  0  "
+    "0\n   -0.2073    0.7208    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n   -0.9218    0.3083    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n   -0.9217   -0.5167    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n   -0.2073   -0.9292    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  "
+    "0\n   -1.6362    0.7208    0.0000 N   0  0  0  0  0  0  0  0  0  3  0  "
+    "0\n    1.5452    1.3661    0.0000 N   0  0  0  0  0  0  0  0  0  5  0  "
+    "0\n    2.3507    1.5443    0.0000 R4  0  0  0  0  0  0  0  0  0  4  0  "
+    "0\n   -2.3507    0.3083    0.0000 R2  0  0  0  0  0  0  0  0  0  2  0  "
+    "0\n  1  2  2  0        0\n  1  3  1  0        0\n  3  4  1  0        0\n  "
+    "4  5  1  0        0\n  5  2  1  0        0\n  3  6  1  0        0\n  2  7 "
+    " 1  0        0\n  7  8  2  0        0\n  8  9  1  0        0\n  9 10  2  "
+    "0        0\n 10  1  1  0        0\n  8 11  1  0        0\n 12 13  1  0    "
+    "    0\n 11 14  1  0        0\n 12  5  1  0        0\nM  END\n";
 
 void testInsaneEnumerations() {
   EnumerationTypes::BBS bbs;
   bbs.resize(3);
 
   ChemicalReaction *rxn2 = RxnBlockToChemicalReaction(rxndata);
-  //RxnOps::sanitizeRxn(*rxn2, MolOps::AdjustQueryParameters());
+  // RxnOps::sanitizeRxn(*rxn2, MolOps::AdjustQueryParameters());
   MatchVectType tvect;
 
   bbs[0].push_back(boost::shared_ptr<ROMol>(SmilesToMol("CCNCC")));
   bbs[0].push_back(boost::shared_ptr<ROMol>(SmilesToMol("NCC")));
-  std::cerr << "0,0 " << (int)SubstructMatch(*bbs[0][0].get(), *rxn2->getReactants()[0].get(), tvect) << std::endl;
-  std::cerr << "0,1 " << (int)SubstructMatch(*bbs[0][1].get(), *rxn2->getReactants()[0].get(), tvect) << std::endl;
+  std::cerr << "0,0 "
+            << (int)SubstructMatch(*bbs[0][0].get(),
+                                   *rxn2->getReactants()[0].get(), tvect)
+            << std::endl;
+  std::cerr << "0,1 "
+            << (int)SubstructMatch(*bbs[0][1].get(),
+                                   *rxn2->getReactants()[0].get(), tvect)
+            << std::endl;
 
   bbs[1].push_back(boost::shared_ptr<ROMol>(SmilesToMol("ClC1CCC1")));
   bbs[1].push_back(boost::shared_ptr<ROMol>(SmilesToMol("ClC1CCC1Cl")));
-  std::cerr << "1,0 " << (int)SubstructMatch(*bbs[1][0].get(), *rxn2->getReactants()[1].get(), tvect) << std::endl;
-  std::cerr << "1,1 " << (int)SubstructMatch(*bbs[1][1].get(), *rxn2->getReactants()[1].get(), tvect) << std::endl;
+  std::cerr << "1,0 "
+            << (int)SubstructMatch(*bbs[1][0].get(),
+                                   *rxn2->getReactants()[1].get(), tvect)
+            << std::endl;
+  std::cerr << "1,1 "
+            << (int)SubstructMatch(*bbs[1][1].get(),
+                                   *rxn2->getReactants()[1].get(), tvect)
+            << std::endl;
 
   bbs[2].push_back(boost::shared_ptr<ROMol>(SmilesToMol("CCNCC")));
   bbs[2].push_back(boost::shared_ptr<ROMol>(SmilesToMol("NCC")));
-  std::cerr << "2,0 " << (int)SubstructMatch(*bbs[2][0].get(), *rxn2->getReactants()[2].get(), tvect) << std::endl;
-  std::cerr << "2,1 " << (int)SubstructMatch(*bbs[2][1].get(), *rxn2->getReactants()[2].get(), tvect) << std::endl;
-
+  std::cerr << "2,0 "
+            << (int)SubstructMatch(*bbs[2][0].get(),
+                                   *rxn2->getReactants()[2].get(), tvect)
+            << std::endl;
+  std::cerr << "2,1 "
+            << (int)SubstructMatch(*bbs[2][1].get(),
+                                   *rxn2->getReactants()[2].get(), tvect)
+            << std::endl;
 
   {
     ChemicalReaction *rxn = RxnBlockToChemicalReaction(rxndata);
     RxnOps::sanitizeRxn(*rxn, MolOps::AdjustQueryParameters());
-    std::cerr << ChemicalReactionToRxnBlock(*rxn) << std::endl;
     EnumerationParams ThereCanBeOnlyOne;
     ThereCanBeOnlyOne.reagentMaxMatchCount = 1;
-    EnumerationTypes::BBS bbs2 = removeNonmatchingReagents(
-        *rxn, bbs,
-        ThereCanBeOnlyOne);
+    EnumerationTypes::BBS bbs2 =
+        removeNonmatchingReagents(*rxn, bbs, ThereCanBeOnlyOne);
     TEST_ASSERT(bbs2[0].size() == 1);
     TEST_ASSERT(bbs2[1].size() == 1);
     TEST_ASSERT(bbs2[2].size() == 1);
@@ -279,6 +323,48 @@ void testInsaneEnumerations() {
     delete rxn;
   }
   delete rxn2;
+}
+
+void testGithub1657() {
+  BOOST_LOG(rdInfoLog) << "-------------------------------------" << std::endl;
+  BOOST_LOG(rdInfoLog) << "Testing github #1657: EnumerateLibrary with "
+                          "initFromString called twice doesn't clear the "
+                          "reaction"
+                       << std::endl;
+  EnumerationTypes::BBS bbs;
+  bbs.resize(2);
+
+  bbs[0].push_back(boost::shared_ptr<ROMol>(SmilesToMol("C=CCN=C=S")));
+  bbs[0].push_back(boost::shared_ptr<ROMol>(SmilesToMol("CC=CCN=C=S")));
+
+  bbs[1].push_back(boost::shared_ptr<ROMol>(SmilesToMol("NCc1ncc(Cl)cc1Br")));
+  bbs[1].push_back(boost::shared_ptr<ROMol>(SmilesToMol("NCCc1ncc(Cl)cc1Br")));
+  bbs[1].push_back(boost::shared_ptr<ROMol>(SmilesToMol("NCCCc1ncc(Cl)cc1Br")));
+
+  ChemicalReaction *rxn = RxnSmartsToChemicalReaction(
+      "[N;$(N-[#6]):3]=[C;$(C=S):1].[N;$(N[#6]);!$(N=*);!$([N-]);!$(N#*);"
+      "!$([ND3]);!$([ND4]);!$(N[O,N]);!$(N[C,S]=[S,O,N]):2]>>[N:3]-[C:1]-[N+0:"
+      "2]");
+
+  {  // we'll also test that initFromString() works at all
+    EnumerateLibrary en1(*rxn, bbs);
+    std::stringstream sstr;
+    en1.toStream(sstr);
+    EnumerateLibrary en2;
+    en2.initFromString(sstr.str());
+
+    EnumerateLibrary en3;
+    en3.initFromString(sstr.str());
+    bool ok = false;
+    try {
+      en3.initFromString(sstr.str());
+    } catch (const ValueErrorException &) {
+      ok = true;
+    }
+    TEST_ASSERT(ok);
+  }
+  delete rxn;
+  BOOST_LOG(rdInfoLog) << "\tdone" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -289,10 +375,11 @@ int main(int argc, char *argv[]) {
       doLong = true;
     }
   }
-#if 0
-    testSamplers();
-    testEvenSamplers();
-    testEnumerations();
-  //testInsaneEnumerations();
+#if 1
+  testSamplers();
+  testEvenSamplers();
+  testEnumerations();
+  testInsaneEnumerations();
 #endif
+  testGithub1657();
 }
