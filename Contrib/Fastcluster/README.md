0df31bd71cda3d907cd5ddf7026dcfe92f71a7a0
iwatobipen <seritaka@gmail.com>
2017-12-10 10:12:15 +0900

    readme

---
+++ Contrib/Fastcluster/README.md
@@ -0,0 +1,20 @@
+============
+FastCluster
+============
+
+This is simple work flow for clustering molecules
+==================================================
+
+Author: iwatobipen
+===================
+
+Date: 201712010
+================
+
+Requirements
+============
+
+- python3.x
+- bayon https://github.com/fujimizu/bayon
+- rdkit
+
--- Contrib/Fastcluster/fastcluster.py
+++ Contrib/Fastcluster/fastcluster.py
@@ -1,3 +1,9 @@
+# This script perform fast clustering of SMILES
+# Clustering method is repeated bi section, the method looks like -k-means.
+# To use following script, user need to install bayon at first.
+# Iput format: Tab separeted smiles srings, SMILES \t molID \n ...
+# Please see more dietails in README.
+
 import argparse
 import subprocess
 import pickle
@@ -10,7 +16,7 @@ parser.add_argument( "input" )
 parser.add_argument( "n", help = "the number of clusters" )
 #parser.add_argument( "-l", help = "limit value of cluster bisection" )
 #parser.add_argument( "-p", help = "output similarity points" )
-parser.add_argument( "--output", default = "cluster.tsv" )
+parser.add_argument( "--output", default = "clustered.tsv" )
 parser.add_argument( "-c", help = "filename of centroid", default = "centroid.tsv" )
 
 def smi2fp( molid, smiles ):
@@ -24,14 +30,13 @@ def smi2fp( molid, smiles ):
 
 
 if __name__ == "__main__":
-    currentdir = os.getcwd()
     args = parser.parse_args()
-    inf = open( args.input, "r" )
+    inputf = open( args.input, "r" )
     n = args.n
     c = args.c
     output = args.output
     tempf = open( "fp.tsv", "w" )
-    for line in inf:
+    for line in inputf:
         line = line.rstrip().split( "\t" )
         tempf.write( smi2fp( line[0], line[1] ))
     tempf.close()
@@ -46,8 +51,7 @@ if __name__ == "__main__":
         for i in range( 1, len( line )-1, 2 ) :
             molid = line[ i ]
             point = line[ i + 1 ]
-            #print(itr)
-            parsefile.write("{}\t{}\tCLS_{}\n".format( molid, point, cluster_id ))
+            parsefile.write("{}\t{}\tCLS_ID_{}\n".format( molid, point, cluster_id ))
     parsefile.close()
 
 
