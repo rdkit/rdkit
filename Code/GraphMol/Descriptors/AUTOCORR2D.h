ab8fc3008177419b3bd61aee83778de11c222f27
Greg Landrum <greg.landrum@gmail.com>
2018-01-04 16:34:47 +0000

    Fixes github #1702 (#1706)
    * Fixes #1702
    
    * more on #1702

--- Code/GraphMol/Descriptors/AUTOCORR2D.h
+++ Code/GraphMol/Descriptors/AUTOCORR2D.h
@@ -36,7 +36,6 @@
 #ifndef AUTOCORR2DRDKIT_H_SEPT2016
 #define AUTOCORR2DRDKIT_H_SEPT2016
 
-#ifdef RDK_BUILD_DESCRIPTORS3D
 namespace RDKit {
 class ROMol;
 namespace Descriptors {
@@ -44,5 +43,4 @@ const std::string AUTOCORR2DVersion = "1.0.0";
 void AUTOCORR2D(const ROMol &, std::vector<double> &res);
 }
 }
-#endif
 #endif
