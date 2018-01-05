1d40c55ae451b004989300fc88a142ecd01338bd
iwatobipen <seritaka@gmail.com>
2018-01-05 22:04:36 +0900

    remove gitignore and revised sdf2smi.py with SmilesMolWriter

--- Contrib/Fastcluster/testdata/sdf2smi.py
+++ Contrib/Fastcluster/testdata/sdf2smi.py
@@ -1,10 +1,14 @@
 import argparse
 from rdkit import Chem
-sdf = Chem.SDMolSupplier("cdk2.sdf")
-f = open("cdk2.smi","w")
+from rdkit.Chem.rdmolfiles import SmilesWriter
+
+parser = argparse.ArgumentParser()
+parser.add_argument('inputfile', help="sdf filename for convert to smiles")
+args = parser.parse_args()
+sdf = Chem.SDMolSupplier( args.inputfile )
+writer = SmilesWriter("converted.smi")
+
 for mol in sdf:
-    name = mol.GetProp("_Name")
-    smi = Chem.MolToSmiles( mol )
-    f.write("{}\t{}\n".format(name,smi))
-f.close()
+    writer.write( mol )
+writer.close()
 
