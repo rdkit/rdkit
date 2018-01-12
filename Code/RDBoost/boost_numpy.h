11a4b52e391bd821553ed560d46280604adc9bd1
Greg Landrum <greg.landrum@gmail.com>
2018-01-12 17:12:53 +0100

    Fix a bug reported by Roger S (#1720)

--- Code/RDBoost/boost_numpy.h
+++ Code/RDBoost/boost_numpy.h
@@ -2,8 +2,8 @@
 // Boost python numeric removed in Boost 1.65+
 #if BOOST_VERSION < 106500
 #include <boost/python/numeric.hpp>
-typedef typename boost::python::numeric::array NumpyArrayType;
+typedef boost::python::numeric::array NumpyArrayType;
 #else
 #include <boost/python/numpy.hpp>
-typedef typename boost::python::numpy::ndarray NumpyArrayType;
+typedef boost::python::numpy::ndarray NumpyArrayType;
 #endif
