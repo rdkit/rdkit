ea3bb346b0f7b7ddb2734e56128b00f064c7f8f7
ptosco <paolo.tosco.mail@gmail.com>
2018-05-08 15:00:22 +0100

    - insert spaces between multiple '>' symbols in function prototypes to avoid breaking ancient SWIG versions

--- Code/GraphMol/ChemReactions/Reaction.h
+++ Code/GraphMol/ChemReactions/Reaction.h
@@ -429,7 +429,7 @@ VECT_INT_VECT getReactingAtoms(const ChemicalReaction &rxn,
 void addRecursiveQueriesToReaction(
     ChemicalReaction &rxn, const std::map<std::string, ROMOL_SPTR> &queries,
     const std::string &propName,
-    std::vector<std::vector<std::pair<unsigned int, std::string>>>
+    std::vector<std::vector<std::pair<unsigned int, std::string> > >
         *reactantLabels = NULL);
 
 }  // end of RDKit namespace
