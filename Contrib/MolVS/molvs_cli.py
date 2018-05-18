14499a4ef08d0d75eb9f9b7472ce225844bc7eb6
leung@stats <susan.leung@st-hildas.ox.ac.uk>
2018-05-18 12:05:32 +0100

    adapted and renamed molvs/cli.py to work within $RDBASE/Contrib/MolVS/

--- Contrib/MolVS/molvs_cli.py
+++ Contrib/MolVS/molvs_cli.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 molvs.cli
 ~~~~~~~~~
@@ -7,17 +6,16 @@ This module contains a command line interface for standardization.
 
 :copyright: Copyright 2016 by Matt Swain.
 :license: MIT, see LICENSE file for more details.
+
+*Adapted for purposes of integration of MolVS into RDKit
 """
 
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
 import argparse
 import logging
-from rdkit import Chem
 import sys
 
-from molvs import Standardizer, Validator
+from rdkit import Chem
+from rdkit.Chem.MolStandardize import Standardizer, Validator
 
 
 log = logging.getLogger(__name__)
@@ -31,39 +29,6 @@ class MolvsParser(argparse.ArgumentParser):
     def error(self, message):
         sys.stderr.write('Error: %s\n\n'.encode() % message)
         self.print_help()
-        sys.exit(2)
-
-
-def main():
-    """Main function for molvs command line interface."""
-
-    # Root options
-    parser = MolvsParser(epilog='use "molvs <command> -h" to show help for a specific command')
-    subparsers = parser.add_subparsers(title='Available commands')
-
-    # Options common to all commands
-
-    common_parser = MolvsParser(add_help=False)
-    common_parser.add_argument('infile', nargs='?', help='input filename', type=argparse.FileType('r'), default=sys.stdin)
-    common_parser.add_argument('-i', '--intype', help='input filetype', choices=FILETYPES)
-    common_parser.add_argument('-:', '--smiles', help='input SMILES instead of file', metavar='<smiles>')
-    common_parser.add_argument('-O', '--outfile', help='output filename', type=argparse.FileType('w'), default=sys.stdout, metavar='<outfile>')
-
-    # Standardize options
-    standardize_parser = subparsers.add_parser('standardize', help='standardize a molecule', parents=[common_parser])
-    standardize_parser.add_argument('-o', '--outtype', help='output filetype', choices=FILETYPES)
-    standardize_parser.set_defaults(func=standardize_main)
-
-    # Validate options
-    validate_parser = subparsers.add_parser('validate', help='validate a molecule', parents=[common_parser])
-    validate_parser.set_defaults(func=validate_main)
-
-    args = parser.parse_args()
-    try:
-        args.func(args)
-    except Exception as e:
-        sys.stderr.write('Error: %s\n\n'.encode() % e.message)
-        parser.print_help()
         sys.exit(2)
 
 
@@ -103,4 +68,38 @@ def validate_main(args):
     for log in logs:
         args.outfile.write(log)
         args.outfile.write('\n')
+
+
+if __name__=='__main__':
+    """Main function for molvs command line interface."""
+
+    # Root options
+#    parser = MolvsParser(epilog='use "molvs <command> -h" to show help for a specific command')
+    parser = MolvsParser(usage="usage: python cli.py [-h] {standardize,validate} ...")
+    subparsers = parser.add_subparsers(title='Available commands')
+
+    # Options common to all commands
+
+    common_parser = MolvsParser(add_help=False)
+    common_parser.add_argument('infile', nargs='?', help='input filename', type=argparse.FileType('r'), default=sys.stdin)
+    common_parser.add_argument('-i', '--intype', help='input filetype', choices=FILETYPES)
+    common_parser.add_argument('-:', '--smiles', help='input SMILES instead of file', metavar='<smiles>')
+    common_parser.add_argument('-O', '--outfile', help='output filename', type=argparse.FileType('w'), default=sys.stdout, metavar='<outfile>')
+
+    # Standardize options
+    standardize_parser = subparsers.add_parser('standardize', help='standardize a molecule', parents=[common_parser])
+    standardize_parser.add_argument('-o', '--outtype', help='output filetype', choices=FILETYPES)
+    standardize_parser.set_defaults(func=standardize_main)
+
+    # Validate options
+    validate_parser = subparsers.add_parser('validate', help='validate a molecule', parents=[common_parser])
+    validate_parser.set_defaults(func=validate_main)
+
+    args = parser.parse_args()
+    try:
+        args.func(args)
+    except Exception as e:
+        sys.stderr.write('Error: %s\n\n'.encode() % e.message)
+        parser.print_help()
+        sys.exit(2)
 
