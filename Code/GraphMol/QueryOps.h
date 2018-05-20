6f2ba4409e2c7b4da887a949ac2f7a6f7cbe9e65
Brian Kelley <brian.kelley@novartis.com>
2018-05-20 10:09:21 -0400

    Really revert SLN change

--- Code/GraphMol/QueryOps.h
+++ Code/GraphMol/QueryOps.h
@@ -78,10 +78,10 @@ static inline int queryAtomExplicitDegree(Atom const *at) {
 static inline int queryAtomTotalDegree(Atom const *at) {
   return at->getTotalDegree();
 };
-static inline int queryAtomHeavyAtomDegree(Atom const *at) {
+static inline int queryAtomNonHydrogenDegree(Atom const *at) {
   return at->getTotalDegree() - at->getTotalNumHs(true);
 }
-static inline int queryAtomNonHydrogenDegree(Atom const *at) {
+static inline int queryAtomHeavyAtomDegree(Atom const *at) {
   int heavyDegree = 0;
   ROMol::ADJ_ITER nbrIdx, endNbrs;
   boost::tie(nbrIdx, endNbrs) = at->getOwningMol().getAtomNeighbors(at);
