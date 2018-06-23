20e7bfdc1055efd57afedadd8ded8b7e1575d6af
Greg Landrum <greg.landrum@gmail.com>
2018-06-23 04:57:21 +0200

    add clarifying statement about license

--- Contrib/Fastcluster/fastcluster.py
+++ Contrib/Fastcluster/fastcluster.py
@@ -1,3 +1,9 @@
+#  Original Author: iwatobipen
+#
+#  This file is part of the RDKit.
+#  The contents are covered by the terms of the BSD license
+#  which is included in the file license.txt, found at the root
+#  of the RDKit source tree.
 """
 This script performs fast clustering of SMILES
 
