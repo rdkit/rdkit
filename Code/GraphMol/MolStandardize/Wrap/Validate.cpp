227a12d2b9b59c8a6dd12a45e17b8f797badaaf6
Greg Landrum <greg.landrum@gmail.com>
2018-08-02 16:33:22 +0200

    move the validate code into the rdMolStandardize module

--- Code/GraphMol/MolStandardize/Wrap/Validate.cpp
+++ Code/GraphMol/MolStandardize/Wrap/Validate.cpp
@@ -34,62 +34,62 @@ MolStandardize::MolVSValidation *getMolVSValidation(
 }
 
 }  // namespace
+struct validate_wrapper {
+  static void wrap() {
+    std::string docString = "";
 
-BOOST_PYTHON_MODULE(Validate) {
-  python::scope().attr("__doc__") =
-      "Module containing tools to perform all validations";
+    python::class_<MolStandardize::RDKitValidation, boost::noncopyable>(
+        "RDKitValidation", python::init<>())
+        .def("validate", rdkitValidate,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures") = false),
+             "");
 
-  std::string docString = "";
+    python::class_<MolStandardize::MolVSValidations, boost::noncopyable>(
+        "MolVSValidations", python::no_init)
+        .def("run", &MolStandardize::MolVSValidations::run,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures"), python::arg("errors")),
+             "");
 
-  python::class_<MolStandardize::RDKitValidation, boost::noncopyable>(
-      "RDKitValidation", python::init<>())
-      .def("validate", rdkitValidate,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures") = false),
-           "");
+    python::class_<MolStandardize::NoAtomValidation,
+                   python::bases<MolStandardize::MolVSValidations>>(
+        "NoAtomValidation", python::init<>())
+        .def("run", &MolStandardize::NoAtomValidation::run,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures"), python::arg("errors")),
+             "");
 
-  python::class_<MolStandardize::MolVSValidations, boost::noncopyable>(
-      "MolVSValidations", python::no_init)
-      .def("run", &MolStandardize::MolVSValidations::run,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures"), python::arg("errors")),
-           "");
+    python::class_<MolStandardize::FragmentValidation,
+                   python::bases<MolStandardize::MolVSValidations>>(
+        "FragmentValidation", python::init<>())
+        .def("run", &MolStandardize::FragmentValidation::run,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures"), python::arg("errors")),
+             "");
+    python::class_<MolStandardize::NeutralValidation,
+                   python::bases<MolStandardize::MolVSValidations>>(
+        "NeutralValidation", python::init<>())
+        .def("run", &MolStandardize::NeutralValidation::run,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures"), python::arg("errors")),
+             "");
+    python::class_<MolStandardize::IsotopeValidation,
+                   python::bases<MolStandardize::MolVSValidations>>(
+        "IsotopeValidation", python::init<>())
+        .def("run", &MolStandardize::IsotopeValidation::run,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures"), python::arg("errors")),
+             "");
 
-  python::class_<MolStandardize::NoAtomValidation,
-                 python::bases<MolStandardize::MolVSValidations>>(
-      "NoAtomValidation", python::init<>())
-      .def("run", &MolStandardize::NoAtomValidation::run,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures"), python::arg("errors")),
-           "");
+    python::class_<MolStandardize::MolVSValidation, boost::noncopyable>(
+        "MolVSValidation", python::init<>())
+        .def("__init__", python::make_constructor(&getMolVSValidation))
+        .def("Validate", &MolStandardize::MolVSValidation::validate,
+             (python::arg("self"), python::arg("mol"),
+              python::arg("reportAllFailures") = false),
+             "");
+  }
+};
 
-  python::class_<MolStandardize::FragmentValidation,
-                 python::bases<MolStandardize::MolVSValidations>>(
-      "FragmentValidation", python::init<>())
-      .def("run", &MolStandardize::FragmentValidation::run,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures"), python::arg("errors")),
-           "");
-  python::class_<MolStandardize::NeutralValidation,
-                 python::bases<MolStandardize::MolVSValidations>>(
-      "NeutralValidation", python::init<>())
-      .def("run", &MolStandardize::NeutralValidation::run,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures"), python::arg("errors")),
-           "");
-  python::class_<MolStandardize::IsotopeValidation,
-                 python::bases<MolStandardize::MolVSValidations>>(
-      "IsotopeValidation", python::init<>())
-      .def("run", &MolStandardize::IsotopeValidation::run,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures"), python::arg("errors")),
-           "");
-
-  python::class_<MolStandardize::MolVSValidation, boost::noncopyable>(
-      "MolVSValidation", python::init<>())
-      .def("__init__", python::make_constructor(&getMolVSValidation))
-      .def("Validate", &MolStandardize::MolVSValidation::validate,
-           (python::arg("self"), python::arg("mol"),
-            python::arg("reportAllFailures") = false),
-           "");
-}
+void wrap_validate() { validate_wrapper::wrap(); }
--- Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp
+++ Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp
@@ -27,24 +27,27 @@ RDKit::ROMol *cleanupHelper(const RDKit::ROMol *mol, python::object params) {
       *static_cast<const RDKit::RWMol *>(mol), *ps));
 }
 
-RDKit::ROMol *fragmentParentHelper(const RDKit::ROMol *mol, python::object params, bool skip_standardize) {
-	const RDKit::MolStandardize::CleanupParameters *ps = 
-			&RDKit::MolStandardize::defaultCleanupParameters;
-	if (params) {
-		ps = python::extract<RDKit::MolStandardize::CleanupParameters *>(params);
-	}
-	return static_cast<RDKit::ROMol *>(RDKit::MolStandardize::fragmentParent(
-			*static_cast<const RDKit::RWMol *>(mol), *ps, skip_standardize));
+RDKit::ROMol *fragmentParentHelper(const RDKit::ROMol *mol,
+                                   python::object params,
+                                   bool skip_standardize) {
+  const RDKit::MolStandardize::CleanupParameters *ps =
+      &RDKit::MolStandardize::defaultCleanupParameters;
+  if (params) {
+    ps = python::extract<RDKit::MolStandardize::CleanupParameters *>(params);
+  }
+  return static_cast<RDKit::ROMol *>(RDKit::MolStandardize::fragmentParent(
+      *static_cast<const RDKit::RWMol *>(mol), *ps, skip_standardize));
 }
 
-RDKit::ROMol *chargeParentHelper(const RDKit::ROMol *mol, python::object params, bool skip_standardize) {
-	const RDKit::MolStandardize::CleanupParameters *ps = 
-			&RDKit::MolStandardize::defaultCleanupParameters;
-	if (params) {
-		ps = python::extract<RDKit::MolStandardize::CleanupParameters *>(params);
-	}
-	return static_cast<RDKit::ROMol *>(RDKit::MolStandardize::chargeParent(
-			*static_cast<const RDKit::RWMol *>(mol), *ps, skip_standardize));
+RDKit::ROMol *chargeParentHelper(const RDKit::ROMol *mol, python::object params,
+                                 bool skip_standardize) {
+  const RDKit::MolStandardize::CleanupParameters *ps =
+      &RDKit::MolStandardize::defaultCleanupParameters;
+  if (params) {
+    ps = python::extract<RDKit::MolStandardize::CleanupParameters *>(params);
+  }
+  return static_cast<RDKit::ROMol *>(RDKit::MolStandardize::chargeParent(
+      *static_cast<const RDKit::RWMol *>(mol), *ps, skip_standardize));
 }
 
 RDKit::ROMol *normalizeHelper(const RDKit::ROMol *mol, python::object params) {
@@ -68,6 +71,8 @@ RDKit::ROMol *reionizeHelper(const RDKit::ROMol *mol, python::object params) {
 }
 
 }  // namespace
+
+void wrap_validate();
 
 BOOST_PYTHON_MODULE(rdMolStandardize) {
   python::scope().attr("__doc__") =
@@ -108,22 +113,26 @@ BOOST_PYTHON_MODULE(rdMolStandardize) {
               (python::arg("smiles")), docString.c_str());
   docString = "";
   python::def("FragmentParent", fragmentParentHelper,
-              (python::arg("mol"), python::arg("params") = python::object(), 
-							 python::arg("skipStandardize") = false ), docString.c_str(),
-							 python::return_value_policy<python::manage_new_object>());
-	docString = "";
-	python::def("ChargeParent", chargeParentHelper,
-							(python::arg("mol"), python::arg("params") = python::object(),
-							 python::arg("skipStandardize") = false ), docString.c_str(),
-							 python::return_value_policy<python::manage_new_object>());
+              (python::arg("mol"), python::arg("params") = python::object(),
+               python::arg("skipStandardize") = false),
+              docString.c_str(),
+              python::return_value_policy<python::manage_new_object>());
+  docString = "";
+  python::def("ChargeParent", chargeParentHelper,
+              (python::arg("mol"), python::arg("params") = python::object(),
+               python::arg("skipStandardize") = false),
+              docString.c_str(),
+              python::return_value_policy<python::manage_new_object>());
   docString = "";
   python::def("Normalize", normalizeHelper,
               (python::arg("mol"), python::arg("params") = python::object()),
               docString.c_str(),
               python::return_value_policy<python::manage_new_object>());
-	docString = "";
+  docString = "";
   python::def("Reionize", reionizeHelper,
               (python::arg("mol"), python::arg("params") = python::object()),
               docString.c_str(),
               python::return_value_policy<python::manage_new_object>());
+
+  wrap_validate();
 }
