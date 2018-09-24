0d5e473fa56ec5e221952f42a018213186a1da42
greg landrum <greg.landrum@gmail.com>
2018-09-24 16:40:59 +0200

    enable testing of the java code

--- Code/JavaWrappers/gmwrapper/src-test/org/RDKit/MolStandardizeTest.java
+++ Code/JavaWrappers/gmwrapper/src-test/org/RDKit/MolStandardizeTest.java
@@ -9,11 +9,12 @@ import org.junit.*;
 /**
  * JUnit tests for MolStandardize wrappers.
  */
-public class MolStandardizeTest {
+ public class MolStandardizeTest extends GraphMolTest {
 	@Test
 	public void testStandardize1() {
 		assertEquals("fail", RDKFuncs.standardizeSmiles("[Na]OC(=O)c1ccccc1"),"O=C([O-])c1ccccc1.[Na+]");
 	}
+	public static void main(String args[]) {
+						org.junit.runner.JUnitCore.main("org.RDKit.MolStandardizeTest");
+	}
 }
-
-
