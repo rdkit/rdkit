49e2a58ff1296854e4411ee922c84f61c843874e
Greg Landrum <greg.landrum@gmail.com>
2018-10-07 04:50:11 +0200

    add a couple more tests; changes in response to review

--- Code/GraphMol/FileParsers/catch_tests.cpp
+++ Code/GraphMol/FileParsers/catch_tests.cpp
@@ -1,3 +1,12 @@
+//
+//  Copyright (C) 2018 Greg Landrum
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
                            // this in one cpp file
 #include "RDBoost/test.h"
@@ -80,8 +89,12 @@ TEST_CASE(
     CHECK(mol->getBondWithIdx(0)->getBondType() == Bond::SINGLE);
     CHECK(
         mol->getBondWithIdx(0)->hasProp(common_properties::_MolFileBondEndPts));
+    CHECK(mol->getBondWithIdx(0)->getProp<std::string>(
+              common_properties::_MolFileBondEndPts) == "(3 5 4 3)");
     CHECK(
         mol->getBondWithIdx(0)->hasProp(common_properties::_MolFileBondAttach));
+    CHECK(mol->getBondWithIdx(0)->getProp<std::string>(
+              common_properties::_MolFileBondAttach) == "ANY");
     CHECK(mol->getBondWithIdx(1)->getBondType() == Bond::AROMATIC);
   }
 }
