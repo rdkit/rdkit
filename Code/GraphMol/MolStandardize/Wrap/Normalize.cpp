7b34127250519d25d3026b2b15ef8a10349f7a6c
Greg Landrum <greg.landrum@gmail.com>
2018-10-19 06:31:24 +0200

    python doc updates

--- Code/GraphMol/MolStandardize/Wrap/Normalize.cpp
+++ Code/GraphMol/MolStandardize/Wrap/Normalize.cpp
@@ -17,30 +17,27 @@ using namespace RDKit;
 
 namespace {
 
-ROMol* normalizeHelper(MolStandardize::Normalizer &self, const ROMol &mol) {
-	return self.normalize(mol);
+ROMol *normalizeHelper(MolStandardize::Normalizer &self, const ROMol &mol) {
+  return self.normalize(mol);
 }
 
-} // namespace
+}  // namespace
 
 struct normalize_wrapper {
-	static void wrap() {
-	python::scope().attr("__doc__") = 
-					"Module containing tools for normalizing molecules defined by SMARTS patterns";
+  static void wrap() {
+    python::scope().attr("__doc__") =
+        "Module containing tools for normalizing molecules defined by SMARTS "
+        "patterns";
 
-	std::string docString = "";
+    std::string docString = "";
 
-	python::class_<MolStandardize::Normalizer, boost::noncopyable>(
-					"Normalizer", python::init<>())
-					.def(python::init<std::string, unsigned int>())
-					.def("normalize", &normalizeHelper,
-								(python::arg("self"), python::arg("mol")),
-								"",
-								python::return_value_policy<python::manage_new_object>())
-					;
-	}
+    python::class_<MolStandardize::Normalizer, boost::noncopyable>(
+        "Normalizer", python::init<>())
+        .def(python::init<std::string, unsigned int>())
+        .def("normalize", &normalizeHelper,
+             (python::arg("self"), python::arg("mol")), "",
+             python::return_value_policy<python::manage_new_object>());
+  }
 };
 
 void wrap_normalize() { normalize_wrapper::wrap(); }
-
-
