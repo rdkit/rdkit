257ac1c7c97f234a047886f4b3b2b0ad0e381a35
Francois Berenger <unixjunkie@sdf.org>
2018-11-07 12:09:11 +0900

    corrected script after many tests...

--- Scripts/linux_python3_dev_build.sh
+++ Scripts/linux_python3_dev_build.sh
@@ -3,38 +3,36 @@
 #set -x # DEBUG
 
 # developper build for Linux with Python-3 and conda
+# you must have conda in your PATH
+# go to https://www.anaconda.com/download/#linux if you don't
+# you may need to source ~/.bashrc after the conda install
+# so that the default python is the one installed by conda
 
-## some system-wide dependencies that may be needed
-# sudo apt install gcc make
-
-conda install -y \
-      cmake cairo numpy pillow pandas eigen pkg-config \
-      boost-cpp boost py-boost gxx_linux-64
-
-PYTHON_VERSION=`python -c 'import sys; print("%d.%d" % (sys.version_info[0], sys.version_info[1]))'`
-
-echo "BEFORE SCRIPT"
+# configuration
 export RDBASE=`pwd`
 echo $RDBASE
-export PYTHONPATH=${RDBASE}
-export LD_LIBRARY_PATH=${RDBASE}/lib
+export PYTHONPATH=$RDBASE
 
 export PYTHON=`which python`
 echo $PYTHON
 export PY_PREFIX=`$PYTHON -c "import sys; print(sys.prefix)"`
 echo $PY_PREFIX
-export PY_SP_DIR=$PY_PREFIX/lib/python${PYTHON_VERSION}/site-packages
+export LD_LIBRARY_PATH=${RDBASE}/lib:${PY_PREFIX}/lib
+export PY_SP_DIR=${PY_PREFIX}/lib/python${PYTHON_VERSION}/site-packages
 echo $PY_SP_DIR
 
+PYTHON_VERSION=`$PYTHON -c 'import sys; print("%d.%d" % (sys.version_info[0], sys.version_info[1]))'`
+# some system-wide dependencies that may be needed
+sudo apt install gcc make python${PYTHON_VERSION}-dev
+
+conda install -y \
+      cmake cairo numpy pillow pandas eigen pkg-config \
+      boost-cpp boost py-boost gxx_linux-64
+
+# build
 mkdir -p build
 cd build
-
-echo "SCRIPT"
-cd $RDBASE
-mkdir build
-cd build
-export REGEX_EXTRA="-DRDK_USE_BOOST_REGEX=ON"
-cmake $REGEX_EXTRA \
+cmake -D RDK_USE_BOOST_REGEX=ON \
       -D PYTHON_EXECUTABLE=$PYTHON \
       -D BOOST_ROOT=$PY_PREFIX \
       -D Boost_NO_SYSTEM_PATHS=ON \
@@ -46,9 +44,14 @@ cmake $REGEX_EXTRA \
       -D RDK_BUILD_SWIG_WRAPPERS=OFF \
       -D RDK_SWIG_STATIC=OFF \
       -D RDK_BUILD_PYTHON_WRAPPERS=ON \
-      -D RDK_BUILD_FREESASA_SUPPORT=ON ../
+      -D RDK_BUILD_FREESASA_SUPPORT=ON \
+      -D PYTHON_NUMPY_INCLUDE_PATH=${PY_SP_DIR}/numpy/core/include \
+      ../
 nprocs=`getconf _NPROCESSORS_ONLN`
-make -j ${nprocs}
+make -j $nprocs
+
+# install
 make install
-LD_LIBRARY_PATH="$PY_PREFIX/lib:$PREFIX/lib;$SRC_DIR/lib;$LD_LIBRARY_PATH" \
-ctest -j ${nprocs} --output-on-failure
+
+# test
+ctest -j $nprocs --output-on-failure
