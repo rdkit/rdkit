f0cc4bffbfe018c434776257b0ec49041eb13ade
Susan Leung <37052711+susanhleung@users.noreply.github.com>
2018-11-08 13:42:10 +0000

    ShapeTverskyIndex python function (#2156)
    * trying to add shape tversky
    
    * alpha and beta no defaults, added more tverskyIndex tests to testGrid
    
    * added python function ShapeTverskyIndex to call tverskyMolShapes
    
    * rebasing
    
    * add python test for ShapeTverskyIndex
    
    * changing to aassertAlmostEqual in testShapeHelpers

--- Code/GraphMol/ShapeHelpers/Wrap/testShapeHelpers.py
+++ Code/GraphMol/ShapeHelpers/Wrap/testShapeHelpers.py
@@ -12,10 +12,6 @@ from rdkit.Chem import rdShapeHelpers as rdshp
 from rdkit.Chem import rdMolTransforms as rdmt
 
 
-def feq(v1, v2, tol=1.0e-4):
-  return abs(v1 - v2) < tol
-
-
 class TestCase(unittest.TestCase):
 
   def setUp(self):
@@ -37,8 +33,8 @@ class TestCase(unittest.TestCase):
     dims, offset = rdshp.ComputeConfDimsAndOffset(m.GetConformer(), trans=trans)
     dims -= dims1
     offset -= offset1
-    self.assertTrue(feq(dims.Length(), 0.0))
-    self.assertTrue(feq(offset.Length(), 0.0))
+    self.assertAlmostEqual(dims.Length(), 0.0, 4)
+    self.assertAlmostEqual(offset.Length(), 0.0, 4)
 
     grd1 = geom.UniformGrid3D(30.0, 16.0, 10.0)
     rdshp.EncodeShape(m, grd1, 0, trans)
@@ -53,12 +49,16 @@ class TestCase(unittest.TestCase):
                           '1oir_conf.mol')
     m2 = Chem.MolFromMolFile(fileN2)
 
+    self.assertAlmostEqual(rdshp.ShapeTanimotoDist(m, m), 0.0, 4)
+    self.assertAlmostEqual(rdshp.ShapeTverskyIndex(m, m, 1.0, 1.0), 1.0, 4)
+
     rmsd = rdMolAlign.AlignMol(m, m2)
-    self.assertTrue(feq(rdshp.ShapeTanimotoDist(m, m2), 0.2813))
+    self.assertAlmostEqual(rdshp.ShapeTanimotoDist(m, m2), 0.2813, 4)
+    self.assertAlmostEqual(rdshp.ShapeTverskyIndex(m, m2, 1.0, 1.0), 0.7187, 4)
 
     dist = rdshp.ShapeTanimotoDist(mol1=m, mol2=m2, confId1=0, confId2=0, gridSpacing=0.25,
                                    stepSize=0.125)
-    self.assertTrue(feq(dist, 0.3021))
+    self.assertAlmostEqual(dist, 0.3021, 4)
 
     m = Chem.MolFromMolFile(fileN)
     cpt = rdmt.ComputeCentroid(m.GetConformer())
@@ -68,8 +68,8 @@ class TestCase(unittest.TestCase):
                              offset)
     dims -= geom.Point3D(13.927, 16.97, 9.775)
     offset -= geom.Point3D(-4.353, 16.829, 2.782)
-    self.assertTrue(feq(dims.Length(), 0.0))
-    self.assertTrue(feq(offset.Length(), 0.0))
+    self.assertAlmostEqual(dims.Length(), 0.0, 4)
+    self.assertAlmostEqual(offset.Length(), 0.0, 4)
     rdshp.EncodeShape(m, grd, 0)
 
     ovect = grd.GetOccupancyVect()
@@ -85,18 +85,18 @@ class TestCase(unittest.TestCase):
     lc2, uc2 = rdshp.ComputeUnionBox((lc, uc), (lc1, uc1))
     lc -= geom.Point3D(-4.353, 16.829, 2.782)
     uc -= geom.Point3D(9.574, 33.799, 12.557)
-    self.assertTrue(feq(lc.Length(), 0.0))
-    self.assertTrue(feq(uc.Length(), 0.0))
+    self.assertAlmostEqual(lc.Length(), 0.0, 4)
+    self.assertAlmostEqual(uc.Length(), 0.0, 4)
 
     lc1 -= geom.Point3D(-10.7519, -6.0778, -3.0123)
     uc1 -= geom.Point3D(8.7163, 5.3279, 3.1621)
-    self.assertTrue(feq(lc1.Length(), 0.0))
-    self.assertTrue(feq(uc1.Length(), 0.0))
+    self.assertAlmostEqual(lc1.Length(), 0.0, 4)
+    self.assertAlmostEqual(uc1.Length(), 0.0, 3)
 
     lc2 -= geom.Point3D(-10.7519, -6.0778, -3.01226)
     uc2 -= geom.Point3D(9.574, 33.799, 12.557)
-    self.assertTrue(feq(lc2.Length(), 0.0))
-    self.assertTrue(feq(uc2.Length(), 0.0))
+    self.assertAlmostEqual(lc2.Length(), 0.0, 4)
+    self.assertAlmostEqual(uc2.Length(), 0.0, 4)
 
 
 if __name__ == '__main__':
