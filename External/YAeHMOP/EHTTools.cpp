e8934d19e822d4ede5bcaa415318a336c869320b
Greg Landrum <greg.landrum@gmail.com>
2019-01-13 06:48:32 +0100

    handle the parameter file

--- External/YAeHMOP/EHTTools.cpp
+++ External/YAeHMOP/EHTTools.cpp
@@ -5,7 +5,7 @@
 #include <GraphMol/RDKitBase.h>
 #include <GraphMol/MolOps.h>
 #include <mutex>
-
+#include <fstream>
 
 extern "C" {
 #include <yaehmop/bind.h>
@@ -17,12 +17,11 @@ const std::string _EHTCharge = "_EHTCharge";
 const std::string _EHTMullikenOP = "_EHTMullikenOP";
 const std::string _EHTChargeMatrix = "_EHTChargeMatrix";
 
-
-// we should only call into the C code, which uses tons of globals, from one thread
-// at a time. This mutex enforces that.
+// we should only call into the C code, which uses tons of globals, from one
+// thread at a time. This mutex enforces that.
 std::mutex yaehmop_mutex;
 
-bool runMol(const ROMol &mol, int confId){
+bool runMol(const ROMol &mol, int confId) {
   std::lock_guard<std::mutex> lock(yaehmop_mutex);
 
   // -----------------------------
@@ -31,9 +30,9 @@ bool runMol(const ROMol &mol, int confId){
   FILE *nullfile = fopen("nul", "w");
   status_file = nullfile;
   output_file = nullfile;
-  FILE *dest=fopen("run.out","w+");
+  FILE *dest = fopen("run.out", "w+");
 
-  unit_cell = (cell_type *)calloc(1,sizeof(cell_type));
+  unit_cell = (cell_type *)calloc(1, sizeof(cell_type));
   details = (detail_type *)calloc(1, sizeof(detail_type));
 
   /*******
@@ -63,9 +62,7 @@ bool runMol(const ROMol &mol, int confId){
   details->k_offset = K_OFFSET;
   unit_cell->equiv_atoms = 0;
 
-
-  safe_strcpy(details->title,(char *)"RDKit job");
-
+  safe_strcpy(details->title, (char *)"RDKit job");
 
   // molecular calculation
   details->Execution_Mode = MOLECULAR;
@@ -75,9 +72,9 @@ bool runMol(const ROMol &mol, int confId){
   details->avg_props = 0;
   details->use_symmetry = 1;
   details->find_princ_axes = 0;
-  details->net_chg_PRT=1;
-  details->ROP_mat_PRT=1;
-  details->Rchg_mat_PRT=1;
+  details->net_chg_PRT = 1;
+  details->ROP_mat_PRT = 1;
+  details->Rchg_mat_PRT = 1;
 
   unit_cell->using_Zmat = 0;
   unit_cell->using_xtal_coords = 0;
@@ -88,11 +85,13 @@ bool runMol(const ROMol &mol, int confId){
   unit_cell->num_atoms = mol.getNumAtoms();
   unit_cell->num_raw_atoms = unit_cell->num_atoms;
 
-  unit_cell->atoms = (atom_type *)calloc(unit_cell->num_atoms, sizeof(atom_type));
+  unit_cell->atoms =
+      (atom_type *)calloc(unit_cell->num_atoms, sizeof(atom_type));
 
   const Conformer &conf = mol.getConformer(confId);
-  for(unsigned int i=0;i<mol.getNumAtoms();++i){
-    safe_strcpy(unit_cell->atoms[i].symb,(char *)mol.getAtomWithIdx(i)->getSymbol().c_str());
+  for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
+    safe_strcpy(unit_cell->atoms[i].symb,
+                (char *)mol.getAtomWithIdx(i)->getSymbol().c_str());
     auto p = conf.getAtomPos(i);
     unit_cell->atoms[i].loc.x = p.x;
     unit_cell->atoms[i].loc.y = p.y;
@@ -104,30 +103,47 @@ bool runMol(const ROMol &mol, int confId){
   // -----------------------------
   // ----- BOILERPLATE -----------
   // -----------------------------
-  fill_atomic_parms(unit_cell->atoms,unit_cell->num_atoms,NULL,NULL);
+  const char *parmFilePtr = nullptr;
+  std::string pfName = "";
+  if (std::getenv("BIND_PARM_FILE") == nullptr) {
+    auto rdbase = std::getenv("RDBASE");
+    if (rdbase != nullptr) {
+      pfName += rdbase;
+      pfName += "/Data/eht_parms.dat";
+      std::ifstream f(pfName.c_str());
+      if (f.good()) {
+        parmFilePtr = pfName.c_str();
+      } else {
+        std::cerr << "file " << pfName << " doesn't seem to exist" << std::endl;
+      }
+    }
+  }
+
+  fill_atomic_parms(unit_cell->atoms, unit_cell->num_atoms, NULL,
+                    const_cast<char *>(parmFilePtr));
   unit_cell->num_raw_atoms = unit_cell->num_atoms;
   charge_to_num_electrons(unit_cell);
-  build_orbital_lookup_table(unit_cell,&num_orbs,&orbital_lookup_table);
+  build_orbital_lookup_table(unit_cell, &num_orbs, &orbital_lookup_table);
 
   run_eht(dest);
   // ---------------------------------
   // ----- END BOILERPLATE -----------
   // ---------------------------------
 
-  //pull properties
-  for(auto &atom : mol.atoms() ){
-    atom->setProp(_EHTCharge,properties.net_chgs[atom->getIdx()],true);
+  // pull properties
+  for (auto &atom : mol.atoms()) {
+    atom->setProp(_EHTCharge, properties.net_chgs[atom->getIdx()], true);
   }
 
-  size_t sz = mol.getNumAtoms()*mol.getNumAtoms();
+  size_t sz = mol.getNumAtoms() * mol.getNumAtoms();
   auto ropMat = new double[sz];
-  memcpy(ropMat,properties.ROP_mat,sz*sizeof(double));
+  memcpy(ropMat, properties.ROP_mat, sz * sizeof(double));
   boost::shared_array<double> sptr(ropMat);
-  mol.setProp(_EHTMullikenOP,sptr,true);
+  mol.setProp(_EHTMullikenOP, sptr, true);
   auto rchgMat = new double[sz];
-  memcpy(rchgMat,properties.Rchg_mat,sz*sizeof(double));
+  memcpy(rchgMat, properties.Rchg_mat, sz * sizeof(double));
   boost::shared_array<double> sptr2(rchgMat);
-  mol.setProp(_EHTChargeMatrix,sptr2,true);
+  mol.setProp(_EHTChargeMatrix, sptr2, true);
 
   cleanup_memory();
 
@@ -136,6 +152,5 @@ bool runMol(const ROMol &mol, int confId){
   return true;
 }
 
-
-} // end of namespace EHTTools
-} // end of namespace RDKit
+}  // end of namespace EHTTools
+}  // end of namespace RDKit
