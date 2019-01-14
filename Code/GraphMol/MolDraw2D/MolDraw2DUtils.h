ed13e86673e9f8acf72e1632f2f4db12e906b2c6
Greg Landrum <greg.landrum@gmail.com>
2019-01-14 07:34:09 +0100

    untested

--- Code/GraphMol/MolDraw2D/MolDraw2DUtils.h
+++ Code/GraphMol/MolDraw2D/MolDraw2DUtils.h
@@ -13,10 +13,14 @@
 #define MOLDRAW2DUTILS_H
 #include <GraphMol/RWMol.h>
 
+#include <boost/tuple/tuple.hpp>
+
 // ****************************************************************************
 
 namespace RDKit {
+typedef boost::tuple<float, float, float> DrawColour;
 class MolDraw2D;
+
 namespace MolDraw2DUtils {
 //! Does some cleanup operations on the molecule to prepare it to draw nicely
 /*
@@ -35,12 +39,42 @@ happens the molecule will be in an inconsistent, partially kekulized, state.
 This isn't normally a problem for molecules that have been sanitized, but can be
 problematic if the molecules have been modified post santitization.
 */
-RDKIT_MOLDRAW2D_EXPORT void prepareMolForDrawing(RWMol &mol, bool kekulize = true,
-                          bool addChiralHs = true, bool wedgeBonds = true,
-                          bool forceCoords = false);
+RDKIT_MOLDRAW2D_EXPORT void prepareMolForDrawing(RWMol &mol,
+                                                 bool kekulize = true,
+                                                 bool addChiralHs = true,
+                                                 bool wedgeBonds = true,
+                                                 bool forceCoords = false);
 
-RDKIT_MOLDRAW2D_EXPORT void updateDrawerParamsFromJSON(MolDraw2D &drawer, const char *json);
-RDKIT_MOLDRAW2D_EXPORT void updateDrawerParamsFromJSON(MolDraw2D &drawer, const std::string &json);
-}
-}
+//! prepare a molecule for drawing and draw it
+/*
+  \param mol: the molecule to draw
+  \param legend: (optional) the legend (to be drawn under the molecule)
+  \param highlight_atoms: (optional) vector of atom ids to highlight
+  \param highlight_atoms: (optional) vector of bond ids to highlight
+  \param highlight_atom_map: (optional) map from atomId -> DrawColour
+            providing the highlight colors. If not provided the default
+            higlight colour from \c drawOptions() will be used.
+  \param highlight_bond_map: (optional) map from bondId -> DrawColour
+            providing the highlight colors. If not provided the default
+            highlight colour from \c drawOptions() will be used.
+  \param highlight_radii: (optional) map from atomId -> radius (in molecule
+            coordinates) for the radii of atomic highlights. If not provided
+            the default value from \c drawOptions() will be used.
+  \param confId: (optional) conformer ID to be used for atomic coordinates
+
+*/
+RDKIT_MOLDRAW2D_EXPORT void prepareAndDrawMolecule(
+    MolDraw2D &drawer, const ROMol &mol, const std::string &legend = "",
+    const std::vector<int> *highlight_atoms = nullptr,
+    const std::vector<int> *highlight_bonds = nullptr,
+    const std::map<int, DrawColour> *highlight_atom_map = nullptr,
+    const std::map<int, DrawColour> *highlight_bond_map = nullptr,
+    const std::map<int, double> *highlight_radii = nullptr, int confId = -1);
+
+RDKIT_MOLDRAW2D_EXPORT void updateDrawerParamsFromJSON(MolDraw2D &drawer,
+                                                       const char *json);
+RDKIT_MOLDRAW2D_EXPORT void updateDrawerParamsFromJSON(MolDraw2D &drawer,
+                                                       const std::string &json);
+}  // namespace MolDraw2DUtils
+}  // namespace RDKit
 #endif  // MOLDRAW2DUTILS_H
