d6d58224489ab79ea7a69b23cfa424d984733300
Francois Berenger <unixjunkie@sdf.org>
2019-01-30 17:29:14 +0900

    added optional build dir parameter

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -3,13 +3,20 @@
 #set -x # DEBUG
 
 # launch the unit tests suite.
-# run this script like this: ./Scripts/test.sh
+# run this script like this: ./Scripts/test.sh [build_dir]
+# if no build_dir is specified, ./build is assumed to be the default
+# build directory
+
+build_dir="build"
+if [ "$#" -eq 1 ]; then
+    build_dir=$1
+fi
 
 RDBASE=$PWD
 PYTHONPATH=$RDBASE
 LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
 
-# run them in parallel
+# run tests in parallel
 nprocs=`getconf _NPROCESSORS_ONLN`
 
-cd build && ctest --output-on-failure -j ${nprocs}
+cd $build_dir && ctest --output-on-failure -j ${nprocs}
