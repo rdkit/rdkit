9c336565fd820d837646ecb54f8237b8b6cc9b24
Ric <ricrogz@users.noreply.github.com>
2019-02-21 22:51:26 -0500

    Parse enhanced stereo information from CXSMILES (#2282)
    * parser implemented
    
    * added a couple of tests
    
    * use logger instead of stderr

--- Code/GraphMol/SmilesParse/CXSmilesOps.cpp
+++ Code/GraphMol/SmilesParse/CXSmilesOps.cpp
@@ -184,8 +184,8 @@ bool parse_coordinate_bonds(Iterator &first, Iterator last, RDKit::RWMol &mol) {
     if (read_int_pair(first, last, aidx, bidx)) {
       Bond *bnd = mol.getBondWithIdx(bidx);
       if (bnd->getBeginAtomIdx() != aidx && bnd->getEndAtomIdx() != aidx) {
-        std::cerr << "BOND NOT FOUND! " << bidx << " involving atom " << aidx
-                  << std::endl;
+        BOOST_LOG(rdWarningLog) << "BOND NOT FOUND! " << bidx
+                                << " involving atom " << aidx << std::endl;
         return false;
       }
       bnd->setBondType(Bond::DATIVE);
@@ -245,10 +245,58 @@ bool parse_radicals(Iterator &first, Iterator last, RDKit::RWMol &mol) {
         if (!processRadicalSection(first, last, mol, 3)) return false;
         break;
       default:
-        BOOST_LOG(rdWarningLog) << "Radical specification " << *first
-                                << " ignored.";
+        BOOST_LOG(rdWarningLog)
+            << "Radical specification " << *first << " ignored.";
     }
   }
+  return true;
+}
+
+template <typename Iterator>
+bool parse_enhanced_stereo(Iterator &first, Iterator last, RDKit::RWMol &mol) {
+  StereoGroupType group_type = StereoGroupType::STEREO_ABSOLUTE;
+  if (*first == 'a') {
+    group_type = StereoGroupType::STEREO_ABSOLUTE;
+  } else if (*first == 'o') {
+    group_type = StereoGroupType::STEREO_OR;
+  } else if (*first == '&') {
+    group_type = StereoGroupType::STEREO_AND;
+  }
+  ++first;
+
+  // OR and AND groups carry a group number
+  if (group_type != StereoGroupType::STEREO_ABSOLUTE) {
+    unsigned int group_id = 0;
+    read_int(first, last, group_id);
+  }
+
+  if (first >= last || *first != ':') return false;
+  ++first;
+
+  std::vector<Atom *> atoms;
+  while (first != last && *first >= '0' && *first <= '9') {
+    unsigned int aidx;
+    if (read_int(first, last, aidx)) {
+      Atom *atom = mol.getAtomWithIdx(aidx);
+      if (!atom) {
+        BOOST_LOG(rdWarningLog)
+            << "Atom " << aidx << " not found!" << std::endl;
+        return false;
+      }
+      atoms.push_back(atom);
+    } else {
+      return false;
+    }
+
+    if (first < last && *first == ',') {
+      ++first;
+    }
+  }
+
+  std::vector<StereoGroup> mol_stereo_groups(mol.getStereoGroups());
+  mol_stereo_groups.emplace_back(group_type, std::move(atoms));
+  mol.setStereoGroups(std::move(mol_stereo_groups));
+
   return true;
 }
 
@@ -275,6 +323,8 @@ bool parse_it(Iterator &first, Iterator last, RDKit::RWMol &mol) {
       if (!parse_coordinate_bonds(first, last, mol)) return false;
     } else if (*first == '^') {
       if (!parse_radicals(first, last, mol)) return false;
+    } else if (*first == 'a' || *first == 'o' || *first == '&') {
+      if (!parse_enhanced_stereo(first, last, mol)) return false;
     } else {
       ++first;
     }
@@ -345,8 +395,8 @@ void processCXSmilesLabels(RDKit::RWMol &mol) {
 
 void parseCXExtensions(RDKit::RWMol &mol, const std::string &extText,
                        std::string::const_iterator &first) {
-  // std::cerr << "parseCXNExtensions: " << extText << std::endl;
-  if (!extText.size() || extText[0] != '|') return;
+  // BOOST_LOG(rdWarningLog) << "parseCXNExtensions: " << extText << std::endl;
+  if (extText.empty() || extText[0] != '|') return;
   first = extText.begin();
   bool ok = parser::parse_it(first, extText.end(), mol);
   if (!ok)
@@ -355,4 +405,4 @@ void parseCXExtensions(RDKit::RWMol &mol, const std::string &extText,
     processCXSmilesLabels(mol);
   }
 }
-}  // end of namespace SmilesParseOps
+}  // namespace SmilesParseOps
