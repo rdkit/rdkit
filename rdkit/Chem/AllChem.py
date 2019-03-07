99b5cbf829e9297fc932b9be65bf1dfc9ca7cb06
Sunhwan Jo <sunhwanj@gmail.com>
2019-03-07 09:27:16 -0800

    simple docstring fix

--- rdkit/Chem/AllChem.py
+++ rdkit/Chem/AllChem.py
@@ -192,7 +192,7 @@ def GetConformerRMSMatrix(mol, atomIds=None, prealigned=False):
 
 def EnumerateLibraryFromReaction(reaction, sidechainSets, returnReactants=False):
   """ Returns a generator for the virtual library defined by
-   a reaction and a sequence of sidechain sets
+  a reaction and a sequence of sidechain sets
 
   >>> from rdkit import Chem
   >>> from rdkit.Chem import AllChem
@@ -207,15 +207,19 @@ def EnumerateLibraryFromReaction(reaction, sidechainSets, returnReactants=False)
   be done without worrying about running out of memory. Your patience will run out first:
 
   Define a set of 10000 amines:
+
   >>> amines = (Chem.MolFromSmiles('N'+'C'*x) for x in range(10000))
 
   ... a set of 10000 acids
+
   >>> acids = (Chem.MolFromSmiles('OC(=O)'+'C'*x) for x in range(10000))
 
   ... now the virtual library (1e8 compounds in principle):
+
   >>> r = AllChem.EnumerateLibraryFromReaction(rxn,[acids,amines])
 
   ... look at the first 4 compounds:
+
   >>> [Chem.MolToSmiles(next(r)[0]) for x in range(4)]
   ['NC=O', 'CNC=O', 'CCNC=O', 'CCCNC=O']
 
@@ -263,29 +267,33 @@ def ConstrainedEmbed(mol, core, useTethers=True, coreConfId=-1, randomseed=2342,
     - randomSeed: (optional) seed for the random number generator
 
 
-    An example, start by generating a template with a 3D structure:
-    >>> from rdkit.Chem import AllChem
-    >>> template = AllChem.MolFromSmiles("c1nn(Cc2ccccc2)cc1")
-    >>> AllChem.EmbedMolecule(template)
-    0
-    >>> AllChem.UFFOptimizeMolecule(template)
-    0
+  An example, start by generating a template with a 3D structure:
 
-    Here's a molecule:
-    >>> mol = AllChem.MolFromSmiles("c1nn(Cc2ccccc2)cc1-c3ccccc3")
+  >>> from rdkit.Chem import AllChem
+  >>> template = AllChem.MolFromSmiles("c1nn(Cc2ccccc2)cc1")
+  >>> AllChem.EmbedMolecule(template)
+  0
+  >>> AllChem.UFFOptimizeMolecule(template)
+  0
 
-    Now do the constrained embedding
-    >>> newmol=AllChem.ConstrainedEmbed(mol, template)
+  Here's a molecule:
 
-    Demonstrate that the positions are the same:
-    >>> newp=newmol.GetConformer().GetAtomPosition(0)
-    >>> molp=mol.GetConformer().GetAtomPosition(0)
-    >>> list(newp-molp)==[0.0,0.0,0.0]
-    True
-    >>> newp=newmol.GetConformer().GetAtomPosition(1)
-    >>> molp=mol.GetConformer().GetAtomPosition(1)
-    >>> list(newp-molp)==[0.0,0.0,0.0]
-    True
+  >>> mol = AllChem.MolFromSmiles("c1nn(Cc2ccccc2)cc1-c3ccccc3")
+
+  Now do the constrained embedding
+
+  >>> newmol=AllChem.ConstrainedEmbed(mol, template)
+
+  Demonstrate that the positions are the same:
+
+  >>> newp=newmol.GetConformer().GetAtomPosition(0)
+  >>> molp=mol.GetConformer().GetAtomPosition(0)
+  >>> list(newp-molp)==[0.0,0.0,0.0]
+  True
+  >>> newp=newmol.GetConformer().GetAtomPosition(1)
+  >>> molp=mol.GetConformer().GetAtomPosition(1)
+  >>> list(newp-molp)==[0.0,0.0,0.0]
+  True
 
   """
   match = mol.GetSubstructMatch(core)
@@ -342,39 +350,42 @@ def ConstrainedEmbed(mol, core, useTethers=True, coreConfId=-1, randomseed=2342,
 
 def AssignBondOrdersFromTemplate(refmol, mol):
   """ assigns bond orders to a molecule based on the
-      bond orders in a template molecule
+  bond orders in a template molecule
 
   Arguments
     - refmol: the template molecule
     - mol: the molecule to assign bond orders to
 
-    An example, start by generating a template from a SMILES
-    and read in the PDB structure of the molecule
-    >>> import os
-    >>> from rdkit.Chem import AllChem
-    >>> template = AllChem.MolFromSmiles("CN1C(=NC(C1=O)(c2ccccc2)c3ccccc3)N")
-    >>> mol = AllChem.MolFromPDBFile(os.path.join(RDConfig.RDCodeDir, 'Chem', 'test_data', '4DJU_lig.pdb'))
-    >>> len([1 for b in template.GetBonds() if b.GetBondTypeAsDouble() == 1.0])
-    8
-    >>> len([1 for b in mol.GetBonds() if b.GetBondTypeAsDouble() == 1.0])
-    22
+  An example, start by generating a template from a SMILES
+  and read in the PDB structure of the molecule
 
-    Now assign the bond orders based on the template molecule
-    >>> newMol = AllChem.AssignBondOrdersFromTemplate(template, mol)
-    >>> len([1 for b in newMol.GetBonds() if b.GetBondTypeAsDouble() == 1.0])
-    8
+  >>> import os
+  >>> from rdkit.Chem import AllChem
+  >>> template = AllChem.MolFromSmiles("CN1C(=NC(C1=O)(c2ccccc2)c3ccccc3)N")
+  >>> mol = AllChem.MolFromPDBFile(os.path.join(RDConfig.RDCodeDir, 'Chem', 'test_data', '4DJU_lig.pdb'))
+  >>> len([1 for b in template.GetBonds() if b.GetBondTypeAsDouble() == 1.0])
+  8
+  >>> len([1 for b in mol.GetBonds() if b.GetBondTypeAsDouble() == 1.0])
+  22
 
-    Note that the template molecule should have no explicit hydrogens
-    else the algorithm will fail.
+  Now assign the bond orders based on the template molecule
 
-    It also works if there are different formal charges (this was github issue 235):
-    >>> template=AllChem.MolFromSmiles('CN(C)C(=O)Cc1ccc2c(c1)NC(=O)c3ccc(cc3N2)c4ccc(c(c4)OC)[N+](=O)[O-]')
-    >>> mol = AllChem.MolFromMolFile(os.path.join(RDConfig.RDCodeDir, 'Chem', 'test_data', '4FTR_lig.mol'))
-    >>> AllChem.MolToSmiles(mol)
-    'COC1CC(C2CCC3C(O)NC4CC(CC(O)N(C)C)CCC4NC3C2)CCC1N(O)O'
-    >>> newMol = AllChem.AssignBondOrdersFromTemplate(template, mol)
-    >>> AllChem.MolToSmiles(newMol)
-    'COc1cc(-c2ccc3c(c2)Nc2ccc(CC(=O)N(C)C)cc2NC3=O)ccc1[N+](=O)[O-]'
+  >>> newMol = AllChem.AssignBondOrdersFromTemplate(template, mol)
+  >>> len([1 for b in newMol.GetBonds() if b.GetBondTypeAsDouble() == 1.0])
+  8
+
+  Note that the template molecule should have no explicit hydrogens
+  else the algorithm will fail.
+
+  It also works if there are different formal charges (this was github issue 235):
+
+  >>> template=AllChem.MolFromSmiles('CN(C)C(=O)Cc1ccc2c(c1)NC(=O)c3ccc(cc3N2)c4ccc(c(c4)OC)[N+](=O)[O-]')
+  >>> mol = AllChem.MolFromMolFile(os.path.join(RDConfig.RDCodeDir, 'Chem', 'test_data', '4FTR_lig.mol'))
+  >>> AllChem.MolToSmiles(mol)
+  'COC1CC(C2CCC3C(O)NC4CC(CC(O)N(C)C)CCC4NC3C2)CCC1N(O)O'
+  >>> newMol = AllChem.AssignBondOrdersFromTemplate(template, mol)
+  >>> AllChem.MolToSmiles(newMol)
+  'COc1cc(-c2ccc3c(c2)Nc2ccc(CC(=O)N(C)C)cc2NC3=O)ccc1[N+](=O)[O-]'
 
   """
   refmol2 = rdchem.Mol(refmol)
