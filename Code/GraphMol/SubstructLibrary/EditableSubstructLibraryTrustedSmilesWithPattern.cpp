f38a8b816d3f98c3332b88546ff62fba518e52bd
Gareth Jones <java.jones@gmail.com>
2019-03-15 16:29:48 -0600

    New Files

---
+++ Code/GraphMol/SubstructLibrary/EditableSubstructLibraryTrustedSmilesWithPattern.cpp
@@ -0,0 +1,45 @@
+//
+// Created by gareth on 2/10/19.
+//
+
+#include "EditableSubstructLibraryTrustedSmilesWithPattern.h"
+
+namespace RDKit {
+
+int EditableSubstructLibraryTrustedSmilesWithPattern::addSmiles(
+    const std::string &smi, const std::string &fp) {
+  const std::vector<std::string> smiles{smi};
+  const std::vector<std::string> fps{fp};
+  return addSmiles(smiles, fps);
+}
+
+int EditableSubstructLibraryTrustedSmilesWithPattern::addSmiles(
+    const std::vector<std::string> smilesVector,
+    const std::vector<std::string> fpVector) {
+  boost::unique_lock<boost::shared_mutex> lock(access_mutex);
+
+  auto start = ids.size();
+  for (const auto &smiStr : smilesVector) {
+    auto spacePos = smiStr.find(" ");
+    if (spacePos == std::string::npos) {
+      throw std::invalid_argument("No structure name in smiles " + smiStr);
+    }
+    auto smi = smiStr.substr(0, spacePos);
+    auto name = smiStr.substr(spacePos);
+    checkIdNotPresent(name);
+    ids.insert(idInfo(ids.size(), name));
+    molHolder->addSmiles(smi);
+    assert(molHolder->size() == ids.size());
+  }
+  auto nAdded = ids.size() - start;
+  assert(nAdded == smilesVector.size());
+  addFingerprints(fpVector);
+  return static_cast<int>(nAdded);
+}
+
+ ExplicitBitVect *EditableSubstructLibraryTrustedSmilesWithPattern::makeFingerprint(const std::string &smiles) const {
+    auto mol = SmilesToMol(smiles, 0, false);
+    mol->updatePropertyCache();
+    return fpHolder->makeFingerprint(*mol);
+  }
+}  // namespace RDKit
---
+++ Code/GraphMol/SubstructLibrary/EditableSubstructLibraryTrustedSmilesWithPattern.h
@@ -0,0 +1,29 @@
+
+#include <RDGeneral/export.h>
+
+#ifndef RDKIT_EDITABLESUBSTRUCTLIBRARY_TSP_H
+#define RDKIT_EDITABLESUBSTRUCTLIBRARY_TSP_H
+
+#include "EditableSubstructLibrary.h"
+
+namespace RDKit {
+
+class RDKIT_SUBSTRUCTLIBRARY_EXPORT EditableSubstructLibraryTrustedSmilesWithPattern
+    : public EditableSubstructLibrary<std::string, CachedTrustedSmilesMolHolder,
+                                      PatternHolder> {
+ public:
+  EditableSubstructLibraryTrustedSmilesWithPattern()
+      : EditableSubstructLibrary() {}
+
+  int addSmiles(const std::string &smi, const std::string &fp);
+
+  int addSmiles(const std::vector<std::string> smilesVector,
+                const std::vector<std::string> fpVector);
+
+  ExplicitBitVect * makeFingerprint(const std::string &smiles) const;
+  
+  };
+
+}  // namespace RDKit
+
+#endif
