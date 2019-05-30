b01105168801836f161b3819f9c731fd2a95ab88
Greg Landrum <greg.landrum@gmail.com>
2019-05-30 15:50:36 +0200

    remove redundancy
    response to review

--- Code/GraphMol/MolPickler.cpp
+++ Code/GraphMol/MolPickler.cpp
@@ -1322,7 +1322,7 @@ void MolPickler::_pickleAtom(std::ostream &ss, const Atom *atom) {
   }
   if (getAtomMapNumber(atom, tmpInt)) {
     if (tmpInt >= 0 && tmpInt < 128) {
-      tmpChar = static_cast<char>(tmpInt % 128);
+      tmpChar = static_cast<char>(tmpInt);
       streamWrite(ss, ATOM_MAPNUMBER, tmpChar);
     } else {
       tmpChar = static_cast<char>(255);
