1294ec15b81046c850f0c421e9883a082fcd7eca
Brian Kelley <fustigator@gmail.com>
2019-08-19 19:48:30 -0400

    Automatically load the ipython extensions if we are running in an jupyter notebook

--- rdkit/__init__.py
+++ rdkit/__init__.py
@@ -3,3 +3,13 @@ try:
 except ImportError:
   __version__ = 'Unknown'
   raise
+
+# if we are running in a jupyter notebook, enable the extensions
+try:
+  get_ipython()
+  from rdkit.Chem.Draw import IPythonConsole
+  from rdkit.Chem import LogWarningMsg, WrapLogs
+  WrapLogs()
+  LogWarningMsg("Enabling RDKit %s jupyter extensions"%__version__)
+except:
+  pass
