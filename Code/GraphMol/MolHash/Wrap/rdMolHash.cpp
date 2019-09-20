04f0a04849ce0cc86f6f6e9a6bce8508e6ed6106
Greg Landrum <greg.landrum@gmail.com>
2019-09-20 08:01:46 +0200

    Add initial python wrapper (and tests)

--- Code/GraphMol/MolHash/Wrap/rdMolHash.cpp
+++ Code/GraphMol/MolHash/Wrap/rdMolHash.cpp
@@ -10,6 +10,7 @@
 #include <RDBoost/python.h>
 #include <GraphMol/ROMol.h>
 #include <GraphMol/MolHash/MolHash.h>
+#include <GraphMol/NMMolHash/molhash.h>
 #include <RDBoost/Wrap.h>
 
 namespace python = boost::python;
@@ -18,12 +19,12 @@ using namespace RDKit;
 namespace {
 std::string GenMolHashString(const ROMol &mol, python::object atomsToUse,
                              python::object bondsToUse) {
-  std::unique_ptr<std::vector<unsigned> > avect;
+  std::unique_ptr<std::vector<unsigned>> avect;
   if (atomsToUse) {
     avect = pythonObjectToVect(atomsToUse,
                                static_cast<unsigned>(mol.getNumAtoms()));
   }
-  std::unique_ptr<std::vector<unsigned> > bvect;
+  std::unique_ptr<std::vector<unsigned>> bvect;
   if (bondsToUse) {
     bvect = pythonObjectToVect(bondsToUse,
                                static_cast<unsigned>(mol.getNumBonds()));
@@ -32,15 +33,43 @@ std::string GenMolHashString(const ROMol &mol, python::object atomsToUse,
       MolHash::generateMoleculeHashSet(mol, avect.get(), bvect.get());
   return res;
 }
+
+std::string MolHashHelper(const ROMol &mol, MolHash::HashFunction func) {
+  RWMol cpy(mol);
+  return MolHash::MolHash(&cpy, func);
 }
+}  // namespace
 
 BOOST_PYTHON_MODULE(rdMolHash) {
   python::scope().attr("__doc__") =
       "Module containing functions to generate a hash/key for molecules";
+
+  python::enum_<MolHash::HashFunction>("HashFunction")
+      .value("AnonymousGraph", MolHash::HashFunction::AnonymousGraph)
+      .value("ElementGraph", MolHash::HashFunction::ElementGraph)
+      .value("CanonicalSmiles", MolHash::HashFunction::CanonicalSmiles)
+      .value("MurckoScaffold", MolHash::HashFunction::MurckoScaffold)
+      .value("ExtendedMurcko", MolHash::HashFunction::ExtendedMurcko)
+      .value("MolFormula", MolHash::HashFunction::MolFormula)
+      .value("AtomBondCounts", MolHash::HashFunction::AtomBondCounts)
+      .value("DegreeVector", MolHash::HashFunction::DegreeVector)
+      .value("Mesomer", MolHash::HashFunction::Mesomer)
+      .value("HetAtomTautomer", MolHash::HashFunction::HetAtomTautomer)
+      .value("HetAtomProtomer", MolHash::HashFunction::HetAtomProtomer)
+      .value("RedoxPair", MolHash::HashFunction::RedoxPair)
+      .value("Regioisomer", MolHash::HashFunction::Regioisomer)
+      .value("NetCharge", MolHash::HashFunction::NetCharge)
+      .value("SmallWorldIndexBR", MolHash::HashFunction::SmallWorldIndexBR)
+      .value("SmallWorldIndexBRL", MolHash::HashFunction::SmallWorldIndexBRL)
+      .value("ArthorSubstructureOrder",
+             MolHash::HashFunction::ArthorSubstructureOrder);
 
   std::string docString = "Generates a hash string for a molecule";
   python::def("GenerateMoleculeHashString", GenMolHashString,
               (python::arg("mol"), python::arg("atomsToUse") = python::list(),
                python::arg("bondsToUse") = python::list()),
               docString.c_str());
+  python::def("MolHash", MolHashHelper,
+              (python::arg("mol"), python::arg("func")),
+              "Generate a hash for a molecule");
 }
