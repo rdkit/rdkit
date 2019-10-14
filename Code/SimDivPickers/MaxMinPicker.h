528f9fb18443a5a299d9ca10ac2ad29141f08fc5
Greg Landrum <greg.landrum@gmail.com>
2019-10-14 17:23:45 +0200

    initial commit of Roger's contrib

--- Code/SimDivPickers/MaxMinPicker.h
+++ Code/SimDivPickers/MaxMinPicker.h
@@ -24,19 +24,6 @@
 
 namespace RDPickers {
 
-namespace {
-class RDKIT_SIMDIVPICKERS_EXPORT distmatFunctor {
- public:
-  distmatFunctor(const double *distMat) : dp_distMat(distMat){};
-  double operator()(unsigned int i, unsigned int j) {
-    return getDistFromLTM(this->dp_distMat, i, j);
-  }
-
- private:
-  const double *dp_distMat;
-};
-}  // namespace
-
 /*! \brief Implements the MaxMin algorithm for picking a subset of item from a
  *pool
  *
@@ -272,7 +259,7 @@ RDKit::INT_VECT MaxMinPicker::lazyPick(T &func, unsigned int poolSize,
     } while (*prev != 0);
 
     // if the current distance is closer then threshold, we're done
-    if (threshold >= 0.0 && maxOFmin < threshold) break;
+    if (maxOFmin <= threshold && threshold >= 0.0) break;
     tmpThreshold = maxOFmin;
     // now add the new pick to picks and remove it from the pool
     *pick_prev = pinfo[pick].next;
