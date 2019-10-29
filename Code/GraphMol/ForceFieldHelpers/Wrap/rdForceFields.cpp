454fcb77ed2998175a622d9f2777242bc5f9869b
Yang Yang <31322025+yy692@users.noreply.github.com>
2019-10-29 15:31:17 -0400

    Update rdForceFields.cpp

--- Code/GraphMol/ForceFieldHelpers/Wrap/rdForceFields.cpp
+++ Code/GraphMol/ForceFieldHelpers/Wrap/rdForceFields.cpp
@@ -172,6 +172,24 @@ bool MMFFHasAllMoleculeParams(const ROMol &mol) {
 };
 
 namespace ForceFields {
+PyObject *getUFFAtomTypes(const RDKit::ROMol &mol) {
+  PyObject *res = nullptr;
+  std::pair<AtomicParamVect, bool> uffAtomTypes
+  AtomicParamVect paramVect;
+  bool foundAll;
+  
+  paramVect.resize(mol.getNumAtoms());
+  foundAll = ture
+  uffAtomTypes = std::make_pair(paramVect, foundAll);
+  uffAtomTypes = RDKit::UFF::getAtomTypes(mol,const std::string &paramData = "");
+  
+  res = PyTuple_New(mol.getNumAtoms());
+  for (unsigned int i = 0; i < mol.getNumAtoms(); i++) {
+    PyTuple_SetItem(res, i, PyFloat_FromDouble(uffAtomTypes.first(i)));
+  }
+  return res;
+};  
+
 PyObject *getUFFBondStretchParams(const RDKit::ROMol &mol,
                                   const unsigned int idx1,
                                   const unsigned int idx2) {
