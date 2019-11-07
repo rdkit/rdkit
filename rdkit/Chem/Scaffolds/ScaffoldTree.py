51dc5a1a49d2bcb8d50fa4512d3b2b4a475d3c26
Greg Landrum <greg.landrum@gmail.com>
2019-11-07 18:09:47 +0100

    getting close to v0.1

--- rdkit/Chem/Scaffolds/ScaffoldTree.py
+++ rdkit/Chem/Scaffolds/ScaffoldTree.py
@@ -16,7 +16,8 @@ class ScaffoldTreeParams(object):
   bondBreakers = ('[!#0;R:1]-!@[!#0:2]>>[*:1]-[#0].[#0]-[*:2]', )
   includeGenericScaffolds = True
   includeGenericBondScaffolds = False
-  keepOnlyFirstFragment = False
+  keepOnlyFirstFragment = True
+  includeScaffoldsWithoutAttachments = True
 
 
 def _addReactionsToParams(params):
@@ -41,28 +42,25 @@ def getMolFragments(mol, params):
     >>> m = Chem.MolFromSmiles('c1ccccc1CC1NC(=O)CCC1')
     >>> frags = getMolFragments(m,ps)
     >>> len(frags)
-    8
+    2
 
     The results are 2-tuples with SMILES for the parent and then the fragment as a molecule:
     >>> frags[0]
     ('O=C1CCCC(Cc2ccccc2)N1', <rdkit.Chem.rdchem.Mol object at 0x...>)
+    >>> sorted((x,Chem.MolToSmiles(y)) for x,y in frags)
+    [('O=C1CCCC(Cc2ccccc2)N1', '*C1CCCC(=O)N1'), ('O=C1CCCC(Cc2ccccc2)N1', '*c1ccccc1')]
 
     Here's what the actual results look like:
-    >>> sorted((x,Chem.MolToSmiles(y)) for x,y in frags)    #doctest: +NORMALIZE_WHITESPACE
+
+    Setting keepOnlyFirstFragment results in us getting all the fragments with linkers:
+    >>> ps.keepOnlyFirstFragment = False
+    >>> frags = getMolFragments(m,ps)
+    >>> len(frags)
+    8
+    >>> sorted((x,Chem.MolToSmiles(y)) for x,y in frags)
     [('*CC1CCCC(=O)N1', '*C*'), ('*CC1CCCC(=O)N1', '*C1CCCC(=O)N1'), ('*Cc1ccccc1', '*C*'), 
     ('*Cc1ccccc1', '*c1ccccc1'), ('O=C1CCCC(Cc2ccccc2)N1', '*C1CCCC(=O)N1'), ('O=C1CCCC(Cc2ccccc2)N1', 
     '*CC1CCCC(=O)N1'), ('O=C1CCCC(Cc2ccccc2)N1', '*Cc1ccccc1'), ('O=C1CCCC(Cc2ccccc2)N1', '*c1ccccc1')]
-
-    Setting keepOnlyFirstFragment results in us not getting all the fragments with linkers:
-    >>> ps.keepOnlyFirstFragment = True
-    >>> frags = getMolFragments(m,ps)
-    >>> len(frags)
-    2
-    >>> sorted((x,Chem.MolToSmiles(y)) for x,y in frags)    #doctest: +NORMALIZE_WHITESPACE
-    [('O=C1CCCC(Cc2ccccc2)N1', '*C1CCCC(=O)N1'), ('O=C1CCCC(Cc2ccccc2)N1', '*c1ccccc1')]
-
-
-
 
   """
   if not hasattr(params, '_breakers'):
@@ -83,6 +81,28 @@ def getMolFragments(mol, params):
           stack.append(p[1])
           res.append((parent_smi, p[1]))
   return res
+
+
+def removeAttachmentPoints(mol):
+  """
+  
+  >>> m = Chem.MolFromSmiles('*c1ccc(*)cc1')
+  >>> m.GetNumAtoms()
+  8
+  >>> sm = removeAttachmentPoints(m)
+  >>> sm.GetNumAtoms()
+  6
+  >>> Chem.MolToSmiles(sm)
+  'c1ccccc1'
+  
+  """
+  res = Chem.RWMol(mol)
+  aids = sorted(
+    (x.GetIdx() for x in mol.GetAtoms() if x.GetAtomicNum() == 0 and x.GetDegree() == 1),
+    reverse=True)
+  for aid in aids:
+    res.RemoveAtom(aid)
+  return Chem.Mol(res)
 
 
 def makeScaffoldGeneric(mol, doAtoms=True, doBonds=False):
@@ -120,6 +140,86 @@ def makeScaffoldGeneric(mol, doAtoms=True, doBonds=False):
   return res
 
 
+from collections import namedtuple
+NetworkEdge = namedtuple('NetworkEdge', ('start', 'end', 'type'))
+
+
+class EdgeTypes:
+  FragmentEdge = 1
+  GenericEdge = 2
+  GenericBondEdge = 3
+  RemoveAttachmentEdge = 4
+
+
+def generateScaffoldNetwork(mol, params=None):
+  """
+
+  >>> m = Chem.MolFromSmiles('c1ccccc1CC1NC(=O)CCC1')
+  >>> nodes,edges = generateScaffoldNetwork(m)
+  >>> len(nodes)
+  9
+  >>> sorted(nodes)         
+  ['**1****(=*)*1', '**1:*:*:*:*:*:1', '*1*****1', '*1:*:*:*:*:*:1', 
+   '*C1CCCC(=O)N1', '*c1ccccc1', 'O=C1CCCC(Cc2ccccc2)N1', 'O=C1CCCCN1', 
+   'c1ccccc1']
+  >>> len(edges)
+  8
+  >>> sorted(x for x in edges if x.type==EdgeTypes.FragmentEdge)          
+  [NetworkEdge(start='O=C1CCCC(Cc2ccccc2)N1', end='*C1CCCC(=O)N1', type=1), 
+   NetworkEdge(start='O=C1CCCC(Cc2ccccc2)N1', end='*c1ccccc1', type=1)]
+  >>> sorted(x for x in edges if x.type==EdgeTypes.GenericEdge)           
+  [NetworkEdge(start='*C1CCCC(=O)N1', end='**1****(=*)*1', type=2), 
+   NetworkEdge(start='*c1ccccc1', end='**1:*:*:*:*:*:1', type=2)]
+  >>> sorted(x for x in edges if x.type==EdgeTypes.RemoveAttachmentEdge)  
+  [NetworkEdge(start='**1****(=*)*1', end='*1*****1', type=4), 
+   NetworkEdge(start='**1:*:*:*:*:*:1', end='*1:*:*:*:*:*:1', type=4), 
+   NetworkEdge(start='*C1CCCC(=O)N1', end='O=C1CCCCN1', type=4), 
+   NetworkEdge(start='*c1ccccc1', end='c1ccccc1', type=4)]
+
+
+
+  """
+  if params is None:
+    params = ScaffoldTreeParams()
+  nodes = set()
+  edges = []
+  frags = getMolFragments(mol, params)
+  for smi, fmol in frags:
+    nodes.add(smi)
+    fsmi = Chem.MolToSmiles(fmol)
+    nodes.add(fsmi)
+    edges.append(NetworkEdge(smi, fsmi, EdgeTypes.FragmentEdge))
+
+    if params.includeGenericScaffolds:
+      gmol = makeScaffoldGeneric(fmol, doAtoms=True, doBonds=False)
+      gsmi = Chem.MolToSmiles(gmol)
+      nodes.add(gsmi)
+      edges.append(NetworkEdge(fsmi, gsmi, EdgeTypes.GenericEdge))
+
+      if params.includeScaffoldsWithoutAttachments:
+        asmi = Chem.MolToSmiles(removeAttachmentPoints(gmol))
+        nodes.add(asmi)
+        edges.append(NetworkEdge(gsmi, asmi, EdgeTypes.RemoveAttachmentEdge))
+
+      if params.includeGenericBondScaffolds:
+        gbmol = makeScaffoldGeneric(fmol, doAtoms=True, doBonds=True)
+        gbsmi = Chem.MolToSmiles(gbmol)
+        nodes.add(gbsmi)
+        edges.append(NetworkEdge(fsmi, gbsmi, EdgeTypes.GenericBondEdge))
+
+        if params.includeScaffoldsWithoutAttachments:
+          asmi = Chem.MolToSmiles(removeAttachmentPoints(gbmol))
+          nodes.add(asmi)
+          edges.append(NetworkEdge(gbsmi, asmi, EdgeTypes.RemoveAttachmentEdge))
+
+    if params.includeScaffoldsWithoutAttachments:
+      asmi = Chem.MolToSmiles(removeAttachmentPoints(fmol))
+      nodes.add(asmi)
+      edges.append(NetworkEdge(fsmi, asmi, EdgeTypes.RemoveAttachmentEdge))
+
+  return nodes, edges
+
+
 # ------------------------------------
 #
 #  doctest boilerplate
@@ -127,7 +227,8 @@ def makeScaffoldGeneric(mol, doAtoms=True, doBonds=False):
 def _runDoctests(verbose=None):  # pragma: nocover
   import sys
   import doctest
-  failed, _ = doctest.testmod(optionflags=doctest.ELLIPSIS, verbose=verbose)
+  failed, _ = doctest.testmod(optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
+                              verbose=verbose)
   sys.exit(failed)
 
 
