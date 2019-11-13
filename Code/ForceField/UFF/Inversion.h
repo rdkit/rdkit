64ad081a6ad731799812ee5d7aedcc0fb17439ce
Yang Yang <y@dhcp-ccc-5210.eduroam.cornell.edu>
2019-11-13 15:22:21 -0500

    add getEnergyTerms function

--- Code/ForceField/UFF/Inversion.h
+++ Code/ForceField/UFF/Inversion.h
@@ -41,7 +41,7 @@ class RDKIT_FORCEFIELD_EXPORT InversionContrib : public ForceFieldContrib {
                    bool isCBoundToO, double oobForceScalingFactor = 1.0);
 
   double getEnergy(double *pos) const;
-  void getEnergyTerms(double *pos, std::vector<double>& resvec) const;
+//  void getEnergyTerms(double *pos, std::vector<double>& resvec) const;
   void getGrad(double *pos, double *grad) const;
   virtual InversionContrib *copy() const {
     return new InversionContrib(*this);
