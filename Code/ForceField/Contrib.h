e0c999ace750c31e013cf298ebd525843256939c
Yang Yang <y@Yangs-MBP.lan1>
2019-11-14 17:33:24 -0500

    add getEnergyTerms function at branch

--- Code/ForceField/Contrib.h
+++ Code/ForceField/Contrib.h
@@ -26,7 +26,14 @@ class RDKIT_FORCEFIELD_EXPORT ForceFieldContrib {
   //! returns our contribution to the energy of a position
   virtual double getEnergy(double *pos) const = 0;
     
-  virtual void getEnergyTerms(double *pos, std::vector<double> &resvec) const = 0;
+    virtual void getEnergyTerms(double *pos, std::vector<double> &resvec) const {
+        std::vector<double> e;
+        e.push_back(0.0);
+        e.push_back(0.0);
+        e.push_back(0.0);
+        resvec.push_back(e);
+        resvec.push_back(e);
+    }
 
   //! calculates our contribution to the gradients of a position
   virtual void getGrad(double *pos, double *grad) const = 0;
