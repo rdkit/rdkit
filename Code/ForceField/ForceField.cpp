6c4fe22a57f8194f5066c86d79aef0c44bb1246c
Yang Yang <y@Yangs-MBP.lan1>
2019-11-14 21:59:57 -0500

    add getEnergyTerms function at branch

--- Code/ForceField/ForceField.cpp
+++ Code/ForceField/ForceField.cpp
@@ -339,7 +339,10 @@ void ForceField::calcEnergyTerms(std::vector<std::vector<double>> &res) const{
   // now loop over the contribs
   for (const auto &d_contrib : d_contribs) {
     std::vector<double> e;
-    d_contrib->getEnergyTerms(pos, e);
+    e.push_back(0.0);
+    e.push_back(0.0);
+    e.push_back(0.0);
+//    d_contrib->getEnergyTerms(pos, e);
     res.push_back(e);
   }
   delete[] pos;
