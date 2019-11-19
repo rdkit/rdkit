d74e0d8cb48e7b0b723fadc261df44d2ea1dd770
Yang Yang <y@Yangs-MBP.lan1>
2019-11-18 22:51:11 -0500

    add overbonds function at branch2

--- Code/GraphMol/ForceFieldHelpers/Wrap/rdForceFields.cpp
+++ Code/GraphMol/ForceFieldHelpers/Wrap/rdForceFields.cpp
@@ -108,18 +108,15 @@ python::object UFFGetForceFieldTerms(
   UFF::getForceFieldTerms(
         mol, terms, vdwThresh, confId, ignoreInterfragInteractions);
 //  }
-//  for (std::vector<double> term: terms) {
+  for (std::vector<double> term: terms) {
 //    for (double e: term) {
 //      ETerm.append(e);
 //    }
-//    ETerms.append(python::make_tuple(term[0],term[1],term[2],term[3],term[4],term[5]));
+    ETerms.append(python::make_tuple(term[0],term[1],term[2],term[3],term[4],term[5]));
 //    ETerms.append(0.01);
 //    ETerms.append(0.01);
 //    ETerms.append(0.01);
-//  }
-  ETerms.append(0.01);
-  ETerms.append(0.01);
-  ETerms.append(0.01);
+  }
   return ETerms;
 }
 
