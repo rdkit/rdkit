bfdd5e1fd3db1ae6a25e075ab3f473120c45b3a9
ptosco <paolo.tosco.mail@gmail.com>
2019-12-04 13:40:57 +0000

    - response to PR review

--- Code/GraphMol/ForceFieldHelpers/MMFF/MMFF.h
+++ Code/GraphMol/ForceFieldHelpers/MMFF/MMFF.h
@@ -92,8 +92,9 @@ void MMFFOptimizeMoleculeConfs(ROMol &mol,
     ForceFieldsHelper::OptimizeMoleculeConfs(mol, *ff, res, numThreads, maxIters);
     delete ff;
   } else {
+    res.resize(mol.getNumConformers());
     for (unsigned int i = 0; i < mol.getNumConformers(); ++i) {
-      res.emplace_back(std::make_pair(static_cast<int>(-1), static_cast<double>(-1)));
+      res[i] = std::make_pair(static_cast<int>(-1), static_cast<double>(-1));
     }
   }
 }
