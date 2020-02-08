rdkit/rdkit

commit e245349c5a30a99230345e5d543f65abf3179614

Go To CR (#2431)

rdkit/Chem/MolStandardize/standardize.py
@@ -89,6 +89,7 @@ class Standardizer(object):
         :returns: The standardized molecule.
         :rtype: :rdkit:`Mol <Chem.rdchem.Mol-class.html>`
         """
+        mol_props = mol.GetPropsAsDict()
         mol = copy.deepcopy(mol)
         Chem.SanitizeMol(mol)
         mol = Chem.RemoveHs(mol)
@@ -96,6 +97,8 @@ class Standardizer(object):
         mol = self.normalize(mol)
         mol = self.reionize(mol)
         Chem.AssignStereochemistry(mol, force=True, cleanIt=True)
+        for k, v in mol_props.items():
+            mol.SetProp(k, v)
         # TODO: Check this removes symmetric stereocenters
         return mol
 

rdkit/Chem/MolStandardize/test_standardizer.py
@@ -1,0 +1,26 @@
+import unittest
+from rdkit import Chem
+from rdkit.Chem.MolStandardize.standardize import Standardizer
+
+
+class FakeStandardizer(Standardizer):
+    def normalize(self):
+        def fake_normalize(y):
+            props = y.GetPropsAsDict()
+            for k, v in props:
+                y.ClearProp(k)
+            return y
+        return fake_normalize
+
+class TestCase(unittest.TestCase):
+
+    def testPreserveProps(self):
+        PROP_NAME = "MyProp"
+        PROP_VALUE = "foo"
+        standardizer = FakeStandardizer()
+        m = Chem.MolFromSmiles("C")
+        m.SetProp(PROP_NAME, PROP_VALUE)
+
+        standardized_mol = standardizer.standardize(m)
+        self.assertTrue(standardized_mol.HasProp(PROP_NAME))
+        self.assertEqual(PROP_VALUE, standardized_mol.GetProp(PROP_NAME))

commit 5ac8547eebf2a1c9c5711022a68614b7c94d64d0

Fix regression introduced by e245349c

rdkit/Chem/MolStandardize/standardize.py
@@ -98,7 +98,7 @@ class Standardizer(object):
         mol = self.reionize(mol)
         Chem.AssignStereochemistry(mol, force=True, cleanIt=True)
         for k, v in mol_props.items():
-            mol.SetProp(k, v)
+            mol.SetProp(k, str(v))
         # TODO: Check this removes symmetric stereocenters
         return mol
 