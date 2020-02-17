d27845d9ea07248d1ac5106d0fa0e78b80b6786d
Brian Kelley <fustigator@gmail.com>
2020-02-17 09:10:21 -0500

    Add make_burden function, optimize symmetric eigenvalues

--- Code/GraphMol/Descriptors/BCUT.cpp
+++ Code/GraphMol/Descriptors/BCUT.cpp
@@ -21,66 +21,71 @@ namespace Descriptors {
   // off diagonal are 1/sqrt(bond_order)
   //  Original burden matrix was .1, .2, .3, .15 for single,double,triple or aromatic
   //  all other elements are .001
-  std::pair<double,double> BCUT2D(const ROMol &m, const std::vector<double> &atom_props) {
-    unsigned int num_atoms = m.getNumAtoms();
-    PRECONDITION(atom_props.size() == num_atoms, "Number of atom props not equal to number of atoms");
+namespace {
+std::unique_ptr<Eigen::MatrixXd>  make_burden(const ROMol &m) {
+  auto num_atoms = m.getNumAtoms();
+  std::unique_ptr<Eigen::MatrixXd> burden(
+	 new Eigen::MatrixXd(num_atoms, num_atoms));
     
-    if (num_atoms == 0) {
-        return std::pair<double,double>(0,0);
+  for(unsigned int i=0;i<num_atoms; ++i) {
+    for(unsigned int j=0;j<num_atoms; ++j) {
+      (*burden)(i,j) = (*burden)(j,i) = 0.001;
     }
-    
-    Eigen::MatrixXd burden(num_atoms, num_atoms); //, 0.001); Doesn't work?
-    
-    for(unsigned int i=0;i<num_atoms; ++i) {
-      for(unsigned int j=0;j<num_atoms; ++j) {
-	burden(i,j) = burden(j,i) = 0.001;
-      }
-      burden(i,i) = atom_props[i];
-    }
-
-    for(auto &bond : m.bonds()) {
-      unsigned int i = bond->getBeginAtomIdx();
-      unsigned int j = bond->getEndAtomIdx();
-      double score = 0.0;
-      switch(bond->getBondType()) {
-      case Bond::AROMATIC:
-	// score = 0.15; orig burden
-	score = 0.8164965809277261; // 1/sqrt(1.5)
-	break;
-      case Bond::SINGLE:
-	// score = 0.1;
-	score = 1.0; // 1/sqrt(1.0)
-	break;
-      case Bond::DOUBLE:
-	// score = 0.2;
-	score = 0.7071067811865475; // 1/sqrt(2.0)
-	break;
-      case Bond::TRIPLE:
-	// score = 0.3;	
-	score = 0.5773502691896258; // 1/sqrt(3);
-	break;
-      default:
-	CHECK_INVARIANT(0, "Bond order must be Single, Double, Triple or Aromatic");
-      }
-      burden(i,j) = burden(j,i) = score;
-    }    
-
-    /*
-      Eigen::IOFormat OctaveFmt(Eigen::StreamPrecision, 0, ", ", ";\n", "", "", "[", "]");
-      std::cerr << burden.format(OctaveFmt) << std::endl;
-    */
-    
-    Eigen::VectorXcd eivals = burden.eigenvalues();
-    double highest, lowest;
-    highest = lowest = eivals(0,0).real();
-    for (size_t i = 1, nRows = eivals.rows(); i < nRows; ++i) {
-      double value = eivals(i,0).real();
-      if(highest<value) highest = value;
-      if(lowest>value) lowest = value;
-    }
-    return std::pair<double,double>(highest,lowest);
   }
-
+  
+  for(auto &bond : m.bonds()) {
+    unsigned int i = bond->getBeginAtomIdx();
+    unsigned int j = bond->getEndAtomIdx();
+    double score = 0.0;
+    switch(bond->getBondType()) {
+    case Bond::AROMATIC:
+      // score = 0.15; orig burden
+      score = 0.8164965809277261; // 1/sqrt(1.5)
+      break;
+    case Bond::SINGLE:
+      // score = 0.1;
+      score = 1.0; // 1/sqrt(1.0)
+      break;
+    case Bond::DOUBLE:
+      // score = 0.2;
+      score = 0.7071067811865475; // 1/sqrt(2.0)
+      break;
+    case Bond::TRIPLE:
+      // score = 0.3;	
+      score = 0.5773502691896258; // 1/sqrt(3);
+      break;
+    default:
+      CHECK_INVARIANT(0, "Bond order must be Single, Double, Triple or Aromatic");
+    }
+    (*burden)(i,j) = (*burden)(j,i) = score;
+  }    
+  return burden;
+}
+  
+std::pair<double,double> BCUT2D(std::unique_ptr<Eigen::MatrixXd> &burden,
+				const std::vector<double> &atom_props) {
+  for(unsigned int i=0; i<atom_props.size(); ++i) {
+    (*burden)(i,i) = atom_props[i];
+  }
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(*burden);
+  auto eivals = es.eigenvalues();
+  double lowest = eivals(0);
+  double highest = eivals(atom_props.size()-1);
+  return std::pair<double,double>(highest,lowest);
+}
+}
+    
+std::pair<double,double> BCUT2D(const ROMol &m, const std::vector<double> &atom_props) {
+  unsigned int num_atoms = m.getNumAtoms();
+  PRECONDITION(atom_props.size() == num_atoms, "Number of atom props not equal to number of atoms");
+  
+  if (num_atoms == 0) {
+    return std::pair<double,double>(0,0);
+  }
+  auto burden = make_burden(m);
+  return BCUT2D(burden, atom_props);
+}
+  
 std::pair<double,double> BCUT2D(const ROMol &m, const std::string &atom_double_prop) {
   std::vector<double> props;
   props.reserve(m.getNumAtoms());
@@ -95,17 +100,24 @@ std::vector<double> BCUT2D(const ROMol &m) {
   // Atom elemno?  atomic mass? so many options...
   const std::string atom_prop = "atom_elemno";
   std::vector<double> masses;
+  std::vector<double> charges;
   masses.reserve(mol->getNumAtoms());
+  charges.reserve(mol->getNumAtoms());
   
   for(auto &atom: mol->atoms()) {
     masses.push_back(atom->getMass());
   }
+  
   // Gasteiger
   RDKit::computeGasteigerCharges(*mol, 12, true);
+  for(auto charge: mol->getProp<std::vector<double>>(common_properties::_GasteigerCharge)) {
+    charges.push_back(charge);
+  }
   // polarizability? - need model
   // slogp?  sasa?
-  auto atom_bcut = BCUT2D(*mol, masses);
-  auto gasteiger = BCUT2D(*mol, common_properties::_GasteigerCharge);
+  auto burden = make_burden(m);
+  auto atom_bcut = BCUT2D(burden, masses);
+  auto gasteiger = BCUT2D(burden, charges);
   std::vector<double> res = {atom_bcut.first, atom_bcut.second, gasteiger.first, gasteiger.second};
   return res;
 }
