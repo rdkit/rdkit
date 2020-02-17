e460c8698032050928be9e3a5eb325c24e3c5ad4
David Cosgrove <david@cozchemix.co.uk>
2020-02-17 09:46:43 +0000

    Took clang-tidy's advice about use of override.

--- Code/GraphMol/MolDraw2D/MolDraw2DSVG.h
+++ Code/GraphMol/MolDraw2D/MolDraw2DSVG.h
@@ -41,29 +41,29 @@ class RDKIT_MOLDRAW2D_EXPORT MolDraw2DSVG : public MolDraw2D {
   // set font size in molecule coordinate units. That's probably Angstrom for
   // RDKit. It will turned into drawing units using scale_, which might be
   // changed as a result, to make sure things still appear in the window.
-  virtual void setFontSize(double new_size) override;
-  virtual void setColour(const DrawColour &col) override;
+  void setFontSize(double new_size) override;
+  void setColour(const DrawColour &col) override;
 
   // not sure if this goes here or if we should do a dtor since initDrawing() is
   // called in the ctor,
   // but we'll start here
   void finishDrawing();
 
-  virtual void drawLine(const Point2D &cds1, const Point2D &cds2) override;
-  virtual void drawString(const std::string &str, const Point2D &cds) override;
-  virtual void drawPolygon(const std::vector<Point2D> &cds) override;
-  virtual void drawEllipse(const Point2D &cds1, const Point2D &cds2) override;
-  virtual void clearDrawing() override;
+  void drawLine(const Point2D &cds1, const Point2D &cds2) override;
+  void drawString(const std::string &str, const Point2D &cds) override;
+  void drawPolygon(const std::vector<Point2D> &cds) override;
+  void drawEllipse(const Point2D &cds1, const Point2D &cds2) override;
+  void clearDrawing() override;
 
-  virtual void drawWavyLine(const Point2D &cds1, const Point2D &cds2,
-                            const DrawColour &col1, const DrawColour &col2,
-                            unsigned int nSegments = 16,
-                            double vertOffset = 0.05) override;
+  void drawWavyLine(const Point2D &cds1, const Point2D &cds2,
+                    const DrawColour &col1, const DrawColour &col2,
+                    unsigned int nSegments = 16,
+                    double vertOffset = 0.05) override;
 
   // using the current scale, work out the size of the label in molecule
   // coordinates
-  virtual void getStringSize(const std::string &label, double &label_width,
-                             double &label_height) const override;
+  void getStringSize(const std::string &label, double &label_width,
+                     double &label_height) const override;
 
   // this only makes sense if the object was initialized without a stream
   std::string getDrawingText() const { return d_ss.str(); };
@@ -71,7 +71,7 @@ class RDKIT_MOLDRAW2D_EXPORT MolDraw2DSVG : public MolDraw2D {
   // adds additional tags to the atoms and bonds in the SVG. This should be
   // invoked *after* the molecule has been drawn
   using MolDraw2D::tagAtoms;  // Avoid overload warning.
-  void tagAtoms(const ROMol &mol) { tagAtoms(mol, 0.2); }
+  void tagAtoms(const ROMol &mol) override { tagAtoms(mol, 0.2); }
   void tagAtoms(const ROMol &mol, double radius,
                 const std::map<std::string, std::string> &events = {});
 
@@ -86,11 +86,11 @@ class RDKIT_MOLDRAW2D_EXPORT MolDraw2DSVG : public MolDraw2D {
   std::stringstream d_ss;
   std::string d_activeClass;
 
-  virtual void drawChar(char c, const Point2D &cds) override;
+  void drawChar(char c, const Point2D &cds) override;
   void initDrawing();
 
  protected:
-  virtual void drawBond(
+  void drawBond(
       const ROMol &mol, const Bond *bond, int at1_idx, int at2_idx,
       const std::vector<int> *highlight_atoms = nullptr,
       const std::map<int, DrawColour> *highlight_atom_map = nullptr,
