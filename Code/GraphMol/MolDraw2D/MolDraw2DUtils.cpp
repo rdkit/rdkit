81729759eadeb37f73ff2309b861bbf7d3e9db66
David Cosgrove <david@cozchemix.co.uk>
2020-03-17 09:54:54 +0000

    Updated JSON to drawOptionns.

--- Code/GraphMol/MolDraw2D/MolDraw2DUtils.cpp
+++ Code/GraphMol/MolDraw2D/MolDraw2DUtils.cpp
@@ -150,6 +150,7 @@ void updateDrawerParamsFromJSON(MolDraw2D &drawer, const std::string &json) {
   PT_OPT_GET(fixedBondLength);
   PT_OPT_GET(rotate);
   PT_OPT_GET(addStereoAnnotation);
+  PT_OPT_GET(atomHighlightCircles);
   get_colour_option(&pt, "highlightColour", opts.highlightColour);
   get_colour_option(&pt, "backgroundColour", opts.backgroundColour);
   get_colour_option(&pt, "legendColour", opts.legendColour);
