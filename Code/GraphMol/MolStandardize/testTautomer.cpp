c7e7d889405157090512a612940dc63766190c0c
Greg Landrum <greg.landrum@gmail.com>
2020-03-22 04:58:01 +0100

    Fixes #2990 (#3016)
    * add the test
    
    * Fixes #2990
    
    Needs more testing!
    
    * additional testing

--- Code/GraphMol/MolStandardize/testTautomer.cpp
+++ Code/GraphMol/MolStandardize/testTautomer.cpp
@@ -1030,7 +1030,6 @@ void testEnumerateDetails() {
       << "-----------------------\n Testing getting details back "
          "from tautomer enumeration"
       << std::endl;
-
   std::string rdbase = getenv("RDBASE");
   std::string tautomerFile =
       rdbase + "/Data/MolStandardize/tautomerTransforms.in";
@@ -1038,7 +1037,6 @@ void testEnumerateDetails() {
       new TautomerCatalogParams(tautomerFile));
   unsigned int ntautomers = tautparams->getNumTautomers();
   TEST_ASSERT(ntautomers == 34);
-
   TautomerEnumerator te(new TautomerCatalog(tautparams.get()));
   {
     auto mol = "c1ccccc1CN=c1[nH]cccc1"_smiles;
@@ -1060,6 +1058,79 @@ void testEnumerateDetails() {
   BOOST_LOG(rdInfoLog) << "Finished" << std::endl;
 }
 
+void testGithub2990() {
+  BOOST_LOG(rdInfoLog) << "-----------------------\n Testing Github #2990: "
+                          "Tautomer enumeration "
+                          "should remove stereo in all tautomers"
+                       << std::endl;
+  std::string rdbase = getenv("RDBASE");
+  std::string tautomerFile =
+      rdbase + "/Data/MolStandardize/tautomerTransforms.in";
+  auto tautparams = std::unique_ptr<TautomerCatalogParams>(
+      new TautomerCatalogParams(tautomerFile));
+  unsigned int ntautomers = tautparams->getNumTautomers();
+  TEST_ASSERT(ntautomers == 34);
+  TautomerEnumerator te(new TautomerCatalog(tautparams.get()));
+  {
+    // atom stereo
+    auto mol = "COC(=O)[C@@H](N)CO"_smiles;
+    TEST_ASSERT(mol);
+    auto tauts = te.enumerate(*mol);
+    for (const auto taut : tauts) {
+      auto smi = MolToSmiles(*taut);
+      // std::cerr << smi << std::endl;
+      TEST_ASSERT(smi.find("@H") == std::string::npos);
+    }
+  }
+  {
+    // atom stereo, atoms not in the tautomer zone are still ok
+    auto mol = "C[C@](Cl)(F)COC(=O)[C@@H](N)CO"_smiles;
+    TEST_ASSERT(mol);
+    auto tauts = te.enumerate(*mol);
+    for (const auto taut : tauts) {
+      auto smi = MolToSmiles(*taut);
+      // std::cerr << smi << std::endl;
+      TEST_ASSERT(smi.find("@H") == std::string::npos);
+      TEST_ASSERT(smi.find("@]") != std::string::npos);
+    }
+  }
+  {
+    // bond stereo
+    auto mol = "C/C=C/C/N=c1/[nH]cccc1"_smiles;
+    TEST_ASSERT(mol);
+    TEST_ASSERT(mol->getBondBetweenAtoms(0, 1)->getBondDir() !=
+                Bond::BondDir::NONE);
+    TEST_ASSERT(mol->getBondBetweenAtoms(2, 3)->getBondDir() !=
+                Bond::BondDir::NONE);
+    TEST_ASSERT(mol->getBondBetweenAtoms(3, 4)->getBondDir() !=
+                Bond::BondDir::NONE);
+    TEST_ASSERT(mol->getBondBetweenAtoms(5, 6)->getBondDir() !=
+                Bond::BondDir::NONE);
+    TEST_ASSERT(mol->getBondBetweenAtoms(1, 2)->getStereo() >
+                Bond::BondStereo::STEREOANY);
+    TEST_ASSERT(mol->getBondBetweenAtoms(4, 5)->getStereo() >
+                Bond::BondStereo::STEREOANY);
+
+    auto tauts = te.enumerate(*mol);
+    for (const auto taut : tauts) {
+      TEST_ASSERT(taut->getBondBetweenAtoms(0, 1)->getBondDir() !=
+                  Bond::BondDir::NONE);
+      TEST_ASSERT(taut->getBondBetweenAtoms(2, 3)->getBondDir() !=
+                  Bond::BondDir::NONE);
+      TEST_ASSERT(taut->getBondBetweenAtoms(3, 4)->getBondDir() ==
+                  Bond::BondDir::NONE);
+      TEST_ASSERT(taut->getBondBetweenAtoms(5, 6)->getBondDir() ==
+                  Bond::BondDir::NONE);
+      TEST_ASSERT(taut->getBondBetweenAtoms(1, 2)->getStereo() >
+                  Bond::BondStereo::STEREOANY);
+      TEST_ASSERT(taut->getBondBetweenAtoms(4, 5)->getStereo() ==
+                  Bond::BondStereo::STEREONONE);
+    }
+  }
+
+  BOOST_LOG(rdInfoLog) << "Finished" << std::endl;
+}
+
 int main() {
   RDLog::InitLogs();
 #if 1
@@ -1071,5 +1142,6 @@ int main() {
 #endif
   testPickCanonical2();
   testEnumerateDetails();
+  testGithub2990();
   return 0;
 }
