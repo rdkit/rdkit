07f214cc94d6caa29959bc050c13c44f51daadce
autodataming <1017044504@qq.com>
2020-06-21 15:30:37 +0800

    Add files via upload

---
+++ Docs/BOOK_zhCN/_static/baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1076f8c2100d9ac8370143ea7a39a2a4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
