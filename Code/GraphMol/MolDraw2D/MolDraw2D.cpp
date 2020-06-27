c1d3b1f980cdc52cc344b1d2a55b8847e2191786
Brian Kelley <fustigator@gmail.com>
2020-06-27 00:30:01 -0400

    fix molstogridimage on certain fragments/smarts patterns (#3217)
    * Fix issues with rendering fragments/smarts patterns in MolsToGridImage
    
    * Add LogStatus and BlockLogs functionality to temporarily block logging
    
    * Move BlockLogs to C++
    
    * Fix kekulization errors in reactions
    
    * Reenable tests
    
    * Use using over typedef
    
    * Fix comment placement
    
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>

--- Code/GraphMol/MolDraw2D/MolDraw2D.cpp
+++ Code/GraphMol/MolDraw2D/MolDraw2D.cpp
@@ -368,10 +368,12 @@ void MolDraw2D::get2DCoordsMol(RWMol &mol, double &offset, double spacing,
                                double &maxY, double &minY, int confId,
                                bool shiftAgents, double coordScale) {
   try {
+    RDLog::BlockLogs blocker;
     MolOps::sanitizeMol(mol);
   } catch (const MolSanitizeException &) {
     mol.updatePropertyCache(false);
     try {
+      RDLog::BlockLogs blocker;
       MolOps::Kekulize(mol, false);  // kekulize, but keep the aromatic flags!
     } catch (const MolSanitizeException &) {
       // don't need to do anything
@@ -380,9 +382,10 @@ void MolDraw2D::get2DCoordsMol(RWMol &mol, double &offset, double spacing,
   }
 
   const bool canonOrient = true;
+  const bool kekulize = false;  // don't kekulize, we just did that
   RDDepict::compute2DCoords(mol, nullptr, canonOrient);
   MolDraw2DUtils::prepareMolForDrawing(
-      mol, false);  // don't kekulize, we just did that
+      mol, kekulize);
   double minX = 1e8;
   double maxX = -1e8;
   double vShift = 0;
@@ -553,6 +556,9 @@ void MolDraw2D::drawReaction(
   get2DCoordsForReaction(nrxn, arrowBegin, arrowEnd, plusLocs, spacing,
                          confIds);
 
+  const bool originalPrepMols = drawOptions().prepareMolsBeforeDrawing;
+  drawOptions().prepareMolsBeforeDrawing = false;
+  
   ROMol *tmol = ChemicalReactionToRxnMol(nrxn);
   MolOps::findSSSR(*tmol);
 
@@ -684,7 +690,7 @@ void MolDraw2D::drawReaction(
 
   setColour(odc);
   text_drawer_->setFontScale(o_font_scale);
-
+  drawOptions().prepareMolsBeforeDrawing = originalPrepMols;
 }
 
 // ****************************************************************************
--- rdkit/Chem/Draw/UnitTestDraw.py
+++ rdkit/Chem/Draw/UnitTestDraw.py
@@ -13,6 +13,7 @@ import tempfile
 import unittest
 
 from rdkit import Chem
+from rdkit.Chem import AllChem
 from rdkit.Chem import rdDepictor
 from rdkit.Chem import Draw
 
@@ -232,6 +233,10 @@ class TestCase(unittest.TestCase):
         Draw.DrawRDKitBit(m,32,bi)
 
 
+  def testDrawReaction(self):
+    # this shouldn't throw an exception...
+    rxn = AllChem.ReactionFromSmarts("[c;H1:3]1:[c:4]:[c:5]:[c;H1:6]:[c:7]2:[nH:8]:[c:9]:[c;H1:1]:[c:2]:1:2.O=[C:10]1[#6;H2:11][#6;H2:12][N:13][#6;H2:14][#6;H2:15]1>>[#6;H2:12]3[#6;H1:11]=[C:10]([c:1]1:[c:9]:[n:8]:[c:7]2:[c:6]:[c:5]:[c:4]:[c:3]:[c:2]:1:2)[#6;H2:15][#6;H2:14][N:13]3")
+    img = Draw.ReactionToImage(rxn)
 
 
 if __name__ == '__main__':
