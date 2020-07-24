514ae750d9c15a0b09a50b79186fd9a9bc5702c9
Shrey Aryan <shrey183@gmail.com>
2020-07-24 16:48:21 +0200

    need to fix blocking

--- Code/GraphMol/FileParsers/testMultithreadedMolSupplier.cpp
+++ Code/GraphMol/FileParsers/testMultithreadedMolSupplier.cpp
@@ -9,6 +9,7 @@
 //
 #include <GraphMol/MonomerInfo.h>
 #include <GraphMol/RDKitBase.h>
+#include <GraphMol/SmilesParse/SmilesWrite.h>
 #include <RDGeneral/FileParseException.h>
 #include <RDGeneral/RDLog.h>
 #include <RDGeneral/test.h>
@@ -32,20 +33,26 @@ void testSmi(){
  	std::string mname;
   std::string rdbase = getenv("RDBASE");
   std::string fname =
-      rdbase + "/Code/GraphMol/FileParsers/test_data/fewSmi.2.csv";
+      rdbase + "/Code/GraphMol/FileParsers/test_data/fewSmi.csv";
 	unsigned int i;
-	MultithreadedSmilesMolSupplier sup(fname);
+	MultithreadedSmilesMolSupplier sup(fname, ",", 1, 0, true);
+	sup.startThreads();
+	std::cout << "threads started" << std::endl;
 	while(!sup.atEnd()){
+		std::cout << "we are calling next" << std::endl;
 		ROMol* mol = sup.next();
     if (i == 3) {
-      mol->getProp(common_properties::_Name, mname);
-      CHECK_INVARIANT(mname == "4", "");
-      mol->getProp("TPSA", mname);
-      CHECK_INVARIANT(mname == "82.78", "");
+			mol->getProp(common_properties::_Name, mname);
+  		CHECK_INVARIANT(mname == "4", "");
+  		mol->getProp("Column_2", mname);
+  		CHECK_INVARIANT(mname == "82.78", "");
     }
     delete mol;
     i++;
 	}
+
+	std::cout << "Number of lines = " << i << std::endl;
+
 	TEST_ASSERT(i == 10);
 }
 
