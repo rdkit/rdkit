803e01e3d2b1768703d677cd467a4bc992b4098e
Shrey Aryan <shrey183@gmail.com>
2020-07-24 16:51:47 +0200

    fix indentation

--- Code/GraphMol/FileParsers/testMultithreadedMolSupplier.cpp
+++ Code/GraphMol/FileParsers/testMultithreadedMolSupplier.cpp
@@ -10,10 +10,10 @@
 #include <GraphMol/MonomerInfo.h>
 #include <GraphMol/RDKitBase.h>
 #include <GraphMol/SmilesParse/SmilesWrite.h>
+#include <RDGeneral/ConcurrentQueue.h>
 #include <RDGeneral/FileParseException.h>
 #include <RDGeneral/RDLog.h>
 #include <RDGeneral/test.h>
-#include <RDGeneral/ConcurrentQueue.h>
 
 #include <boost/iostreams/device/file.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
@@ -29,31 +29,31 @@
 namespace io = boost::iostreams;
 using namespace RDKit;
 
-void testSmi(){
- 	std::string mname;
+void testSmi() {
+  std::string mname;
   std::string rdbase = getenv("RDBASE");
   std::string fname =
       rdbase + "/Code/GraphMol/FileParsers/test_data/fewSmi.csv";
-	unsigned int i;
-	MultithreadedSmilesMolSupplier sup(fname, ",", 1, 0, true);
-	sup.startThreads();
-	std::cout << "threads started" << std::endl;
-	while(!sup.atEnd()){
-		std::cout << "we are calling next" << std::endl;
-		ROMol* mol = sup.next();
+  unsigned int i;
+  MultithreadedSmilesMolSupplier sup(fname, ",", 1, 0, true);
+  sup.startThreads();
+  std::cout << "threads started" << std::endl;
+  while (!sup.atEnd()) {
+    std::cout << "we are calling next" << std::endl;
+    ROMol* mol = sup.next();
     if (i == 3) {
-			mol->getProp(common_properties::_Name, mname);
-  		CHECK_INVARIANT(mname == "4", "");
-  		mol->getProp("Column_2", mname);
-  		CHECK_INVARIANT(mname == "82.78", "");
+      mol->getProp(common_properties::_Name, mname);
+      CHECK_INVARIANT(mname == "4", "");
+      mol->getProp("Column_2", mname);
+      CHECK_INVARIANT(mname == "82.78", "");
     }
     delete mol;
     i++;
-	}
+  }
 
-	std::cout << "Number of lines = " << i << std::endl;
+  std::cout << "Number of lines = " << i << std::endl;
 
-	TEST_ASSERT(i == 10);
+  TEST_ASSERT(i == 10);
 }
 
 int main() {
@@ -63,5 +63,5 @@ int main() {
   testSmi();
   BOOST_LOG(rdErrorLog) << "Finished: testSmi()\n";
   BOOST_LOG(rdErrorLog) << "-----------------------------------------\n\n";
-	return 0;
+  return 0;
 }
