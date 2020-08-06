00df54b3aef9682eaef7fc9425a3e024a5c317fc
greg landrum <greg.landrum@gmail.com>
2020-08-06 13:59:12 +0200

    improve doctest

--- rdkit/Chem/__init__.py
+++ rdkit/Chem/__init__.py
@@ -102,12 +102,29 @@ def FindMolChiralCenters(mol, force=True, includeUnassigned=False, includeCIP=Tr
     >>> FindMolChiralCenters(mol,force=True,includeUnassigned=True)
     [(1, 'S'), (3, '?')]
 
-    The handling of unassigned stereocenters for dependent stereochemistry is not correct:
+    The handling of unassigned stereocenters for dependent stereochemistry is not correct 
+    using the legacy implementation:
 
     >>> Chem.FindMolChiralCenters(Chem.MolFromSmiles('C1CC(C)C(C)C(C)C1'),includeUnassigned=True)
     [(2, '?'), (6, '?')]
     >>> Chem.FindMolChiralCenters(Chem.MolFromSmiles('C1C[C@H](C)C(C)[C@H](C)C1'),includeUnassigned=True)
     [(2, 'S'), (4, '?'), (6, 'R')]
+
+    But works with the new implementation:
+
+    >>> Chem.FindMolChiralCenters(Chem.MolFromSmiles('C1CC(C)C(C)C(C)C1'),includeUnassigned=True, useLegacyImplementation=False)
+    [(2, '?'), (4, '?'), (6, '?')]
+
+    Note that the new implementation also gets the correct descriptors for para-stereochemistry:
+
+    >>> Chem.FindMolChiralCenters(Chem.MolFromSmiles('C1C[C@H](C)[C@H](C)[C@H](C)C1'),useLegacyImplementation=False)
+    [(2, 'S'), (4, 's'), (6, 'R')]
+
+    With the new implementation, if you don't care about the CIP labels of stereocenters, you can save
+    some time by disabling those:
+
+    >>> Chem.FindMolChiralCenters(Chem.MolFromSmiles('C1C[C@H](C)[C@H](C)[C@H](C)C1'), includeCIP=False, useLegacyImplementation=False)
+    [(2, 'Tet_CCW'), (4, 'Tet_CCW'), (6, 'Tet_CCW')]
 
   """
   if useLegacyImplementation:
