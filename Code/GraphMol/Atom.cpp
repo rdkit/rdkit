rdkit/rdkit

commit acf318c188220d686b21aee2401d7dbb5744f26a

Fixes #3470 (#3473)

Code/GraphMol/Atom.cpp
@@ -25,6 +25,9 @@ namespace RDKit {
 
 // Determine whether or not a molecule is to the left of Carbon
 bool isEarlyAtom(int atomicNum) {
+  if ( atomicNum <= 1 ) {
+    return false;
+  }
   switch (PeriodicTable::getTable()->getNouterElecs(atomicNum)) {
     case 1:
     case 2:

Code/GraphMol/catch_graphmol.cpp
@@ -1375,3 +1375,14 @@ TEST_CASE("hybridization of unknown atom types", "[bug][molops]") {
     }
   }
 }
+
+
+TEST_CASE("Github #3470: Hydrogen is incorrectly identified as an early atom", "[bug][chemistry]") {
+  SECTION("Basics") {
+    RWMol m;
+    m.addAtom(new Atom(1));
+    m.getAtomWithIdx(0)->setFormalCharge(-1);
+    m.updatePropertyCache();
+    CHECK(m.getAtomWithIdx(0)->getNumImplicitHs()==0);
+  }
+}

commit 2ef0a151f695f254c83f95fa42caa8995357e42a

fix problem with H+ caused by #3473

Code/GraphMol/Atom.cpp
@@ -25,7 +25,7 @@ namespace RDKit {
 
 // Determine whether or not a molecule is to the left of Carbon
 bool isEarlyAtom(int atomicNum) {
-  if ( atomicNum <= 1 ) {
+  if (atomicNum <= 1) {
     return false;
   }
   switch (PeriodicTable::getTable()->getNouterElecs(atomicNum)) {
@@ -329,6 +329,21 @@ int Atom::calcImplicitValence(bool strict) {
   if (d_explicitValence == -1) {
     this->calcExplicitValence(strict);
   }
+  // special cases
+  if (d_atomicNum == 0) {
+    d_implicitValence = 0;
+    return 0;
+  }
+  if (d_atomicNum == 1 && d_numRadicalElectrons == 0) {
+    if (d_formalCharge == 1 || d_formalCharge == -1) {
+      d_implicitValence = 0;
+      return 0;
+    } else if (d_formalCharge == 0) {
+      d_implicitValence = 1;
+      return 1;
+    }
+  }
+
   // this is basically the difference between the allowed valence of
   // the atom and the explicit valence already specified - tells how
   // many Hs to add

Code/GraphMol/catch_graphmol.cpp
@@ -1384,6 +1384,30 @@ TEST_CASE("Github #3470: Hydrogen is incorrectly identified as an early atom",
     m.getAtomWithIdx(0)->setFormalCharge(-1);
     m.updatePropertyCache();
     CHECK(m.getAtomWithIdx(0)->getNumImplicitHs() == 0);
+    m.getAtomWithIdx(0)->setFormalCharge(1);
+    m.updatePropertyCache();
+    CHECK(m.getAtomWithIdx(0)->getNumImplicitHs() == 0);
+    m.getAtomWithIdx(0)->setFormalCharge(0);
+    m.updatePropertyCache();
+    CHECK(m.getAtomWithIdx(0)->getNumImplicitHs() == 1);
+
+    // make sure we still generate errors for stupid stuff
+    m.getAtomWithIdx(0)->setFormalCharge(-2);
+    CHECK_THROWS_AS(m.updatePropertyCache(), AtomValenceException);
+    CHECK(m.getAtomWithIdx(0)->getNumImplicitHs() == 1);
+  }
+  SECTION("confirm with SMILES") {
+    RWMol m;
+    m.addAtom(new Atom(1));
+    m.getAtomWithIdx(0)->setFormalCharge(-1);
+    m.updatePropertyCache();
+    CHECK(MolToSmiles(m) == "[H-]");
+    m.getAtomWithIdx(0)->setFormalCharge(+1);
+    m.updatePropertyCache();
+    CHECK(MolToSmiles(m) == "[H+]");
+    m.getAtomWithIdx(0)->setFormalCharge(0);
+    m.updatePropertyCache();
+    CHECK(MolToSmiles(m) == "[HH]");  // ugly, but I think [H] would be worse
   }
 }
 