a7e0b3dc5c9de3764e36617076a0d95e92941ece
greg landrum <greg.landrum@gmail.com>
2020-11-19 06:45:31 +0100

    header fix

--- Code/DataStructs/Wrap/wrap_BitOps.cpp
+++ Code/DataStructs/Wrap/wrap_BitOps.cpp
@@ -1,6 +1,5 @@
-// $Id$
 //
-//  Copyright (C) 2003-2012 greg Landrum and Rational Discovery LLC
+//  Copyright (C) 2003-2020 greg Landrum and Rational Discovery LLC
 //
 //  @@ All Rights Reserved @@
 //  This file is part of the RDKit.
