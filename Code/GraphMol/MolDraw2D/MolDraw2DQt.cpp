708305c1bd56a172f0e9f3a7f95197d22f22d613
greg landrum <greg.landrum@gmail.com>
2020-12-02 06:21:41 +0100

    fix VS build problem

--- Code/GraphMol/MolDraw2D/MolDraw2DQt.cpp
+++ Code/GraphMol/MolDraw2D/MolDraw2DQt.cpp
@@ -104,13 +104,6 @@ void MolDraw2DQt::drawChar(char c, const Point2D &cds) {
 // ****************************************************************************
 void MolDraw2DQt::drawPolygon(const vector<Point2D> &cds) {
   PRECONDITION(cds.size() >= 3, "must have at least three points");
-#ifdef NOTYET
-  QBrush brush("Black");
-  brush.setStyle(Qt::SolidPattern);
-  DrawColour cc = colour();
-  brush.setColor(QColor(255.0 * cc.r, 255.0 * cc.g, 255.0 * cc.b));
-#endif
-
   d_qp.save();
   QBrush brush = d_qp.brush();
   if (fillPolys())
@@ -119,13 +112,14 @@ void MolDraw2DQt::drawPolygon(const vector<Point2D> &cds) {
     brush.setStyle(Qt::NoBrush);
   d_qp.setBrush(brush);
 
-  QPointF points[cds.size()];
+  QPointF *points = new QPointF[cds.size()];
   for (unsigned int i = 0; i < cds.size(); ++i) {
     Point2D lc = getDrawCoords(cds[i]);
     points[i] = QPointF(lc.x, lc.y);
   }
   d_qp.drawConvexPolygon(points, cds.size());
   d_qp.restore();
+  delete[] points;
 }
 
 // ****************************************************************************
@@ -138,75 +132,5 @@ void MolDraw2DQt::clearDrawing() {
   d_qp.setBackground(QBrush(this_col));
   d_qp.fillRect(offset().x, offset().y, width(), height(), this_col);
 }
-
-#if 0
-// ****************************************************************************
-void MolDraw2DQt::setFontSize(double new_size) {
-  MolDraw2D::setFontSize(new_size);
-  double font_size_in_points = fontSize() * scale();
-#ifdef NOTYET
-  cout << "initial font size in points : " << d_qp.font().pointSizeF() << endl;
-  cout << "font_size_in_points : " << font_size_in_points << endl;
-#endif
-  QFont font(d_qp.font());
-  font.setPointSizeF(font_size_in_points);
-  d_qp.setFont(font);
-
-  while (1) {
-    double old_font_size_in_points = font_size_in_points;
-    double font_size_in_points = fontSize() * scale();
-    if (fabs(font_size_in_points - old_font_size_in_points) < 0.1) {
-      break;
-    }
-    QFont font(d_qp.font());
-    font.setPointSizeF(font_size_in_points);
-    d_qp.setFont(font);
-    calculateScale();
-  }
-}
-#endif
-
-#if 0
-// ****************************************************************************
-// using the current scale, work out the size of the label in molecule
-// coordinates
-void MolDraw2DQt::getStringSize(const string &label, double &label_width,
-                                double &label_height) const {
-  label_width = 0.0;
-  label_height = 0.0;
-
-  TextDrawType draw_mode =
-      TextDrawNormal;  // 0 for normal, 1 for superscript, 2 for subscript
-  QString next_char(" ");
-  bool had_a_super = false;
-
-  for (int i = 0, is = label.length(); i < is; ++i) {
-    // setStringDrawMode moves i along to the end of any <sub> or <sup>
-    // markup
-    if ('<' == label[i] && setStringDrawMode(label, draw_mode, i)) {
-      continue;
-    }
-
-    next_char[0] = label[i];
-    QRectF br = d_qp.boundingRect(0, 0, 100, 100,
-                                 Qt::AlignBottom | Qt::AlignLeft, next_char);
-    label_height = br.height() / scale();
-    double char_width = br.width() / scale();
-    if (TextDrawSubscript == draw_mode) {
-      char_width *= 0.5;
-    } else if (TextDrawSuperscript == draw_mode) {
-      char_width *= 0.5;
-      had_a_super = true;
-    }
-    label_width += char_width;
-  }
-
-  // subscript keeps its bottom in line with the bottom of the bit chars,
-  // superscript goes above the original char top by a quarter
-  if (had_a_super) {
-    label_height *= 1.25;
-  }
-}
-#endif
 
 }  // namespace RDKit
