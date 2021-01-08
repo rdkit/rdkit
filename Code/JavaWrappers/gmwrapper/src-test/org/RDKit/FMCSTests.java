683ed34e6bd283b7217829a558c4278663b078fa
Braxton Robbason <braxton.robbason@gmail.com>
2021-01-08 17:50:16 -0500

    Fixing Java test

--- Code/JavaWrappers/gmwrapper/src-test/org/RDKit/FMCSTests.java
+++ Code/JavaWrappers/gmwrapper/src-test/org/RDKit/FMCSTests.java
@@ -1,20 +1,20 @@
-/* 
+/*
  *
  *  Copyright (c) 2014, Novartis Institutes for BioMedical Research Inc.
  *  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
- * met: 
+ * met:
  *
- *     * Redistributions of source code must retain the above copyright 
+ *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
- *       copyright notice, this list of conditions and the following 
- *       disclaimer in the documentation and/or other materials provided 
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *     * Neither the name of Novartis Institutes for BioMedical Research Inc. 
- *       nor the names of its contributors may be used to endorse or promote 
+ *     * Neither the name of Novartis Institutes for BioMedical Research Inc.
+ *       nor the names of its contributors may be used to endorse or promote
  *       products derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -41,13 +41,13 @@ public class FMCSTests extends GraphMolTest {
 	@Before public void setUp() {
 	}
 
-	@Test 
+	@Test
 	public void test1Basics() {
 		ROMol_Vect mols = new ROMol_Vect();
 		mols.add(RWMol.MolFromSmiles("c1ccccc1OC"));
 		mols.add(RWMol.MolFromSmiles("c1ccccc1C"));
 		mols.add(RWMol.MolFromSmiles("c1c(C)cccc1OC"));
-                
+
                 MCSResult mcs=RDKFuncs.findMCS(mols);
                 assertEquals(6,mcs.getNumAtoms());
                 assertEquals(6,mcs.getNumBonds());
@@ -55,15 +55,15 @@ public class FMCSTests extends GraphMolTest {
                 assertEquals(false,mcs.getCanceled());
 
 	}
-	@Test 
+	@Test
 	public void test2Basics() {
 		ROMol_Vect mols = new ROMol_Vect();
 		mols.add(RWMol.MolFromSmiles("c1ccccc1OC"));
 		mols.add(RWMol.MolFromSmiles("c1ccccc1C"));
 		mols.add(RWMol.MolFromSmiles("c1c(C)cccc1OC"));
 		mols.add(RWMol.MolFromSmiles("C1CCCCC1C"));
-                
-                MCSResult mcs=RDKFuncs.findMCS(mols,true,1,60,false,false,false,false,false,
+
+                MCSResult mcs=RDKFuncs.findMCS(mols,true,1,60,false,false,false,false,false, -1.0, new Int_Vect(),
                                                AtomComparator.AtomCompareElements,
                                                BondComparator.BondCompareAny);
                 assertEquals(6,mcs.getNumAtoms());
@@ -72,13 +72,13 @@ public class FMCSTests extends GraphMolTest {
                 assertEquals(false,mcs.getCanceled());
 
 	}
-	@Test 
+	@Test
 	public void test3Chirality() {
 		ROMol_Vect mols = new ROMol_Vect();
-        
+
 		mols.add(RWMol.MolFromSmiles("C[C@H](F)CCl"));
 		mols.add(RWMol.MolFromSmiles("C[C@H](F)C"));
-                
+
                 MCSResult mcs=RDKFuncs.findMCS_P(mols,"{\"MatchChiralTag\":true}");
                 assertEquals(4,mcs.getNumAtoms());
                 assertEquals(3,mcs.getNumBonds());
@@ -86,14 +86,14 @@ public class FMCSTests extends GraphMolTest {
                 assertEquals(false,mcs.getCanceled());
 
 	}
-	
+
 	@Test
 	public void testAtomCompareAnyHeavyAtom() {
 		ROMol_Vect mols = new ROMol_Vect();
 		mols.add(RWMol.MolFromSmiles("[H]c1ccccc1C",0, false));
 		mols.add(RWMol.MolFromSmiles("[H]c1ccccc1O",0, false));
 		        // H matches H, O matches C
-                MCSResult mcs=RDKFuncs.findMCS(mols,true,1,60,false,false,false,false,false,
+                MCSResult mcs=RDKFuncs.findMCS(mols,true,1,60,false,false,false,false,false, -1.0, new Int_Vect(),
                                                AtomComparator.AtomCompareAnyHeavyAtom,
                                                BondComparator.BondCompareAny);
                 assertEquals(8,mcs.getNumAtoms());
@@ -105,17 +105,17 @@ public class FMCSTests extends GraphMolTest {
 	@Test
 	public void testAtomCompareAnyHeavyAtom1() {
 		ROMol_Vect mols = new ROMol_Vect();
-			mols.add(RWMol.MolFromSmiles("[H]c1ccccc1C",0, false));
-			mols.add(RWMol.MolFromSmiles("Oc1ccccc1O",0, false));
-					// O matches C, H does not match O
-					MCSResult mcs=RDKFuncs.findMCS(mols,true,1,60,false,false,false,false,false,
-												   AtomComparator.AtomCompareAnyHeavyAtom,
-												   BondComparator.BondCompareAny);
+		mols.add(RWMol.MolFromSmiles("[H]c1ccccc1C",0, false));
+		mols.add(RWMol.MolFromSmiles("Oc1ccccc1O",0, false));
+		// O matches C, H does not match O
+		MCSResult mcs=RDKFuncs.findMCS(mols,true,1,60,false,false,false,false,false, -1.0, new Int_Vect(),
+		   AtomComparator.AtomCompareAnyHeavyAtom,
+                   BondComparator.BondCompareAny);
 					assertEquals(7,mcs.getNumAtoms());
 					assertEquals(7,mcs.getNumBonds());
 					//assertEquals("[#6]1:,-[#6]:,-[#6]:,-[#6]:,-[#6]:,-[#6]:,-1",mcs.getSmartsString());
 					assertEquals(false,mcs.getCanceled());
-	  
+
 	}
 
 	public static void main(String args[]) {
