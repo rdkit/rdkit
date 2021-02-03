6659668c7c4affb609510cbc45e96202e8cbc7b4
greg landrum <greg.landrum@gmail.com>
2021-02-03 08:42:55 +0100

    switch to using JSON from files

--- Code/GraphMol/RGroupDecomposition/catch_rgd.cpp
+++ Code/GraphMol/RGroupDecomposition/catch_rgd.cpp
@@ -37,7 +37,18 @@ void initDataset(T &suppl, ROMOL_SPTR &core, std::vector<ROMOL_SPTR> &mols) {
 
 std::string flatten_whitespace(const std::string &txt) {
   auto res = txt;
-  boost::algorithm::trim_all_if(res, boost::is_any_of(" \t\n"));
+  boost::algorithm::trim_fill_if(res, "", boost::is_any_of(" \t\n"));
+  return res;
+}
+
+std::string readReferenceData(const std::string &fname) {
+  std::ifstream ins(fname);
+  std::string res;
+  ins.seekg(0, std::ios::end);
+  res.reserve(ins.tellg());
+  ins.seekg(0, std::ios::beg);
+  res.assign((std::istreambuf_iterator<char>(ins)),
+             std::istreambuf_iterator<char>());
   return res;
 }
 TEST_CASE("simple1") {
@@ -54,23 +65,9 @@ TEST_CASE("simple1") {
     auto n = RGroupDecompose(cores, mols, rows);
     CHECK(n == mols.size());
     CHECK(rows.size() == mols.size());
-    CHECK(flatten_whitespace(toJSON(rows)) == flatten_whitespace(R"JSON([
-  {
-    "Core":"Cc1cccc([*:2])c1[*:1]",
-    "R1":"CO[*:1]",
-    "R2":"[H][*:2]"
-  },
-  {
-    "Core":"Cc1cccc([*:2])c1[*:1]",
-    "R1":"CO[*:1]",
-    "R2":"[H][*:2]"
-  },
-  {
-    "Core":"Cc1cccc([*:2])c1[*:1]",
-    "R1":"[H][*:1]",
-    "R2":"CO[*:2]"
-  }
-  ])JSON"));
+    CHECK(flatten_whitespace(toJSON(rows)) ==
+          flatten_whitespace(
+              readReferenceData(testDataDir + "simple1.out1.json")));
   }
   SECTION("no symmetrization") {
     RGroupRows rows;
@@ -79,26 +76,9 @@ TEST_CASE("simple1") {
     auto n = RGroupDecompose(cores, mols, rows, nullptr, ps);
     CHECK(n == mols.size());
     CHECK(rows.size() == mols.size());
-    CHECK(flatten_whitespace(toJSON(rows)) == flatten_whitespace(R"JSON([
-  {
-  "Core":"Cc1c([*:3])ccc([*:2])c1[*:1]",
-  "R1":"[H][*:1]",
-  "R2":"[H][*:2]",
-  "R3":"CO[*:3]"
-  },
-  {
-  "Core":"Cc1c([*:3])ccc([*:2])c1[*:1]",
-  "R1":"CO[*:1]",
-  "R2":"[H][*:2]",
-  "R3":"[H][*:3]"
-  },
-  {
-  "Core":"Cc1c([*:3])ccc([*:2])c1[*:1]",
-  "R1":"[H][*:1]",
-  "R2":"CO[*:2]",
-  "R3":"[H][*:3]"
-  }
-  ])JSON"));
+    CHECK(flatten_whitespace(toJSON(rows)) ==
+          flatten_whitespace(
+              readReferenceData(testDataDir + "simple1.out2.json")));
   }
 }
 
@@ -116,23 +96,9 @@ TEST_CASE("simple2 with specified R groups") {
     auto n = RGroupDecompose(cores, mols, rows);
     CHECK(n == mols.size());
     CHECK(rows.size() == mols.size());
-    CHECK(flatten_whitespace(toJSON(rows)) == flatten_whitespace(R"JSON([
-  {
-  "Core":"Cc1cc([*:1])ccc1[*:3]",
-  "R1":"[H][*:1]",
-  "R3":"CO[*:3]"
-  },
-  {
-  "Core":"Cc1cc([*:1])ccc1[*:3]",
-  "R1":"[H][*:1]",
-  "R3":"CO[*:3]"
-  },
-  {
-  "Core":"Cc1cc([*:1])ccc1[*:3]",
-  "R1":"CO[*:1]",
-  "R3":"[H][*:3]"
-  }
-  ])JSON"));
+    CHECK(flatten_whitespace(toJSON(rows)) ==
+          flatten_whitespace(
+              readReferenceData(testDataDir + "simple2.out1.json")));
   }
   SECTION("only match at r groups") {
     RGroupRows rows;
@@ -144,16 +110,9 @@ TEST_CASE("simple2 with specified R groups") {
     CHECK(rows.size() == n);
     CHECK(unmatched.size() == mols.size() - n);
     CHECK(unmatched[0] == 2);
-    CHECK(flatten_whitespace(toJSON(rows)) == flatten_whitespace(R"JSON([
-  {
-  "Core":"Cc1ccccc1[*:3]",
-  "R3":"CO[*:3]"
-  },
-  {
-  "Core":"Cc1ccccc1[*:3]",
-  "R3":"CO[*:3]"
-  }
-  ])JSON"));
+    CHECK(flatten_whitespace(toJSON(rows)) ==
+          flatten_whitespace(
+              readReferenceData(testDataDir + "simple2.out2.json")));
   }
 }
 
@@ -175,33 +134,9 @@ TEST_CASE("jm7b00306 Snippet") {
     // there is one structure in there that doesn't match the core
     CHECK(unmatched.size() == mols.size() - n);
     CHECK(unmatched[0] == 1);
-    CHECK(flatten_whitespace(toJSON(rows)) == flatten_whitespace(R"JSON([
-  {
-  "Core":"Cn1c(=O)n(C)c2cc([*:3])c(N3C(=O)c4cccc5c([*:1])c([*:2])cc(c45)C3=O)cc21",
-  "R1":"OCCC[*:1]",
-  "R3":"C[*:3]"
-  },
-  {
-  "Core":"Cn1c(=O)n(C)c2cc([*:3])c(N3C(=O)c4cccc5c([*:1])c([*:2])cc(c45)C3=O)cc21",
-  "R1":"[H][*:1]",
-  "R3":"[H][*:3]"
-  },
-  {
-  "Core":"Cn1c(=O)n(C)c2cc([*:3])c(N3C(=O)c4cccc5c([*:1])c([*:2])cc(c45)C3=O)cc21",
-  "R1":"[H][*:1]",
-  "R3":"C[*:3]"
-  },
-  {
-  "Core":"Cn1c(=O)n(C)c2cc([*:3])c(N3C(=O)c4cccc5c([*:1])c([*:2])cc(c45)C3=O)cc21",
-  "R1":"[H][*:1]",
-  "R3":"CO[*:3]"
-  },
-  {
-  "Core":"Cn1c(=O)n(C)c2cc([*:3])c(N3C(=O)c4cccc5c([*:1])c([*:2])cc(c45)C3=O)cc21",
-  "R1":"[H][*:1]",
-  "R3":"CN(C)[*:3]"
-  }
-  ])JSON"));
+    CHECK(flatten_whitespace(toJSON(rows)) ==
+          flatten_whitespace(
+              readReferenceData(testDataDir + "jm7b00306.excerpt.out1.json")));
   }
 }
 
@@ -223,42 +158,8 @@ TEST_CASE("jm200186n Snippet") {
     // there is one structure in there that doesn't match the core
     CHECK(unmatched.size() == mols.size() - n);
     CHECK(unmatched[0] == 3);
-    CHECK(flatten_whitespace(toJSON(rows)) == flatten_whitespace(R"JSON([
-  {
-  "Core":"c1c([*:3])cc([*:5])c(O[*:1])c1-c1cc([*:4])c(O[*:2])c([*:6])c1",
-  "R2":"C[*:2]",
-  "R3":"C=CC[*:3]",
-  "R4":"CC(Br)C[*:4]",
-  "R6":"[H][*:6]"
-  },
-  {
-  "Core":"c1c([*:3])cc([*:5])c(O[*:1])c1-c1cc([*:4])c(O[*:2])c([*:6])c1",
-  "R2":"C[*:2]",
-  "R3":"CC(Br)C[*:3]",
-  "R4":"C=CC[*:4]",
-  "R6":"[H][*:6]"
-  },
-  {
-  "Core":"c1c([*:3])cc([*:5])c(O[*:1])c1-c1cc([*:4])c(O[*:2])c([*:6])c1",
-  "R2":"[H][*:2]",
-  "R3":"C=CC[*:3]",
-  "R4":"C=CC[*:4]",
-  "R6":"[H][*:6]"
-  },
-  {
-  "Core":"c1c([*:3])cc([*:5])c(O[*:1])c1-c1cc([*:4])c(O[*:2])c([*:6])c1",
-  "R2":"C[*:2]",
-  "R3":"C=CC[*:3]",
-  "R4":"C=CC[*:4]",
-  "R6":"[H][*:6]"
-  },
-  {
-  "Core":"c1c([*:3])cc([*:5])c(O[*:1])c1-c1cc([*:4])c(O[*:2])c([*:6])c1",
-  "R2":"C[*:2]",
-  "R3":"C=CC[*:3]",
-  "R4":"C=CC[*:4]",
-  "R6":"F[*:6]"
-  }
-  ])JSON"));
+    CHECK(flatten_whitespace(toJSON(rows)) ==
+          flatten_whitespace(
+              readReferenceData(testDataDir + "jm200186n.excerpt.out1.json")));
   }
 }
