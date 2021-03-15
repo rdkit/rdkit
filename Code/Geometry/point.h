rdkit/rdkit

commit 0e89b2dbea1c16fc5a82a70cefa888090835a684

cleanup a bunch of compiler warnings (#3849)
* cleanup a bunch of g++ warnings

* make it work with clang

* remove some additional warnings based on CI builds

* fix that version number

* stop being verbose when building

Code/Geometry/point.h
@@ -41,6 +41,12 @@ class RDKIT_RDGEOMETRYLIB_EXPORT Point {
 
   virtual Point *copy() const = 0;
 };
+// g++ (at least as of v9.3.0) generates some spurious warnings from here.
+// disable them
+#if !defined(__clang__) and defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 
 // typedef class Point3D Point;
 class RDKIT_RDGEOMETRYLIB_EXPORT Point3D : public Point {
@@ -501,6 +507,9 @@ class RDKIT_RDGEOMETRYLIB_EXPORT PointND : public Point {
     return dp_storage.get();
   }
 };
+#if !defined(__clang__) and defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
 
 typedef std::vector<RDGeom::Point *> PointPtrVect;
 typedef PointPtrVect::iterator PointPtrVect_I;

.azure-pipelines/linux_build_java.yml
@@ -38,7 +38,7 @@ steps:
   displayName: Configure build (Run CMake)
 - bash: |
     cd build
-    VERBOSE=1 make -j $( $(number_of_cores) ) install
+    make -j $( $(number_of_cores) ) install
   displayName: Build
 - bash: |
     export RDBASE=`pwd`

Code/GraphMol/ChemTransforms/MolFragmenter.cpp
@@ -807,7 +807,7 @@ struct ZipBond {
         std::vector<Atom *> atoms;
         bool has_dummy = false;
         for (auto idx : bond->getStereoAtoms()) {
-          if (idx == dummy_atom->getIdx()) {
+          if (static_cast<unsigned>(idx) == dummy_atom->getIdx()) {
             atoms.push_back(new_atom);
             has_dummy = true;
           } else {

Code/GraphMol/DistGeomHelpers/testDgeomHelpers.cpp
@@ -2319,6 +2319,7 @@ void testGithub3019() {
 
 namespace {
 void throwerror(unsigned int notUsedHere) {
+  RDUNUSED_PARAM(notUsedHere);
   throw ValueErrorException("embedder is abortable");
 }
 }  // namespace

Code/GraphMol/FileParsers/MolSGroupWriting.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2002-2018 Greg Landrum and T5 Informatics GmbH
+//  Copyright (C) 2002-2021 Greg Landrum and T5 Informatics GmbH
 //
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <boost/algorithm/string/trim.hpp>
+#include <boost/format.hpp>
 #include <GraphMol/SubstanceGroup.h>
 
 namespace RDKit {
@@ -20,21 +21,18 @@ typedef std::unordered_map<int, SubstanceGroup> IDX_TO_SGROUP_MAP;
 /* ------------------ Inlined Formatters ------------------ */
 
 inline std::string FormatV2000IntField(int value) {
-  char output[5];
-  snprintf(output, 5, " %3d", value);
-  return std::string(output);
+  auto fmt = boost::format(" %3d") % value;
+  return fmt.str();
 }
 
 inline std::string FormatV2000NumEntriesField(int value) {
-  char output[4];
-  snprintf(output, 4, " %2d", value);
-  return std::string(output);
+  auto fmt = boost::format(" %2d") % value;
+  return fmt.str();
 }
 
 inline std::string FormatV2000DoubleField(double value) {
-  char output[11];
-  snprintf(output, 11, "%10.4f", value);
-  return std::string(output);
+  auto fmt = boost::format("%10.4f") % value;
+  return fmt.str();
 }
 
 inline std::string FormatV2000StringField(const std::string &value,

Code/GraphMol/FileParsers/testMolSupplier.cpp
@@ -2829,6 +2829,7 @@ void testGitHub3517() {
   SDMolSupplier sdsup(fname);
   TEST_ASSERT(!sdsup.atEnd());
   size_t l = sdsup.length();
+  TEST_ASSERT(l > 0);
   TEST_ASSERT(!sdsup.atEnd());
 }
 

Code/GraphMol/Fingerprints/MorganFingerprints.cpp
@@ -39,9 +39,12 @@
 #include <GraphMol/SmilesParse/SmilesParse.h>
 #include <GraphMol/Substruct/SubstructMatch.h>
 
+#include <RDGeneral/BoostStartInclude.h>
 #include <boost/dynamic_bitset.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/tuple/tuple_comparison.hpp>
+#include <RDGeneral/BoostEndInclude.h>
+
 #include <algorithm>
 
 #include <GraphMol/Fingerprints/FingerprintUtil.h>

Code/GraphMol/Fingerprints/MorganGenerator.cpp
@@ -15,9 +15,11 @@
 #include <GraphMol/SmilesParse/SmilesParse.h>
 #include <GraphMol/Substruct/SubstructMatch.h>
 
+#include <RDGeneral/BoostStartInclude.h>
 #include <boost/dynamic_bitset.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/tuple/tuple_comparison.hpp>
+#include <RDGeneral/BoostEndInclude.h>
 
 #include <GraphMol/Fingerprints/FingerprintUtil.h>
 

commit c30bc19781166b0be89ee47ce5e86a55d4d3c8c9

remove an MSVC++ warning caused by #3849

Code/Geometry/point.h
@@ -41,11 +41,13 @@ class RDKIT_RDGEOMETRYLIB_EXPORT Point {
 
   virtual Point *copy() const = 0;
 };
+#ifndef _MSC_VER
 // g++ (at least as of v9.3.0) generates some spurious warnings from here.
 // disable them
 #if !defined(__clang__) and defined(__GNUC__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
 #endif
 
 // typedef class Point3D Point;
@@ -507,8 +509,10 @@ class RDKIT_RDGEOMETRYLIB_EXPORT PointND : public Point {
     return dp_storage.get();
   }
 };
+#ifndef _MSC_VER
 #if !defined(__clang__) and defined(__GNUC__)
 #pragma GCC diagnostic pop
+#endif
 #endif
 
 typedef std::vector<RDGeom::Point *> PointPtrVect;