939f8f148a40378e91c616fbe54022324e65372a
Ric <ricrogz@users.noreply.github.com>
2021-03-15 23:56:36 -0400

    fix exports (#3935)

--- Code/GraphMol/MolDraw2D/DrawText.h
+++ Code/GraphMol/MolDraw2D/DrawText.h
@@ -17,6 +17,7 @@
 #include <string>
 #include <vector>
 
+#include <RDGeneral/export.h>
 #include <Geometry/point.h>
 #include <GraphMol/MolDraw2D/MolDraw2D.h>
 
@@ -34,7 +35,7 @@ std::ostream &operator<<(std::ostream &oss, const TextDrawType &tdt);
 std::ostream &operator<<(std::ostream &oss, const OrientType &o);
 
 // ****************************************************************************
-class DrawText {
+class RDKIT_MOLDRAW2D_EXPORT DrawText {
  public:
   static constexpr double FONT_SIZE = 0.6;  // seems to be a good number
 
@@ -165,8 +166,9 @@ class DrawText {
 //! mode based on contents of instring from i onwards. Increments i
 //! appropriately
 //! \returns true or false depending on whether it did something or not
-bool setStringDrawMode(const std::string &instring, TextDrawType &draw_mode,
-                       size_t &i);
+RDKIT_MOLDRAW2D_EXPORT bool setStringDrawMode(const std::string &instring,
+                                              TextDrawType &draw_mode,
+                                              size_t &i);
 
 // take the label for the given atom and return the individual pieces
 // that need to be drawn for it.  So NH<sub>2</sub> will return
