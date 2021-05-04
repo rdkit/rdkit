848204a4da0bb7e6b54f8d7bce164636aa19e317
greg landrum <greg.landrum@gmail.com>
2021-05-04 10:47:58 +0200

    cleanup

--- Code/GraphMol/MolStandardize/TransformCatalog/TransformCatalogParams.h
+++ Code/GraphMol/MolStandardize/TransformCatalog/TransformCatalogParams.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2018 Susan H. Leung
+//  Copyright (C) 2018-2021 Susan H. Leung and other RDKit contributors
 //
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
@@ -8,8 +8,8 @@
 //  of the RDKit source tree.
 //
 #include <RDGeneral/export.h>
-#ifndef __RD_TRANSFORM_CATALOG_PARAMS_H__
-#define __RD_TRANSFORM_CATALOG_PARAMS_H__
+#ifndef RD_TRANSFORM_CATALOG_PARAMS_H
+#define RD_TRANSFORM_CATALOG_PARAMS_H
 
 #include <Catalogs/CatalogParams.h>
 #include "TransformCatalogUtils.h"
