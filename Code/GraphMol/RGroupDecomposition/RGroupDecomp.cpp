96225806607aa7724c8bb2203ef6a0c773fe298d
Gareth Jones <java.jones@gmail.com>
2021-06-01 06:49:18 -0600

    Comments added to RGD core matching (#4189)
    * Comments added to RGD core matching
    
    * Reformatted code

--- Code/GraphMol/RGroupDecomposition/RGroupDecomp.cpp
+++ Code/GraphMol/RGroupDecomposition/RGroupDecomp.cpp
@@ -100,11 +100,18 @@ int RGroupDecomposition::add(const ROMol &inmol) {
       const bool uniquify = false;
       const bool recursionPossible = true;
       const bool useChirality = true;
+      // matching the core to the molecule is a two step process
+      // First match to a reduced representation (the core minus terminal
+      // R-groups). Next, match the R-groups. We do this as the core may not be
+      // a substructure match for the molecule if a single molecule atom matches
+      // 2 RGroup attachments (see https://github.com/rdkit/rdkit/pull/4002)
       std::vector<MatchVectType> baseMatches;
+      // match reduced representation
       SubstructMatch(mol, *core.second.matchingMol, baseMatches, uniquify,
                      recursionPossible, useChirality);
       tmatches.clear();
       for (const auto &baseMatch : baseMatches) {
+        // Match the R Groups
         auto matchesWithDummy =
             core.second.matchTerminalUserRGroups(mol, baseMatch);
         tmatches.insert(tmatches.end(), matchesWithDummy.cbegin(),
