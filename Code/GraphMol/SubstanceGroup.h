48aef1c6ce8dbe51a1436e9489ab91597b676ff1
Ric <ricrogz@gmail.com>
2021-06-03 10:11:41 -0400

    use copy/move instead of ref

--- Code/GraphMol/SubstanceGroup.h
+++ Code/GraphMol/SubstanceGroup.h
@@ -141,9 +141,9 @@ class RDKIT_GRAPHMOL_EXPORT SubstanceGroup : public RDProps {
   const std::vector<unsigned int> &getParentAtoms() const { return d_patoms; }
   const std::vector<unsigned int> &getBonds() const { return d_bonds; }
 
-  void setAtoms(const std::vector<unsigned int> & atoms) { d_atoms = atoms; }
-  void setParentAtoms(const std::vector<unsigned int> & patoms) { d_patoms = patoms; }
-  void setBonds(const std::vector<unsigned int> &bonds) { d_bonds = bonds; }
+  void setAtoms(std::vector<unsigned int> atoms) { d_atoms = std::move(atoms); }
+  void setParentAtoms(std::vector<unsigned int> patoms) { d_patoms = std::move(patoms); }
+  void setBonds(std::vector<unsigned int> bonds) { d_bonds = std::move(bonds); }
 
   const std::vector<Bracket> &getBrackets() const { return d_brackets; }
   const std::vector<CState> &getCStates() const { return d_cstates; }
