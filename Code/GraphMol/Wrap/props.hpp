0f18dba0ed86b1eeadfa0ea8f66f9d3464543600
Brian Kelley <fustigator@gmail.com>
2021-06-07 22:19:35 -0400

    Fix/rdprop integer conversions (#4194)
    * Allow int->int64 conversions
    
    * Add original python test
    
    * Add self to class method
    
    * Quick cout to see what the error really is
    
    * Add overflow checks to integer conversions in getProp
    
    * Fix conversions, convert to ValueError in python for bad conversions
    
    * Trigger tests to run again
    
    * Revert "Quick cout to see what the error really is"
    
    This reverts commit 70feea687e6ee3d2bd831a3814dd61826ed77291.

--- Code/GraphMol/Wrap/props.hpp
+++ Code/GraphMol/Wrap/props.hpp
@@ -95,6 +95,7 @@ boost::python::dict GetPropsAsDict(const T &obj, bool includePrivate,
   return dict;
 }
 
+  
 template <class RDOb, class T>
 T GetProp(RDOb *ob, const char *key) {
   T res;
@@ -104,11 +105,12 @@ T GetProp(RDOb *ob, const char *key) {
       throw python::error_already_set();
     }
     return res;
-  } catch (const boost::bad_any_cast &) {
+  } catch (const std::exception&e ) {
     throw ValueErrorException(std::string("key `") + key +
                               "` exists but does not result in " +
-                              GetTypeName<T>());
+                              GetTypeName<T>() + " reason: " + e.what());
   }
+
   return res;
 }
 
