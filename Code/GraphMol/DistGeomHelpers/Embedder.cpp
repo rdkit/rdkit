4144a813cafc243a0a209ffd201167cc181a2f74
Greg Landrum <greg.landrum@gmail.com>
2021-07-23 20:30:33 +0200

    Fixes #4346 : trans bonds are now properly honored in macrocycles (#4355)
    * remove some compile warnings
    
    * Fixes #4346

--- Code/GraphMol/DistGeomHelpers/Embedder.cpp
+++ Code/GraphMol/DistGeomHelpers/Embedder.cpp
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2004-2019 Greg Landrum and Rational Discovery LLC
+//  Copyright (C) 2004-2021 Greg Landrum and other RDKit contributors
 //
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
