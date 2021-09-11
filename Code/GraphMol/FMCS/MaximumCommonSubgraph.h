404eabcd27df41203e280dae9a9c493b52b7c6b9
Ric <ricrogz@gmail.com>
2021-09-10 23:28:43 -0400

    change fix

--- Code/GraphMol/FMCS/MaximumCommonSubgraph.h
+++ Code/GraphMol/FMCS/MaximumCommonSubgraph.h
@@ -90,13 +90,7 @@ class RDKIT_FMCS_EXPORT MaximumCommonSubgraph {
  private:
   void clear() {
     Targets.clear();
-    auto pos = faked_ring_info.find_first();
-    while (pos != boost::dynamic_bitset<>::npos) {
-      Molecules[pos]->getRingInfo()->reset();
-      pos = faked_ring_info.find_next(pos);
-    }
     Molecules.clear();
-    faked_ring_info.clear();
     To = nanoClock();
   }
   void init();
@@ -109,8 +103,6 @@ class RDKIT_FMCS_EXPORT MaximumCommonSubgraph {
 
   bool match(Seed& seed);
   bool matchIncrementalFast(Seed& seed, unsigned itarget);
-
-  boost::dynamic_bitset<> faked_ring_info;
 };
 }  // namespace FMCS
 }  // namespace RDKit
