7bff066f8e6e91caa1eb018dcd264cf94e7efbe4
Gareth Jones <java.jones@gmail.com>
2021-11-14 19:23:13 -0700

    Fix for cores with atom lists and unit test

--- Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
+++ Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
@@ -2458,6 +2458,54 @@ C[*:2]
   }
 }
 
+void testCoreWithAlsRecords() {
+  auto core = R"CTAB(
+  Mrv2008 11112113312D
+
+  6  6  6  0  0  0            999 V2000
+  -13.7277    2.6107    0.0000 L   0  0  0  0  0  0  0  0  0  0  0  0
+  -14.4421    2.1982    0.0000 L   0  0  0  0  0  0  0  0  0  0  0  0
+  -14.4421    1.3732    0.0000 L   0  0  0  0  0  0  0  0  0  0  0  0
+  -13.7277    0.9607    0.0000 L   0  0  0  0  0  0  0  0  0  0  0  0
+  -13.0132    1.3732    0.0000 L   0  0  0  0  0  0  0  0  0  0  0  0
+  -13.0132    2.1982    0.0000 L   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  1  0  0  0  0
+  2  3  2  0  0  0  0
+  3  4  1  0  0  0  0
+  4  5  2  0  0  0  0
+  5  6  1  0  0  0  0
+  1  6  2  0  0  0  0
+  1 F    2   6   7
+  2 F    2   6   7
+  3 F    2   6   7
+  4 F    2   6   7
+  5 F    2   6   7
+  6 F    2   6   7
+M  ALS   1  2 F C   N   
+M  ALS   2  2 F C   N   
+M  ALS   3  2 F C   N   
+M  ALS   4  2 F C   N   
+M  ALS   5  2 F C   N   
+M  ALS   6  2 F C   N   
+M  END
+
+)CTAB"_ctab;
+  TEST_ASSERT(core);
+
+  auto structure = "ClC1=CN=C(C=C1)N1CCCC1"_smiles;
+  RGroupDecomposition decomp(*core);
+  TEST_ASSERT(decomp.add(*structure) == 0);
+  decomp.process();
+  auto rows = decomp.getRGroupsAsRows();
+  auto core_out = rows[0]["Core"];
+  auto core_mol_block = MolToMolBlock(*rows[0]["Core"]);
+  auto pos = core_mol_block.find("ALS");
+  TEST_ASSERT(pos == std::string::npos);
+  std::string expected("Core:c1cc([*:2])ncc1[*:1] R1:Cl[*:1] R2:C1CCN([*:2])C1");
+  RGroupRows::const_iterator it = rows.begin();
+  CHECK_RGROUP(it, expected);
+}
+
 int main() {
   RDLog::InitLogs();
   boost::logging::disable_logs("rdApp.debug");
@@ -2505,6 +2553,7 @@ int main() {
   testNoTempLabels();
   testNoSideChains();
   testDoNotAddUnnecessaryRLabels();
+  testCoreWithAlsRecords();
   BOOST_LOG(rdInfoLog)
       << "********************************************************\n";
   return 0;
