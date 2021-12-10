bf3ec372aa2dc68a99a065bca239903a0d9ec737
Greg Landrum <greg.landrum@gmail.com>
2021-12-10 16:04:28 +0100

    Fixes #4787 (#4788)
    * fixes #4787
    
    * additional test
    
    * version bump
    
    * do the same thing with SMILES
    
    * Update Code/PgSQL/rdkit/adapter.cpp
    
    Co-authored-by: Paolo Tosco <paolo.tosco.mail@gmail.com>
    
    Co-authored-by: Paolo Tosco <paolo.tosco.mail@gmail.com>

--- Code/PgSQL/rdkit/adapter.cpp
+++ Code/PgSQL/rdkit/adapter.cpp
@@ -171,7 +171,8 @@ extern "C" CROMol parseMolText(char *data, bool asSmarts, bool warnOnFail,
       } else {
         mol = SmilesToMol(data, 0, false);
         if (mol != nullptr) {
-          MolOps::sanitizeMol(*mol);
+          mol->updatePropertyCache(false);
+          MolOps::setAromaticity(*mol);
           MolOps::mergeQueryHs(*mol);
         }
       }
@@ -222,8 +223,10 @@ extern "C" CROMol parseMolCTAB(char *data, bool keepConformer, bool warnOnFail,
     if (!asQuery) {
       mol = MolBlockToMol(data);
     } else {
-      mol = MolBlockToMol(data, true, false);
+      mol = MolBlockToMol(data, false, false);
       if (mol != nullptr) {
+        mol->updatePropertyCache(false);
+        MolOps::setAromaticity(*mol);
         MolOps::mergeQueryHs(*mol);
       }
     }
