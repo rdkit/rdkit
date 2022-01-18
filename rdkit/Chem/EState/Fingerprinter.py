ca2b74a94c9ed4a9253498eab2a1bf3fac1f0b7c
Ichiru Take <66363520+IchiruTake@users.noreply.github.com>
2022-01-18 11:26:35 +0700

    Clean-up Python #4815 - P1.4: Chem\EState
    This is the sub-PR breaking from the large PR 4815: https://github.com/rdkit/rdkit/pull/4815
    
    -----------------------------------
    Updated Changes:
    - [New]: Update function `EStateIndicies` in Estate.py

--- rdkit/Chem/EState/Fingerprinter.py
+++ rdkit/Chem/EState/Fingerprinter.py
@@ -35,15 +35,14 @@ def FingerprintMol(mol):
   esIndices = EStateIndices(mol)
 
   nPatts = len(AtomTypes.esPatterns)
-  counts = numpy.zeros(nPatts, numpy.int)
-  sums = numpy.zeros(nPatts, numpy.float)
-
+  counts = [0] * nPatts
+  sums = [0.0] * nPatts
   for i, (_, pattern) in enumerate(AtomTypes.esPatterns):
     matches = mol.GetSubstructMatches(pattern, uniquify=1)
     counts[i] = len(matches)
     for match in matches:
       sums[i] += esIndices[match[0]]
-  return counts, sums
+  return numpy.array(counts, dtype='int'), numpy.array(sums, dtype='float')
 
 
 def _exampleCode():
