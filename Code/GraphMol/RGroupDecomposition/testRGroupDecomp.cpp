57373910df834d4385332ad6e5b2fe07e5a075bd
Paolo Tosco <paolo.tosco.mail@gmail.com>
2022-01-22 17:37:44 +0100

    Update Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>

--- Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
+++ Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
@@ -2748,7 +2748,6 @@ void testDoNotChooseUnrelatedCores() {
         TEST_ASSERT(decomp.add(*m) == j++);
       }
       TEST_ASSERT(decomp.process());
-      std::stringstream ss;
       auto cols = decomp.getRGroupsAsColumns();
       const auto &core = cols["Core"];
       TEST_ASSERT(core.size() == 3);
