5f452630961b8b8076d61345a7a0d4c290deac01
Ichiru Take <66363520+IchiruTake@users.noreply.github.com>
2022-01-26 16:23:33 +0700

    Update BRICS.py

--- rdkit/Chem/BRICS.py
+++ rdkit/Chem/BRICS.py
@@ -398,10 +398,10 @@ def BRICSDecompose(mol, allNodes=None, minFragmentSize=1, onlyUseReactions=None,
     >>> sorted(res)
     ['[14*]c1ccccn1', '[16*]c1cccc([16*])c1', '[3*]O[3*]', '[4*]CCC', '[4*]C[8*]']
 
-    >>> res = list(BRICSDecompose(m,returnMols=True))
+    >>> res = list(BRICSDecompose(m, returnMols=True))
     >>> res[0]
     <rdkit.Chem.rdchem.Mol object ...>
-    >>> smis = [Chem.MolToSmiles(x,True) for x in res]
+    >>> smis = [Chem.MolToSmiles(x) for x in res]
     >>> sorted(smis)
     ['[14*]c1ccccn1', '[16*]c1cccc([16*])c1', '[3*]O[3*]', '[4*]CCC', '[4*]C[8*]']
 
@@ -415,33 +415,33 @@ def BRICSDecompose(mol, allNodes=None, minFragmentSize=1, onlyUseReactions=None,
     it's also possible to keep pieces that haven't been fully decomposed:
 
     >>> m = Chem.MolFromSmiles('CCCOCC')
-    >>> res = list(BRICSDecompose(m,keepNonLeafNodes=True))
+    >>> res = list(BRICSDecompose(m, keepNonLeafNodes=True))
     >>> sorted(res)
     ['CCCOCC', '[3*]OCC', '[3*]OCCC', '[3*]O[3*]', '[4*]CC', '[4*]CCC']
 
     >>> m = Chem.MolFromSmiles('CCCOCc1cc(c2ncccc2)ccc1')
-    >>> res = list(BRICSDecompose(m,keepNonLeafNodes=True))
+    >>> res = list(BRICSDecompose(m, keepNonLeafNodes=True))
     >>> sorted(res)
     ['CCCOCc1cccc(-c2ccccn2)c1', '[14*]c1ccccn1', '[16*]c1cccc(-c2ccccn2)c1', '[16*]c1cccc(COCCC)c1', '[16*]c1cccc([16*])c1', '[3*]OCCC', '[3*]OC[8*]', '[3*]OCc1cccc(-c2ccccn2)c1', '[3*]OCc1cccc([16*])c1', '[3*]O[3*]', '[4*]CCC', '[4*]C[8*]', '[4*]Cc1cccc(-c2ccccn2)c1', '[4*]Cc1cccc([16*])c1', '[8*]COCCC']
 
     or to only do a single pass of decomposition:
 
     >>> m = Chem.MolFromSmiles('CCCOCc1cc(c2ncccc2)ccc1')
-    >>> res = list(BRICSDecompose(m,singlePass=True))
+    >>> res = list(BRICSDecompose(m, singlePass=True))
     >>> sorted(res)
     ['CCCOCc1cccc(-c2ccccn2)c1', '[14*]c1ccccn1', '[16*]c1cccc(-c2ccccn2)c1', '[16*]c1cccc(COCCC)c1', '[3*]OCCC', '[3*]OCc1cccc(-c2ccccn2)c1', '[4*]CCC', '[4*]Cc1cccc(-c2ccccn2)c1', '[8*]COCCC']
 
     setting a minimum size for the fragments:
 
     >>> m = Chem.MolFromSmiles('CCCOCC')
-    >>> res = list(BRICSDecompose(m,keepNonLeafNodes=True,minFragmentSize=2))
+    >>> res = list(BRICSDecompose(m, keepNonLeafNodes=True, minFragmentSize=2))
     >>> sorted(res)
     ['CCCOCC', '[3*]OCC', '[3*]OCCC', '[4*]CC', '[4*]CCC']
     >>> m = Chem.MolFromSmiles('CCCOCC')
-    >>> res = list(BRICSDecompose(m,keepNonLeafNodes=True,minFragmentSize=3))
+    >>> res = list(BRICSDecompose(m, keepNonLeafNodes=True, minFragmentSize=3))
     >>> sorted(res)
     ['CCCOCC', '[3*]OCC', '[4*]CCC']
-    >>> res = list(BRICSDecompose(m,minFragmentSize=2))
+    >>> res = list(BRICSDecompose(m, minFragmentSize=2))
     >>> sorted(res)
     ['[3*]OCC', '[3*]OCCC', '[4*]CC', '[4*]CCC']
 
