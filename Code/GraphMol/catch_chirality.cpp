c27eff688c4154949e289008279479a726bae0a4
greg landrum <greg.landrum@gmail.com>
2022-02-04 05:16:56 +0100

    Nonchiral atoms should be removed from StereoGroups
    This fixes a bug where atoms which have no chirality were left in StereoGroups
    
    In order to make this work, ROMol::setStereoGroups() needed to be made public.
    That shouldn't be a problem since it doesn't change connectivity.

--- Code/GraphMol/catch_chirality.cpp
+++ Code/GraphMol/catch_chirality.cpp
@@ -1739,22 +1739,6 @@ TEST_CASE("StereoGroup Testing") {
   }
 }
 
-TEST_CASE("replaceAtom and StereoGroups") {
-  SECTION("basics") {
-    auto mol = "C[C@](O)(Cl)[C@H](F)Cl |o1:1,4|"_smiles;
-    REQUIRE(mol);
-    CHECK(mol->getStereoGroups().size() == 1);
-    CHECK(mol->getStereoGroups()[0].getAtoms().size() == 2);
-    CHECK(mol->getStereoGroups()[0].getAtoms()[0] == mol->getAtomWithIdx(1));
-
-    Atom acp(*mol->getAtomWithIdx(1));
-    mol->replaceAtom(1, &acp);
-    CHECK(mol->getStereoGroups().size() == 1);
-    CHECK(mol->getStereoGroups()[0].getAtoms().size() == 2);
-    CHECK(mol->getStereoGroups()[0].getAtoms()[0] == mol->getAtomWithIdx(1));
-  }
-}
-
 TEST_CASE("Removing stereogroups from unspecified atoms") {
   SECTION("basics") {
     auto mol = "C[C@](O)(Cl)F |o1:1|"_smiles;
@@ -1775,5 +1759,21 @@ TEST_CASE("Removing stereogroups from unspecified atoms") {
     CHECK(mol->getStereoGroups().size() == 1);
     CHECK(mol->getStereoGroups()[0].getAtoms().size() == 1);
     CHECK(mol->getStereoGroups()[0].getAtoms()[0]->getIdx() == 4);
+  }
+}
+
+TEST_CASE("replaceAtom and StereoGroups") {
+  SECTION("basics") {
+    auto mol = "C[C@](O)(Cl)[C@H](F)Cl |o1:1,4|"_smiles;
+    REQUIRE(mol);
+    CHECK(mol->getStereoGroups().size() == 1);
+    CHECK(mol->getStereoGroups()[0].getAtoms().size() == 2);
+    CHECK(mol->getStereoGroups()[0].getAtoms()[0] == mol->getAtomWithIdx(1));
+
+    Atom acp(*mol->getAtomWithIdx(1));
+    mol->replaceAtom(1, &acp);
+    CHECK(mol->getStereoGroups().size() == 1);
+    CHECK(mol->getStereoGroups()[0].getAtoms().size() == 2);
+    CHECK(mol->getStereoGroups()[0].getAtoms()[0] == mol->getAtomWithIdx(1));
   }
 }
