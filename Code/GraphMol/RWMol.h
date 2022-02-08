7e575db3c495d5546832be2317291861fa7b1030
Greg Landrum <greg.landrum@gmail.com>
2022-02-08 05:17:34 +0100

    AssignStereochemistry should remove nonchiral atoms from StereoGroups (#4986)
    * Nonchiral atoms should be removed from StereoGroups
    
    This fixes a bug where atoms which have no chirality were left in StereoGroups
    
    In order to make this work, ROMol::setStereoGroups() needed to be made public.
    That shouldn't be a problem since it doesn't change connectivity.
    
    * fix some broken old tests
    
    * update cartridge tests

--- Code/GraphMol/RWMol.h
+++ Code/GraphMol/RWMol.h
@@ -192,18 +192,7 @@ class RDKIT_GRAPHMOL_EXPORT RWMol : public ROMol {
 
   //@}
 
-  //! Sets groups of atoms with relative stereochemistry
-  /*!
-    \param stereo_groups the new set of stereo groups. All will be replaced.
-
-    Stereo groups are also called enhanced stereochemistry in the SDF/Mol3000
-    file format. stereo_groups should be std::move()ed into this function.
-  */
-  void setStereoGroups(std::vector<StereoGroup> &&stereo_groups) {
-    return ROMol::setStereoGroups(std::move(stereo_groups));
-  }
-
-  //! removes all atoms, bonds, properties, bookmarks, etc.
+    //! removes all atoms, bonds, properties, bookmarks, etc.
   void clear() {
     destroy();
     d_confs.clear();
--- Code/GraphMol/SmilesParse/catch_tests.cpp
+++ Code/GraphMol/SmilesParse/catch_tests.cpp
@@ -255,10 +255,10 @@ TEST_CASE("github #2257: writing cxsmiles", "[smiles][cxsmiles]") {
     CHECK(smi == "CN |$_AV:val1;val2$,atomProp:0.p2.v2:1.p1.v1|");
   }
   SECTION("enhanced stereo 1") {
-    auto mol = "C[C@H](F)[C@H](C)[C@@H](C)Br |a:1,o1:4,5|"_smiles;
+    auto mol = "C[C@H](F)[C@H](C)[C@@H](C)Br |a:1,o1:3,5|"_smiles;
     REQUIRE(mol);
     auto smi = MolToCXSmiles(*mol);
-    CHECK(smi == "C[C@H](F)[C@H](C)[C@@H](C)Br |a:1,o1:4,5|");
+    CHECK(smi == "C[C@H](F)[C@H](C)[C@@H](C)Br |a:1,o1:3,5|");
   }
 
   SECTION("enhanced stereo 2") {
