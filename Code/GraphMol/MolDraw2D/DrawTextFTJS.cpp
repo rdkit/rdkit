rdkit/rdkit

commit f93016a77fc67c66d662545f21f8f0f479365095

Refactor mol draw2 d (#4948)
* MolDraw2D refactoring
- rename setupMoleculeDraw->initMoleculeDraw
- track whether or not initDrawing() has been called
- centralize calls to initDrawing() and clearDrawing() into initMoleculeDraw()
- update svg hashes in tests

* update some expected test results

* support changing font scale and default scale
add reaction test

* does not work... this is hard

* all tests pass

* do something about legends

* docs

* more tests

* more docs

* cleanup

* going around in circles...
hopefully converging

* cleanup

* Single bond skeleton works.

* Simple bond drawing seems to be working.

* Initial addition of atom symbols.

* Stash of not-quite-working atom symbols prior to major surgery.

* Atom symbols seem to be working.  Major surgery not required, just inverted Y coords at the outset.

* Add classes to atom labels.

* Renamed AtomLabel AtomSymbol.

* Add highlights.

* Fix bug from PR4839.

* Molecule note.

* Added atom notes.

* Added bond notes.

* Extract radicals.

* Annotation via new DrawAnnotation class.

* Add brackets.

* Add linknodes.

* Add close contacts.

* Add attachment points.  Fix wavy lines.

* Draw molecules in grid.

* Tidy.

* Fix radicals when font has hit maxFontSize.
Make getDrawCoords work.

* Draw primitives take atom or draw coords.

* Fix legends.

* More fixing for tests.  DrawMol::setScale now takes font scale as well.

* tidy debug writes

* Variable fraction of panel for legend.

* Better legend positioning.

* Fix sub- and super-script spacing.
Added spaces to Freetype strings.

* Basic reaction drawing.

* Reaction highlighting.

* Minor tweak to reacctions.

* Tweaked reaction DrawMol widths.

* Fix atomTags.

* Fix catch tests except contours.

* Contouring working in catch_tests.cpp.

* Fix catch tests.

* AtomSymbol and DrawAnnotation into MolDraw2D_detail.

Code/GraphMol/MolDraw2D/DrawTextFTJS.cpp
@@ -1,4 +1,6 @@
 //
+//  Copyright (C) 2020-2022 Greg Landrum and other RDKit contributors
+//
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
 //  The contents are covered by the terms of the BSD license

Code/GraphMol/MolDraw2D/DrawTextFTJS.h
@@ -1,4 +1,6 @@
 //
+//  Copyright (C) 2020-2022 Greg Landrum and other RDKit contributors
+//
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
 //  The contents are covered by the terms of the BSD license
@@ -17,12 +19,18 @@
 #include <GraphMol/MolDraw2D/DrawTextFT.h>
 
 namespace RDKit {
-
+class MolDraw2DJS;
+namespace MolDraw2D_detail {
 // ****************************************************************************
 class DrawTextFTJS : public DrawTextFT {
- public:
+
+ pubilc:
   DrawTextFTJS(double max_fnt_sz, double min_fnt_sz,
                const std::string &font_file, emscripten::val &context);
+  DrawTextFTJS(const DrawTextFTJS &) = delete;
+  DrawTextFTJS(DrawTextFTJS &&) = delete;
+  DrawTextFTJS &operator=(const DrawTextFTJS &) = delete;
+  DrawTextFTJS &operator=(DrawTextFTJS &&) = delete;
 
   int MoveToFunctionImpl(const FT_Vector *to) override;
   int LineToFunctionImpl(const FT_Vector *to) override;
@@ -32,14 +40,13 @@ class DrawTextFTJS : public DrawTextFT {
                           const FT_Vector *controlTwo,
                           const FT_Vector *to) override;
 
- protected:
   // adds x_trans_ and y_trans_ to coords returns x advance distance
   virtual double extractOutline() override;
 
- private:
   emscripten::val &context_;
 };
 
+}  // namespace MolDraw2D_detail
 }  // namespace RDKit
 
 #endif  // RDKIT_DRAWTEXTFTSVG_H

Code/GraphMol/MolDraw2D/DrawTextJS.cpp
@@ -1,4 +1,6 @@
 //
+//  Copyright (C) 2020-2022 Greg Landrum and other RDKit contributors
+//
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
 //  The contents are covered by the terms of the BSD license

Code/GraphMol/MolDraw2D/MolDraw2DJS.cpp
@@ -59,9 +59,10 @@ void MolDraw2DJS::initTextDrawer(bool noFreetype) {
 }
 
 // ****************************************************************************
-void MolDraw2DJS::drawLine(const Point2D &cds1, const Point2D &cds2) {
-  Point2D c1 = getDrawCoords(cds1);
-  Point2D c2 = getDrawCoords(cds2);
+void MolDraw2DJS::drawLine(const Point2D &cds1, const Point2D &cds2,
+                           bool rawCoords) {
+  Point2D c1 = rawCoords ? cds1 : getDrawCoords(cds1);
+  Point2D c2 = rawCoords ? cds2 : getDrawCoords(cds2);
   std::string col = DrawColourToSVG(colour());
   double width = getDrawLineWidth();
   std::string dashString = "";
@@ -84,7 +85,8 @@ void MolDraw2DJS::drawLine(const Point2D &cds1, const Point2D &cds2) {
   }
 }
 // ****************************************************************************
-void MolDraw2DJS::drawPolygon(const std::vector<Point2D> &cds) {
+void MolDraw2DJS::drawPolygon(const std::vector<Point2D> &cds,
+                              bool rawCoords) {
   PRECONDITION(cds.size() >= 3, "must have at least three points");
 
   std::string col = DrawColourToSVG(colour());
@@ -95,10 +97,10 @@ void MolDraw2DJS::drawPolygon(const std::vector<Point2D> &cds) {
   d_context.set("lineCap", std::string("butt"));
   d_context.set("lineJoin", std::string("round"));
   d_context.set("strokeStyle", col);
-  Point2D c0 = getDrawCoords(cds[0]);
+  Point2D c0 = rawCoords ? cds[0] : getDrawCoords(cds[0]);
   d_context.call<void>("moveTo", c0.x, c0.y);
   for (unsigned int i = 1; i < cds.size(); ++i) {
-    Point2D ci = getDrawCoords(cds[i]);
+    Point2D ci = rawCoords ? cds[i] : getDrawCoords(cds[i]);
     d_context.call<void>("lineTo", ci.x, ci.y);
   }
   if (fillPolys()) {
@@ -110,9 +112,10 @@ void MolDraw2DJS::drawPolygon(const std::vector<Point2D> &cds) {
 }
 
 // ****************************************************************************
-void MolDraw2DJS::drawEllipse(const Point2D &cds1, const Point2D &cds2) {
-  Point2D c1 = getDrawCoords(cds1);
-  Point2D c2 = getDrawCoords(cds2);
+void MolDraw2DJS::drawEllipse(const Point2D &cds1, const Point2D &cds2,
+                              bool rawCoords) {
+  Point2D c1 = rawCoords ? cds1 : getDrawCoords(cds1);
+  Point2D c2 = rawCoords ? cds2 : getDrawCoords(cds2);
   double w = c2.x - c1.x;
   double h = c2.y - c1.y;
   double cx = c1.x + w / 2;

Code/GraphMol/FileParsers/testPropertyLists.cpp
@@ -150,7 +150,7 @@ M  END
 >  <atom.iprop.NumHeavyNeighbors>  (1) 
 2 2 2
 
->  <atom.prop.AtomLabel>  (1) 
+>  <atom.prop.AtomLabel>  (1)
 C1 N2 C3
 
 >  <atom.bprop.IsCarbon>  (1) 

Code/GraphMol/MolDraw2D/AtomSymbol.cpp
@@ -1,0 +1,160 @@
+//
+//  Copyright (C) 2021-2022 David Cosgrove and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+// Original author: David Cosgrove (CozChemIx Limited)
+//
+
+#include <boost/format.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <GraphMol/MolDraw2D/AtomSymbol.h>
+#include <GraphMol/MolDraw2D/DrawText.h>
+#include <GraphMol/MolDraw2D/MolDraw2D.h>
+
+namespace RDKit {
+namespace MolDraw2D_detail {
+
+// ****************************************************************************
+AtomSymbol::AtomSymbol(const std::string &symbol, int atIdx, OrientType orient,
+                       const Point2D &cds, const DrawColour &colour,
+                       DrawText &textDrawer)
+    : symbol_(symbol),
+      atIdx_(atIdx),
+      orient_(orient),
+      cds_(cds),
+      colour_(colour),
+      textDrawer_(textDrawer) {
+  if (symbol_.empty()) {
+    return;
+  }
+  textDrawer_.getStringRects(symbol_, orient_, rects_, drawModes_, drawChars_,
+                             false, TextAlignType::MIDDLE);
+  adjustColons();
+}
+
+// ****************************************************************************
+void AtomSymbol::findExtremes(double &xmin, double &xmax, double &ymin,
+                              double &ymax) const {
+  Point2D bl, br, tl, tr, origTrans;
+  for (auto rect : rects_) {
+    origTrans = rect->trans_;
+    rect->trans_ += cds_;
+    rect->calcCorners(tl, tr, br, bl, 0.0);
+    // sometimes the rect is in a coordinate frame where +ve y is down,
+    // sometimes it's up.  For these purposes, we don't care so long as
+    // the ymax is larger than the ymin.  We probably don't need to do
+    // all the tests for xmin and xmax.
+    xmin = std::min({tr.x, bl.x, xmin});
+    ymin = std::min({tr.y, bl.y, ymin});
+    xmax = std::max({tr.x, bl.x, xmax});
+    ymax = std::max({tr.y, bl.y, ymax});
+    rect->trans_ = origTrans;
+  }
+}
+
+// ****************************************************************************
+void AtomSymbol::scale(const Point2D &scaleFactor) {
+  cds_.x *= scaleFactor.x;
+  cds_.y *= scaleFactor.y;
+
+  // rebuild the rectangles, because the fontScale may be different,
+  // and the widths etc might not scale by the same amount.
+  rects_.clear();
+  drawModes_.clear();
+  drawChars_.clear();
+  textDrawer_.getStringRects(symbol_, orient_, rects_, drawModes_, drawChars_,
+                             false, TextAlignType::MIDDLE);
+}
+
+// ****************************************************************************
+void AtomSymbol::move(const Point2D &trans) { cds_ += trans; }
+
+// ****************************************************************************
+void AtomSymbol::draw(MolDraw2D &molDrawer) const {
+  std::string o_class = molDrawer.getActiveClass();
+  std::string actClass = o_class;
+  if (!actClass.empty()) {
+    actClass += " ";
+  }
+  actClass += (boost::format("atom-%d") % atIdx_).str();
+  molDrawer.setActiveClass(actClass);
+  textDrawer_.setColour(colour_);
+  textDrawer_.drawString(symbol_, cds_, orient_);
+  molDrawer.setActiveClass(o_class);
+  //  drawRects(molDrawer);
+}
+
+// ****************************************************************************
+bool AtomSymbol::doesRectClash(const StringRect &rect, double padding) const {
+  for (auto &alrect : rects_) {
+    auto oldTrans = alrect->trans_;
+    alrect->trans_ += cds_;
+    bool dii = alrect->doesItIntersect(rect, padding);
+    alrect->trans_ = oldTrans;
+    if (dii) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// ****************************************************************************
+void AtomSymbol::adjustColons() {
+  if (symbol_.empty()) {
+    return; // but probably it's always got something in it.
+  }
+  size_t colonPos = symbol_.find(':');
+  if (colonPos == std::string::npos) {
+    return;
+  }
+  // we need to allow for markup in the symbol, such as <lit>[CH2;X2:4]</lit>
+  // and the easiest way to do that is to use the fact that atomLabelToPieces
+  // strips it out.
+  std::string tmpSym = symbol_;
+  while (true) {
+    size_t ltPos = tmpSym.find('<');
+    if (ltPos == std::string::npos) {
+      break;
+    }
+    size_t gtPos = tmpSym.find('>');
+    tmpSym = tmpSym.substr(0, ltPos) + tmpSym.substr(gtPos + 1);
+  }
+  colonPos = tmpSym.find(':');
+  if (colonPos == std::string::npos) {
+    return;
+  }
+  CHECK_INVARIANT(colonPos<=rects_.size(),"bad rects_ size");
+  double leftHeight = colonPos ? rects_[colonPos - 1]->height_ : 0;
+  double rightHeight =
+      colonPos < symbol_.size() - 1 ? rects_[colonPos + 1]->height_ : 0;
+  rects_[colonPos]->height_ = std::min(leftHeight, rightHeight);
+}
+
+// ****************************************************************************
+void AtomSymbol::drawRects(MolDraw2D &molDrawer) const {
+  Point2D tl, tr, br, bl, origTrans;
+  for (auto &rect : rects_) {
+    origTrans = rect->trans_;
+    rect->trans_ += cds_;
+    rect->calcCorners(tl, tr, br, bl, 0.0);
+    molDrawer.setColour(DrawColour(1.0, 0.0, 0.0));
+    molDrawer.drawLine(tl, tr, true);
+    molDrawer.setColour(DrawColour(0.0, 1.0, 0.0));
+    molDrawer.drawLine(tr, br, true);
+    molDrawer.setColour(DrawColour(0.0, 0.0, 1.0));
+    molDrawer.drawLine(br, bl, true);
+    molDrawer.setColour(DrawColour(0.0, 0.95, 0.95));
+    molDrawer.drawLine(bl, tl, true);
+    rect->trans_ = origTrans;
+  }
+}
+
+}  // namespace MolDraw2D_detail
+} // namespace RDKit
+

Code/GraphMol/MolDraw2D/AtomSymbol.h
@@ -1,0 +1,87 @@
+//
+//  Copyright (C) 2021-2022 David Cosgrove and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+// Original author: David Cosgrove (CozChemIx Limited)
+//
+// This class is a helper class used by MolDraw2D to draw an ROMol.
+// It is not part of the public API and is not intended to be used except
+// by MolDraw2D.
+// It holds the information needed to draw an atom symbol, including
+// all the extra bits like isotope labels.
+
+#ifndef RDKIT_ATOMSYMBOL_H
+#define RDKIT_ATOMSYMBOL_H
+
+#include <string>
+
+#include <GraphMol/MolDraw2D/DrawText.h>
+#include <GraphMol/MolDraw2D/MolDraw2DHelpers.h>
+
+namespace RDKit {
+
+class MolDraw2D;
+
+namespace MolDraw2D_detail {
+
+class AtomSymbol {
+
+ public:
+  ~AtomSymbol() = default;
+
+  /*!
+   *
+   * @param symbol     : the full symbol
+   * @param orient     : text orientation (up, down, left, right)
+   * @param textDrawer : instance of DrawText to get the character sizes
+   * etc.
+   */
+  AtomSymbol(const std::string &symbol, int atIdx, OrientType orient,
+             const Point2D &cds, const DrawColour &colour,
+             DrawText &textDrawer);
+
+  AtomSymbol(const AtomSymbol &) = delete;
+  AtomSymbol(AtomSymbol &&) = delete;
+  AtomSymbol &operator=(const AtomSymbol &) = delete;
+  AtomSymbol &operator=(AtomSymbol &&) = delete;
+
+  std::string symbol_;
+  int atIdx_;
+  OrientType orient_;
+  Point2D cds_;
+  DrawColour colour_;
+  DrawText &textDrawer_;
+
+  std::vector<std::shared_ptr<StringRect>> rects_;
+  std::vector<TextDrawType> drawModes_;
+  std::vector<char> drawChars_;
+
+  // expects xmin etc to be initialised to something sensible.
+  virtual void findExtremes(double &xmin, double &xmax, double &ymin,
+                            double &ymax) const;
+  // scaleFactor moves the cds_, but the fontScaleFactor changes rects_, because
+  // we might be scaling the font differently from the drawing as a whole.
+  virtual void scale(const Point2D &scaleFactor);
+  virtual void move(const Point2D &trans);
+  void draw(MolDraw2D &molDrawer) const;
+  bool doesRectClash(const StringRect &rect, double padding) const;
+  // Because a colon is a lot shorter than other characters, there are cases,
+  // such as rxn_test1_2 in rxn_test1.cpp, where a vertical bond can slip
+  // between the atom symbol and the atom map (C:8 in that case) which looks
+  // a bit pants.  This stretches the colon to be the same height as the
+  // smaller of the chars on either side.
+  void adjustColons();
+
+  // this is for debugging almost always.
+  void drawRects(MolDraw2D &molDrawer) const;
+};
+
+}  // namespace MolDraw2D_detail
+} // namespace RDKit
+
+#endif  // RDKIT_ATOMSYMBOL_H

Code/GraphMol/MolDraw2D/CMakeLists.txt
@@ -27,15 +27,14 @@ rdkit_headers(MolDraw2D.h
   MolDraw2Dwx.h
   MolDraw2DJS.h
   MolDraw2DUtils.h
-  DrawText.h
-  DrawTextSVG.h
-  DrawTextJS.h
+  MolDraw2DHelpers.h
   DEST GraphMol/MolDraw2D
 )
 
 rdkit_library(MolDraw2D MolDraw2D.cpp MolDraw2DSVG.cpp
-  MolDraw2DDetails.cpp MolDraw2DUtils.cpp DrawText.cpp
-  DrawTextSVG.cpp 
+  MolDraw2DDetails.cpp MolDraw2DUtils.cpp AtomSymbol.cpp
+  DrawAnnotation.cpp DrawMol.cpp DrawMolMCH.cpp
+  DrawShape.cpp DrawText.cpp DrawTextNotFT.cpp DrawTextSVG.cpp
   MolDraw2DJS.cpp DrawTextJS.cpp
   LINK_LIBRARIES
   ChemReactions Depictor MolTransforms 

commit 64311f9a2c8f819b7593399562de48d572141739

Fixes JS build broken by #4948

Code/GraphMol/MolDraw2D/DrawTextFTJS.cpp
@@ -18,6 +18,7 @@ namespace RDKit {
 
 std::string DrawColourToSVG(const RDKit::DrawColour &col);
 
+namespace MolDraw2D_detail {
 // ****************************************************************************
 DrawTextFTJS::DrawTextFTJS(double max_fnt_sz, double min_fnt_sz,
                            const std::string &font_file,
@@ -81,5 +82,6 @@ int DrawTextFTJS::CubicToFunctionImpl(const FT_Vector *controlOne,
   return 0;
 }
 
+}  // namespace MolDraw2D_detail
 }  // namespace RDKit
 #endif  // __EMSCRIPTEN__

Code/GraphMol/MolDraw2D/DrawTextFTJS.h
@@ -24,7 +24,7 @@ namespace MolDraw2D_detail {
 // ****************************************************************************
 class DrawTextFTJS : public DrawTextFT {
 
- pubilc:
+ public:
   DrawTextFTJS(double max_fnt_sz, double min_fnt_sz,
                const std::string &font_file, emscripten::val &context);
   DrawTextFTJS(const DrawTextFTJS &) = delete;

Code/GraphMol/MolDraw2D/DrawTextJS.cpp
@@ -23,10 +23,11 @@ namespace RDKit {
 
 std::string DrawColourToSVG(const RDKit::DrawColour &col);
 
+namespace MolDraw2D_detail {
 // ****************************************************************************
 DrawTextJS::DrawTextJS(double max_fnt_sz, double min_fnt_sz,
                        emscripten::val &context)
-    : DrawText(max_fnt_sz, min_fnt_sz), context_(context) {}
+    : DrawTextNotFT(max_fnt_sz, min_fnt_sz), context_(context) {}
 
 // ****************************************************************************
 // draw the char, with the bottom left hand corner at cds
@@ -103,5 +104,6 @@ void DrawTextJS::getStringRects(const std::string &text,
   adjustStringRectsForSuperSubScript(draw_modes, rects);
 }
 
+}  // namespace MolDraw2D_detail
 }  // namespace RDKit
 #endif  // __EMSCRIPTEN__

Code/GraphMol/MolDraw2D/MolDraw2DJS.cpp
@@ -37,20 +37,20 @@ void MolDraw2DJS::initTextDrawer(bool noFreetype) {
   double min_fnt_sz = drawOptions().minFontSize;
 
   if (noFreetype) {
-    text_drawer_.reset(new DrawTextJS(max_fnt_sz, min_fnt_sz, d_context));
+    text_drawer_.reset(new MolDraw2D_detail::DrawTextJS(max_fnt_sz, min_fnt_sz, d_context));
   } else {
 #ifdef RDK_BUILD_FREETYPE_SUPPORT
     try {
-      text_drawer_.reset(new DrawTextFTJS(max_fnt_sz, min_fnt_sz,
+      text_drawer_.reset(new MolDraw2D_detail::DrawTextFTJS(max_fnt_sz, min_fnt_sz,
                                           drawOptions().fontFile, d_context));
     } catch (std::runtime_error &e) {
       BOOST_LOG(rdWarningLog)
           << e.what() << std::endl
           << "Falling back to native JS text handling." << std::endl;
-      text_drawer_.reset(new DrawTextJS(max_fnt_sz, min_fnt_sz, d_context));
+      text_drawer_.reset(new MolDraw2D_detail::DrawTextJS(max_fnt_sz, min_fnt_sz, d_context));
     }
 #else
-    text_drawer_.reset(new DrawTextJS(max_fnt_sz, min_fnt_sz, d_context));
+    text_drawer_.reset(new MolDraw2D_detail::DrawTextJS(max_fnt_sz, min_fnt_sz, d_context));
 #endif
   }
   if (drawOptions().baseFontSize > 0.0) {