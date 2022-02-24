27b92a24ae9b8308cb3d67dfd3f21bdf9ac072e8
Paolo Tosco <paolo.tosco.mail@gmail.com>
2022-02-24 06:35:32 +0100

    Fixes JS build broken by #4948 (#5042)
    Co-authored-by: Tosco, Paolo <paolo.tosco@novartis.com>

--- Code/GraphMol/MolDraw2D/DrawTextFTJS.cpp
+++ Code/GraphMol/MolDraw2D/DrawTextFTJS.cpp
@@ -18,6 +18,7 @@ namespace RDKit {
 
 std::string DrawColourToSVG(const RDKit::DrawColour &col);
 
+namespace MolDraw2D_detail {
 // ****************************************************************************
 DrawTextFTJS::DrawTextFTJS(double max_fnt_sz, double min_fnt_sz,
                            const std::string &font_file,
@@ -81,5 +82,6 @@ int DrawTextFTJS::CubicToFunctionImpl(const FT_Vector *controlOne,
   return 0;
 }
 
+}  // namespace MolDraw2D_detail
 }  // namespace RDKit
 #endif  // __EMSCRIPTEN__
