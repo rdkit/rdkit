4bbbc6611dbe4bf05ed36ea795b3c6dc39bbdebc
Greg Landrum <greg.landrum@gmail.com>
2022-03-03 16:00:38 +0100

    Add a CXSMILES option to the MolHash (#5058)
    * Nonchiral atoms should be removed from StereoGroups
    
    This fixes a bug where atoms which have no chirality were left in StereoGroups
    
    In order to make this work, ROMol::setStereoGroups() needed to be made public.
    That shouldn't be a problem since it doesn't change connectivity.
    
    * support CXSmiles extensions in the hashes
    
    * initial tests for that
    
    * some cleanup
    
    * copyright header cleanup
    
    * minor refactoring
    
    * call out the changes in the release notes
    
    * extension and more testing
    
    * add python wrappers

--- Code/GraphMol/MolHash/catch_tests.cpp
+++ Code/GraphMol/MolHash/catch_tests.cpp
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2019 Greg Landrum
+//  Copyright (C) 2019-2022 Greg Landrum
 //
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
@@ -7,12 +7,11 @@
 //  which is included in the file license.txt, found at the root
 //  of the RDKit source tree.
 //
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
-                           // this in one cpp file
 #include "catch.hpp"
 
 #include <GraphMol/RDKitBase.h>
 #include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/FileParsers/FileParsers.h>
 #include <GraphMol/SmilesParse/SmilesWrite.h>
 #include "MolHash.h"
 
@@ -228,5 +227,181 @@ TEST_CASE("Github issues", "[molhash]") {
     REQUIRE(mol);
     auto hsh = MolHash::MolHash(mol.get(), MolHash::HashFunction::MolFormula);
     CHECK(hsh == "C2H6Cl");
+  }
+}
+
+TEST_CASE("MolHash with CX extensions", "[molhash]") {
+  SECTION("Tautomer") {
+    auto mol =
+        "C[C@@H](O)[C@@H](C)[C@@H](C)C[C@H](C1=CN=CN1)C1=CNC=N1 "
+        "|o1:8,5,&1:1,3,r,c:11,18,t:9,15|"_smiles;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::HetAtomTautomer);
+      CHECK(
+          hsh ==
+          "C[C@@H](CC([C]1[CH][N][CH][N]1)[C]1[CH][N][CH][N]1)[C@H](C)[C@@H](C)"
+          "[O]_3_0");
+    }
+    {
+      RWMol cp(*mol);
+
+      auto hsh =
+          MolHash::MolHash(&cp, MolHash::HashFunction::HetAtomTautomer, true);
+      CHECK(
+          hsh ==
+          "C[C@@H](CC([C]1[CH][N][CH][N]1)[C]1[CH][N][CH][N]1)[C@H](C)[C@@H](C)"
+          "[O]_3_0 |o1:1,&1:14,16|");
+    }
+  }
+  SECTION("no coordinates please") {
+    auto mol = R"CTAB(
+  Mrv2108 03032205502D          
+
+  0  0  0     0  0            999 V3000
+M  V30 BEGIN CTAB
+M  V30 COUNTS 15 16 0 0 1
+M  V30 BEGIN ATOM
+M  V30 1 C -0.4657 -3.589 0 0 CFG=1
+M  V30 2 C -0.4657 -2.049 0 0
+M  V30 3 C 0.8679 -4.359 0 0
+M  V30 4 C 2.2016 -3.589 0 0 CFG=2
+M  V30 5 C 3.5353 -4.359 0 0
+M  V30 6 C 4.9422 -3.7327 0 0
+M  V30 7 N 5.9726 -4.8771 0 0
+M  V30 8 C 5.2026 -6.2108 0 0
+M  V30 9 N 3.6963 -5.8906 0 0
+M  V30 10 C 2.2016 -2.049 0 0
+M  V30 11 C 0.9557 -1.1438 0 0
+M  V30 12 N 1.4316 0.3208 0 0
+M  V30 13 C 2.9716 0.3208 0 0
+M  V30 14 N 3.4475 -1.1438 0 0
+M  V30 15 F -1.7994 -4.359 0 0
+M  V30 END ATOM
+M  V30 BEGIN BOND
+M  V30 1 1 1 2 CFG=1
+M  V30 2 1 1 3
+M  V30 3 1 4 3 CFG=1
+M  V30 4 1 4 5
+M  V30 5 2 5 6
+M  V30 6 1 6 7
+M  V30 7 2 7 8
+M  V30 8 1 8 9
+M  V30 9 1 5 9
+M  V30 10 1 4 10
+M  V30 11 2 10 11
+M  V30 12 1 11 12
+M  V30 13 1 12 13
+M  V30 14 2 13 14
+M  V30 15 1 10 14
+M  V30 16 1 1 15
+M  V30 END BOND
+M  V30 BEGIN COLLECTION
+M  V30 MDLV30/STEREL1 ATOMS=(1 1)
+M  V30 MDLV30/STERAC1 ATOMS=(1 4)
+M  V30 END COLLECTION
+M  V30 END CTAB
+M  END
+)CTAB"_ctab;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh =
+          MolHash::MolHash(&cp, MolHash::HashFunction::HetAtomTautomer, true);
+      CHECK(hsh ==
+            "C[C@H](F)CC([C]1[CH][N][CH][N]1)[C]1[CH][N][CH][N]1_2_0 |o1:1|");
+    }
+  }
+
+  SECTION("Mesomer") {
+    auto mol = "C[C@H](F)C[C@@](C([NH-])=O)C([O-])=N |o1:1,&1:4|"_smiles;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::Mesomer);
+      CHECK(hsh == "C[C@H](F)C[C]([C]([NH])[O])[C]([NH])[O]_-2");
+    }
+    {
+      RWMol cp(*mol);
+
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::Mesomer, true);
+      CHECK(hsh == "C[C@H](F)C[C]([C]([NH])[O])[C]([NH])[O]_-2 |o1:1|");
+    }
+  }
+  SECTION("Extended Murcko") {
+    auto mol =
+        "CC1=CC=CC=C1[C@@H](C[C@@H](C1CC1)C1CCC1)C1=CC=CC=C1O |o1:9,&1:7|"_smiles;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::ExtendedMurcko);
+      CHECK(hsh == "*c1ccccc1C(C[C@H](C1CCC1)C1CC1)c1ccccc1*");
+    }
+    {
+      RWMol cp(*mol);
+
+      auto hsh =
+          MolHash::MolHash(&cp, MolHash::HashFunction::ExtendedMurcko, true);
+      CHECK(hsh == "*c1ccccc1C(C[C@H](C1CCC1)C1CC1)c1ccccc1* |o1:9|");
+    }
+  }
+  SECTION("Murcko") {
+    auto mol =
+        "CC1=CC=CC=C1[C@@H](C[C@@H](C1CC1)C1CCC1)C1=CC=CC=C1O |o1:9,&1:7|"_smiles;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::MurckoScaffold);
+      CHECK(hsh == "c1ccc(C(C[C@H](C2CCC2)C2CC2)c2ccccc2)cc1");
+    }
+    {
+      RWMol cp(*mol);
+
+      auto hsh =
+          MolHash::MolHash(&cp, MolHash::HashFunction::MurckoScaffold, true);
+      CHECK(hsh == "c1ccc(C(C[C@H](C2CCC2)C2CC2)c2ccccc2)cc1 |o1:6|");
+    }
+  }
+  SECTION("Element") {
+    auto mol =
+        "C([C@@H](C1CC1)C1CCC1)[C@@H](C1CCCCC1)C1=CC=CC=C1 |o1:1,&1:9,c:21,23,t:19|"_smiles;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::ElementGraph);
+      CHECK(hsh == "C1CCC(C(C[C@H](C2CCC2)C2CC2)C2CCCCC2)CC1");
+    }
+    {
+      RWMol cp(*mol);
+
+      auto hsh =
+          MolHash::MolHash(&cp, MolHash::HashFunction::ElementGraph, true);
+      CHECK(hsh == "C1CCC(C(C[C@H](C2CCC2)C2CC2)C2CCCCC2)CC1 |o1:6|");
+    }
+  }
+  SECTION("Anonymous") {
+    auto mol =
+        "C([C@@H](C1CC1)C1CCC1)[C@@H](C1CCCCC1)C1=CC=CC=N1 |o1:1,&1:9,c:21,23,t:19|"_smiles;
+    REQUIRE(mol);
+
+    {
+      RWMol cp(*mol);
+      auto hsh = MolHash::MolHash(&cp, MolHash::HashFunction::AnonymousGraph);
+      CHECK(hsh == "*1***(*(**(*2***2)*2**2)*2*****2)**1");
+    }
+    {
+      RWMol cp(*mol);
+
+      auto hsh =
+          MolHash::MolHash(&cp, MolHash::HashFunction::AnonymousGraph, true);
+      CHECK(hsh == "*1***(*(**(*2***2)*2**2)*2*****2)**1");
+    }
   }
 }
