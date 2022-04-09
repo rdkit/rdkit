96162eaa5c0f07c1c3f886ad4fbfe6d56102f04a
Ric <ricrogz@gmail.com>
2022-04-08 21:35:56 -0400

    add test

--- Code/RDGeneral/catch_logs.cpp
+++ Code/RDGeneral/catch_logs.cpp
@@ -63,3 +63,51 @@ TEST_CASE("LogStateSetter") {
     }
   }
 }
+
+TEST_CASE("GitHub Issue #5172", "[bug][logging]") {
+  std::stringstream err_ostrm;
+  std::stringstream warn_ostrm;
+  rdErrorLog->df_enabled = true;
+  rdWarningLog->df_enabled = true;
+  rdErrorLog->SetTee(err_ostrm);
+  rdWarningLog->SetTee(warn_ostrm);
+
+  {
+    RDLog::LogStateSetter disabler;
+
+    BOOST_LOG(rdErrorLog) << "should be silent" << std::endl;
+    auto txt = err_ostrm.str();
+    CHECK(txt.find("should") == std::string::npos);
+
+    BOOST_LOG(rdWarningLog) << "should be silent" << std::endl;
+    txt = warn_ostrm.str();
+    CHECK(txt.find("should") == std::string::npos);
+
+    {
+      // The second setter overrides the first one:
+      RDLog::LogStateSetter disabler2({rdWarningLog});
+
+      BOOST_LOG(rdErrorLog) << "should be silent" << std::endl;
+      txt = err_ostrm.str();
+      CHECK(txt.find("should") == std::string::npos);
+
+      BOOST_LOG(rdWarningLog) << "should not be silent" << std::endl;
+      txt = warn_ostrm.str();
+      CHECK(txt.find("should") != std::string::npos);
+      warn_ostrm.clear();
+    }
+  }
+
+  // Both setters are destroyed, and we revert to initial state
+
+  BOOST_LOG(rdErrorLog) << "should not be silent" << std::endl;
+  auto txt = err_ostrm.str();
+  CHECK(txt.find("should") != std::string::npos);
+
+  BOOST_LOG(rdWarningLog) << "should not be silent" << std::endl;
+  txt = warn_ostrm.str();
+  CHECK(txt.find("should") != std::string::npos);
+
+  rdErrorLog->ClearTee();
+  rdWarningLog->ClearTee();
+}
