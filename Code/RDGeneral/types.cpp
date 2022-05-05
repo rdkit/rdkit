36c2f91092008c410fefa99d2208a9517a94f264
Greg Landrum <greg.landrum@gmail.com>
2022-05-05 05:05:10 +0200

    reading pseudoatoms

--- Code/RDGeneral/types.cpp
+++ Code/RDGeneral/types.cpp
@@ -45,6 +45,7 @@ const std::string _MolFileBondAttach = "_MolFileBondAttach";
 const std::string _MolFileBondType = "_MolFileBondType";
 const std::string _MolFileBondStereo = "_MolFileBondStereo";
 const std::string _MolFileBondCfg = "_MolFileBondCfg";
+const std::string _MolFilePseudoAtom = "_MolFilePseudoAtom";
 
 const std::string _Name = "_Name";
 const std::string _NeedsQueryScan = "_NeedsQueryScan";
--- Code/RDGeneral/types.h
+++ Code/RDGeneral/types.h
@@ -179,6 +179,7 @@ RDKIT_RDGENERAL_EXPORT extern const std::string _MolFileAtomQuery;   // int
 RDKIT_RDGENERAL_EXPORT extern const std::string _MolFileBondQuery;   // int
 RDKIT_RDGENERAL_EXPORT extern const std::string _MolFileBondEndPts;  // string
 RDKIT_RDGENERAL_EXPORT extern const std::string _MolFileBondAttach;  // string
+RDKIT_RDGENERAL_EXPORT extern const std::string _MolFilePseudoAtom;  // string
 RDKIT_RDGENERAL_EXPORT extern const std::string
     _MolFileBondType;  // unsigned int
 RDKIT_RDGENERAL_EXPORT extern const std::string
