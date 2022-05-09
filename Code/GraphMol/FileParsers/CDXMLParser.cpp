2fdb86c5452f0ba20607e9f4323641c8f419eb45
Brian Kelley <fustigator@gmail.com>
2022-05-09 14:41:38 -0400

    First pass at a CDXML parser

---
+++ Code/GraphMol/FileParsers/CDXMLParser.cpp
@@ -0,0 +1,281 @@
+#include "CDXMLParser.h"
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <GraphMol/MolOps.h>
+#include <GraphMol/ChemTransforms/MolFragmenter.h>
+#include <GraphMol/SmilesParse/SmilesWrite.h>
+
+// TODO
+//  I'm currently using atom map numbers to join structures
+//   this is not very forward thinking as chemdraw can also have
+//   atom map numbers in addition to the ids in the xml file.
+//  [] enhance molzip to be able to zip on on an atom tag
+//  [] what happens to stereo chemistry when the fuse_frags below is called
+//    get cases in the "wild" and see what the expectation is.
+//  [] add coordinates for the atoms.  Might not work for nicknames like Boc?
+
+using boost::property_tree::ptree;
+namespace RDKit {
+
+namespace {
+const std::string NEEDS_FUSE(NEEDS_FUSE);
+const std::string CDXML_FRAG_ID("CDXML_FRAG_ID");
+const std::string FUSE_LABEL("CDXML_NODE_ID");
+
+struct BondInfo {
+    int start;
+    int end;
+    int order;
+    std::string display;
+    Bond::BondType getBondType() {
+        if(order && order <= 4)
+            return static_cast<Bond::BondType>(order);
+        // handle aromatic bond type
+        return Bond::BondType::UNSPECIFIED;
+    }
+};
+
+struct SchemeInfo {
+    int scheme_id;
+    int step_id;
+    std::vector<unsigned int> ReactionStepProducts;
+    std::vector<unsigned int> ReactionStepReactants;
+    std::vector<unsigned int> ReactionStepObjectsAboveArrow;
+    std::vector<unsigned int> ReactionStepObjectsBelowArrow;
+};
+
+unsigned int get_fuse_label(Atom*atm) {
+    // return atm->getAtomMapNum(); easier debugging
+    unsigned int label = 0; // default is no label
+    atm->getPropIfPresent<unsigned int>(FUSE_LABEL, label);
+    return label;
+}
+
+void set_fuse_label(Atom*atm, unsigned int idx) {
+    //atm->setAtomMapNum(idx); //for debugging
+    if(idx) {
+        atm->setProp<unsigned int>(FUSE_LABEL, idx);
+    } else {
+        atm->clearProp(FUSE_LABEL);
+    }
+}
+  
+void parse_fragment(RWMol &mol,
+                    ptree &frag,
+		            std::map<unsigned int, Atom *> &ids,
+                    int external_attachment = -1,
+                    bool sanitize=false) {
+  // XXX Need to put the fragid on the molecule so we can properly make reactions
+    
+  int frag_id = frag.get<int>("<xmlattr>.id", -1);
+  if(frag_id == -1) {
+      CHECK_INVARIANT(frag_id >= 0, "Invalid or missing id from CDXML fragment");
+  }
+  mol.setProp<int>(CDXML_FRAG_ID, frag_id);
+  int elemno = -1;
+  int fragmap = 10000;
+  bool fuse_fragments = false;
+  // for atom in frag
+  int atom_id;
+  std::vector<BondInfo> bonds;
+  // nodetypes = https://www.cambridgesoft.com/services/documentation/sdk/chemdraw/cdx/properties/Node_Type.htm
+  for(auto &node: frag) {
+    if(node.first == "n") { // atom node
+      int elemno = 6; // default to carbon
+      int num_hydrogens = 0;
+      int charge = 0;
+      int atommap = 0;
+      int mergeparent = -1;
+      int rgroup_num = -1;
+      std::string nodetype = "";
+      for(auto &attr: node.second.get_child("<xmlattr>")) {
+          if(attr.first == "id") {
+              atom_id = stoi(attr.second.data());
+          } else if (attr.first == "Element") {
+              elemno = stoi(attr.second.data());
+          } else if(attr.first == "NumHydrogens") {
+              num_hydrogens = stoi(attr.second.data());
+          } else if (attr.first == "Charge") {
+              charge = stoi(attr.second.data());
+          } else if(attr.first == "NodeType") {
+              nodetype = attr.second.data();
+              if (nodetype == "Nickname" || nodetype == "Fragment") {
+                  elemno = 0;
+                  atommap = atom_id;
+              } else if (nodetype == "ExternalConnectionPoint") {
+                  PRECONDITION(external_attachment > 0, "External Connection Point is not set");
+                  elemno = 0;
+                  atommap = external_attachment;
+                  mergeparent = external_attachment;
+              } else if (nodetype == "GenericNickname") {
+                  // RGroups for example?
+                  for(auto &tnode : node.second) {
+                      if(tnode.first == "t") {
+                          for(auto &snode : tnode.second) {
+                              if(snode.first == "s") {
+                                  auto s = snode.second.data();
+                                  if(s.size()  && s[0] == 'R') {
+                                      rgroup_num = stoi(s.substr(1));
+                                      elemno = 0;
+                                  }
+                                  break;
+                              }
+                          }
+                          break;
+                      }
+                  }
+              } else if (nodetype == "ElementList") {
+                  // query atom?
+              }
+          } else if (attr.first == "p") {
+              // grab coordinates
+          }
+      }
+      // add the atom
+      Atom *rd_atom = new Atom(elemno);
+      rd_atom->setFormalCharge(charge);
+      rd_atom->setNumExplicitHs(num_hydrogens);
+        if(rgroup_num >= 0) {
+            rd_atom->setAtomMapNum(rgroup_num);
+        }
+      set_fuse_label(rd_atom, atommap);
+      if(mergeparent > 0) {
+          rd_atom->setProp<int>("MergeParent", mergeparent);
+      }
+      
+      if(ids.find(atom_id) != ids.end()) {
+          // error fail processing
+      }
+      ids[atom_id] = rd_atom;
+      const bool updateLabels=true;
+      const bool takeOwnership=true;
+      auto idx = mol.addAtom(rd_atom, updateLabels, takeOwnership);
+        assert (idx == rd_atom->getIdx());
+      
+      if (nodetype == "Nickname" || nodetype == "Fragment") {
+          for(auto &fragment: node.second) {
+              if(fragment.first == "fragment") {
+                  parse_fragment(mol, fragment.second, ids, atom_id, sanitize=sanitize);
+                  mol.setProp<bool>(NEEDS_FUSE, true);
+              }
+          }
+      }
+    } else if (node.first == "b") { // bond
+          int start_atom = -1;
+          int end_atom = -1;
+          int order = 1;
+          std::string display;
+          for(auto &attr: node.second.get_child("<xmlattr>")) {
+              if(attr.first == "B") {
+                  start_atom = stoi(attr.second.data());
+              } else if (attr.first == "E") {
+                  end_atom = stoi(attr.second.data());
+              } else if (attr.first == "Order") {
+                  order = stoi(attr.second.data());
+              } else if (attr.first == "Display") { // gets wedge/hash stuff and probably more
+                  display = attr.second.data();
+              }
+          }
+        CHECK_INVARIANT(start_atom>=0 && end_atom>=0 && start_atom != end_atom, "Bad bond in CDXML");
+        bonds.push_back({start_atom, end_atom, order, display});
+          
+    } // end if atom or bond
+  } // for node
+    
+  for(auto &bond: bonds) {
+      unsigned int bond_idx;
+      if(bond.display == "WedgeEnd" || bond.display == "WedgeHashBegin") {
+          bond_idx = mol.addBond(ids[bond.end]->getIdx(), ids[bond.start]->getIdx(),
+                       bond.getBondType());
+      } else {
+          bond_idx = mol.addBond(ids[bond.start]->getIdx(), ids[bond.end]->getIdx(),
+                       bond.getBondType());
+      }
+      
+      if(bond.display == "WedgeEnd" || bond.display == "WedgeHashBegin" ||
+         bond.display == "WedgeBegin" || bond.display == "WedgeHashEnd") {
+          mol.getBondWithIdx(bond_idx-1)->setBondDir(Bond::BondDir::BEGINWEDGE);
+      }
+    }
+}
+
+std::vector<unsigned int> ints_to_vec(const std::string &s) {
+    std::vector<unsigned int> n;
+    std::stringstream ss(s);
+    std::copy(std::istream_iterator<unsigned int>(ss), std::istream_iterator<unsigned int>(), std::back_inserter(n));
+    return n;
+}
+} // namepspace
+
+std::vector<std::unique_ptr<RWMol>> CDXMLToMols(
+			       std::istream &inStream,
+			       bool sanitize,
+			       bool removeHs,
+			       bool strictParsing) {
+  
+  // populate tree structure pt
+  using boost::property_tree::ptree;
+  ptree pt;
+  read_xml(inStream, pt);
+  std::map<unsigned int, Atom*> ids;
+  std::vector<std::unique_ptr<RWMol>> mols;
+  std::map<unsigned int, size_t> fragment_lookup;
+  std::vector<SchemeInfo> schemes;
+    
+  MolzipParams molzip_params;
+  molzip_params.label = MolzipLabel::AtomProperty;
+  molzip_params.atomProperty = FUSE_LABEL;
+    
+  for( auto &cdxml : pt ) {
+      if(cdxml.first == "CDXML") {
+          for( auto &node : cdxml.second) {
+              if (node.first == "page") {
+                  for( auto &frag: node.second ) {
+                      if (frag.first == "fragment") { // chemical matter
+                          std::unique_ptr<RWMol> mol = std::make_unique<RWMol>();
+                          parse_fragment(*mol, frag.second, ids);
+                          MolOps::assignChiralTypesFromBondDirs(*mol);
+                          MolOps::assignStereochemistry(*mol);
+                          unsigned int frag_id = mol->getProp<unsigned int>(CDXML_FRAG_ID);
+                          fragment_lookup[frag_id] = mols.size();
+                          if(mol->hasProp(NEEDS_FUSE)) {
+                              mol->clearProp(NEEDS_FUSE);
+                              auto fused = molzip(*mol, molzip_params);
+                              mols.emplace_back(dynamic_cast<RWMol*>(fused.release()));
+                          } else {
+                              mols.push_back(std::move(mol));
+                          }
+                      } else if (frag.first == "scheme") { // get the reaction info
+                          int scheme_id = frag.second.get<int>("<xmlattr>.id", -1);
+                          for(auto &node : frag.second) {
+                              if(node.first == "step") {
+                                  auto step_id = node.second.get<int>("<xmlattr>.id", -1);
+                                  SchemeInfo scheme;
+                                  scheme.scheme_id = scheme_id;
+                                  scheme.step_id = step_id;
+                                  for(auto &attrib : node.second.get_child("<xmlattr>")) {
+                                      if(attrib.first == "ReactionStepProducts") {
+                                          scheme.ReactionStepProducts = ints_to_vec(attrib.second.data());
+                                      } else if (attrib.first == "ReactionStepReactants") {
+                                          scheme.ReactionStepReactants = ints_to_vec(attrib.second.data());
+                                      } else if (attrib.first == "ReactionStepObjectsAboveArrow") {
+                                          scheme.ReactionStepObjectsAboveArrow = ints_to_vec(attrib.second.data());
+                                      } else if (attrib.first == "ReactionStepObjectsBelowArrow") {
+                                          scheme.ReactionStepObjectsBelowArrow = ints_to_vec(attrib.second.data());
+                                      }
+                                  }
+                                  schemes.push_back(std::move(scheme));
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+  } 
+
+  // what do we do with the reaction schemes here???
+  return mols;
+}
+
+}
---
+++ Code/GraphMol/FileParsers/CDXMLParser.h
@@ -0,0 +1,32 @@
+#include <RDGeneral/export.h>
+#ifndef _RD_CDXML_FILEPARSERS_H
+#define _RD_CDXML_FILEPARSERS_H
+
+#include <RDGeneral/types.h>
+#include <GraphMol/RDKitBase.h>
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <exception>
+
+#include <boost/shared_ptr.hpp>
+
+namespace RDKit {
+// \brief construct a molecule from an CDXML file
+/*!
+ *   \param molBlock - string containing the mol block
+ *   \param sanitize - toggles sanitization and stereochemistry
+ *                     perception of the molecule
+ *   \param removeHs - toggles removal of Hs from the molecule. H removal
+ *                     is only done if the molecule is sanitized
+ *   \param strictParsing - if set to false, the parser is more lax about
+ * correctness of the contents.
+ */
+RDKIT_FILEPARSERS_EXPORT std::vector<std::unique_ptr<RWMol>> CDXMLToMols(
+			       std::istream &inStream,
+			       bool sanitize = true,
+			       bool removeHs = true,
+			       bool strictParsing = true);  
+}
+#endif // _RD_CDXML_FILEPARSERS_H
