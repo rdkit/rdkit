315797fb98079ce0572ece74b93ae8bcb1db74fb
greg landrum <greg.landrum@gmail.com>
2022-05-13 08:00:29 +0200

    reorg the catch tests the goal here is to make the builds faster

--- External/YAeHMOP/test1.cpp
+++ External/YAeHMOP/test1.cpp
@@ -1,8 +1,6 @@
 //
 // Copyright (C) 2018 Greg Landrum
 //
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
-                           // this in one cpp file per test
 #include "catch.hpp"
 
 #include <GraphMol/RDKitBase.h>
