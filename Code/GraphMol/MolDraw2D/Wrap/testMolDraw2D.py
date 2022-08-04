f0499d38e1fc0a2e2ce6d774e299fd77e242409a
David Cosgrove <david@cozchemix.co.uk>
2022-08-04 10:16:20 +0100

    Remove debugging file write.

--- Code/GraphMol/MolDraw2D/Wrap/testMolDraw2D.py
+++ Code/GraphMol/MolDraw2D/Wrap/testMolDraw2D.py
@@ -185,8 +185,6 @@ M  END""")
                          highlightAtomColors=acolors)
     drawer.FinishDrawing()
     svg = drawer.GetDrawingText()
-    with open('testDrawMoleculeArgs_1.svg', 'w') as f:
-      f.write(svg)
     # 4 molecules, 6 bonds each:
     re_str = r"path class='bond-\d+ atom-\d+ atom-\d+' d='M \d+.\d+,\d+.\d+ L \d+.\d+,\d+.\d+ L \d+.\d+,\d+.\d+ L \d+.\d+,\d+.\d+ Z' style='fill:#FF7F7F;"
     patt = re.compile(re_str)
