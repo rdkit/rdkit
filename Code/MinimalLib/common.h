948f7d231a543ae8966315818b3e9db2222a8bd6
Paolo Tosco <paolo.tosco.mail@gmail.com>
2022-09-06 14:27:10 +0200

    Expose reaction drawing and additional FPs in MinimalLib (#5277)
    * - exposed reaction drawing in MinimalLib
    - fixed a typo in the error message "JSON doesn't contain 'atoms' field, or it is not an array"
    - replaced RapidJson HasMember() with FindMember() to avoid a duplicate lookup in case the member exists and can be accessed
    - some cosmetic style changes (avoid multiple variable declarations on a single line, use curly bracket also for one-liner if clauses, use auto where possible)
    - capitalized "greg Landrum" to "Greg Landrum" (well deserved)
    - exposed other FPs in addition to Morgan and Pattern FPs in MinimalLib
    - added relevant tests
    
    * - update CXSMARTS test in MinimalLib
    
    * Changes in response to review:
    - exposed reaction drawing functionality to CFFI and added relevant tests
    - refactored fingerprint code to use JSON details and deprecated the Morgan/pattern fingerprint functions that used multiple parameters
    - all fingerprints are now exposed to both JS and CFFI with no code duplication
    - fixed a potential crash bug in the CFFI library where calling get_morgan_fp(), get_rdkit_fp() or get_pattern_fp with a NULL mol_pkl would result in dereferencing a nullptr
    
    * removed debugging printouts committed accidentally
    
    Co-authored-by: Tosco, Paolo <paolo.tosco@novartis.com>

--- Code/MinimalLib/common.h
+++ Code/MinimalLib/common.h
@@ -25,6 +25,10 @@
 #include <GraphMol/Descriptors/Property.h>
 #include <GraphMol/Descriptors/MolDescriptors.h>
 #include <GraphMol/Fingerprints/MorganFingerprints.h>
+#include <GraphMol/Fingerprints/AtomPairs.h>
+#ifdef RDK_BUILD_AVALON_SUPPORT
+#include <External/AvalonTools/AvalonTools.h>
+#endif
 #include <GraphMol/Depictor/RDDepictor.h>
 #include <GraphMol/CIPLabeler/CIPLabeler.h>
 #include <GraphMol/Abbreviations/Abbreviations.h>
@@ -32,6 +36,9 @@
 #include <GraphMol/MolStandardize/MolStandardize.h>
 #include <GraphMol/MolStandardize/Charge.h>
 #include <GraphMol/MolStandardize/Tautomer.h>
+#include <GraphMol/ChemReactions/Reaction.h>
+#include <GraphMol/ChemReactions/ReactionParser.h>
+#include <GraphMol/ChemReactions/SanitizeRxn.h>
 
 #include <sstream>
 #include <RDGeneral/BoostStartInclude.h>
@@ -183,77 +190,204 @@ RWMol *qmol_from_input(const std::string &input, const char *details_json) {
   return qmol_from_input(input, json);
 }
 
+ChemicalReaction *rxn_from_input(const std::string &input,
+                                 const std::string &details_json = "") {
+  bool useSmiles = false;
+  bool sanitize = false;
+  ChemicalReaction *rxn = nullptr;
+  boost::property_tree::ptree pt;
+  if (!details_json.empty()) {
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(sanitize);
+    LPT_OPT_GET(useSmiles);
+  }
+  try {
+    if (input.find("$RXN") != std::string::npos) {
+      bool removeHs = false;
+      bool strictParsing = false;
+      LPT_OPT_GET(removeHs);
+      LPT_OPT_GET(strictParsing);
+      rxn = RxnBlockToChemicalReaction(input, false, removeHs, strictParsing);
+    } else {
+      rxn = RxnSmartsToChemicalReaction(input, nullptr, useSmiles);
+    }
+  } catch (...) {
+    // we really don't want exceptions to be thrown in here
+    rxn = nullptr;
+  }
+  if (rxn) {
+    try {
+      if (sanitize) {
+        unsigned int failedOp;
+        unsigned int sanitizeOps = RxnOps::SANITIZE_ALL;
+        bool adjustReactants = true;
+        bool mergeQueryHs = true;
+        LPT_OPT_GET(adjustReactants);
+        LPT_OPT_GET(mergeQueryHs);
+        if (!adjustReactants) {
+          sanitizeOps ^= RxnOps::SANITIZE_ADJUST_REACTANTS;
+        }
+        if (!mergeQueryHs) {
+          sanitizeOps ^= RxnOps::SANITIZE_MERGEHS;
+        }
+        RxnOps::sanitizeRxn(*rxn, failedOp, sanitizeOps);
+      }
+    } catch (...) {
+      delete rxn;
+      rxn = nullptr;
+    }
+  }
+  return rxn;
+}
+
+ChemicalReaction *rxn_from_input(const std::string &input,
+                                 const char *details_json) {
+  std::string json;
+  if (details_json) {
+    json = details_json;
+  }
+  return rxn_from_input(input, json);
+}
+
 std::string process_details(const std::string &details, int &width, int &height,
                             int &offsetx, int &offsety, std::string &legend,
                             std::vector<int> &atomIds,
                             std::vector<int> &bondIds, bool &kekulize) {
   rj::Document doc;
   doc.Parse(details.c_str());
-  if (!doc.IsObject()) return "Invalid JSON";
+  if (!doc.IsObject()) {
+    return "Invalid JSON";
+  }
 
-  if (doc.HasMember("atoms")) {
-    if (!doc["atoms"].IsArray()) {
-      return "JSON doesn't contain 'atoms' field, or it is not an array";
+  const auto atomsIt = doc.FindMember("atoms");
+  if (atomsIt != doc.MemberEnd()) {
+    if (!atomsIt->value.IsArray()) {
+      return "JSON contains 'atoms' field, but it is not an array";
     }
-    for (const auto &molval : doc["atoms"].GetArray()) {
-      if (!molval.IsInt()) return ("Atom IDs should be integers");
+    for (const auto &molval : atomsIt->value.GetArray()) {
+      if (!molval.IsInt()) {
+        return ("Atom IDs should be integers");
+      }
       atomIds.push_back(molval.GetInt());
     }
   }
-  if (doc.HasMember("bonds")) {
-    if (!doc["bonds"].IsArray()) {
-      return "JSON contain 'bonds' field, but it is not an array";
+
+  const auto bondsIt = doc.FindMember("bonds");
+  if (bondsIt != doc.MemberEnd()) {
+    if (!bondsIt->value.IsArray()) {
+      return "JSON contains 'bonds' field, but it is not an array";
     }
-    for (const auto &molval : doc["bonds"].GetArray()) {
-      if (!molval.IsInt()) return ("Bond IDs should be integers");
+    for (const auto &molval : bondsIt->value.GetArray()) {
+      if (!molval.IsInt()) {
+        return ("Bond IDs should be integers");
+      }
       bondIds.push_back(molval.GetInt());
     }
   }
 
-  if (doc.HasMember("width")) {
-    if (!doc["width"].IsInt()) {
+  const auto widthIt = doc.FindMember("width");
+  if (widthIt != doc.MemberEnd()) {
+    if (!widthIt->value.IsInt()) {
       return "JSON contains 'width' field, but it is not an int";
     }
-    width = doc["width"].GetInt();
+    width = widthIt->value.GetInt();
   }
 
-  if (doc.HasMember("height")) {
-    if (!doc["height"].IsInt()) {
+  const auto heightIt = doc.FindMember("height");
+  if (heightIt != doc.MemberEnd()) {
+    if (!heightIt->value.IsInt()) {
       return "JSON contains 'height' field, but it is not an int";
     }
-    height = doc["height"].GetInt();
+    height = heightIt->value.GetInt();
   }
 
-  if (doc.HasMember("offsetx")) {
-    if (!doc["offsetx"].IsInt()) {
+  const auto offsetxIt = doc.FindMember("offsetx");
+  if (offsetxIt != doc.MemberEnd()) {
+    if (!offsetxIt->value.IsInt()) {
       return "JSON contains 'offsetx' field, but it is not an int";
     }
-    offsetx = doc["offsetx"].GetInt();
+    offsetx = offsetxIt->value.GetInt();
   }
 
-  if (doc.HasMember("offsety")) {
-    if (!doc["offsety"].IsInt()) {
+  const auto offsetyIt = doc.FindMember("offsety");
+  if (offsetyIt != doc.MemberEnd()) {
+    if (!offsetyIt->value.IsInt()) {
       return "JSON contains 'offsety' field, but it is not an int";
     }
-    offsety = doc["offsety"].GetInt();
+    offsety = offsetyIt->value.GetInt();
   }
 
-  if (doc.HasMember("legend")) {
-    if (!doc["legend"].IsString()) {
+  const auto legendIt = doc.FindMember("legend");
+  if (legendIt != doc.MemberEnd()) {
+    if (!legendIt->value.IsString()) {
       return "JSON contains 'legend' field, but it is not a string";
     }
-    legend = doc["legend"].GetString();
+    legend = legendIt->value.GetString();
   }
 
-  if (doc.HasMember("kekulize")) {
-    if (!doc["kekulize"].IsBool()) {
+  const auto kekulizeIt = doc.FindMember("kekulize");
+  if (kekulizeIt != doc.MemberEnd()) {
+    if (!kekulizeIt->value.IsBool()) {
       return "JSON contains 'kekulize' field, but it is not a bool";
     }
-    kekulize = doc["kekulize"].GetBool();
+    kekulize = kekulizeIt->value.GetBool();
   } else {
     kekulize = true;
   }
 
+  return "";
+}
+
+std::string process_rxn_details(
+    const std::string &details, int &width, int &height, int &offsetx,
+    int &offsety, std::string &legend, std::vector<int> &atomIds,
+    std::vector<int> &bondIds, bool &kekulize, bool &highlightByReactant,
+    std::vector<DrawColour> &highlightColorsReactants) {
+  auto problems = process_details(details, width, height, offsetx, offsety,
+                                  legend, atomIds, bondIds, kekulize);
+  if (!problems.empty()) {
+    return problems;
+  }
+  rj::Document doc;
+  doc.Parse(details.c_str());
+  if (!doc.IsObject()) {
+    return "Invalid JSON";
+  }
+  auto highlightByReactantIt = doc.FindMember("highlightByReactant");
+  if (highlightByReactantIt != doc.MemberEnd()) {
+    if (!highlightByReactantIt->value.IsBool()) {
+      return "JSON contains 'highlightByReactant' field, but it is not a bool";
+    }
+    highlightByReactant = highlightByReactantIt->value.GetBool();
+  } else {
+    highlightByReactant = false;
+  }
+  auto highlightColorsReactantsIt = doc.FindMember("highlightColorsReactants");
+  if (highlightColorsReactantsIt != doc.MemberEnd()) {
+    if (!highlightColorsReactantsIt->value.IsArray()) {
+      return "JSON contains 'highlightColorsReactants' field, but it is not an "
+             "array";
+    }
+    for (const auto &color : highlightColorsReactantsIt->value.GetArray()) {
+      if (!color.IsArray() || color.Size() < 3 || color.Size() > 4) {
+        return "JSON contains 'highlightColorsReactants' array, but the "
+               "elements are not R,G,B[,A] arrays";
+      }
+      std::vector<double> rgba(4, 1.0);
+      unsigned int i = 0;
+      for (const auto &component : color.GetArray()) {
+        if (!component.IsDouble()) {
+          return "JSON contains 'highlightColorsReactants' array, but the "
+                 "R,G,B[,A] arrays contain non-float values";
+        }
+        CHECK_INVARIANT(i < 4, "");
+        rgba[i++] = component.GetDouble();
+      }
+      highlightColorsReactants.emplace_back(rgba[0], rgba[1], rgba[2], rgba[3]);
+    }
+  }
   return "";
 }
 
@@ -288,7 +422,8 @@ std::string mol_to_svg(const ROMol &m, int w, int h,
   std::vector<int> atomIds;
   std::vector<int> bondIds;
   std::string legend = "";
-  int offsetx = 0, offsety = 0;
+  int offsetx = 0;
+  int offsety = 0;
   bool kekulize = true;
   if (!details.empty()) {
     auto problems = process_details(details, w, h, offsetx, offsety, legend,
@@ -309,6 +444,37 @@ std::string mol_to_svg(const ROMol &m, int w, int h,
                                          kekulize);
   drawer.finishDrawing();
 
+  return drawer.getDrawingText();
+}
+
+std::string rxn_to_svg(const ChemicalReaction &rxn, int w, int h,
+                       const std::string &details = "") {
+  std::vector<int> atomIds;
+  std::vector<int> bondIds;
+  std::string legend = "";
+  int offsetx = 0;
+  int offsety = 0;
+  bool kekulize = true;
+  bool highlightByReactant = false;
+  std::vector<DrawColour> highlightColorsReactants;
+  if (!details.empty()) {
+    auto problems = process_rxn_details(
+        details, w, h, offsetx, offsety, legend, atomIds, bondIds, kekulize,
+        highlightByReactant, highlightColorsReactants);
+    if (!problems.empty()) {
+      return problems;
+    }
+  }
+
+  MolDraw2DSVG drawer(w, h);
+  if (!kekulize) {
+    drawer.drawOptions().prepareMolsBeforeDrawing = false;
+  }
+  drawer.drawReaction(rxn, highlightByReactant,
+                      !highlightByReactant || highlightColorsReactants.empty()
+                          ? nullptr
+                          : &highlightColorsReactants);
+  drawer.finishDrawing();
   return drawer.getDrawingText();
 }
 
@@ -425,6 +591,133 @@ std::unique_ptr<RWMol> do_fragment_parent(RWMol &mol,
       MolStandardize::fragmentParent(mol, ps, skipStandardize));
   return res;
 }
+
+std::unique_ptr<ExplicitBitVect> morgan_fp_as_bitvect(
+    const RWMol &mol, const char *details_json) {
+  size_t radius = 2;
+  size_t nBits = 2048;
+  bool useChirality = false;
+  bool useBondTypes = true;
+  bool includeRedundantEnvironments = false;
+  bool onlyNonzeroInvariants = false;
+  if (details_json && strlen(details_json)) {
+    // FIX: this should eventually be moved somewhere else
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(radius);
+    LPT_OPT_GET(nBits);
+    LPT_OPT_GET(useChirality);
+    LPT_OPT_GET(useBondTypes);
+    LPT_OPT_GET(includeRedundantEnvironments);
+    LPT_OPT_GET(onlyNonzeroInvariants);
+  }
+  auto fp = MorganFingerprints::getFingerprintAsBitVect(
+      mol, radius, nBits, nullptr, nullptr, useChirality, useBondTypes,
+      onlyNonzeroInvariants, nullptr, includeRedundantEnvironments);
+  return std::unique_ptr<ExplicitBitVect>{fp};
+}
+
+std::unique_ptr<ExplicitBitVect> rdkit_fp_as_bitvect(const RWMol &mol,
+                                                     const char *details_json) {
+  unsigned int minPath = 1;
+  unsigned int maxPath = 7;
+  unsigned int nBits = 2048;
+  unsigned int nBitsPerHash = 2;
+  bool useHs = true;
+  bool branchedPaths = true;
+  bool useBondOrder = true;
+  if (details_json && strlen(details_json)) {
+    // FIX: this should eventually be moved somewhere else
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(minPath);
+    LPT_OPT_GET(maxPath);
+    LPT_OPT_GET(nBits);
+    LPT_OPT_GET(nBitsPerHash);
+    LPT_OPT_GET(useHs);
+    LPT_OPT_GET(branchedPaths);
+    LPT_OPT_GET(useBondOrder);
+  }
+  auto fp = RDKFingerprintMol(mol, minPath, maxPath, nBits, nBitsPerHash, useHs,
+                              0, 128, branchedPaths, useBondOrder);
+  return std::unique_ptr<ExplicitBitVect>{fp};
+}
+
+std::unique_ptr<ExplicitBitVect> pattern_fp_as_bitvect(
+    const RWMol &mol, const char *details_json) {
+  unsigned int nBits = 2048;
+  bool tautomericFingerprint = false;
+  if (details_json && strlen(details_json)) {
+    // FIX: this should eventually be moved somewhere else
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(nBits);
+    LPT_OPT_GET(tautomericFingerprint);
+  }
+  auto fp = PatternFingerprintMol(mol, nBits, nullptr, nullptr,
+                                  tautomericFingerprint);
+  return std::unique_ptr<ExplicitBitVect>{fp};
+}
+
+std::unique_ptr<ExplicitBitVect> topological_torsion_fp_as_bitvect(
+    const RWMol &mol, const char *details_json) {
+  unsigned int nBits = 2048;
+  if (details_json && strlen(details_json)) {
+    // FIX: this should eventually be moved somewhere else
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(nBits);
+  }
+  auto fp =
+      AtomPairs::getHashedTopologicalTorsionFingerprintAsBitVect(mol, nBits);
+  return std::unique_ptr<ExplicitBitVect>{fp};
+}
+
+std::unique_ptr<ExplicitBitVect> atom_pair_fp_as_bitvect(
+    const RWMol &mol, const char *details_json) {
+  unsigned int nBits = 2048;
+  unsigned int minLength = 1;
+  unsigned int maxLength = 30;
+  if (details_json && strlen(details_json)) {
+    // FIX: this should eventually be moved somewhere else
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(nBits);
+    LPT_OPT_GET(minLength);
+    LPT_OPT_GET(maxLength);
+  }
+  auto fp = AtomPairs::getHashedAtomPairFingerprintAsBitVect(
+      mol, nBits, minLength, maxLength);
+  return std::unique_ptr<ExplicitBitVect>{fp};
+}
+
+#ifdef RDK_BUILD_AVALON_SUPPORT
+std::unique_ptr<ExplicitBitVect> avalon_fp_as_bitvect(
+    const RWMol &mol, const char *details_json) {
+  unsigned int nBits = 512;
+  if (details_json && strlen(details_json)) {
+    // FIX: this should eventually be moved somewhere else
+    std::istringstream ss;
+    ss.str(details_json);
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(ss, pt);
+    LPT_OPT_GET(nBits);
+  }
+  std::unique_ptr<ExplicitBitVect> fp(new ExplicitBitVect(nBits));
+  AvalonTools::getAvalonFP(mol, *fp, nBits);
+  return fp;
+}
+#endif
 
 }  // namespace MinimalLib
 }  // namespace RDKit
