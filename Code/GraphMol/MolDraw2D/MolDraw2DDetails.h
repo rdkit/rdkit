dc16efd42937ffa4733ed3d78fa1051d000d526d
Greg Landrum <greg.landrum@gmail.com>
2022-09-09 05:04:18 +0200

    fix windows dll ci builds (#5543)
    * fix windows dll ci builds
    
    * late breaking mac ci changes
    
    * more flex, get the mac_java one too
    
    * oops

--- Code/GraphMol/MolDraw2D/MolDraw2DDetails.h
+++ Code/GraphMol/MolDraw2D/MolDraw2DDetails.h
@@ -110,21 +110,27 @@ inline std::string formatDouble(double val) {
   return boost::str(boost::format("%.1f") % val);
 }
 
-bool doesLineIntersect(const StringRect &rect, const Point2D &end1,
-                       const Point2D &end2, double padding);
+RDKIT_MOLDRAW2D_EXPORT bool doesLineIntersect(const StringRect &rect,
+                                              const Point2D &end1,
+                                              const Point2D &end2,
+                                              double padding);
 // returns true if any corner of triangle is inside the rectangle.
-bool doesTriangleIntersect(const StringRect &rect, const Point2D &pt1,
-                           const Point2D &pt2, const Point2D &pt3,
-                           double padding);
-bool doesLineIntersectEllipse(const Point2D &centre, double xradius,
-                              double yradius, double padding,
-                              const Point2D &end1, const Point2D &end2);
+RDKIT_MOLDRAW2D_EXPORT bool doesTriangleIntersect(const StringRect &rect,
+                                                  const Point2D &pt1,
+                                                  const Point2D &pt2,
+                                                  const Point2D &pt3,
+                                                  double padding);
+RDKIT_MOLDRAW2D_EXPORT bool doesLineIntersectEllipse(
+    const Point2D &centre, double xradius, double yradius, double padding,
+    const Point2D &end1, const Point2D &end2);
 // angles expected in degrees, between 0 and 360.
-bool doesLineIntersectArc(const Point2D &centre, double xradius, double yradius,
-                          double start_ang, double stop_ang, double padding,
-                          const Point2D &end1, const Point2D &end2);
-bool doLinesIntersect(const Point2D &l1s, const Point2D &l1f,
-                      const Point2D &l2s, const Point2D &l2f, Point2D *ip);
+RDKIT_MOLDRAW2D_EXPORT bool doesLineIntersectArc(
+    const Point2D &centre, double xradius, double yradius, double start_ang,
+    double stop_ang, double padding, const Point2D &end1, const Point2D &end2);
+RDKIT_MOLDRAW2D_EXPORT bool doLinesIntersect(const Point2D &l1s,
+                                             const Point2D &l1f,
+                                             const Point2D &l2s,
+                                             const Point2D &l2f, Point2D *ip);
 // This uses the barycentric coordinate system method from
 // http://totologic.blogspot.com/2014/01/accurate-point-in-triangle-test.html
 // where it notes and provides a solution for instabilities when the point
@@ -133,8 +139,10 @@ bool doLinesIntersect(const Point2D &l1s, const Point2D &l1f,
 // issue when, for example, two triangles share an edge and the point is on that
 // edge, when it might give the disappointing result that the point is in
 // neither triangle.
-bool isPointInTriangle(const Point2D &pt, const Point2D &t1, const Point2D &t2,
-                       const Point2D &t3);
+RDKIT_MOLDRAW2D_EXPORT bool isPointInTriangle(const Point2D &pt,
+                                              const Point2D &t1,
+                                              const Point2D &t2,
+                                              const Point2D &t3);
 
 // returns a vector of p1,c1,c2,p2 tuples for bezier curves
 RDKIT_MOLDRAW2D_EXPORT
