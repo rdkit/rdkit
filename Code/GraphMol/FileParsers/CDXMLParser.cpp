6ae696046ab129b9686a5bd6b1e250bd13d31f41
Brian Kelley <bkelley@relaytx.com>
2022-09-11 18:20:15 -0400

    Fix auto signature

--- Code/GraphMol/FileParsers/CDXMLParser.cpp
+++ Code/GraphMol/FileParsers/CDXMLParser.cpp
@@ -97,7 +97,7 @@ template <typename Q>
 Atom *addquery(Q *qry, std::string symbol, RWMol &mol, unsigned int idx) {
   PRECONDITION(qry, "bad query");
   auto *atm = mol.getAtomWithIdx(idx);
-  auto *qa = std::make_unique<QueryAtom>(*atm);
+  auto qa = std::make_unique<QueryAtom>(*atm);
   qa->setQuery(qry);
   qa->setNoImplicit(true);
   mol.replaceAtom(idx, qa);
