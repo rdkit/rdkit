a0d81e20d5e5e5e16ea53f0ed8a48a093adb94da
Gareth Jones <java.jones@gmail.com>
2022-10-13 01:54:40 -0600

    Undo change to master

--- Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
+++ Code/GraphMol/RGroupDecomposition/testRGroupDecomp.cpp
@@ -2882,7 +2882,7 @@ M  END
 void testGithub5569() {
   BOOST_LOG(rdInfoLog)
       << "********************************************************\n";
-  BOOST_LOG(rdInfoLog) << "Test that Github5569 is fixed" << std::endl;
+  BOOST_LOG(rdInfoLog) << "Test that Github5269 is fixed" << std::endl;
   auto core = R"CTAB(
 ChemDraw09152209202D
 
