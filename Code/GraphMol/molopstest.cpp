82106731974008cec68f493ae5a56d1f8f8988c3
juius <hwg245@alumni.ku.dk>
2022-10-24 12:26:15 +0000

    add dative carbene test case

--- Code/GraphMol/molopstest.cpp
+++ Code/GraphMol/molopstest.cpp
@@ -3342,6 +3342,15 @@ void testHybridization() {
     delete m;
   }
 
+  {
+    RWMol *m;
+    std::string smi = "[Pd]<-[C]1N(C)CCN(C)1";
+    m = SmilesToMol(smi);
+    TEST_ASSERT(m);
+    TEST_ASSERT(m->getAtomWithIdx(1)->getHybridization() == Atom::SP2);
+    delete m;
+  }
+
   BOOST_LOG(rdInfoLog) << "Finished" << std::endl;
 }
 
