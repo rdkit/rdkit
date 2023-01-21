a28243b21d0f80d9566a54cc5cf950304b693aee
David Cosgrove <david@cozchemix.co.uk>
2023-01-21 17:06:30 +0000

    Python wrapping for disconnectOrganometallics.

--- Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp
+++ Code/GraphMol/MolStandardize/Wrap/rdMolStandardize.cpp
@@ -107,6 +107,9 @@ RDKit::ROMol *superParentHelper(const RDKit::ROMol *mol, python::object params,
   return parentHelper(mol, params, skip_standardize,
                       RDKit::MolStandardize::superParent);
 }
+RDKit::ROMol *disconnectOrganometallicsHelper(RDKit::ROMol &mol) {
+  return RDKit::MolStandardize::disconnectOrganometallics(mol);
+}
 
 }  // namespace
 
@@ -261,6 +264,9 @@ BOOST_PYTHON_MODULE(rdMolStandardize) {
   python::def("CanonicalTautomer", canonicalTautomerHelper,
               (python::arg("mol"), python::arg("params") = python::object()),
               docString.c_str(),
+              python::return_value_policy<python::manage_new_object>());
+  python::def("DisconnectOrganometallics", &disconnectOrganometallicsHelper,
+              (python::arg("mol")), docString.c_str(),
               python::return_value_policy<python::manage_new_object>());
 
   wrap_validate();
