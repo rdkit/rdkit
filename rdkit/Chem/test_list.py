6e427a01af54c952e6e210e373689c72236b7384
greg landrum <greg.landrum@gmail.com>
2023-02-02 15:15:07 +0100

    Fixes #6045 enables the doc tests for EnumerateStereoisomers.py Re-enables the BRICS tests

--- rdkit/Chem/test_list.py
+++ rdkit/Chem/test_list.py
@@ -10,6 +10,7 @@ tests = [("python", "UnitTestChem.py", {}), ("python", "UnitTestChemv2.py", {}),
          ("python", "UnitTestFunctionalGroups.py", {}), ("python", "UnitTestCrippen.py", {}),
          ("python", "UnitTestPandasTools.py", {}), ("python", "UnitTestDocTestsChem.py", {}),
          ("python", "UnitTestFeatFinderCLI.py", {}), ("python", "UnitTestQED.py", {}),
+         ("python", "BRICS.py", {}), ("python", "EnumerateStereoisomers.py", {}),
          ("python", "UnitTestSaltRemover.py", {}), ("python", "test_list.py", {
            'dir': 'AtomPairs'
          }), ("python", "test_list.py", {
