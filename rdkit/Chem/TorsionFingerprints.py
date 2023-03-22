70d0e490153cf61725e55f2d83dc3a4178c9017c
Kazuya Ujihara <ujihara.kazuya@gmail.com>
2023-03-23 00:49:16 +0900

    Fix TorsionFingerprints for 15 membered rings

--- rdkit/Chem/TorsionFingerprints.py
+++ rdkit/Chem/TorsionFingerprints.py
@@ -272,7 +272,7 @@ def CalculateTorsionLists(mol, maxDev='equal', symmRadius=2, ignoreColinearBonds
     # get the torsions
     tmp = []
     num = len(r)
-    maxdev = 180.0 * math.exp(-0.025 * (num - 14) * (num - 14))
+    maxdev = 180.0 if 14 <= num else 180.0 * math.exp(-0.025 * (num - 14) * (num - 14))
     for i in range(len(r)):
       tmp.append((r[i], r[(i + 1) % num], r[(i + 2) % num], r[(i + 3) % num]))
     tors_list_rings.append((tmp, maxdev))
