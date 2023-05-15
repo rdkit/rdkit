f1a76c43f84bd4a4dd5738f85fe578d11c7de91d
Daniel McNeela <daniel.mcneela@gmail.com>
2023-05-15 16:49:52 -0500

    delete StereoEnumerationOptions constructor

--- Code/GraphMol/EnumerateStereoisomers.h
+++ Code/GraphMol/EnumerateStereoisomers.h
@@ -24,11 +24,7 @@ namespace RDKit {
     struct StereoEnumerationOptions {
         public:
             bool try_embedding, only_unassigned, only_stereo_groups, unique;
-            unsigned int max_isomers, rand;
-            StereoEnumerationOptions(
-                bool try_embedding=false, bool only_unassigned=true,
-                bool only_stereo_groups=false, bool unique=true,
-                unsigned int max_isomers=1024, unsigned int rand=0);
+            unsigned int max_isomers {1024}, rand {0};
     };
 
     class _Flipper {
