3fa7dfe5a8f2c1714f472f42ec14af217a50eb2b
greg landrum <greg.landrum@gmail.com>
2023-05-21 16:07:30 +0200

    we really need to pickle mol properties

--- Code/PgSQL/rdkit/adapter.cpp
+++ Code/PgSQL/rdkit/adapter.cpp
@@ -107,6 +107,10 @@
 using namespace std;
 using namespace RDKit;
 
+constexpr unsigned int pickleWhat =
+    PicklerOps::PropertyPickleOptions::MolProps |
+    PicklerOps::PropertyPickleOptions::PrivateProps;
+
 class ByteA : public std::string {
  public:
   ByteA() : string(){};
@@ -174,7 +178,7 @@ extern "C" Mol *deconstructROMol(CROMol data) {
   ByteA b;
 
   try {
-    MolPickler::pickleMol(mol, b);
+    MolPickler::pickleMol(mol, b, pickleWhat);
   } catch (MolPicklerException &e) {
     elog(ERROR, "pickleMol: %s", e.what());
   } catch (...) {
@@ -478,7 +482,7 @@ extern "C" char *makeMolBlob(CROMol data, int *len) {
   auto *mol = (ROMol *)data;
   StringData.clear();
   try {
-    MolPickler::pickleMol(*mol, StringData);
+    MolPickler::pickleMol(*mol, StringData, pickleWhat);
   } catch (...) {
     elog(ERROR, "makeMolBlob: Unknown exception");
   }
