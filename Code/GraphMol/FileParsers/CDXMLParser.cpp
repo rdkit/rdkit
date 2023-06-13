3f613f74f2881feba94d37453d0329813685c268
Brian Kelley <bkelley@relaytx.com>
2023-06-13 09:51:41 -0400

    Readd comment

--- Code/GraphMol/FileParsers/CDXMLParser.cpp
+++ Code/GraphMol/FileParsers/CDXMLParser.cpp
@@ -570,9 +570,11 @@ std::vector<std::unique_ptr<RWMol>> CDXMLDataStreamToMols(
                       atm->getProp<std::vector<double>>(CDX_ATOM_POS);
 
                   RDGeom::Point3D p;
-                  if (coord.size() >=2) {
-                        p.x = coord[0];
-                        p.y = -coord[1];
+		  if (coord.size() == 2) {
+                    p.x = coord[0];
+                    p.y = -1 * coord[1];  // CDXML uses an inverted coordinate
+                                          // system, so we need to reverse that
+                    p.z = 0.0;
                   }
                   conf->setAtomPos(atm->getIdx(), p);
                   atm->clearProp(CDX_ATOM_POS);
