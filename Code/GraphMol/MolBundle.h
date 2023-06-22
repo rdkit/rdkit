e944b8e9cbb97c1669567337709fe9d689630bff
Greg Landrum <greg.landrum@gmail.com>
2023-06-22 10:54:35 +0200

    Add support for generalized substructure searching (#6443)
    * bring over XQMol.h
    
    * bring XQMol over for a separate PR
    
    * backup... need to move these files
    
    * refactor
    
    * substruct
    
    * oops
    
    * testing basics
    
    * basic tests
    
    * more test
    
    * more testing
    
    * add empty() method to MolBundle
    
    * easy creation of ExtendedQueryMols
    
    * SRU test
    
    * reorg
    
    * the MolEnumerator should clear out residual reaction properties
    
    * support to/from JSON
    
    * cleanup
    
    * first past at python wrappers
    put the new code in a namespace
    
    * less stupid names
    more tests
    
    * undo some unnecessary changes
    
    * remove compiler warning
    
    * release GIL during hasSubstructMatch too
    
    * support adjustQueryProperties
    
    * support toggling which operations apply
    
    Fix the tautomer tests to include a molecule which actually requires the tautomer matching

--- Code/GraphMol/MolBundle.h
+++ Code/GraphMol/MolBundle.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2017-2020 Greg Landrum
+//  Copyright (C) 2017-2023 Greg Landrum and other RDKit contributors
 //
 //   @@ All Rights Reserved @@
 //  This file is part of the RDKit.
@@ -25,11 +25,31 @@
 #include <boost/smart_ptr.hpp>
 #include <RDGeneral/BoostEndInclude.h>
 
+#ifdef RDK_USE_BOOST_SERIALIZATION
+#include <RDGeneral/BoostStartInclude.h>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/archive_exception.hpp>
+#include <RDGeneral/BoostEndInclude.h>
+#endif
+
 // our stuff
 #include <RDGeneral/Exceptions.h>
+#include <GraphMol/MolPickler.h>
 
 namespace RDKit {
 class ROMol;
+
+inline bool MolBundleCanSerialize() {
+#ifdef RDK_USE_BOOST_SERIALIZATION
+  return true;
+#else
+  return false;
+#endif
+};
 
 //! MolBundle contains a collection of related ROMols
 /*!
@@ -42,8 +62,7 @@ class MolBundle : public RDProps {
 
   //! copy constructor
   MolBundle(const MolBundle &other) : RDProps(other) { d_mols = other.d_mols; }
-  // FIX: need serialization/deserialization
-
+  MolBundle(const std::string &pkl) { initFromString(pkl); }
   virtual ~MolBundle() {}
 
   //! returns our molecules
@@ -59,6 +78,9 @@ class MolBundle : public RDProps {
   }
   //! returns the number of molecules from the bundle
   virtual size_t size() const { return d_mols.size(); }
+  //! returns whether or not the bundle is empty
+  virtual bool empty() const { return d_mols.empty(); }
+
   //! returns a particular molecule in the bundle
   virtual const boost::shared_ptr<ROMol> getMol(size_t idx) const {
     if (idx >= d_mols.size()) {
@@ -70,6 +92,64 @@ class MolBundle : public RDProps {
   virtual const boost::shared_ptr<ROMol> operator[](size_t idx) const {
     return getMol(idx);
   }
+
+  //! serializes (pickles) to a stream
+  void toStream(std::ostream &ss) const {
+#ifndef RDK_USE_BOOST_SERIALIZATION
+    PRECONDITION(0, "Boost SERIALIZATION is not enabled")
+#else
+    boost::archive::text_oarchive ar(ss);
+    ar << *this;
+#endif
+  };
+  //! returns a string with a serialized (pickled) representation
+  std::string serialize() const {
+    std::stringstream ss;
+    toStream(ss);
+    return ss.str();
+  };
+  //! initializes from a stream pickle
+  void initFromStream(std::istream &ss) {
+#ifndef RDK_USE_BOOST_SERIALIZATION
+    PRECONDITION(0, "Boost SERIALIZATION is not enabled")
+#else
+    boost::archive::text_iarchive ar(ss);
+    ar >> *this;
+#endif
+  };
+  //! initializes from a string pickle
+  void initFromString(const std::string &text) {
+    std::stringstream ss(text);
+    initFromStream(ss);
+  };
+
+#ifdef RDK_USE_BOOST_SERIALIZATION
+  // FIX: we don't currently serialize properties
+  template <class Archive>
+  void save(Archive &ar, const unsigned int version) const {
+    RDUNUSED_PARAM(version);
+    std::vector<std::string> pkls;
+    for (const auto &mol : d_mols) {
+      std::string pkl;
+      MolPickler::pickleMol(*mol, pkl);
+      pkls.push_back(pkl);
+    }
+    ar << pkls;
+  }
+
+  template <class Archive>
+  void load(Archive &ar, const unsigned int version) {
+    RDUNUSED_PARAM(version);
+
+    std::vector<std::string> pkls;
+    ar >> pkls;
+    d_mols.clear();
+    for (const auto &pkl : pkls) {
+      d_mols.push_back(ROMOL_SPTR(new ROMol(pkl)));
+    }
+  }
+  BOOST_SERIALIZATION_SPLIT_MEMBER()
+#endif
 
  protected:
   std::vector<boost::shared_ptr<ROMol>> d_mols;
