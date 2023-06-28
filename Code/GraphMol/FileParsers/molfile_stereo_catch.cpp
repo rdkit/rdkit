813ceebf9ea22e161905ba7928c34b4f82602328
Ric <ricrogz@gmail.com>
2023-06-27 20:05:55 -0400

    add a test

--- Code/GraphMol/FileParsers/molfile_stereo_catch.cpp
+++ Code/GraphMol/FileParsers/molfile_stereo_catch.cpp
@@ -554,3 +554,14 @@ M  END
     }
   }
 }
+
+TEST_CASE(
+    "GitHub Issue #6502: MolToMolBlock writes \"either\" stereo for double bonds "
+    "which shouldn't be stereo.",
+    "[bug][molblock][stereo]") {
+  auto m = "CP1(O)=NP(C)(O)=NP(C)(O)=NP(C)(O)=N1"_smiles;
+  REQUIRE(m);
+
+  auto mb = MolToV3KMolBlock(*m);
+  CHECK(mb.find("CFG=2") == std::string::npos);
+}
