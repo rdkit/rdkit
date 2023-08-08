d6114dc6c7c4728667de6d67ec61ce0a3f2d3ca1
Gareth Jones <java.jones@gmail.com>
2023-08-08 17:13:46 -0600

    Added query comment

--- Code/GraphMol/RGroupDecomposition/RGroupCore.cpp
+++ Code/GraphMol/RGroupDecomposition/RGroupCore.cpp
@@ -824,6 +824,8 @@ std::shared_ptr<TautomerQuery> RCore::getMatchingTautomerQuery() {
     try {
       // Enumerate tautomers from a sanitized copy of the matching molecule
       const auto copy = new RWMol(*matchingMol);
+      // If the core has had rgroup labels removed when creating the matching mol
+      // then we need to update properties.  Should a full sanitization be done?
       // MolOps::sanitizeMol(*copy);
       copy->updatePropertyCache(false);
       std::shared_ptr<TautomerQuery> tautomerQuery(
