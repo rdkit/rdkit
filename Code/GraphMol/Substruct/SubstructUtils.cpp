fda25fce6e2bd59eb129e4c109a492374c427fda
Meng Wu <wu3meng4@gmail.com>
2023-10-07 19:05:57 -0400

    Fix testSubstructMatch

--- Code/GraphMol/Substruct/SubstructUtils.cpp
+++ Code/GraphMol/Substruct/SubstructUtils.cpp
@@ -246,11 +246,14 @@ std::vector<MatchVectType> sortMatchesByDegreeOfCoreSubstitution(
 }
 
 bool isAtomTerminalRGroupOrQueryHydrogen(const Atom *atom) {
-  return atom->getDegree() == 1 &&
-         (atom->getAtomicNum() == 0 ||
-          (atom->hasQuery() &&
-           describeQuery(atom).find("AtomAtomicNum 1 = val") !=
-               std::string::npos));
+  auto isDummyAtom = [](const Atom &atom) {
+    return (!atom.hasQuery() && atom.getAtomicNum() == 0) ||
+           (atom.hasQuery() && atom.getQuery()->getDescription() == "AtomNull");
+  };
+  return (atom->getDegree() == 1 && isDummyAtom(*atom)) ||
+         (atom->hasQuery() &&
+          describeQuery(atom).find("AtomAtomicNum 1 = val") !=
+              std::string::npos);
 }
 
 #define PT_OPT_GET(opt) params.opt = pt.get(#opt, params.opt)
