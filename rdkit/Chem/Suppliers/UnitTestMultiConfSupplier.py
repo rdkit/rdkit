2cc423d402e6219649e4d0ae593f705d3a288194
Rachael Pirie <56546141+RPirie96@users.noreply.github.com>
2023-10-24 10:49:35 +0100

    Fix paths to data in tests

--- rdkit/Chem/Suppliers/UnitTestMultiConfSupplier.py
+++ rdkit/Chem/Suppliers/UnitTestMultiConfSupplier.py
@@ -1,29 +1,31 @@
 import unittest
+from rdkit import RDConfig
 from rdkit.Chem.Suppliers.MultiConfSupplier import MultiConfSupplier
 
 
 class TestCase(unittest.TestCase):
     def testMultiConfParse(self):
+        path = os.path.join(RDConfig.RDCodeDir, 'Chem', 'test_data')
         # check ValueError raised when no 3D coordinates supplied
         with self.assertRaises(ValueError):
-            suppl = MultiConfSupplier('Chem/test_data/read_multi_conf_error.sdf')
+            suppl = MultiConfSupplier(os.path.join(path, 'read_multi_conf_error.sdf'))
             mols = [mol for mol in suppl]
 
         # test function returns expected outputs
         truth = [62, 7, 200]
 
         # check default
-        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier('Chem/test_data/read_multi_conf.sdf')]], truth)
+        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier(os.path.join(path, 'read_multi_conf.sdf'))]], truth)
 
         # check propertyName = SMILES
-        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier('Chem/test_data/read_multi_conf.sdf', "SMILES")]], truth)
+        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier(os.path.join(path, 'read_multi_conf.sdf'), "SMILES")]], truth)
 
         # check propertyName = countLine
-        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier('Chem/test_data/read_multi_conf.sdf', "countLine")]],
+        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier(os.path.join(path, 'read_multi_conf.sdf'), "countLine")]],
                          truth)
 
         # check propertyName using GetProp
-        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier('Chem/test_data/read_multi_conf.sdf', "ID")]], truth)
+        self.assertEqual([m.GetNumConformers() for m in [x for x in MultiConfSupplier(os.path.join(path, 'read_multi_conf.sdf'), "ID")]], truth)
 
 
 if __name__ == '__main__':
