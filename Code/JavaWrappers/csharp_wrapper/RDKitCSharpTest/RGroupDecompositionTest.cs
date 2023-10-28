81e9cb23868c7d8e6fe391b7809acfedae306661
Gareth Jones <java.jones@gmail.com>
2023-10-28 13:47:58 +0300

    Fix stereo bond corruption on RGD. (#6832)

--- Code/JavaWrappers/csharp_wrapper/RDKitCSharpTest/RGroupDecompositionTest.cs
+++ Code/JavaWrappers/csharp_wrapper/RDKitCSharpTest/RGroupDecompositionTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GraphMolWrap;
+﻿using GraphMolWrap;
 using Xunit;
 
 namespace RdkitTests
@@ -39,9 +34,10 @@ M  RGP  2   8   1   9   2
 M  END";
             var core = RWMol.MolFromMolBlock(block);
 
-            var rgdParameters = new RGroupDecompositionParameters {
-                matchingStrategy = (uint) RGroupMatching.GreedyChunks,
-                scoreMethod = (uint) RGroupScore.FingerprintVariance,
+            var rgdParameters = new RGroupDecompositionParameters
+            {
+                matchingStrategy = (uint)RGroupMatching.GreedyChunks,
+                scoreMethod = (uint)RGroupScore.FingerprintVariance,
                 onlyMatchAtRGroups = false,
                 removeHydrogensPostMatch = true,
                 removeAllHydrogenRGroups = true,
@@ -60,6 +56,65 @@ M  END";
 
             var rows = rgd.getRGroupsAsRows();
             Assert.Equal(2, rows.Count);
+        }
+
+        [Fact]
+        public void TestBreakOnStereoBond()
+        {
+            var block = @"ACS Document 1996
+  ChemDraw10242316092D
+
+  6  6  0  0  0  0  0  0  0  0999 V2000
+   -0.7145    0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+   -0.7145   -0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.0000   -0.8250    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.7145   -0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.7145    0.4125    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+    0.0000    0.8250    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
+  1  2  2  0      
+  2  3  1  0      
+  3  4  2  0      
+  4  5  1  0      
+  5  6  2  0      
+  6  1  1  0      
+M  END
+";
+            var core = RWMol.MolFromMolBlock(block);
+            var rgdParameters = new RGroupDecompositionParameters
+            {
+                matchingStrategy = (uint)RGroupMatching.GreedyChunks,
+                scoreMethod = (uint)RGroupScore.FingerprintVariance,
+                onlyMatchAtRGroups = false,
+                removeHydrogensPostMatch = true,
+                removeAllHydrogenRGroups = true,
+                allowMultipleRGroupsOnUnlabelled = true,
+                doTautomers = false,
+                doEnumeration = false
+            };
+
+            var cores = new ROMol_Vect();
+            cores.Add(core);
+            var rgd = new RGroupDecomposition(cores, rgdParameters);
+            var mol1 = RWMol.MolFromSmiles("C/C=C/C1=CC=CC=C1");
+            Assert.Equal(0, rgd.add(mol1));
+            Assert.True(rgd.process());
+            var rows = rgd.getRGroupsAsRows();
+            var columns = rgd.getRGroupsAsColumns();
+            Assert.Equal(1, rows.Count);
+            var r1 = rows[0]["R1"];
+            var foundBond = false;
+            for (var bondIdx = 0U; bondIdx < r1.getNumBonds(); bondIdx++)
+            {
+                var bond = r1.getBondWithIdx(bondIdx);
+                if (bond.getStereo() > Bond.BondStereo.STEREOANY)
+                {
+                    Assert.False(foundBond);
+                    foundBond = true;
+                    var stereoAtoms = bond.getStereoAtoms();
+                    Assert.Equal(2, stereoAtoms?.Count);
+                }
+            }
+            Assert.True(foundBond);
         }
     }
 }
