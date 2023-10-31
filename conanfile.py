f0842155edb88a16b5589d8deed35148f775f0b5
Niklas Paulicks <paulicks@justtrack.io>
2023-10-31 11:04:11 +0100

    working

--- conanfile.py
+++ conanfile.py
@@ -18,7 +18,7 @@ class RDKitRecipe(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
 
-    requires = "freetype/2.13.0", "boost/1.83.0"#"eigen/3.3.9", "boost/1.83.0"
+    requires = "freetype/2.10.4", "boost/1.83.0"# "eigen/3.3.9"# "boost/1.83.0"
 
     # Sources are located in the same place as this recipe, copy them to the recipe
     exports_sources = "CMakeLists.txt", "*"
@@ -50,9 +50,70 @@ class RDKitRecipe(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["RDKit"]
+        self.cpp_info.sourcedirs = ["Code"]
+        self.cpp_info.components["RDKitAbbreviations"].libs = ["RDKitAbbreviations"]
+        self.cpp_info.components["RDKitAlignment"].libs = ["RDKitAlignment"]
+        self.cpp_info.components["RDKitCatalogs"].libs = ["RDKitCatalogs"]
+        self.cpp_info.components["RDKitChemicalFeatures"].libs = ["RDKitChemicalFeatures"]
+        self.cpp_info.components["RDKitChemReactions"].libs = ["RDKitChemReactions"]
+        self.cpp_info.components["RDKitChemTransforms"].libs = ["RDKitChemTransforms"]
+        self.cpp_info.components["RDKitCIPLabeler"].libs = ["RDKitCIPLabeler"]
+        self.cpp_info.components["RDKitcoordgen"].libs = ["RDKitcoordgen"]
+        self.cpp_info.components["RDKitDataStructs"].libs = ["RDKitDataStructs"]
+        self.cpp_info.components["RDKitDepictor"].libs = ["RDKitDepictor"]
+        self.cpp_info.components["RDKitDeprotect"].libs = ["RDKitDeprotect"]
+        self.cpp_info.components["RDKitDescriptors"].libs = ["RDKitDescriptors"]
+        self.cpp_info.components["RDKitDistGeometry"].libs = ["RDKitDistGeometry"]
+        self.cpp_info.components["RDKitDistGeomHelpers"].libs = ["RDKitDistGeomHelpers"]
+        self.cpp_info.components["RDKitEigenSolvers"].libs = ["RDKitEigenSolvers"]
+        self.cpp_info.components["RDKitFileParsers"].libs = ["RDKitFileParsers"]
+        self.cpp_info.components["RDKitFilterCatalog"].libs = ["RDKitFilterCatalog"]
+        self.cpp_info.components["RDKitFingerprints"].libs = ["RDKitFingerprints"]
+        self.cpp_info.components["RDKitFMCS"].libs = ["RDKitFMCS"]
+        self.cpp_info.components["RDKitForceFieldHelpers"].libs = ["RDKitForceFieldHelpers"]
+        self.cpp_info.components["RDKitForceField"].libs = ["RDKitForceField"]
+        self.cpp_info.components["RDKitFragCatalog"].libs = ["RDKitFragCatalog"]
+        self.cpp_info.components["RDKitga"].libs = ["RDKitga"]
+        self.cpp_info.components["RDKitGeneralizedSubstruct"].libs = ["RDKitGeneralizedSubstruct"]
+        self.cpp_info.components["RDKitGenericGroups"].libs = ["RDKitGenericGroups"]
+        self.cpp_info.components["RDKitGraphMol"].libs = ["RDKitGraphMol"]
+        self.cpp_info.components["RDKithc"].libs = ["RDKithc"]
+        self.cpp_info.components["RDKitInfoTheory"].libs = ["RDKitInfoTheory"]
+        self.cpp_info.components["RDKitmaeparser"].libs = ["RDKitmaeparser"]
+        self.cpp_info.components["RDKitMarvinParser"].libs = ["RDKitMarvinParser"]
+        self.cpp_info.components["RDKitMMPA"].libs = ["RDKitMMPA"]
+        self.cpp_info.components["RDKitMolAlign"].libs = ["RDKitMolAlign"]
+        self.cpp_info.components["RDKitMolCatalog"].libs = ["RDKitMolCatalog"]
+        self.cpp_info.components["RDKitMolChemicalFeatures"].libs = ["RDKitMolChemicalFeatures"]
+        self.cpp_info.components["RDKitMolDraw2D"].libs = ["RDKitMolDraw2D"]
+        self.cpp_info.components["RDKitMolEnumerator"].libs = ["RDKitMolEnumerator"]
+        self.cpp_info.components["RDKitMolHash"].libs = ["RDKitMolHash"]
+        self.cpp_info.components["RDKitMolInterchange"].libs = ["RDKitMolInterchange"]
+        self.cpp_info.components["RDKitMolStandardize"].libs = ["RDKitMolStandardize"]
+        self.cpp_info.components["RDKitMolTransforms"].libs = ["RDKitMolTransforms"]
+        self.cpp_info.components["RDKitO3AAlign"].libs = ["RDKitO3AAlign"]
+        self.cpp_info.components["RDKitOptimizer"].libs = ["RDKitOptimizer"]
+        self.cpp_info.components["RDKitPartialCharges"].libs = ["RDKitPartialCharges"]
+        self.cpp_info.components["RDKitRascalMCES"].libs = ["RDKitRascalMCES"]
+        self.cpp_info.components["RDKitRDGeneral"].libs = ["RDKitRDGeneral"]
+        self.cpp_info.components["RDKitRDGeometryLib"].libs = ["RDKitRDGeometryLib"]
+        self.cpp_info.components["RDKitRDStreams"].libs = ["RDKitRDStreams"]
+        self.cpp_info.components["RDKitReducedGraphs"].libs = ["RDKitReducedGraphs"]
+        self.cpp_info.components["RDKitRGroupDecomposition"].libs = ["RDKitRGroupDecomposition"]
+        self.cpp_info.components["RDKitRingDecomposerLib"].libs = ["RDKitRingDecomposerLib"]
+        self.cpp_info.components["RDKitScaffoldNetwork"].libs = ["RDKitScaffoldNetwork"]
+        self.cpp_info.components["RDKitShapeHelpers"].libs = ["RDKitShapeHelpers"]
+        self.cpp_info.components["RDKitSimDivPickers"].libs = ["RDKitSimDivPickers"]
+        self.cpp_info.components["RDKitSLNParse"].libs = ["RDKitSLNParse"]
+        self.cpp_info.components["RDKitSmilesParse"].libs = ["RDKitSmilesParse"]
+        self.cpp_info.components["RDKitSubgraphs"].libs = ["RDKitSubgraphs"]
+        self.cpp_info.components["RDKitSubstructLibrary"].libs = ["RDKitSubstructLibrary"]
+        self.cpp_info.components["RDKitSubstructMatch"].libs = ["RDKitSubstructMatch"]
+        self.cpp_info.components["RDKitTautomerQuery"].libs = ["RDKitTautomerQuery"]
+        self.cpp_info.components["RDKitTrajectory"].libs = ["RDKitTrajectory"]
 
-    
 
-    
+
+
+
 
