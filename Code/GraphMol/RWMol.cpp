6c1373be142d314395d9ad97cb974bf69454e1d0
Brian Kelley <fustigator@gmail.com>
2023-11-12 16:45:15 -0500

    Removing stereoatoms requires removing CIS/Trans stereo flags

--- Code/GraphMol/RWMol.cpp
+++ Code/GraphMol/RWMol.cpp
@@ -468,6 +468,12 @@ void RWMol::removeBond(unsigned int aid1, unsigned int aid2) {
     }
     if (std::find(obnd->getStereoAtoms().begin(), obnd->getStereoAtoms().end(),
                   aid2) != obnd->getStereoAtoms().end()) {
+        // github #6900 if we remove stereo atoms we need to remove
+        //  the CIS and or TRANS since this requires stereo atoms
+        if (obnd->getStereo() == Bond::BondStereo::STEREOCIS ||
+            obnd->getStereo() == Bond::BondStereo::STEREOTRANS ) {
+          obnd->setStereo(Bond::BondStereo::STEREONONE);
+      }
       obnd->getStereoAtoms().clear();
     }
   }
