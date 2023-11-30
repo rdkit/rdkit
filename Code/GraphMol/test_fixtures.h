ef8d1cb47c87bd240c4685761f0b6e9f73bcf2ae
Ric R <ricrogz@gmail.com>
2023-11-30 10:15:17 -0500

    add copyright note

--- Code/GraphMol/test_fixtures.h
+++ Code/GraphMol/test_fixtures.h
@@ -1,3 +1,13 @@
+//
+//  Copyright (C) 2023 Greg Landrum and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+
 #pragma once
 
 #include <cstdlib>
