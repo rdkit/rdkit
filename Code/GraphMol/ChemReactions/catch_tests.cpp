e1d1bcf0c1590fc69d9454e00d32a191f1c90d42
Greg Landrum <greg.landrum@gmail.com>
2024-01-25 20:59:58 +0100

    Fixes #7028 (#7091)

--- Code/GraphMol/ChemReactions/catch_tests.cpp
+++ Code/GraphMol/ChemReactions/catch_tests.cpp
@@ -1656,3 +1656,61 @@ M  END
 
 }
 }
+
+TEST_CASE("Github #6492: In place transforms incorrectly change atomic numbers") {
+  SECTION("minimal"){
+    auto rxn = "[O-:1]>>[*-0:1]"_rxnsmarts;
+    REQUIRE(rxn);
+    rxn->initReactantMatchers();
+    auto m = "[O-]C"_smiles;
+    REQUIRE(m);
+    rxn->runReactant(*m);
+    CHECK(MolToSmiles(*m)=="CO");
+  }
+  SECTION("as reported"){
+    auto rxn = "[n;+0!H0:1]:[a:2]:[a:3]:[c:4]=[N!$(*[O-]),O;+1H0:5]>>[n+1:1]:[*:2]:[*:3]:[*:4]-[*+0:5]"_rxnsmarts;
+    REQUIRE(rxn);
+    rxn->initReactantMatchers();
+    auto m = "[nH]1ccc(=[N+](C)C)cc1"_smiles;
+    REQUIRE(m);
+    rxn->runReactant(*m);
+    CHECK(MolToSmiles(*m)=="CN(C)c1cc[nH+]cc1");
+  }
+}
+
+
+TEST_CASE("Github #7028: Spacing bug in compute2DCoordsForReaction") {
+  SECTION("minimal"){
+    auto rxn = "C1CCCCC1.C1CCCCC1.C1CCCCC1>>C1CCCCC1.C1CCCCC1"_rxnsmiles;
+    REQUIRE(rxn);
+    RDDepict::compute2DCoordsForReaction(*rxn);
+    std::vector<std::pair<double,double>> xbounds;
+    for(const auto &reactant : rxn->getReactants()){
+      REQUIRE(reactant->getNumConformers()==1);
+      std::pair<double,double> bounds={1e8,-1e8};
+      auto conf = reactant->getConformer();
+      for(unsigned int i=0;i<conf.getNumAtoms();++i){
+        auto pos = conf.getAtomPos(i);
+        bounds.first = std::min(bounds.first,pos.x);
+        bounds.second = std::max(bounds.second,pos.x);
+      }
+      xbounds.push_back(bounds);
+    }
+    for(const auto &product : rxn->getProducts()){
+      REQUIRE(product->getNumConformers()==1);
+      std::pair<double,double> bounds={1e8,-1e8};
+      auto conf = product->getConformer();
+      for(unsigned int i=0;i<conf.getNumAtoms();++i){
+        auto pos = conf.getAtomPos(i);
+        bounds.first = std::min(bounds.first,pos.x);
+        bounds.second = std::max(bounds.second,pos.x);
+      }
+      xbounds.push_back(bounds);
+    }
+    REQUIRE(xbounds.size()==5);
+    CHECK(xbounds[0].second < xbounds[1].first);
+    CHECK(xbounds[1].second < xbounds[2].first);
+    CHECK(xbounds[2].second < xbounds[3].first);
+    CHECK(xbounds[3].second < xbounds[4].first);
+  }   
+}
