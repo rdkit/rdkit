b5f060789fd852265b7a88f5eb4fd8863d964aa4
ptosco <paolo.tosco@novartis.com>
2024-03-17 11:39:30 +0100

    suggested changes

--- Code/MinimalLib/jswrapper.cpp
+++ Code/MinimalLib/jswrapper.cpp
@@ -634,7 +634,7 @@ EMSCRIPTEN_BINDINGS(RDKit_minimal) {
 #ifdef RDK_BUILD_MINIMAL_LIB_RXN
   class_<JSReaction>("Reaction")
 #ifdef __EMSCRIPTEN__
-      .function("run_reactants", select_overload<std::vector<JSMolList>(
+      .function("run_reactants", select_overload<std::vector<JSMolList *>(
                                      const JSMolList &, unsigned int) const>(
                                      &JSReaction::run_reactants))
       .function("draw_to_canvas_with_offset", &draw_rxn_to_canvas_with_offset)
