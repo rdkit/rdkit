0f3692f4f0d588308ab8907f3e13670a303afa51
Ricardo Rodriguez <ricrogz@users.noreply.github.com>
2024-04-03 15:34:37 +0200

    Fixes #7318 (#7319)
    * fix hybridization for atoms with outgoing dative bonds
    
    * expose and wrap C++ numPiElectrons
    
    * deprecate AtomPairs.Utils.NumPiElectrons
    
    * add & update tests
    
    * fix draw2d test
    
    * update expected hash
    
    * add hybridization test
    
    * move numPiElectrons to Atom.h
    
    * take reference instead of ptr

--- Code/GraphMol/Descriptors/Wrap/rdMolDescriptors.cpp
+++ Code/GraphMol/Descriptors/Wrap/rdMolDescriptors.cpp
@@ -1021,7 +1021,6 @@ BOOST_PYTHON_MODULE(rdMolDescriptors) {
        python::arg("includeChirality") = false),
       docString.c_str(),
       python::return_value_policy<python::manage_new_object>());
-
   docString = "Returns a Morgan fingerprint for a molecule";
   python::def(
       "GetMorganFingerprint", GetMorganFingerprint,
