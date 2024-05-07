55a262bdd60caaf819e8d45f28fe9d86a606d03f
tadhurst-cdd <112502803+tadhurst-cdd@users.noreply.github.com>
2024-05-07 10:06:33 -0500

    No coords atropisomers - fix smiles output of atrop wedges after reordering (#7418)
    * removed string_view in favor of string for catch test
    
    * add parsing and generation of atropisomers when coords not present
    
    * changed string_view to string in catch test
    
    * more docs
    
    * reformulation of the docs
    
    * make an error message a little bit more useful
    
    * small optimization
    clang-format
    
    * add `BondWedgingParameters` to new function
    
    * changes for CIP test errors
    
    * Updated internal doc to match what it does
    
    * changes per PR review
    
    * removed cout statements in tests
    
    ---------
    
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>

--- Code/GraphMol/SmilesParse/cxsmiles_test.cpp
+++ Code/GraphMol/SmilesParse/cxsmiles_test.cpp
@@ -790,7 +790,8 @@ void testOneAtropisomers(const SmilesTest *smilesTest) {
           MolToCXSmiles(*smilesMol, ps, flags, RestoreBondDirOptionClear);
 
       generateNewExpectedFilesIfSoSpecified(fName + ".NEW3.cxsmi", smilesOut);
-      CHECK(getExpectedValue(expectedFileName) == smilesOut);
+      auto expected = getExpectedValue(expectedFileName);
+      CHECK(expected == smilesOut);
     }
 
     smilesMol =
@@ -826,7 +827,7 @@ void testOneAtropisomers(const SmilesTest *smilesTest) {
       } catch (const RDKit::KekulizeException &e) {
         outMolStr = "";
       } catch (...) {
-        throw;  // re-trhow the error if not a kekule error
+        throw;  // re-throw the error if not a kekule error
       }
       if (outMolStr == "") {
         RDKit::Chirality::reapplyMolBlockWedging(*smilesMol);
@@ -1023,6 +1024,7 @@ void testOne3dChiral(const SmilesTest *smilesTest) {
 TEST_CASE("testAtropisomersInCXSmiles") {
   {
     std::list<SmilesTest> smiTests{
+        SmilesTest("ShortAtropisomerNoCoords.cxsmi", true, 14, 15),
         SmilesTest("ShortAtropisomer.cxsmi", true, 14, 15),
         SmilesTest("ShortAtropisomerArom.cxsmi", true, 14, 15),
         SmilesTest("AtropManyChirals.cxsmi", true, 20, 20),
@@ -1344,15 +1346,11 @@ TEST_CASE("SMILES CANONICALIZATION") {
       }
       molCount++;
 
-      try {
-        if (molBlock.length() > 25) {
-          std::unique_ptr<RWMol> mol(MolBlockToMol(molBlock));
-          std::string smiles = MolToCXSmiles(*mol);
+      if (molBlock.length() > 25) {
+        std::unique_ptr<RWMol> mol(MolBlockToMol(molBlock));
+        std::string smiles = MolToCXSmiles(*mol);
 
-          testSmilesCanonicalization(smiles);
-        }
-      } catch (...) {
-        std::cout << "failed on mol " << molCount << std::endl;
+        testSmilesCanonicalization(smiles);
       }
     }
   }
