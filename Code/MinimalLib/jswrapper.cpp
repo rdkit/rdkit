c7fd2bbcff214c1003b51ebb3f3c0dd0bce0fe33
Syed Zayyan Masud <44922710+syedzayyan@users.noreply.github.com>
2024-05-09 18:30:49 +0100

    Update MinimalLib for Function Exposure: runReactants (#7210)

--- Code/MinimalLib/jswrapper.cpp
+++ Code/MinimalLib/jswrapper.cpp
@@ -399,6 +399,7 @@ emscripten::val get_mmpa_frags_helper(const JSMol &self, unsigned int minCuts,
   return obj;
 }
 #endif
+
 }  // namespace
 
 using namespace emscripten;
@@ -632,6 +633,9 @@ EMSCRIPTEN_BINDINGS(RDKit_minimal) {
 #ifdef RDK_BUILD_MINIMAL_LIB_RXN
   class_<JSReaction>("Reaction")
 #ifdef __EMSCRIPTEN__
+      .function("run_reactants", select_overload<std::vector<JSMolList *>(
+                                     const JSMolList &, unsigned int) const>(
+                                     &JSReaction::run_reactants))
       .function("draw_to_canvas_with_offset", &draw_rxn_to_canvas_with_offset)
       .function("draw_to_canvas", &draw_rxn_to_canvas)
       .function("draw_to_canvas_with_highlights",
