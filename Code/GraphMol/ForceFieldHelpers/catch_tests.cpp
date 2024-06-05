2a260d61db1dd0b4bcd63277f64496e29100c15d
Niels Maeder <maedern@student.ethz.ch>
2024-06-05 16:37:07 +0200

    deallocate memory

--- Code/GraphMol/ForceFieldHelpers/catch_tests.cpp
+++ Code/GraphMol/ForceFieldHelpers/catch_tests.cpp
@@ -33,15 +33,16 @@ TEST_CASE("Test empty force field") {
     TEST_ASSERT(forceField->calcEnergy() == 0.0);
     TEST_ASSERT(forceField->numPoints() == mol->getNumAtoms());
     TEST_ASSERT(forceField->positions().size() == mol->getNumAtoms());
-    TEST_ASSERT(0 == 1);
-    TEST_ASSERT(forceField->distance(0, 1) < 2.0);
+    auto dist = forceField->distance(0, 1);
+    TEST_ASSERT(1.2 < dist && dist < 1.6);
+    delete forceField;
   }
   SECTION("add contrib and minimize") {
     auto forceField = ForceFieldsHelper::constructEmptyForceField(*mol);
     forceField->initialize();
-    auto *params = new ForceFields::MMFF::MMFFBond{6.0, 100.0};
+    auto params = ForceFields::MMFF::MMFFBond{6.0, 100.0};
     auto *contrib =
-        new ForceFields::MMFF::BondStretchContrib(forceField, 0, 1, params);
+        new ForceFields::MMFF::BondStretchContrib(forceField, 0, 1, &params);
     forceField->contribs().push_back(ForceFields::ContribPtr(contrib));
     TEST_ASSERT(forceField->minimize() == 0);
     TEST_ASSERT(std::round(forceField->distance(0, 1)) == 100);
@@ -49,9 +50,7 @@ TEST_CASE("Test empty force field") {
     auto pos2 = mol->getConformer().getAtomPos(1);
     auto dist = sqrt(pow(pos1.x - pos2.x, 2) + pow(pos1.y - pos2.y, 2) +
                      pow(pos1.z - pos2.z, 2));
-    std::cout << dist << std::endl;
-    std::cout << pos1.directionVector(pos2) << std::endl;
-    REQUIRE(false);
-    TEST_ASSERT(std::round(dist) == 100);
+    TEST_ASSERT(99.0 < dist && dist < 101.0);
+    delete forceField;
   }
 }
