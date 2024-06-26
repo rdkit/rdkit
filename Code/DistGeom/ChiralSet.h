df0224fb7d05df5109cc0f7d762e2c4ee66f64d7
Greg Landrum <greg.landrum@gmail.com>
2024-06-26 04:42:33 +0200

    Fixes #7552 (#7564)
    * Fixes #7552
    
    * Change in response to review
    
    * suppress a warning

--- Code/DistGeom/ChiralSet.h
+++ Code/DistGeom/ChiralSet.h
@@ -17,6 +17,11 @@
 
 namespace DistGeom {
 
+enum class ChiralSetStructureFlags : std::uint64_t {
+  IN_FUSED_SMALL_RINGS =
+      1 << 0,  // a chiral center involved in fusing 2 or more small rings
+};
+
 /*! \brief Class used to store a quartet of points and chiral volume bounds on
  *them
  *
@@ -30,17 +35,20 @@ class RDKIT_DISTGEOMETRY_EXPORT ChiralSet {
   unsigned int d_idx4;
   double d_volumeLowerBound;
   double d_volumeUpperBound;
+  std::uint64_t d_structureFlags =
+      0;  // used to track special cases that may make embedding more difficult
 
   ChiralSet(unsigned int pid0, unsigned int pid1, unsigned int pid2,
             unsigned int pid3, unsigned int pid4, double lowerVolBound,
-            double upperVolBound)
+            double upperVolBound, std::uint64_t structureFlags = 0)
       : d_idx0(pid0),
         d_idx1(pid1),
         d_idx2(pid2),
         d_idx3(pid3),
         d_idx4(pid4),
         d_volumeLowerBound(lowerVolBound),
-        d_volumeUpperBound(upperVolBound) {
+        d_volumeUpperBound(upperVolBound),
+        d_structureFlags(structureFlags) {
     CHECK_INVARIANT(lowerVolBound <= upperVolBound, "Inconsistent bounds\n");
     d_volumeLowerBound = lowerVolBound;
     d_volumeUpperBound = upperVolBound;
