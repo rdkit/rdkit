b8c84531dc35db8196503e1081631276f1d9738c
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2024-08-21 01:46:36 +0900

    refactor: use [[maybe_unused]] attribute

--- Code/GraphMol/MolStandardize/Validate.cpp
+++ Code/GraphMol/MolStandardize/Validate.cpp
@@ -84,10 +84,12 @@ std::vector<ValidationErrorInfo> NoAtomValidation::validate(
 }
 
 std::vector<ValidationErrorInfo> FragmentValidation::validate(
-    const ROMol &mol, bool reportAllFailures) const {
+    const ROMol &mol,
+    [[maybe_unused]] bool
+        reportAllFailures  // REVIEW: reportAllFailures is not being used here.
+                           // is that correct?
+) const {
   std::vector<ValidationErrorInfo> errors;
-  // REVIEW: reportAllFailures is not being used here. is that correct?
-  RDUNUSED_PARAM(reportAllFailures);
   std::shared_ptr<FragmentCatalogParams> fparams(new FragmentCatalogParams(""));
   FragmentCatalog fcat(fparams.get());
 
