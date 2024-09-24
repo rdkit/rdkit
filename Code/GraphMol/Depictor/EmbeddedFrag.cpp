555a5f00509f194be534d6a0ee5408f9949c62d4
Greg Landrum <greg.landrum@gmail.com>
2024-09-24 08:05:15 +0200

    fix an out-of-bounds write

--- Code/GraphMol/Depictor/EmbeddedFrag.cpp
+++ Code/GraphMol/Depictor/EmbeddedFrag.cpp
@@ -454,7 +454,7 @@ bool EmbeddedFrag::matchToTemplate(const RDKit::INT_VECT &ringSystemAtoms,
     }
     // also check if the mol atoms have the same connectivity as the template
     auto degreeCounts = [DUMMY_ATOMIC_NUM](const RDKit::ROMol &mol) {
-      std::array<int, 4> degrees_count({0, 0, 0, 0});
+      std::array<int, 5> degrees_count({0, 0, 0, 0, 0});
       for (auto atom : mol.atoms()) {
         if (atom->getAtomicNum() == DUMMY_ATOMIC_NUM) {
           continue;
