f2736d075109b18551a2ecd8afefc28142427631
ptosco <paolo.tosco@novartis.com>
2024-10-24 11:26:27 +0200

    added missing copyright notices

--- Code/RDGeneral/BetterEnums.h
+++ Code/RDGeneral/BetterEnums.h
@@ -1,3 +1,13 @@
+//
+//  Copyright (C) 2024 Novartis Biomedical Research and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+
 #pragma once
 
 #ifdef USE_BETTER_ENUMS
