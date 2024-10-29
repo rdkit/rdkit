rdkit/rdkit

commit 3f7caf0147e3741931211a50f169f24f07f6188c

Extend RDKit::MolStandardize with a validation and standardization Pipeline (#7582)
* Extend RDKit::MolStandardize with a validation and standardization Pipeline

* suggested changes

* apply clang-format

* apply yapf

* MolStandardize::FeaturesValidation optionally disallow dative bonds

* add allowDativeBondType to MolStandardize::PipelineOptions

* apply clang-format

* make the API of other validation classes more consistent with MolStandardize::FeaturesValidation

* apply clang-format

* PipelineStage to enum class
remove virtual functions from Pipeline class
be explicit about enums

* light refactoring to avoid what I think is an unnecessary call to `parse`

* a bit of modernization

* make the pipeline configurable

* make parse and serialize configurable too

* switch to storing pipeline stages using uints

* add a simple test for providing a pipeline

* update pointer alignment for clang-format

* test modifying the parser and serializer

* update swig requirement

* changes in response to review

* changes in response to review

* rename PipelineResult's *MolBlock members to *MolData

* upgrade swig to 4.2 in the CI environments

* add a few missing export directives

---------

Co-authored-by: greg landrum <greg.landrum@gmail.com>

Code/JavaWrappers/MolStandardize.i
@@ -1,5 +1,27 @@
 %{
 #include <GraphMol/MolStandardize/MolStandardize.h>
+#include <GraphMol/MolStandardize/Pipeline.h>
+
+namespace RDKit {
+namespace MolStandardize {
+    bool operator==(const PipelineLogEntry & rhs, const PipelineLogEntry & lhs) {
+        return (rhs.status == lhs.status) && (rhs.detail == lhs.detail);
+    }
+    bool operator!=(const PipelineLogEntry & rhs, const PipelineLogEntry & lhs) {
+        return !(rhs == lhs);
+    }
+}
+}
 %}
 
+%include <std_vector.i>
+namespace std {
+%template(PipelineLog) std::vector<RDKit::MolStandardize::PipelineLogEntry>;
+}
+
 %include <GraphMol/MolStandardize/MolStandardize.h>
+
+#if defined SWIGJAVA
+%include "enumtypeunsafe.swg"
+#endif
+%include <GraphMol/MolStandardize/Pipeline.h>

Code/JavaWrappers/gmwrapper/src-test/org/RDKit/MolStandardizeTest.java
@@ -14,6 +14,53 @@ import org.junit.*;
 	public void testStandardize1() {
 		assertEquals("fail", RDKFuncs.standardizeSmiles("[Na]OC(=O)c1ccccc1"),"O=C([O-])c1ccccc1.[Na+]");
 	}
+	@Test
+	public void testPipelineBadInput() {
+		Pipeline pipeline = new Pipeline();
+
+		PipelineResult result = pipeline.run(
+			"\n" +
+            "             sldfj;ldskfj sldkjfsd;lkf\n" +
+            "M  V30 BEGIN CTAB"
+		);
+
+	    assertEquals(result.getStage(), PipelineStage.PARSING_INPUT);
+        assertFalse(result.getStatus() == PipelineStatus.NO_EVENT);
+        assertTrue((result.getStatus() & PipelineStatus.INPUT_ERROR) != PipelineStatus.NO_EVENT);
+
+		result.delete();
+		pipeline.delete();
+	}
+	@Test
+	public void testPipelineUnsupportedFeatures() {
+		Pipeline pipeline = new Pipeline();
+
+		PipelineResult result = pipeline.run(
+			"\n" +
+            "  Mrv2311 01162413552D          \n" +
+            "\n" +
+            "  0  0  0     0  0            999 V3000\n" +
+            "M  V30 BEGIN CTAB\n" +
+            "M  V30 COUNTS 2 1 0 0 0\n" +
+            "M  V30 BEGIN ATOM\n" +
+            "M  V30 1 R# -17.3747 6.9367 0 0 RGROUPS=(1 0)\n" +
+            "M  V30 2 C -18.7083 6.1667 0 0\n" +
+            "M  V30 END ATOM\n" +
+            "M  V30 BEGIN BOND\n" +
+            "M  V30 1 1 2 1\n" +
+            "M  V30 END BOND\n" +
+            "M  V30 END CTAB\n" +
+            "M  END"
+		);
+
+	    assertEquals(result.getStage(), PipelineStage.COMPLETED);
+        assertFalse(result.getStatus() == PipelineStatus.NO_EVENT);
+        assertTrue((result.getStatus() & PipelineStatus.VALIDATION_ERROR) != PipelineStatus.NO_EVENT);
+        assertTrue((result.getStatus() & PipelineStatus.FEATURES_VALIDATION_ERROR) != PipelineStatus.NO_EVENT);
+
+		result.delete();
+		pipeline.delete();
+	}
 	public static void main(String args[]) {
 						org.junit.runner.JUnitCore.main("org.RDKit.MolStandardizeTest");
 	}

.azure-pipelines/linux_build_java.yml
@@ -9,7 +9,7 @@ steps:
     conda create --name rdkit_build -c conda-forge cmake \
         libboost=$(boost_version) \
         libboost-devel=$(boost_version) \
-        swig=4.1
+        swig=4.2
   displayName: Setup build environment
 - bash: |
     source ${CONDA}/etc/profile.d/conda.sh

.azure-pipelines/mac_build_java.yml
@@ -21,7 +21,7 @@ steps:
         libcxx=$(compiler_version) cmake=3.26 \
         libboost=$(boost_version) \
         libboost-devel=$(boost_version) \
-        cairo eigen swig=4.1
+        cairo eigen swig=4.2
     conda activate rdkit_build
   displayName: Setup build environment
 - bash: |

.azure-pipelines/vs_build_swig.yml
@@ -15,7 +15,7 @@ steps:
         cmake=3.26 ^
         libboost=$(boost_version) ^
         libboost-devel=$(boost_version) ^
-        cairo eigen swig=4.1
+        cairo eigen swig=4.2
     call activate rdkit_build
   displayName: Install dependencies
 - script: |

.clang-format
@@ -23,7 +23,7 @@ BinPackArguments: true
 ColumnLimit:     80
 ConstructorInitializerAllOnOneLineOrOnePerLine: true
 ConstructorInitializerIndentWidth: 4
-DerivePointerAlignment: true
+DerivePointerAlignment: false
 ExperimentalAutoDetectBinPacking: false
 IndentCaseLabels: true
 IndentWrappedFunctionNames: false
@@ -40,7 +40,7 @@ PenaltyBreakString: 1000
 PenaltyBreakFirstLessLess: 120
 PenaltyExcessCharacter: 1000000
 PenaltyReturnTypeOnItsOwnLine: 200
-PointerAlignment: Left
+PointerAlignment: Right
 SpacesBeforeTrailingComments: 2
 Cpp11BracedListStyle: true
 Standard:        Cpp11

Code/GraphMol/MolStandardize/CMakeLists.txt
@@ -1,55 +1,61 @@
-
 rdkit_library(MolStandardize
-              MolStandardize.cpp
-	      Metal.cpp
-	      Normalize.cpp
-	      Validate.cpp
-	      Charge.cpp
-	      Tautomer.cpp
-	      Fragment.cpp
-        FragmentCatalog/FragmentCatalogEntry.cpp
-        FragmentCatalog/FragmentCatalogParams.cpp
-        FragmentCatalog/FragmentCatalogUtils.cpp
-        AcidBaseCatalog/AcidBaseCatalogEntry.cpp
-        AcidBaseCatalog/AcidBaseCatalogParams.cpp
-        AcidBaseCatalog/AcidBaseCatalogUtils.cpp
-        TransformCatalog/TransformCatalogEntry.cpp
-        TransformCatalog/TransformCatalogParams.cpp
-        TransformCatalog/TransformCatalogUtils.cpp
-        TautomerCatalog/TautomerCatalogEntry.cpp
-        TautomerCatalog/TautomerCatalogParams.cpp
-        TautomerCatalog/TautomerCatalogUtils.cpp
-	      LINK_LIBRARIES ChemReactions ChemTransforms SmilesParse SubstructMatch Descriptors GraphMol )
+    Pipeline.cpp
+    MolStandardize.cpp
+    Metal.cpp
+    Normalize.cpp
+    Validate.cpp
+    Charge.cpp
+    Tautomer.cpp
+    Fragment.cpp
+    FragmentCatalog/FragmentCatalogEntry.cpp
+    FragmentCatalog/FragmentCatalogParams.cpp
+    FragmentCatalog/FragmentCatalogUtils.cpp
+    AcidBaseCatalog/AcidBaseCatalogEntry.cpp
+    AcidBaseCatalog/AcidBaseCatalogParams.cpp
+    AcidBaseCatalog/AcidBaseCatalogUtils.cpp
+    TransformCatalog/TransformCatalogEntry.cpp
+    TransformCatalog/TransformCatalogParams.cpp
+    TransformCatalog/TransformCatalogUtils.cpp
+    TautomerCatalog/TautomerCatalogEntry.cpp
+    TautomerCatalog/TautomerCatalogParams.cpp
+    TautomerCatalog/TautomerCatalogUtils.cpp
+        LINK_LIBRARIES ChemReactions ChemTransforms SmilesParse SubstructMatch Descriptors GraphMol )
 target_compile_definitions(MolStandardize PRIVATE RDKIT_MOLSTANDARDIZE_BUILD)
 
-rdkit_headers(MolStandardize.h
-	      Metal.h
-	      Normalize.h
-	      Validate.h
-	      Charge.h
-	      Tautomer.h
-	      Fragment.h
-	      DEST GraphMol/MolStandardize)
+rdkit_headers(
+    Pipeline.h
+    MolStandardize.h
+    Metal.h
+    Normalize.h
+    Validate.h
+    Charge.h
+    Tautomer.h
+    Fragment.h
+        DEST GraphMol/MolStandardize)
 
-rdkit_headers(FragmentCatalog/FragmentCatalogEntry.h
-        FragmentCatalog/FragmentCatalogParams.h
-        FragmentCatalog/FragmentCatalogUtils.h
-	    DEST GraphMol/MolStandardize/FragmentCatalog)
+rdkit_headers(
+    FragmentCatalog/FragmentCatalogEntry.h
+    FragmentCatalog/FragmentCatalogParams.h
+    FragmentCatalog/FragmentCatalogUtils.h
+        DEST GraphMol/MolStandardize/FragmentCatalog)
 
-rdkit_headers(AcidBaseCatalog/AcidBaseCatalogEntry.h
-        AcidBaseCatalog/AcidBaseCatalogParams.h
-        AcidBaseCatalog/AcidBaseCatalogUtils.h
-	    DEST GraphMol/MolStandardize/AcidBaseCatalog)
+rdkit_headers(
+    AcidBaseCatalog/AcidBaseCatalogEntry.h
+    AcidBaseCatalog/AcidBaseCatalogParams.h
+    AcidBaseCatalog/AcidBaseCatalogUtils.h
+        DEST GraphMol/MolStandardize/AcidBaseCatalog)
 
-rdkit_headers(TransformCatalog/TransformCatalogEntry.h
-        TransformCatalog/TransformCatalogParams.h
-        TransformCatalog/TransformCatalogUtils.h
-	    DEST GraphMol/MolStandardize/TransformCatalog)
+rdkit_headers(
+    TransformCatalog/TransformCatalogEntry.h
+    TransformCatalog/TransformCatalogParams.h
+    TransformCatalog/TransformCatalogUtils.h
+        DEST GraphMol/MolStandardize/TransformCatalog)
 
-rdkit_headers(TautomerCatalog/TautomerCatalogEntry.h
-        TautomerCatalog/TautomerCatalogParams.h
-        TautomerCatalog/TautomerCatalogUtils.h
-	    DEST GraphMol/MolStandardize/TautomerCatalog)
+rdkit_headers(
+    TautomerCatalog/TautomerCatalogEntry.h
+    TautomerCatalog/TautomerCatalogParams.h
+    TautomerCatalog/TautomerCatalogUtils.h
+        DEST GraphMol/MolStandardize/TautomerCatalog)
 
 if(RDK_BUILD_PYTHON_WRAPPERS)
 add_subdirectory(Wrap)
@@ -63,5 +69,6 @@ rdkit_test(molTautomerTest testTautomer.cpp LINK_LIBRARIES MolStandardize )
 rdkit_test(molStandardizeSmallTest test2.cpp LINK_LIBRARIES MolStandardize )
 rdkit_test(molFragmentTest testFragment.cpp LINK_LIBRARIES MolStandardize )
 rdkit_catch_test(molStandardizeCatchTest catch_tests.cpp LINK_LIBRARIES MolStandardize )
+rdkit_catch_test(molStandardizePipelineTest testPipeline.cpp LINK_LIBRARIES MolStandardize)
 
 

Code/GraphMol/MolStandardize/Charge.cpp
@@ -538,7 +538,7 @@ void Uncharger::unchargeInPlace(RWMol &mol) {
       }
     }
   }
-}  // namespace MolStandardize
+}
 
 }  // namespace MolStandardize
 }  // namespace RDKit

commit 725778d07d07e05898dca99037cff52c362a41a7

fixes a regression introduced in #7582 which made all SWIG enums become type-unsafe

Code/JavaWrappers/MolStandardize.i
@@ -21,7 +21,5 @@ namespace std {
 
 %include <GraphMol/MolStandardize/MolStandardize.h>
 
-#if defined SWIGJAVA
-%include "enumtypeunsafe.swg"
-#endif
+%include "enums.swg"
 %include <GraphMol/MolStandardize/Pipeline.h>

Code/JavaWrappers/gmwrapper/src-test/org/RDKit/MolStandardizeTest.java
@@ -26,7 +26,7 @@ import org.junit.*;
 
 	    assertEquals(result.getStage(), PipelineStage.PARSING_INPUT);
         assertFalse(result.getStatus() == PipelineStatus.NO_EVENT);
-        assertTrue((result.getStatus() & PipelineStatus.INPUT_ERROR) != PipelineStatus.NO_EVENT);
+        assertTrue(PipelineStatus.swigToEnum(result.getStatus().swigValue() & PipelineStatus.INPUT_ERROR.swigValue()) != PipelineStatus.NO_EVENT);
 
 		result.delete();
 		pipeline.delete();
@@ -55,8 +55,8 @@ import org.junit.*;
 
 	    assertEquals(result.getStage(), PipelineStage.COMPLETED);
         assertFalse(result.getStatus() == PipelineStatus.NO_EVENT);
-        assertTrue((result.getStatus() & PipelineStatus.VALIDATION_ERROR) != PipelineStatus.NO_EVENT);
-        assertTrue((result.getStatus() & PipelineStatus.FEATURES_VALIDATION_ERROR) != PipelineStatus.NO_EVENT);
+        assertTrue(PipelineStatus.swigToEnum(result.getStatus().swigValue() & PipelineStatus.VALIDATION_ERROR.swigValue()) != PipelineStatus.NO_EVENT);
+        assertTrue(PipelineStatus.swigToEnum(result.getStatus().swigValue() & PipelineStatus.FEATURES_VALIDATION_ERROR.swigValue()) != PipelineStatus.NO_EVENT);
 
 		result.delete();
 		pipeline.delete();