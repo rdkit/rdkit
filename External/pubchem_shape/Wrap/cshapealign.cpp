112fc3e4539af98bfa2893da05885fe9842a2df5
Greg Landrum <greg.landrum@gmail.com>
2024-11-23 03:58:04 +0100

    improve defaults for the shape-based alignment (#8029)

--- External/pubchem_shape/Wrap/cshapealign.cpp
+++ External/pubchem_shape/Wrap/cshapealign.cpp
@@ -66,8 +66,8 @@ void wrap_pubchemshape() {
       "AlignMol", &helpers::alignMol,
       (python::arg("ref"), python::arg("probe"), python::arg("refConfId") = -1,
        python::arg("probeConfId") = -1, python::arg("useColors") = true,
-       python::arg("opt_param") = 0.5, python::arg("max_preiters") = 3,
-       python::arg("max_postiters") = 16),
+       python::arg("opt_param") = 1.0, python::arg("max_preiters") = 10,
+       python::arg("max_postiters") = 30),
       R"DOC(Aligns a probe molecule to a reference molecule. The probe is modified.
 
 Parameters
@@ -97,8 +97,8 @@ Returns
       "AlignMol", &helpers::alignMol2,
       (python::arg("refShape"), python::arg("probe"),
        python::arg("probeConfId") = -1, python::arg("useColors") = true,
-       python::arg("opt_param") = 0.5, python::arg("max_preiters") = 3,
-       python::arg("max_postiters") = 16),
+       python::arg("opt_param") = 1.0, python::arg("max_preiters") = 10,
+       python::arg("max_postiters") = 30),
       R"DOC(Aligns a probe molecule to a reference shape. The probe is modified.
 
 Parameters
--- External/pubchem_shape/Wrap/test_rdshapealign.py
+++ External/pubchem_shape/Wrap/test_rdshapealign.py
@@ -3,33 +3,35 @@ from rdkit import Chem
 from rdkit.Chem import rdShapeAlign
 from rdkit import RDConfig
 
-datadir = RDConfig.RDBaseDir + '/External/pubchem_shape/test_data';
+datadir = RDConfig.RDBaseDir + '/External/pubchem_shape/test_data'
 
 
 class TestCase(unittest.TestCase):
 
-    def setUp(self):
-        suppl = Chem.SDMolSupplier(datadir + '/test1.sdf')
-        self.ref = suppl[0]
-        self.probe = suppl[1]
+  def setUp(self):
+    suppl = Chem.SDMolSupplier(datadir + '/test1.sdf')
+    self.ref = suppl[0]
+    self.probe = suppl[1]
 
-    def test1_Defaults(self):
-        tpl = rdShapeAlign.AlignMol(self.ref, self.probe)
-        self.assertAlmostEqual(tpl[0], 0.773, places=3)
-        self.assertAlmostEqual(tpl[1], 0.303, places=3)
+  def test1_Defaults(self):
+    tpl = rdShapeAlign.AlignMol(self.ref, self.probe, opt_param=0.5, max_preiters=3,
+                                max_postiters=16)
+    self.assertAlmostEqual(tpl[0], 0.773, places=3)
+    self.assertAlmostEqual(tpl[1], 0.303, places=3)
 
-    def test2_NoColor(self):
-        tpl = rdShapeAlign.AlignMol(self.ref, self.probe, useColors=False)
-        self.assertAlmostEqual(tpl[0], 0.773, places=3)
-        self.assertAlmostEqual(tpl[1], 0.0, places=3)
+  def test2_NoColor(self):
+    tpl = rdShapeAlign.AlignMol(self.ref, self.probe, useColors=False, opt_param=0.5,
+                                max_preiters=3, max_postiters=16)
+    self.assertAlmostEqual(tpl[0], 0.773, places=3)
+    self.assertAlmostEqual(tpl[1], 0.0, places=3)
 
-    def test3_FromShape(self):
-        shp = rdShapeAlign.PrepareConformer(self.ref)
-        self.assertTrue(type(shp) == rdShapeAlign.ShapeInput)
-        tpl = rdShapeAlign.AlignMol(shp, self.probe)
-        self.assertAlmostEqual(tpl[0], 0.773, places=3)
-        self.assertAlmostEqual(tpl[1], 0.303, places=3)
+  def test3_FromShape(self):
+    shp = rdShapeAlign.PrepareConformer(self.ref)
+    self.assertTrue(type(shp) == rdShapeAlign.ShapeInput)
+    tpl = rdShapeAlign.AlignMol(shp, self.probe, opt_param=0.5, max_preiters=3, max_postiters=16)
+    self.assertAlmostEqual(tpl[0], 0.773, places=3)
+    self.assertAlmostEqual(tpl[1], 0.303, places=3)
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
