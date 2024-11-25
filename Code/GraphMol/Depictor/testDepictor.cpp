4d58d2d1c936eb248472ecf02f7df9e62ede176e
Hussein Faara <faarahussein@gmail.com>
2024-11-24 20:33:15 -0800

    test failures due to whitespace changes?

--- Code/GraphMol/Depictor/testDepictor.cpp
+++ Code/GraphMol/Depictor/testDepictor.cpp
@@ -1290,7 +1290,7 @@ M  END)RES"_ctab;
 
   // test that using a reference with query atoms including H works
   auto scaffold = R"CTAB(
-  MJ201100
+  MJ201100                      
 
  12 13  0  0  0  0  0  0  0  0999 V2000
    -0.5398    0.0400    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
@@ -1318,13 +1318,13 @@ M  END)RES"_ctab;
   3 10  1  0  0  0  0
   9 11  1  0  0  0  0
   8 12  1  0  0  0  0
-M  ALS   7 10 F H   C   N   O   F   P   S   Cl  Br  I
-M  ALS  11 10 F H   C   N   O   F   P   S   Cl  Br  I
-M  ALS  12 10 F H   C   N   O   F   P   S   Cl  Br  I
+M  ALS   7 10 F H   C   N   O   F   P   S   Cl  Br  I   
+M  ALS  11 10 F H   C   N   O   F   P   S   Cl  Br  I   
+M  ALS  12 10 F H   C   N   O   F   P   S   Cl  Br  I   
 M  END
 )CTAB"_ctab;
   auto mol = R"CTAB(
-  MJ201100
+  MJ201100                      
 
  13 14  0  0  0  0  0  0  0  0999 V2000
    -0.6112    0.3665    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
@@ -1487,7 +1487,7 @@ M  END)RES"_ctab;
 
     // test that using a reference with query atoms including H works
     auto scaffold = R"CTAB(
-  MJ201100
+  MJ201100                      
 
  12 13  0  0  0  0  0  0  0  0999 V2000
    -0.5398    0.0400    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
@@ -1515,14 +1515,14 @@ M  END)RES"_ctab;
   3 10  1  0  0  0  0
   9 11  1  0  0  0  0
   8 12  1  0  0  0  0
-M  ALS   7 10 F H   C   N   O   F   P   S   Cl  Br  I
-M  ALS  11 10 F H   C   N   O   F   P   S   Cl  Br  I
-M  ALS  12 10 F H   C   N   O   F   P   S   Cl  Br  I
+M  ALS   7 10 F H   C   N   O   F   P   S   Cl  Br  I   
+M  ALS  11 10 F H   C   N   O   F   P   S   Cl  Br  I   
+M  ALS  12 10 F H   C   N   O   F   P   S   Cl  Br  I   
 M  END
 )CTAB"_ctab;
     TEST_ASSERT(scaffold);
     auto mol = R"CTAB(
-  MJ201100
+  MJ201100                      
 
  13 14  0  0  0  0  0  0  0  0999 V2000
    -0.6112    0.3665    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
@@ -1570,7 +1570,7 @@ void testNormalizeStraighten() {
       << std::endl;
 
   auto noradrenalineMJ = R"RES(
-  MJ201100
+  MJ201100                      
 
  12 12  0  0  1  0  0  0  0  0999 V2000
     2.2687    1.0716    0.0000 N   0  0  0  0  0  0  0  0  0  0  0  0
@@ -1743,7 +1743,7 @@ M  END
     // towards a 30-degree angle rotate since it has no bonds
     // whose angle with the X axis is multiple of 60 degrees
     auto cpSittingOnHorizontalBondCTab = R"RES(
-  MJ201100
+  MJ201100                      
 
   5  5  0  0  0  0  0  0  0  0999 V2000
    -2.3660    0.3892    0.0000 C   0  0  0  0  0  0  0  0  0  0  0  0
