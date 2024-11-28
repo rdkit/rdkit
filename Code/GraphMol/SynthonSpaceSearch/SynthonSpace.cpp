854a54115372f4943152256cbed398cbe6c4b020
David Cosgrove <david@cozchemix.co.uk>
2024-11-28 08:47:38 +0000

    Response to review.

--- Code/GraphMol/SynthonSpaceSearch/SynthonSpace.cpp
+++ Code/GraphMol/SynthonSpaceSearch/SynthonSpace.cpp
@@ -477,20 +477,20 @@ int deduceFormat(const std::string &line) {
   // formats are based on the headers which should be one of the forms here.
   // If the columns are white-space separated the return value is 0-2, if
   // comma separated 3-5.
-  std::vector<std::vector<std::string>> firstLineOpts{
+  static const std::vector<std::vector<std::string>> firstLineOpts{
       std::vector<std::string>{"SMILES", "synton_id", "synton#", "reaction_id"},
       std::vector<std::string>{"SMILES", "synton_id", "synton#", "reaction_id",
                                "release"},
       std::vector<std::string>{"SMILES", "synton_id", "synton_role",
                                "reaction_id"}};
-  std::regex regexws("\\s+");
+  static const std::regex regexws("\\s+");
   auto lineParts = splitLine(line, regexws);
   for (size_t i = 0; i < firstLineOpts.size(); ++i) {
     if (lineParts == firstLineOpts[i]) {
       return static_cast<int>(i);
     }
   }
-  std::regex regexc(",+");
+  static const std::regex regexc(",+");
   lineParts = splitLine(line, regexc);
   for (size_t i = 0; i < firstLineOpts.size(); ++i) {
     if (lineParts == firstLineOpts[i]) {
@@ -507,15 +507,9 @@ void SynthonSpace::readTextFile(const std::string &inFilename) {
   if (!ifs.is_open() || ifs.bad()) {
     throw std::runtime_error("Couldn't open file " + d_fileName);
   }
-  std::regex regexws("\\s+");
-  std::regex regexc(",+");
+  static const std::regex regexws("\\s+");
+  static const std::regex regexc(",+");
 
-  std::vector<std::vector<std::string>> firstLineOpts{
-      std::vector<std::string>{"SMILES", "synton_id", "synton#", "reaction_id"},
-      std::vector<std::string>{"SMILES", "synton_id", "synton#", "reaction_id",
-                               "release"},
-      std::vector<std::string>{"SMILES", "synton_id", "synton_role",
-                               "reaction_id"}};
   int format = -1;
   std::string nextLine;
   int lineNum = 1;
