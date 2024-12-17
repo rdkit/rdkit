e6a6f731c5ab9645c21fe184cc91bab22c129920
Greg Landrum <greg.landrum@gmail.com>
2024-12-17 15:15:44 +0100

    allow specified chiral featurs to SSS match unspecified features

--- Code/GraphMol/Substruct/SubstructMatch.h
+++ Code/GraphMol/Substruct/SubstructMatch.h
@@ -67,7 +67,10 @@ struct RDKIT_SUBSTRUCTMATCH_EXPORT SubstructMatchParameters {
                         //!< match
   unsigned int maxRecursiveMatches =
       1000;  //!< maximum number of matches that the recursive substructure
-             //!< matching should return
+  //!< matching should return
+  bool specifiedQueryChiralityMatchesUnspecifiedChirality =
+      false;  //!< If set, query atoms and bonds with specified chirality will
+              //!< match atoms and bonds with unspecified chirality
   SubstructMatchParameters() {}
 };
 
--- Code/GraphMol/Wrap/Mol.cpp
+++ Code/GraphMol/Wrap/Mol.cpp
@@ -335,11 +335,16 @@ struct mol_wrapper {
             "hardware. negative values are added to the number of concurrent"
             "threads supported by the hardware.")
         .def_readwrite(
+            "bondProperties", &RDKit::SubstructMatchParameters::bondProperties,
+            "bond properties that must be equivalent in order to match.")
+        .def_readwrite(
             "atomProperties", &RDKit::SubstructMatchParameters::atomProperties,
             "atom properties that must be equivalent in order to match.")
         .def_readwrite(
-            "bondProperties", &RDKit::SubstructMatchParameters::bondProperties,
-            "bond properties that must be equivalent in order to match.")
+            "specifiedQueryChiralityMatchesUnspecifiedChirality",
+            &RDKit::SubstructMatchParameters::
+                specifiedQueryChiralityMatchesUnspecifiedChirality,
+            "If set, query atoms and bonds with specified chirality will match atoms and bonds with unspecified chirality")
         .def("setExtraFinalCheck", setSubstructMatchFinalCheck,
              python::with_custodian_and_ward<1, 2>(),
              python::args("self", "func"),
