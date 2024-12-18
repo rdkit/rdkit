b6c7c36afeb82503aad4182d79090f231a14a444
Paolo Tosco <paolo.tosco.mail@gmail.com>
2024-12-18 05:39:19 +0100

    Update Code/GraphMol/Depictor/catch_tests.cpp
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>

--- Code/GraphMol/Depictor/catch_tests.cpp
+++ Code/GraphMol/Depictor/catch_tests.cpp
@@ -2334,6 +2334,6 @@ M  END)CTAB"_ctab;
   REQUIRE(m);
   RDDepict::normalizeDepiction(*m, -1, 0);
   auto ctd = MolTransforms::computeCentroid(m->getConformer());
-  CHECK(fabs(ctd.x) < 1.0e-4);
-  CHECK(fabs(ctd.y) < 1.0e-4);
+  CHECK_THAT(ctd.x, Catch::WithinAbs(0, 1e-4));
+  CHECK_THAT(ctd.y, Catch::WithinAbs(0, 1e-4));
 }
