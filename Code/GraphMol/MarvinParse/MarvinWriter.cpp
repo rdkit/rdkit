53e4326ee011cd22db5dfc7b7b421aced0ef6abd
Tad Hurst <tad.hurst@collaborativedrug.com>
2024-12-20 11:18:11 -0600

    Bad radical values are now ignored for MRV generation

--- Code/GraphMol/MarvinParse/MarvinWriter.cpp
+++ Code/GraphMol/MarvinParse/MarvinWriter.cpp
@@ -429,8 +429,13 @@ class MarvinCMLWriter {
         marvinAtom->formalCharge = atom->getFormalCharge();
 
         unsigned int nRadEs = atom->getNumRadicalElectrons();
+        // value of radical electrons has to be one of the expected values or it
+        // is ignored
         if (nRadEs != 0) {
-          marvinAtom->radical = radicalElectronsToMarvinRadical.at(nRadEs);
+          if (radicalElectronsToMarvinRadical.find(nRadEs) !=
+              radicalElectronsToMarvinRadical.end()) {
+            marvinAtom->radical = radicalElectronsToMarvinRadical.at(nRadEs);
+          }
         }
 
         if (marvinAtom->isElement()) {
