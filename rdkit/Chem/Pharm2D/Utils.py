2ccef13f9cba8c13bee112a874a339e998c50283
Jeremy Monat <jemonat@calalum.org>
2025-01-25 22:44:28 -0500

    Remove GetUniqueCombinations, use instead GetUniqueCombinations_new for improved speed. Update related tests.

--- rdkit/Chem/Pharm2D/Utils.py
+++ rdkit/Chem/Pharm2D/Utils.py
@@ -321,30 +321,6 @@ def GetAllCombinations(choices, noDups=1, which=0):
   return res
 
 
-def GetUniqueCombinations(choices, classes, which=0):
-  """  Does the combinatorial explosion of the possible combinations
-    of the elements of _choices_.
-
-    """
-  #   print(choices, classes)
-  assert len(choices) == len(classes)
-  if which >= len(choices):
-    return []
-  if which == len(choices) - 1:
-    return [[(classes[which], x)] for x in choices[which]]
-
-  res = []
-  tmp = GetUniqueCombinations(choices, classes, which=which + 1)
-  for thing in choices[which]:
-    for other in tmp:
-      if not any(x[1] == thing for x in other):
-        newL = [(classes[which], thing)] + other
-        newL.sort()
-        if newL not in res:
-          res.append(newL)
-  return res
-
-
 def GetUniqueCombinations_new(choices, classes, which=0):
   """  Does the combinatorial explosion of the possible combinations
     of the elements of _choices_.
