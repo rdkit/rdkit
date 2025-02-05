b4332325b2c92a45ec588b326292b4dd2203678c
Brian Kelley <fustigator@gmail.com>
2025-02-05 10:09:48 -0500

    Fix writer to round trip all mols in repository

--- External/Revvity/writer.cpp
+++ External/Revvity/writer.cpp
@@ -31,42 +31,128 @@
 //
 #include "chemdraw.h"
 #include <GraphMol/Chirality.h>
+#include <GraphMol/QueryBond.h>
 #include <GraphMol/Depictor/RDDepictor.h>
 #include <CDXStdObjects.h>
+#include <GraphMol/Depictor/RDDepictor.h>
 
 namespace RDKit {
+const double DEFAULT_CDX_BOND_LENGTH = 14.4;
+
+namespace {
+// Do we need to set explicit hs in chemdraw, this uses basically the same
+//  logic as SmilesWriter
+bool needsExplicitHs(const Atom *atom) {
+  auto num = atom->getAtomicNum();
+  const INT_VECT &defaultVs = PeriodicTable::getTable()->getValenceList(num);
+  int totalValence = atom->getTotalValence();
+  bool nonStandard = false;
+ 
+  if (atom->getNumRadicalElectrons()) {
+    nonStandard = true;
+  } else if ((num == 7 || num == 15) && atom->getIsAromatic() &&
+             atom->getNumExplicitHs()) {
+    // another type of "nonstandard" valence is an aromatic N or P with
+    // explicit Hs indicated:
+    nonStandard = true;
+  } else {
+    nonStandard =
+        (totalValence != defaultVs.front() && atom->getTotalNumHs());
+  }
+
+  return nonStandard;
+}
+}
+
 std::string MolToChemDraw(const ROMol &mol, CDXFormat format) {
   RWMol trmol(mol);
   MolOps::Kekulize(trmol);
   if (!trmol.getNumConformers()) {
     RDDepict::compute2DCoords(trmol);
   }
-
+  
   CDXObjectID object_id = 1;
   CDXDocument document(object_id++);
   CDXPage *page = new CDXPage(object_id++);
+  document.m_bondLength = DEFAULT_CDX_BOND_LENGTH;
+  document.m_flags |= CDXDocument::CDXDocumentProperty1::has_bondLength;
   CDXFragment *fragment = new CDXFragment(object_id++);
   page->AddChild(fragment);
-  auto atom_start = object_id;
   std::vector<CDXNode *> nodes;
   nodes.reserve(trmol.getNumAtoms());
 
   const Conformer *conf = nullptr;
-  if (trmol.getNumConformers() > 0) conf = &trmol.getConformer(0);
-
+  if (trmol.getNumConformers() == 0) {
+    RDDepict::compute2DCoords(trmol);
+  }
+  conf = &trmol.getConformer(0);
+  bool is3D = conf->is3D();
+  
+  // I REALLY don't know why this is 2*DEFAULT_CDX_BOND_LENGTH but it looks right
+  //   when loading the CDX into ChemDraw
+  // We convert the average bond length into the target bond length here
+  double target_bond_length = 2*DEFAULT_CDX_BOND_LENGTH;
+  double dist = 0.0;
+  for(auto bond: trmol.bonds()) {
+    auto pos1 = conf->getAtomPos(bond->getBeginAtomIdx());
+    auto pos2 = conf->getAtomPos(bond->getEndAtomIdx());
+    dist += (pos1 - pos2).length();
+  }
+  dist/=trmol.getNumBonds();
+  double scale = target_bond_length/dist;
+  
   auto wedgeBonds = Chirality::pickBondsToWedge(trmol, nullptr, conf);
 
   for (auto &atom : trmol.atoms()) {
     CDXNode *node = new CDXNode(object_id + atom->getIdx());
     auto pos = conf->getAtomPos(atom->getIdx());
-    node->Position(CDXPoint2D(CDXCoordinatefromPoints(pos.x),
-                              CDXCoordinatefromPoints(pos.y)));
+    if(is3D) {
+      node->Position3D(CDXPoint3D(CDXCoordinatefromPoints(pos.x),
+                                -CDXCoordinatefromPoints(pos.y),
+                                CDXCoordinatefromPoints(pos.z)
+                                ));
+    } else {
+      node->Position(CDXPoint2D(CDXCoordinatefromPoints(scale*pos.x),
+                                CDXCoordinatefromPoints(-scale*pos.y)));
+    }
     node->m_nodeType = kCDXNodeType_Element;
     node->m_isotope = atom->getIsotope();
     node->m_elementNum = atom->getAtomicNum();
-    node->m_charge = atom->getFormalCharge();
-    node->m_numHydrogens = atom->getNumExplicitHs() ? atom->getNumExplicitHs()
-                                                    : kNumHydrogenUnspecified;
+    // Use the same logic from the smiles writer needs brackets
+    //
+    //node->m_numHydrogens = atom->getNumExplicitHs() ? atom->getNumExplicitHs()
+    //                                                : kNumHydrogenUnspecified;
+    node->m_numHydrogens = needsExplicitHs(atom) ? atom->getTotalNumHs() : kNumHydrogenUnspecified;
+    node->m_charge = atom->getFormalCharge() * 0x1000000;
+    if(atom->getFormalCharge() || atom->getNumRadicalElectrons() != 0) {
+      node->m_numHydrogens = atom->getTotalNumHs(); // XXX is this right?  We seem to need to set it with charges
+    }
+    if(atom->getNumRadicalElectrons()) {
+      switch(atom->getNumRadicalElectrons()) {
+          case 0: break;
+          case 1: node->m_radical = kCDXRadical_Singlet; break;
+          case 2: break;
+          case 3: break;
+      }
+    }
+    // this might be a bit slow, perhaps make into a map...
+    unsigned int sgnum = 0;
+    for(auto &sg : trmol.getStereoGroups()) {
+      sgnum++;
+      for(auto &sgatom : sg.getAtoms()) {
+        if(atom->getIdx()  == sgatom->getIdx()) {
+          switch(sg.getGroupType()) {
+            case StereoGroupType::STEREO_ABSOLUTE:
+              node->m_enhancedStereoType = kCDXEnhancedStereo_Absolute; break;
+            case StereoGroupType::STEREO_OR:
+              node->m_enhancedStereoType =  kCDXEnhancedStereo_Or; break;
+            case StereoGroupType::STEREO_AND:
+              node->m_enhancedStereoType =  kCDXEnhancedStereo_And; break;
+          }
+          node->m_enhancedStereoGroupNum = sgnum;
+        }
+      }
+    }
     nodes.push_back(node);
     fragment->AddChild(node);
   }
@@ -130,6 +216,19 @@ std::string MolToChemDraw(const ROMol &mol, CDXFormat format) {
         cdxbond->m_bondOrder = kCDXBondOrder_Dative;
         break;
       case Bond::UNSPECIFIED:
+        {
+          auto query = describeQuery(bond);
+          if (query == "DoubleOrAromaticBond 1 = val\n") {
+            cdxbond->m_bondOrder = kCDXBondOrder_DoubleOrAromatic;
+          } else if (query == "SingleOrAromaticBond 1 = val\n") {
+            cdxbond->m_bondOrder = kCDXBondOrder_SingleOrAromatic;
+          } else if (query == "SingleOrDoubleBond 1 = val\n") {
+            cdxbond->m_bondOrder = kCDXBondOrder_SingleOrDouble;
+          } else {
+            cdxbond->m_bondOrder = kCDXBondOrder_Any;
+          }
+        }
+        break;
       case Bond::DATIVEONE:
       case Bond::DATIVEL:
       case Bond::DATIVER:
@@ -143,13 +242,16 @@ std::string MolToChemDraw(const ROMol &mol, CDXFormat format) {
                       nodes[bond->getEndAtomIdx()]);
 
     switch (dirCode) {
-      case 1:
+      case 6: // swap 1 and 6 due to swapped y
         cdxbond->m_display = reverse ? kCDXBondDisplay_WedgedHashEnd
                                      : kCDXBondDisplay_WedgedHashBegin;
         break;
-      case 6:
+      case 1:
         cdxbond->m_display =
             reverse ? kCDXBondDisplay_WedgeEnd : kCDXBondDisplay_WedgeBegin;
+        break;
+      case 3:
+        cdxbond->m_display = kCDXBondDisplay_Wavy;
         break;
       default:
         break;
