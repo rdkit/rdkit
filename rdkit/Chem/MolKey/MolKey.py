36dfc469f284f58e17ffdf86c73740fb5a76bb23
Shivam7-1 <55046031+Shivam7-1@users.noreply.github.com>
2025-03-24 13:09:39 +0530

    Update MolKey.py

--- rdkit/Chem/MolKey/MolKey.py
+++ rdkit/Chem/MolKey/MolKey.py
@@ -315,7 +315,7 @@ def _identify(err, ctab, inchi, stereo_category, extra_structure_desc=None):
   key_string = _get_identification_string(err, ctab, inchi, stereo_category, extra_structure_desc)
   if not key_string:
     return None
-  hash_key = base64.b64encode(hashlib.md5(key_string.encode('UTF-8')).digest()).decode()
+  hash_key = base64.b64encode(hashlib.sha256(key_string.encode('UTF-8')).digest()).decode()
   return f"{MOL_KEY_VERSION}|{hash_key}"
 
 
