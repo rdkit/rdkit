2de1dabdc1ae436e64f57eba2a091b304b536b4a
David Cosgrove <davidacosgroveaz@gmail.com>
2025-04-27 05:51:53 +0100

    ShapeInput from subset of atoms in molecule (#8449)
    * Make shape on subset of atoms in molecule.
    Optional radius for dummy atoms.
    
    * Re-factor.
    Add ShapeInputOptions.
    
    * Python wrapper for options.
    
    * Add non-standard radii option.
    Add function to align a shape on a shape.
    Add function to align conformer based on shape-shape transformation.
    
    * Python wrappers for AlignShapes and TransformConformer.
    
    * Add the new thing for Python options.
    
    * Add option for excluding atoms from color features.
    No partial color features if using atomSubset.
    
    * In AlignShapes, don't change the fit shape (apart from transforming it) in case it will be used again.
    
    * Rename it AlignShape to be consistent with AlignMolecule.
    
    * Response to review.
    
    * Undo clang-format
    
    * Fix botched merge.
    
    * Typo.
    
    ---------
    
    Co-authored-by: David Cosgrove <david@cozchemix.co.uk>

--- External/pubchem_shape/Wrap/cshapealign.cpp
+++ External/pubchem_shape/Wrap/cshapealign.cpp
@@ -45,15 +45,104 @@ python::tuple alignMol(const RDKit::ROMol &ref, RDKit::ROMol &probe,
 }
 python::tuple alignMol2(const ShapeInput &ref, RDKit::ROMol &probe,
                         int probeConfId, bool useColors, double opt_param,
-                        unsigned int max_preiters, unsigned int max_postiters) {
+                        unsigned int max_preiters, unsigned int max_postiters,
+                        bool applyRefShift) {
   std::vector<float> matrix(12, 0.0);
   auto [nbr_st, nbr_ct] =
       AlignMolecule(ref, probe, matrix, probeConfId, useColors, opt_param,
-                    max_preiters, max_postiters);
+                    max_preiters, max_postiters, applyRefShift);
   return python::make_tuple(nbr_st, nbr_ct);
 }
-ShapeInput *prepConf(const RDKit::ROMol &mol, int confId, bool useColors) {
-  return new ShapeInput(PrepareConformer(mol, confId, useColors));
+python::tuple alignShapes(const ShapeInput &refShape, ShapeInput &fitShape,
+                          double opt_param, unsigned int max_preiters,
+                          unsigned int max_postiters) {
+  std::vector<float> matrix(12, 0.0);
+  auto [nbr_st, nbr_ct] = AlignShape(refShape, fitShape, matrix, opt_param,
+                                     max_preiters, max_postiters);
+  python::list pyMatrix;
+  for (auto m : matrix) {
+    pyMatrix.append(m);
+  }
+  return python::make_tuple(nbr_st, nbr_ct, pyMatrix);
+}
+void transformConformer(const python::list &pyFinalTrans,
+                        const python::list &pyMatrix, ShapeInput probeShape,
+                        RDKit::Conformer &probeConf) {
+  std::vector<float> matrix;
+  pythonObjectToVect<float>(pyMatrix, matrix);
+  if (matrix.size() != 12) {
+    throw_value_error(
+        "The transformation matrix must have 12 values.  It had " +
+        std::to_string(matrix.size()) + ".");
+  }
+  std::vector<double> finalTrans;
+  pythonObjectToVect<double>(pyFinalTrans, finalTrans);
+  if (finalTrans.size() != 3) {
+    throw_value_error(
+        "The final translation vector must have 3 values.  It had " +
+        std::to_string(finalTrans.size()) + ".");
+  }
+  TransformConformer(finalTrans, matrix, probeShape, probeConf);
+}
+ShapeInput *prepConf(const RDKit::ROMol &mol, int confId,
+                     const python::object &py_opts) {
+  ShapeInputOptions opts;
+  if (!py_opts.is_none()) {
+    opts = python::extract<ShapeInputOptions>(py_opts);
+  }
+  return new ShapeInput(PrepareConformer(mol, confId, opts));
+}
+void set_atomSubset(ShapeInputOptions &opts, const python::list &as) {
+  pythonObjectToVect<unsigned int>(as, opts.atomSubset);
+}
+
+python::list get_atomSubset(const ShapeInputOptions &opts) {
+  python::list py_list;
+  for (const auto &val : opts.atomSubset) {
+    py_list.append(val);
+  }
+  return py_list;
+}
+
+void set_notColorAtoms(ShapeInputOptions &opts, const python::list &nca) {
+  pythonObjectToVect<unsigned int>(nca, opts.notColorAtoms);
+}
+
+python::list get_notColorAtoms(const ShapeInputOptions &opts) {
+  python::list py_list;
+  for (const auto &val : opts.notColorAtoms) {
+    py_list.append(val);
+  }
+  return py_list;
+}
+
+void set_atomRadii(ShapeInputOptions &opts, const python::list &ar) {
+  int len = python::len(ar);
+  opts.atomRadii.resize(len);
+  for (int i = 0; i < len; i++) {
+    unsigned int atomIdx = python::extract<unsigned int>(ar[i][0]);
+    double radius = python::extract<double>(ar[i][1]);
+    opts.atomRadii[i] = std::make_pair(atomIdx, radius);
+  }
+}
+
+python::list get_atomRadii(const ShapeInputOptions &opts) {
+  python::list py_list;
+  for (const auto &val : opts.atomRadii) {
+    py_list.append(python::make_tuple(static_cast<int>(val.first), val.second));
+  }
+  return py_list;
+}
+
+void set_shapeShift(ShapeInput &shp, const python::list &s) {
+  pythonObjectToVect<double>(s, shp.shift);
+}
+python::list get_shapeShift(const ShapeInput &shp) {
+  python::list py_list;
+  for (const auto &val : shp.shift) {
+    py_list.append(val);
+  }
+  return py_list;
 }
 }  // namespace helpers
 
@@ -61,6 +150,31 @@ void wrap_pubchemshape() {
   RegisterVectorConverter<float>("FloatVector");
   RegisterVectorConverter<double>("DoubleVector");
   RegisterVectorConverter<unsigned int>("UnsignedIntVector");
+
+  python::class_<ShapeInputOptions, boost::noncopyable>("ShapeInputOptions",
+                                                        "Shape Input Options")
+      .def_readwrite(
+          "useColors", &ShapeInputOptions::useColors,
+          "Whether to use colors (pharmacophore features) in the score.  Default=True.")
+      .def_readwrite(
+          "includeDummies", &ShapeInputOptions::includeDummies,
+          "Whether to use dummy atoms in the alignment. Default=False.")
+      .def_readwrite(
+          "dummyRadius", &ShapeInputOptions::dummyRadius,
+          "If using dummy atoms in the alignment, what radius to use for them."
+          "  Default=2.16 (the radius of Xe).")
+      .add_property(
+          "atomSubset", &helpers::get_atomSubset, &helpers::set_atomSubset,
+          "If not empty, use just these atoms in the molecule to form the ShapeInput object.")
+      .add_property(
+          "notColorAtoms", &helpers::get_notColorAtoms,
+          &helpers::set_notColorAtoms,
+          "Any atoms mentioned here by index should not be used in a color feature.")
+      .add_property(
+          "atomRadii", &helpers::get_atomRadii, &helpers::set_atomRadii,
+          "Non-standard radii to use for the atoms specified by their indices"
+          " in the molecule.  A list of tuples of [int, float].")
+      .def("__setattr__", &safeSetattr);
 
   python::def(
       "AlignMol", &helpers::alignMol,
@@ -98,13 +212,14 @@ Returns
       (python::arg("refShape"), python::arg("probe"),
        python::arg("probeConfId") = -1, python::arg("useColors") = true,
        python::arg("opt_param") = 1.0, python::arg("max_preiters") = 10,
-       python::arg("max_postiters") = 30),
+       python::arg("max_postiters") = 30, python::arg("applyRefShift") = false),
       R"DOC(Aligns a probe molecule to a reference shape. The probe is modified.
+Assumes the shapes are both centred on the origin.
 
 Parameters
 ----------
 refShape : ShapeInput
-    Reference molecule
+    Reference shape
 probe : RDKit.ROMol
     Probe molecule
 probeConfId : int, optional
@@ -114,6 +229,9 @@ useColors : bool, optional
 optParam : float, optional
 max_preiters : int, optional
 max_postiters : int, optional
+applyRefShift : bool, optional
+    If True, apply the reference shape's shift translation to the final
+    coordinates.
 
 
 Returns
@@ -122,9 +240,54 @@ Returns
     The results are (shape_score, color_score)
     The color_score is zero if useColors is False)DOC");
 
+  python::def(
+      "AlignShapes", &helpers::alignShapes,
+      (python::arg("refShape"), python::arg("probeShape"),
+       python::arg("opt_param") = 1.0, python::arg("max_preiters") = 10,
+       python::arg("max_postiters") = 30),
+      R"DOC(Aligns a probe shape to a reference shape. The probe is modified.
+
+Parameters
+----------
+refShape : ShapeInput
+    Reference shape
+probeShape : ShapeInput
+    Probe shape
+opt_param : float, optional
+max_preiters : int, optional
+max_postiters : int, optional
+
+
+Returns
+-------
+ 3-tuple of double, double, list of doubles
+    The results are (shape_score, color_score, matrix)
+    The matrix is a 12-float list giving the transformation matrix that
+    overlays the probe onto the reference.)DOC");
+
+  python::def(
+      "TransformConformer", &helpers::transformConformer,
+      (python::arg("finalTrans"), python::arg("matrix"),
+       python::arg("probeShape"), python::arg("probeConformer")),
+      R"DOC(Assuming that probeShape has been overlaid onto refShape to give
+the supplied transformation matrix, applies that transformation to the
+ given conformer.
+
+Parameters
+----------
+refShape : ShapeInput
+    Reference shape
+matrix: list[float * 12]
+    The transformation matrix
+probeShape : ShapeInput
+    Probe shape
+probeConformer : Conformer
+    Probe conformer
+)DOC");
+
   python::def("PrepareConformer", &helpers::prepConf,
               (python::arg("mol"), python::arg("confId") = -1,
-               python::arg("useColors") = true),
+               python::arg("opts") = python::object()),
               R"DOC(Generates a ShapeInput object for a molecule
 
 Parameters
@@ -146,7 +309,8 @@ Returns
       .def_readwrite("atom_type_vector", &ShapeInput::atom_type_vector)
       .def_readwrite("volumeAtomIndexVector",
                      &ShapeInput::volumeAtomIndexVector)
-      .def_readwrite("shift", &ShapeInput::shift)
+      .add_property("shift", &helpers::get_shapeShift, &helpers::set_shapeShift,
+                    "Translation of centre of shape coordinates to origin.")
       .def_readwrite("sov", &ShapeInput::sov)
       .def_readwrite("sof", &ShapeInput::sof);
 }
--- External/pubchem_shape/test.cpp
+++ External/pubchem_shape/test.cpp
@@ -312,13 +312,13 @@ TEST_CASE("d2CutOff set") {
   auto m1 =
       "c1ccc(-c2ccccc2)cc1 |(-3.26053,-0.0841607,-0.741909;-2.93383,0.123873,0.593407;-1.60713,0.377277,0.917966;-0.644758,0.654885,-0.0378428;0.743308,0.219134,0.168663;1.82376,1.0395,-0.0112769;3.01462,0.695405,0.613858;3.18783,-0.589771,1.09649;2.15761,-1.50458,1.01949;0.988307,-1.1313,0.385783;-1.1048,0.797771,-1.34022;-2.39754,0.435801,-1.69921)|"_smiles;
   REQUIRE(m1);
-  auto shape1 = PrepareConformer(*m1, -1, true);
+  auto shape1 = PrepareConformer(*m1, -1);
   std::vector<float> matrix(12, 0.0);
   auto m2 =
       "c1ccc(-c2ccccc2)cc1 |(-3.26053,-0.0841607,-0.741909;-2.93383,0.123873,0.593407;-1.60713,0.377277,0.917966;-0.644758,0.654885,-0.0378428;0.743308,0.219134,0.168663;1.82376,1.0395,-0.0112769;3.01462,0.695405,0.613858;3.18783,-0.589771,1.09649;2.15761,-1.50458,1.01949;0.988307,-1.1313,0.385783;-1.1048,0.797771,-1.34022;-2.39754,0.435801,-1.69921)|"_smiles;
   REQUIRE(m2);
   AlignMolecule(*m2, *m2, matrix);
-  auto shape2 = PrepareConformer(*m1, -1, true);
+  auto shape2 = PrepareConformer(*m1, -1);
   CHECK(shape1.sov == shape2.sov);
 }
 
@@ -344,7 +344,7 @@ TEST_CASE("Overlay onto shape bug (Github8462)") {
     CHECK_THAT((pos1 - pos2).length(), Catch::Matchers::WithinAbs(0.0, 0.005));
   }
 
-  auto s1 = PrepareConformer(*m1, -1, true);
+  auto s1 = PrepareConformer(*m1, -1);
   auto [st1, ct1] = AlignMolecule(s1, m3, matrix);
   CHECK_THAT(st1, Catch::Matchers::WithinAbs(1.0, 0.005));
   CHECK_THAT(ct1, Catch::Matchers::WithinAbs(1.0, 0.005));
@@ -354,4 +354,131 @@ TEST_CASE("Overlay onto shape bug (Github8462)") {
     auto pos2 = m3.getConformer().getAtomPos(i);
     CHECK_THAT((pos1 - pos2).length(), Catch::Matchers::WithinAbs(0.0, 0.005));
   }
+}
+
+TEST_CASE("Shape subset") {
+  auto m1 =
+      "c1ccc(-c2ccccc2)cc1 |(-3.26053,-0.0841607,-0.741909;-2.93383,0.123873,0.593407;-1.60713,0.377277,0.917966;-0.644758,0.654885,-0.0378428;0.743308,0.219134,0.168663;1.82376,1.0395,-0.0112769;3.01462,0.695405,0.613858;3.18783,-0.589771,1.09649;2.15761,-1.50458,1.01949;0.988307,-1.1313,0.385783;-1.1048,0.797771,-1.34022;-2.39754,0.435801,-1.69921)|"_smiles;
+  REQUIRE(m1);
+  ShapeInputOptions shapeOpts;
+  shapeOpts.atomSubset = std::vector<unsigned int>{0, 1, 2, 3, 10, 11};
+  auto partShape = PrepareConformer(*m1, -1, shapeOpts);
+  CHECK(partShape.coord.size() == 21);
+  CHECK_THAT(partShape.sov, Catch::Matchers::WithinAbs(253.929, 0.005));
+  CHECK_THAT(partShape.sof, Catch::Matchers::WithinAbs(5.074, 0.005));
+
+  shapeOpts.atomSubset.clear();
+  auto wholeShape = PrepareConformer(*m1, -1, shapeOpts);
+  CHECK(wholeShape.coord.size() == 42);
+  CHECK_THAT(wholeShape.sov, Catch::Matchers::WithinAbs(542.04, 0.005));
+  CHECK_THAT(wholeShape.sof, Catch::Matchers::WithinAbs(10.148, 0.005));
+}
+
+TEST_CASE("Dummy radii") {
+  auto m1 =
+      "[Xe]c1ccccc1 |(0.392086,-2.22477,0.190651;0.232269,-1.38667,0.118385;-1.06274,-0.918982,0.0342466;-1.26098,0.446053,-0.0811879;-0.244035,1.36265,-0.11691;1.05134,0.875929,-0.031248;1.28797,-0.499563,0.0864097),atomProp:0.dummyLabel.*|"_smiles;
+  auto shape1 = PrepareConformer(*m1, -1);
+  CHECK(shape1.coord.size() == 24);
+
+  // The dummy radius defaults to 2.16, the same as Xe, so these shapes should
+  // come out the same.
+  auto m2 =
+      "*c1ccccc1 |(0.392086,-2.22477,0.190651;0.232269,-1.38667,0.118385;-1.06274,-0.918982,0.0342466;-1.26098,0.446053,-0.0811879;-0.244035,1.36265,-0.11691;1.05134,0.875929,-0.031248;1.28797,-0.499563,0.0864097),atomProp:0.dummyLabel.*|"_smiles;
+  ShapeInputOptions shapeOpts;
+  shapeOpts.includeDummies = true;
+  auto shape2 = PrepareConformer(*m2, -1, shapeOpts);
+  CHECK(shape2.coord.size() == 24);
+  CHECK(shape1.sov == shape2.sov);
+
+  shapeOpts.dummyRadius = 2.5;
+  auto shape3 = PrepareConformer(*m2, -1, shapeOpts);
+  CHECK_THAT(shape3.sov, Catch::Matchers::WithinAbs(427.925, 0.005));
+
+  shapeOpts.includeDummies = false;
+  auto shape4 = PrepareConformer(*m2, -1, shapeOpts);
+  CHECK(shape4.coord.size() == 21);
+  CHECK(shape4.sov < shape2.sov);
+  CHECK_THAT(shape4.sov, Catch::Matchers::WithinAbs(254.578, 0.005));
+}
+
+TEST_CASE("Non-standard radii") {
+  auto m1 =
+      "[Xe]c1ccccc1 |(0.392086,-2.22477,0.190651;0.232269,-1.38667,0.118385;-1.06274,-0.918982,0.0342466;-1.26098,0.446053,-0.0811879;-0.244035,1.36265,-0.11691;1.05134,0.875929,-0.031248;1.28797,-0.499563,0.0864097),atomProp:0.dummyLabel.*|"_smiles;
+  auto shape1 = PrepareConformer(*m1, -1);
+  CHECK(shape1.coord.size() == 24);
+  CHECK_THAT(shape1.sov, Catch::Matchers::WithinAbs(376.434, 0.005));
+
+  ShapeInputOptions shapeOpts;
+  // Benzene derivative with atom 4 with an N radius.
+  shapeOpts.atomRadii =
+      std::vector<std::pair<unsigned int, double>>{{0, 2.5}, {4, 1.55}};
+  auto shape2 = PrepareConformer(*m1, -1, shapeOpts);
+  CHECK_THAT(shape2.sov, Catch::Matchers::WithinAbs(412.666, 0.005));
+
+  // Corresponding pyridine derivative.
+  auto m2 =
+      "[Xe]c1ccncc1 |(0.392086,-2.22477,0.190651;0.232269,-1.38667,0.118385;-1.06274,-0.918982,0.0342466;-1.26098,0.446053,-0.0811879;-0.244035,1.36265,-0.11691;1.05134,0.875929,-0.031248;1.28797,-0.499563,0.0864097),atomProp:0.dummyLabel.*|"_smiles;
+  auto shape3 = PrepareConformer(*m2, -1, shapeOpts);
+  CHECK(shape3.sov == shape2.sov);
+}
+
+TEST_CASE("Shape-Shape alignment") {
+  std::string dirName = getenv("RDBASE");
+  dirName += "/External/pubchem_shape/test_data";
+
+  auto suppl = v2::FileParsers::SDMolSupplier(dirName + "/test1.sdf");
+  auto ref = suppl[0];
+  REQUIRE(ref);
+  auto probe = suppl[1];
+  REQUIRE(probe);
+  RWMol probeCP(*probe);
+
+  auto refShape = PrepareConformer(*ref, -1);
+  auto probeShape = PrepareConformer(*probe, -1);
+
+  std::vector<float> matrix(12, 0.0);
+  auto [mol_st, mol_ct] = AlignMolecule(*ref, *probe, matrix, -1, -1);
+
+  auto [shape_st, shape_ct] = AlignShape(refShape, probeShape, matrix);
+
+  // Check that the same results are achieved when overlaying the probe
+  // molecule onto the reference and the probe shape onto the reference shape
+
+  CHECK_THAT(shape_st, Catch::Matchers::WithinAbs(mol_st, 0.001));
+  CHECK_THAT(shape_ct, Catch::Matchers::WithinAbs(mol_ct, 0.001));
+  for (unsigned int i = 0; i < probe->getNumAtoms(); i++) {
+    const auto &pos = probe->getConformer().getAtomPos(i);
+    CHECK_THAT(pos.x, Catch::Matchers::WithinAbs(
+                          probeShape.coord[3 * i] - refShape.shift[0], 0.001));
+    CHECK_THAT(pos.y,
+               Catch::Matchers::WithinAbs(
+                   probeShape.coord[3 * i + 1] - refShape.shift[1], 0.001));
+    CHECK_THAT(pos.z,
+               Catch::Matchers::WithinAbs(
+                   probeShape.coord[3 * i + 2] - refShape.shift[2], 0.001));
+  }
+
+  // Also check the TransformConformer function
+  TransformConformer(refShape.shift, matrix, probeShape,
+                     probeCP.getConformer(-1));
+  for (unsigned int i = 0; i < probe->getNumAtoms(); i++) {
+    const auto &pos = probe->getConformer().getAtomPos(i);
+    const auto &poscp = probeCP.getConformer().getAtomPos(i);
+    CHECK_THAT(pos.x, Catch::Matchers::WithinAbs(poscp.x, 0.001));
+    CHECK_THAT(pos.y, Catch::Matchers::WithinAbs(poscp.y, 0.001));
+    CHECK_THAT(pos.z, Catch::Matchers::WithinAbs(poscp.z, 0.001));
+  }
+}
+
+TEST_CASE("Atoms excluded from Color features") {
+  auto m1 =
+      "Nc1ccccc1 |(0.392086,-2.22477,0.190651;0.232269,-1.38667,0.118385;-1.06274,-0.918982,0.0342466;-1.26098,0.446053,-0.0811879;-0.244035,1.36265,-0.11691;1.05134,0.875929,-0.031248;1.28797,-0.499563,0.0864097),atomProp:0.dummyLabel.*|"_smiles;
+  auto shape1 = PrepareConformer(*m1, -1);
+  // The aniline N comes out as a donor, acceptor and basic so gets 3
+  // features.
+  CHECK(shape1.coord.size() == 33);
+  ShapeInputOptions opts;
+  opts.notColorAtoms = std::vector<unsigned int>{0};
+  auto shape2 = PrepareConformer(*m1, -1, opts);
+  CHECK(shape2.coord.size() == 24);
 }
