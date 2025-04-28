93b002e638d3510851dadc5ff06880d16d759c91
Greg Landrum <greg.landrum@gmail.com>
2025-04-28 07:38:50 +0200

    Fixes #8460 (#8468)
    * Fixes #8460
    
    Also updates JSON stereogroup handling to support bonds
    
    * fix cartridge tests

--- Code/PgSQL/rdkit/sql/json.sql
+++ Code/PgSQL/rdkit/sql/json.sql
@@ -1,5 +1,5 @@
-select regexp_replace(mol_to_json('CC'::mol)::text,'"toolkitVersion":".*?"','"toolkitVersion":""','g');
-select regexp_replace(mol_to_json('C[O,N]'::qmol)::text,'"toolkitVersion":".*?"','"toolkitVersion":""','g');
+select regexp_replace(mol_to_json('CC'::mol)::text,'ersion":.*?([,}])','ersion":""\1','g');
+select regexp_replace(mol_to_json('C[O,N]'::qmol)::text,'ersion":.*?([,}])','ersion":""\1','g');
 select mol_to_smiles(mol_from_json(mol_to_json('CC'::mol)));
 select mol_to_smarts(mol_from_json(mol_to_json('C[O,N]'::qmol)));
 select mol_to_smarts(qmol_from_json(mol_to_json('C[O,N]'::qmol)));
