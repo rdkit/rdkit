ef5598e18022e414dab764ad469d83c9cd357928
Greg Landrum <greg.landrum@gmail.com>
2025-05-09 15:11:26 +0200

    Update inchi.cpp

--- External/INCHI-API/inchi.cpp
+++ External/INCHI-API/inchi.cpp
@@ -4,7 +4,7 @@
 //  All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions areg
+// modification, are permitted provided that the following conditions are
 // met:
 //
 //     * Redistributions of source code must retain the above copyright
