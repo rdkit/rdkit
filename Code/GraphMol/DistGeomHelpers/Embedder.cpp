4cca1e4f87fccd7f24be2ec4a32b0fef97b4c983
Niels Maeder <92509610+nmaeder@users.noreply.github.com>
2025-05-19 14:28:15 +0200

    Fix ETKDG double bond check from #8518 (#8528)
    * only exclude multiple double bonds from check if degree of center atom is 2
    
    * clang-format
    
    * add test for fix
    
    * try fixing DLL build
    
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>
    
    * fix Widnows build as suggested by @greglandrum
    
    ---------
    
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>

--- Code/GraphMol/DistGeomHelpers/Embedder.cpp
+++ Code/GraphMol/DistGeomHelpers/Embedder.cpp
@@ -993,8 +993,8 @@ bool embedPoints(RDGeom::PointPtrVect *positions, detail::EmbedArgs eargs,
 
   return gotCoords;
 }
-
-void findDoubleBonds(
+// export this since we are going to be testing it
+RDKIT_DISTGEOMHELPERS_EXPORT void findDoubleBonds(
     const ROMol &mol,
     std::vector<std::tuple<unsigned int, unsigned int, unsigned int>>
         &doubleBondEnds,
@@ -1013,8 +1013,10 @@ void findDoubleBonds(
           if (nbr == oatm) {
             continue;
           }
-          const auto obnd = mol.getBondBetweenAtoms(atm->getIdx(), nbr->getIdx());
-          if (!obnd || obnd->getBondType() != Bond::BondType::SINGLE) {
+          const auto obnd =
+              mol.getBondBetweenAtoms(atm->getIdx(), nbr->getIdx());
+          if (!obnd || (obnd->getBondType() != Bond::BondType::SINGLE &&
+                        atm->getDegree() == 2)) {
             continue;
           }
           doubleBondEnds.emplace_back(nbr->getIdx(), atm->getIdx(),
@@ -1126,8 +1128,8 @@ void findChiralSets(const ROMol &mol, DistGeom::VECT_CHIRALSET &chiralCenters,
           }
         }
       }  // if block -chirality check
-    }  // if block - heavy atom check
-  }  // for loop over atoms
+    }    // if block - heavy atom check
+  }      // for loop over atoms
 
   // now do atropisomers
   for (const auto &bond : mol.bonds()) {
