d72a3f975914f12fe0cdd272fab9620e6564df0c
Ric R. <ricrogz@gmail.com>
2025-05-20 19:37:07 +0000

    Add a couple of tests

--- Code/GraphMol/DetermineBonds/Wrap/testDetermineBonds.py
+++ Code/GraphMol/DetermineBonds/Wrap/testDetermineBonds.py
@@ -96,6 +96,29 @@ class TestCase(unittest.TestCase):
           if omol.GetBondBetweenAtoms(aid1, aid2):
             self.assertIsNotNone(mol.GetBondBetweenAtoms(aid1, aid2))
 
+  def testMaxIterations(self):
+    mol = Chem.MolFromSmiles("O=[Cl+]")
+    self.assertIsNotNone(mol)
+
+    bond = mol.GetBondWithIdx(0)
+    bond.SetBondType(Chem.BondType.SINGLE)
+
+    globalCharge = 1
+    embedChiral = False
+
+    # Force a failure
+    with self.assertRaises(RuntimeError):
+      rdDetermineBonds.DetermineBondOrders(mol, charge=globalCharge, embedChiral=embedChiral,
+                                           maxIterations=1)
+
+    self.assertEqual(bond.GetBondType(), Chem.BondType.SINGLE)
+
+    # with more iterations, it should succeed
+    rdDetermineBonds.DetermineBondOrders(mol, charge=globalCharge, embedChiral=embedChiral,
+                                         maxIterations=100)
+
+    self.assertEqual(bond.GetBondType(), Chem.BondType.DOUBLE)
+
   # FIX: this is problematic...
   # def testHueckelBonds(self):
   #   testDir = os.path.join(RDConfig.RDBaseDir, 'Code', 'GraphMol', 'DetermineBonds', 'test_data',
