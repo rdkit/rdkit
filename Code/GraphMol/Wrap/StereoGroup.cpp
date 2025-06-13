a8fca4f8b688853bc08fbed19da274ebf03d560a
paconius <goodeats12@gmail.com>
2025-06-13 12:10:45 -0400

    Update StereoGroup.cpp

--- Code/GraphMol/Wrap/StereoGroup.cpp
+++ Code/GraphMol/Wrap/StereoGroup.cpp
@@ -99,7 +99,7 @@ struct stereogroup_wrap {
                 "creates a StereoGroup associated with a molecule from a list "
                 "of atom Ids",
                 (python::arg("stereoGroupType"), python::arg("mol"),
-                 python::arg("atomIds"), python::arg("bondIds"),
+                 python::arg("atomIds") = [], python::arg("bondIds") = [],
                  python::arg("readId") = 0),
                 python::return_value_policy<
                     python::manage_new_object,
