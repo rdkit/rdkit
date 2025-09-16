b693c5d4df9d73f8b4daed7513150669f758ba76
Greg Landrum <greg.landrum@gmail.com>
2025-09-16 16:28:01 +0200

    Revert "Fixes #8644 (#8768)" (#8787)
    This reverts commit a47a5d91aadb861aa39d58b7ff6f5593ee31f9a8.

--- Code/GraphMol/FileParsers/MultithreadedMolSupplier.h
+++ Code/GraphMol/FileParsers/MultithreadedMolSupplier.h
@@ -43,9 +43,10 @@ class RDKIT_FILEPARSERS_EXPORT MultithreadedMolSupplier : public MolSupplier {
 
   MultithreadedMolSupplier() {}
 
+  
   // Derived classes MUST have a destructor that calls close
   //  to properly end threads while the instance is alive
-  virtual ~MultithreadedMolSupplier() { close(); }
+  virtual ~MultithreadedMolSupplier() {close();}
 
   //! shut down the supplier
   virtual void close() override;
@@ -132,13 +133,10 @@ class RDKIT_FILEPARSERS_EXPORT MultithreadedMolSupplier : public MolSupplier {
   virtual RWMol *processMoleculeRecord(const std::string &record,
                                        unsigned int lineNum) = 0;
 
-  std::vector<std::thread> d_writerThreads;  //!< vector writer threads
-  std::thread d_readerThread;                //!< single reader thread
-
-  // both of these will only be incremented in unique threads
-  // (the reader and the master thread), so no need for atomics/mutexes
-  unsigned int d_mols_read{0};
-  unsigned int d_mols_returned{0};
+  std::mutex d_threadCounterMutex;
+  std::atomic<unsigned int> d_threadCounter{1};  //!< thread counter
+  std::vector<std::thread> d_writerThreads;      //!< vector writer threads
+  std::thread d_readerThread;                    //!< single reader thread
 
  protected:
   std::atomic<bool> df_started = false;
@@ -162,6 +160,7 @@ class RDKIT_FILEPARSERS_EXPORT MultithreadedMolSupplier : public MolSupplier {
       writeCallback = nullptr;
   std::function<std::string(const std::string &, unsigned int)> readCallback =
       nullptr;
+
 };
 }  // namespace FileParsers
 }  // namespace v2
