f438d6438806d7aa28b9ee6a62bc6e1e3ff01c6e
Brian Kelley <bkelley@glysade.com>
2025-10-08 14:46:28 -0400

    Move simpler checks first

--- Code/GraphMol/RGroupDecomposition/RGroupCore.cpp
+++ Code/GraphMol/RGroupDecomposition/RGroupCore.cpp
@@ -428,8 +428,8 @@ void RCore::buildMatchingMol() {
           //  If we are were set as an RLABEL of type Isotope we probably
           //   have a query of named "AtomIsotope"
           //   we DO strip these unless they have an additional query
-          if (!atom->getPropIfPresent(RLABEL_TYPE, type) ||
-              type != RGroupLabels::IsotopeLabels || q != "AtomIsotope") {
+          if (q != "AtomIsotope" || !atom->getPropIfPresent(RLABEL_TYPE, type) ||
+              type != RGroupLabels::IsotopeLabels) {
             continue;
           }
         }
