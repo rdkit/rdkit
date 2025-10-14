a86972caec2bf4cfc452389be7392786f83be711
Andrew Dirksen <andrew@dirksen.com>
2025-10-14 16:10:07 -0700

    DRY repeated cases list

---
+++ Code/Bench/bench_common.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace bench_common {
+constexpr const char *CASES[] = {
+    "COC1/C=C/OC2(C)Oc3c(C)c(O)c4c(O)c(c(/C=N/OC(c5ccccc5)c5ccccc5)cc4c3C2=O)NC(=O)/C(C)=C\\C=C\\C(C)C(O)C(C)C(O)C(C)C(OC(C)=O)C1C",
+    "Cn1cnc2n(C)c(=O)n(C)c(=O)c12",
+    "c1ccc2c(c1)c3ccccc3c4ccccc24",
+    "F[C@@H](Cl)[C@H](Br)[C@](I)(O)[C@@](N)(C#N)C(=O)O",
+    "C[S+](C)(C)[O-]",
+};
+}  // namespace bench_common
