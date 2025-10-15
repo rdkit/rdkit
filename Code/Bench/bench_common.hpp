ad7fbc7f09a475147b67b671e9afb75720e64ac6
Andrew Dirksen <andrew@dirksen.com>
2025-10-15 09:24:25 -0700

    Revert "take advantage of c++20, char* -> std::string_view"
    This reverts commit 10aef0c4705bdf1a6aa0194e6b36dcea139adba1.

--- Code/Bench/bench_common.hpp
+++ Code/Bench/bench_common.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <initializer_list>
-#include <string_view>
-
 namespace bench_common {
-constexpr std::initializer_list<std::string_view> CASES = {
+constexpr const char *CASES[] = {
     "COC1/C=C/OC2(C)Oc3c(C)c(O)c4c(O)c(c(/C=N/OC(c5ccccc5)c5ccccc5)cc4c3C2=O)NC(=O)/C(C)=C\\C=C\\C(C)C(O)C(C)C(O)C(C)C(OC(C)=O)C1C",
     "Cn1cnc2n(C)c(=O)n(C)c(=O)c12",
     "c1ccc2c(c1)c3ccccc3c4ccccc24",
