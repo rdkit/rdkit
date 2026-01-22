0cb7224798c32cf72d203da1de3b16aa6b145048
Rachel Walker <rachel.walker@schrodinger.com>
2026-01-22 23:55:58 +0000

    monomer mol start

---
+++ Code/GraphMol/MonomerMol/Conversions.h
@@ -0,0 +1,36 @@
+/* -------------------------------------------------------------------------
+ * Declares RDKit atomistic ROMol <-> MonomerMol conversion
+ *
+ * Copyright Schrodinger LLC, All Rights Reserved.
+ --------------------------------------------------------------------------- */
+#pragma once
+
+#include <RDGeneral/export.h>
+
+#include <memory>
+
+namespace RDKit
+{
+class ROMol;
+class RWMol;
+
+/**
+ * Converts an atomistic ROMol into a Monomer-based ROMol.
+ *
+ * @param atomistic_mol Atomistic molecule to convert to MonomerMol
+ * @return MonomerMol
+ */
+RDKIT_MONOMERMOL_EXPORT std::unique_ptr<RDKit::RWMol>
+toMonomeric(const RDKit::ROMol& atomistic_mol);
+
+/**
+ * Build an atomistic molecule from a MonomerMol using monomers
+ * in MonomerDatabase.cpp
+ *
+ * @param monomer_mol Monomeric molecule to convert to atomistic
+ * @return Atomistic molecule
+ */
+RDKIT_MONOMERMOL_EXPORT std::unique_ptr<RDKit::RWMol>
+toAtomistic(const RDKit::ROMol& monomer_mol);
+
+} // namespace RDKit
