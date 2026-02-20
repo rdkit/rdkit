f84601c704c2a586da0c23cb40b54cc068e75cc4
Ric R. <ricrogz@gmail.com>
2026-02-20 19:28:51 +0000

    add example required by Dan

--- Code/GraphMol/catch_canon.cpp
+++ Code/GraphMol/catch_canon.cpp
@@ -1340,6 +1340,8 @@ TEST_CASE("Canonicalization issues watch (see GitHub Issue #8775)") {
       {R"smi(O[C@H]1[C@H]2C[C@H](C2)[C@]12CC2)smi", false, true},  // #7759
       {R"smi(N[C@]1(C(=O)O)[C@@H]2C[C@H]3C[C@@H](C2)C[C@H]1C3)smi", false,
        true},  // #8862
+      {R"smi(C/C=C1/C(=C\Cl)/C(=C\F)/C(=C\N)/C/1=C\O)smi", true,
+       true},  // #8965
   };
 
   auto count_features = [](RWMol m) {
