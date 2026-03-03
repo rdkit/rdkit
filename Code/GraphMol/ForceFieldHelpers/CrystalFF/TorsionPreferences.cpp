1e6b0bdab587e0ed8be665e92a2f1b83dc5a8371
Niels Maeder <maedern@student.ethz.ch>
2026-03-03 19:05:06 +0100

    better naming

--- Code/GraphMol/ForceFieldHelpers/CrystalFF/TorsionPreferences.cpp
+++ Code/GraphMol/ForceFieldHelpers/CrystalFF/TorsionPreferences.cpp
@@ -33,7 +33,8 @@ using namespace RDKit;
 
 // the "macrocycle" patterns for ETKDGv3 use a minimum ring size of 9
 constexpr unsigned int MIN_MACROCYCLE_SIZE = 9;
-constexpr double AIO_PLANAR_TORSION_FC = 1500.0;
+constexpr double LEGACY_AROMATIC_TORSION_FC = 1500.0;
+constexpr double AIO_AROMATIC_TORSION_FC = 1500.0;
 
 /* SMARTS patterns for experimental torsion angle preferences
  * Version 1 taken from J. Med. Chem. 56, 1026-2028 (2013)
@@ -351,7 +352,7 @@ void getExperimentalTorsions(
           std::vector<int> signs(6, 1);
           signs[1] = -1;  // MMFF sign for m = 2
           std::vector<double> fconsts(6, 0.0);
-          fconsts[1] = useLegacyImplementation ? 100.0 : AIO_PLANAR_TORSION_FC;
+          fconsts[1] = useLegacyImplementation ? LEGACY_AROMATIC_TORSION_FC : AIO_AROMATIC_TORSION_FC;
           details.expTorsionAngles.emplace_back(signs, fconsts);
           /*if (verbose) {
             std::cout << "SP2 ring: " << aid1 << " " << aid2 << " " << aid3 <<
