7a949d4382085bb5accb6c006adb97e1b74c9321
Niels Maeder <maedern@student.ethz.ch>
2026-03-06 18:12:36 +0100

    try 0.8

--- Code/DistGeom/DistGeomUtils.cpp
+++ Code/DistGeom/DistGeomUtils.cpp
@@ -40,7 +40,7 @@ constexpr double chiral = 1.0;
 constexpr double fourD = 2.15;
 constexpr double KAngle = 0.01;
 constexpr double KPlanar = 0.0005;
-constexpr double ETTermScaling = 0.0075;
+constexpr double ETTermScaling = 0.008;
 }  // namespace FC
 
 double pickRandomDistMat(const BoundsMatrix &mmat,
