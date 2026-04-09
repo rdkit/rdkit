40715173afbf4a1ce9473de92ec05453753e62bc
greg landrum <greg.landrum@gmail.com>
2026-04-09 09:49:20 +0200

    try using precompiled headers

---
+++ Code/stdafx.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <boost/dynamic_bitset.hpp>
+#include <GraphMol/RDKitBase.h>
+#include <RDGeneral/Invariant.h>
+#include <RDGeneral/RDLog.h>
