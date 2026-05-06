356452ba2cfc27aece2b27fb2751790129c47a6b
Kevin Boyd <kboyd@nvidia.com>
2026-05-06 08:59:23 -0400

    Change RMS pruning expectations.
    This was a hard-coded regression test. Total counts pruned are similar

--- Code/GraphMol/DistGeomHelpers/Wrap/testDistGeom.py
+++ Code/GraphMol/DistGeomHelpers/Wrap/testDistGeom.py
@@ -209,7 +209,7 @@ class TestCase(unittest.TestCase):
     ]
 
     nconfs = []
-    expected = [3, 2, 7, 6, 3, 3]
+    expected = [2, 2, 7, 5, 4, 3]
     for smi in smiles:
       mol = Chem.MolFromSmiles(smi)
       cids = rdDistGeom.EmbedMultipleConfs(mol, 50, maxAttempts=30, randomSeed=100,
@@ -227,7 +227,7 @@ class TestCase(unittest.TestCase):
     params.pruneRmsThresh = 1.5
     params.useSymmetryForPruning = False
     nconfs = []
-    expected = [4, 5, 5, 6, 7, 3]
+    expected = [5, 5, 4, 6, 8, 3]
     for smi in smiles:
       mol = Chem.MolFromSmiles(smi)
       cids = rdDistGeom.EmbedMultipleConfs(mol, 50, params)
