5047654a1ed2bc861c9c345d65f556271d41ea5e
Emily Rhodes <70823163+emilyrrhodes@users.noreply.github.com>
2026-06-26 17:36:58 -0400

    MacroMolTemplate is a RWMol object.

--- Code/GraphMol/MacroMolTemplate.cpp
+++ Code/GraphMol/MacroMolTemplate.cpp
@@ -12,24 +12,103 @@
 
 namespace RDKit {
 
-void MacroMolTemplateLibrary::addTemplate(
-    const std::shared_ptr<MacroMolTemplate> &tmpl) {
-  byTemplateName[{tmpl->monomerClass, tmpl->templateName}] = tmpl;
-  bySymbol[{tmpl->monomerClass, tmpl->symbol}] = tmpl;
+namespace {
+const std::string SUP_TYPE = "SUP";
+const std::string LGRP_CLASS = "LGRP";
+
+bool isMainSgroup(const SubstanceGroup &sgroup) {
+  std::string type;
+  if (!sgroup.getPropIfPresent("TYPE", type) || type != SUP_TYPE) {
+    return false;
+  }
+  std::string sgClass;
+  return !sgroup.getPropIfPresent("CLASS", sgClass) || sgClass != LGRP_CLASS;
 }
 
-const std::shared_ptr<MacroMolTemplate> &
+bool isLeavingGroup(const SubstanceGroup &sgroup) {
+  std::string type;
+  if (!sgroup.getPropIfPresent("TYPE", type) || type != SUP_TYPE) {
+    return false;
+  }
+  std::string sgClass;
+  return sgroup.getPropIfPresent("CLASS", sgClass) && sgClass == LGRP_CLASS;
+}
+}  // namespace
+
+unsigned int MacroMolTemplate::setMainGroup(
+    const std::vector<unsigned int> &atomIdxs, const std::string &className) {
+  PRECONDITION(getMainSgroup() == nullptr, "main group already set");
+  SubstanceGroup sgroup(this, SUP_TYPE);
+  sgroup.setProp("CLASS", className);
+  sgroup.setAtoms(atomIdxs);
+  return addSubstanceGroup(*this, sgroup);
+}
+
+unsigned int MacroMolTemplate::addLeavingGroup(
+    const std::vector<unsigned int> &atomIdxs, unsigned int attachAtomIdx,
+    unsigned int leavingAtomIdx, const int attachPt) {
+  PRECONDITION(getMainSgroup() != nullptr, "main group must be set first");
+
+  SubstanceGroup sgroup(this, SUP_TYPE);
+  sgroup.setProp("CLASS", LGRP_CLASS);
+  sgroup.setAtoms(atomIdxs);
+  auto idx = addSubstanceGroup(*this, sgroup);
+
+  getMainSgroup()->addAttachPoint(attachAtomIdx,
+                                  static_cast<int>(leavingAtomIdx),
+                                  std::to_string(attachPt));
+  return idx;
+}
+
+const SubstanceGroup *MacroMolTemplate::getMainSgroup() const {
+  for (const auto &sgroup : getSubstanceGroups(*this)) {
+    if (isMainSgroup(sgroup)) {
+      return &sgroup;
+    }
+  }
+  return nullptr;
+}
+
+SubstanceGroup *MacroMolTemplate::getMainSgroup() {
+  for (auto &sgroup : getSubstanceGroups(*this)) {
+    if (isMainSgroup(sgroup)) {
+      return &sgroup;
+    }
+  }
+  return nullptr;
+}
+
+std::vector<const SubstanceGroup *> MacroMolTemplate::getLeavingGroups() const {
+  std::vector<const SubstanceGroup *> res;
+  for (const auto &sgroup : getSubstanceGroups(*this)) {
+    if (isLeavingGroup(sgroup)) {
+      res.push_back(&sgroup);
+    }
+  }
+  return res;
+}
+
+void MacroMolTemplateLibrary::addEntry(
+    const std::shared_ptr<MacroMolEntry> &macroMolEntry) {
+  byTemplateName[{macroMolEntry->monomerClass, macroMolEntry->templateName}] =
+      macroMolEntry;
+  bySymbol[{macroMolEntry->monomerClass, macroMolEntry->symbol}] =
+      macroMolEntry;
+}
+
+const std::shared_ptr<MacroMolEntry> &
 MacroMolTemplateLibrary::getByTemplateName(
     const std::string &monomerClass, const std::string &templateName) const {
-  static std::shared_ptr<MacroMolTemplate> empty;
+  static std::shared_ptr<MacroMolEntry> empty;
   auto it = byTemplateName.find({monomerClass, templateName});
   if (it != byTemplateName.end()) return it->second;
   return empty;
 }
 
-const std::shared_ptr<MacroMolTemplate> &MacroMolTemplateLibrary::getBySymbol(
-    const std::string &monomerClass, const std::string &symbol) const {
-  static std::shared_ptr<MacroMolTemplate> empty;
+const std::shared_ptr<MacroMolEntry> &
+MacroMolTemplateLibrary::getBySymbol(const std::string &monomerClass,
+                                     const std::string &symbol) const {
+  static std::shared_ptr<MacroMolEntry> empty;
   auto it = bySymbol.find({monomerClass, symbol});
   if (it != bySymbol.end()) return it->second;
   return empty;
