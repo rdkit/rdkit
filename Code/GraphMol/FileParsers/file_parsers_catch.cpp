63148927943255ed2ff482a5a69d95c1c08e6179
= <=>
2026-07-21 14:26:04 +0200

    Test a reported OOB problem in the SGroup parser

--- Code/GraphMol/FileParsers/file_parsers_catch.cpp
+++ Code/GraphMol/FileParsers/file_parsers_catch.cpp
@@ -8135,3 +8135,28 @@ TEST_CASE("mol2 reader out-of-bounds access") {
                       FileParseException);
   }
 }
+
+TEST_CASE("OOB access with bad SGroup") {
+  SECTION("as reported") {
+    std::string molblock = R"CTAB(poc
+     RDKit          2D
+
+  0  0  0  0  0  0  0  0  0  0999 V3000
+M  V30 BEGIN CTAB
+M  V30 COUNTS 2 1 1 0 0
+M  V30 BEGIN ATOM
+M  V30 1 C 0.0 0.0 0.0 0
+M  V30 2 H 1.0 0.0 0.0 0
+M  V30 END ATOM
+M  V30 BEGIN BOND
+M  V30 1 1 1 2
+M  V30 END BOND
+M  V30 BEGIN SGROUP
+M  V30 1 SUP 0 ATOMS=(1 1) SAP=(1
+M  V30 END SGROUP
+M  V30 END CTAB
+M  END)CTAB";
+    REQUIRE_THROWS_AS(v2::FileParsers::MolFromMolBlock(molblock),
+                      FileParseException);
+  }
+}
