c7423fbe86bc52ac1209d9e8828abc9efb16afdc
Paolo Tosco <paolo.tosco.mail@gmail.com>
2026-07-27 17:04:07 +0200

    fragmentOnBonds: enfore unique bondIndices on input (#9430)
    * fragmentOnBonds: catch duplicate bondIndices as precondition rather than segfaulting
    
    * - changes in response to review
    - added Python test
    
    ---------
    
    Co-authored-by: ptosco <paolo.tosco@novartis.com>

--- Code/GraphMol/Wrap/rough_test.py
+++ Code/GraphMol/Wrap/rough_test.py
@@ -3577,6 +3577,10 @@ CAS<~>
     m = Chem.MolFromSmiles('OCCCCN')
     self.assertRaises(ValueError, lambda: Chem.FragmentOnBonds(m, ()))
 
+    # duplicate bond indices
+    m = Chem.MolFromSmiles('OCCN')
+    self.assertRaises(ValueError, lambda: Chem.FragmentOnBonds(m, (0, 2, 2)))
+
   def test88QueryAtoms(self):
     from rdkit.Chem import rdqueries
     m = Chem.MolFromSmiles('c1nc(C)n(CC)c1')
