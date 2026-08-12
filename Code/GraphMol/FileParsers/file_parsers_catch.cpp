76cde2638e7faae0c40639970d11d0b3b2752360
younes ouzza <166823313+younesouzza@users.noreply.github.com>
2026-08-12 17:25:44 +0100

    Update Code/GraphMol/FileParsers/file_parsers_catch.cpp
    Co-authored-by: Greg Landrum <greg.landrum@gmail.com>

--- Code/GraphMol/FileParsers/file_parsers_catch.cpp
+++ Code/GraphMol/FileParsers/file_parsers_catch.cpp
@@ -8186,5 +8186,25 @@ TEST_CASE("CMLWriter handles chiral centers with implicit hydrogens correctly") 
   
   CHECK(ref_count == 4);
   
-  CHECK(refs.find("a1") != std::string::npos);
+
+TEST_CASE("CMLWriter handles chiral centers with implicit hydrogens correctly") {
+  auto mol = v2::SmilesParse::MolFromSmiles("F[C@H](Br)Cl");
+  REQUIRE(mol);
+  
+  std::string cml = MolToCMLBlock(*mol);
+  REQUIRE(cml.find("atomParity") != std::string::npos);
+  
+  size_t pos = cml.find("atomRefs4=\"");
+  REQUIRE(pos != std::string::npos);
+  size_t end_pos = cml.find("\"", pos + 12);
+  std::string refs = cml.substr(pos + 12, end_pos - (pos + 12));
+  
+  int ref_count = 1;
+  for (auto c : refs) {
+    if (c == ' ') {
+      ref_count++;
+    }
+  }
+  
+  CHECK(ref_count == 4);
 }
