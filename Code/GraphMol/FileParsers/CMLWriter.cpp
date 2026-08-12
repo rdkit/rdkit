34c3b506b489f537df6567751a3ef2e73dedfcbe
younes ouzza <younessouzza11@gmail.com>
2026-08-12 17:26:49 +0100

    Remove FIX marker from comment

--- Code/GraphMol/FileParsers/CMLWriter.cpp
+++ Code/GraphMol/FileParsers/CMLWriter.cpp
@@ -132,7 +132,7 @@ boost::property_tree::ptree molToPTree(const ROMol &mol, int confId,
         neighbors.push_back(at->getIdx());
       }
 
-      // FIX: CML spec requires exactly 4 atomRefs. If there are only 3 
+      //CML spec requires exactly 4 atomRefs. If there are only 3 
       // explicit neighbors (e.g., the 4th ligand is an implicit hydrogen), 
       // the current atom itself should be used as the 4th reference.
       unsigned int centralAtomIdx = a->getIdx();
