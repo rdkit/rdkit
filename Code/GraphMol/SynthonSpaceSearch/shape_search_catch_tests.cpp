abb10167be3c8279bc007c05d267195184fc9243
dave <davidacosgroveaz@gmail.com>
2026-08-27 13:04:02 +0100

    Remove unused variable.

--- Code/GraphMol/SynthonSpaceSearch/shape_search_catch_tests.cpp
+++ Code/GraphMol/SynthonSpaceSearch/shape_search_catch_tests.cpp
@@ -101,7 +101,6 @@ TEST_CASE("Shape Small test") {
   params.bestHit = true;
   auto queryMol = v2::SmilesParse::MolFromSmiles(querySmi);
   auto results = synthonspace.shapeSearch(*queryMol, params);
-  unsigned int j = 0;
   CHECK(results.getHitMolecules().size() == 3);
   for (const auto &mol : results.getHitMolecules()) {
     // Different machine/compiler combinations give slightly different results
@@ -113,7 +112,6 @@ TEST_CASE("Shape Small test") {
     auto scores = GaussianShape::ScoreMolecule(*queryMol, *mol);
     CHECK_THAT(mol->getProp<double>("Similarity"),
                Catch::Matchers::WithinAbs(scores[0], 0.001));
-    ++j;
   }
 }
 
