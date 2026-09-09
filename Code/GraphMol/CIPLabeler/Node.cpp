5624d9a1e06a576a9b673302bf2b61492cdc9278
Ric R. <ricrogz@gmail.com>
2026-09-09 12:27:49 -0400

    fall back to isotope numbers if we can't determine atomic mass

--- Code/GraphMol/CIPLabeler/Node.cpp
+++ Code/GraphMol/CIPLabeler/Node.cpp
@@ -54,6 +54,10 @@ Node::Node(Digraph *g, std::vector<char> &&visit, Atom *atom,
       d_atomic_mass = table->getAtomicWeight(atomic_number);
     } else {
       d_atomic_mass = table->getMassForIsotope(atomic_number, isotope);
+      // Fall back to the isotope number if we can't get the mass
+      if (atomic_number != 0 && d_atomic_mass == 0.0) {
+        d_atomic_mass = isotope;
+      }
     }
   }
   if (d_visit.empty() || d_flags & DUPLICATE) {
