a57c19dcd6ca9d97511eda8fa5b8db15253e12a5
FX Coudert <1980544+fxcoudert@users.noreply.github.com>
2026-09-10 15:16:28 +0200

    Adjust distdriver_() return type (#9576)

--- Code/ML/Cluster/Murtagh/Clustering.cpp
+++ Code/ML/Cluster/Murtagh/Clustering.cpp
@@ -17,9 +17,9 @@ namespace python = boost::python;
 
 typedef double real;
 
-extern "C" void distdriver_(boost::int64_t *n, boost::int64_t *len, real *dists,
-                            boost::int64_t *toggle, boost::int64_t *ia,
-                            boost::int64_t *ib, real *crit);
+extern "C" int distdriver_(boost::int64_t *n, boost::int64_t *len, real *dists,
+                           boost::int64_t *toggle, boost::int64_t *ia,
+                           boost::int64_t *ib, real *crit);
 
 //
 // Rather than deal with any nonsense like trying to get
