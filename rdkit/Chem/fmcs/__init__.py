ba2d0beb28a4f5fddf44cc196ddf2d5d00af7a63
greg landrum <greg.landrum@gmail.com>
2026-09-11 05:05:28 +0200

    Add some deprecations

--- rdkit/Chem/fmcs/__init__.py
+++ rdkit/Chem/fmcs/__init__.py
@@ -2,4 +2,10 @@
 This code should be used by importing rdkit.Chem.MCS
 
 """
+import warnings
+
+import warnings
+
+warnings.warn("the spam module is deprecated", DeprecationWarning, stacklevel=2)
+
 from rdkit.Chem.fmcs.fmcs import *
